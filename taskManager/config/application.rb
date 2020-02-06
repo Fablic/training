d41f33b7c4febf7b8f767abcc4bb242776df5484
Nagano <kazuyoshi.nagano@rakuten.com>
2020-02-06 12:11:17 +0900

    install i18n gem

--- taskManager/config/application.rb
+++ taskManager/config/application.rb
@@ -11,9 +11,9 @@ module TaskManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Initialize configuration defaults for i18n locale and path
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
