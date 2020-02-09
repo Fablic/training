4db5d8119468f15069b7df514f38eb102ff08141
Nagano <kazuyoshi.nagano@rakuten.com>
2020-02-10 08:54:18 +0900

    modified typo

--- taskManager/app/controllers/application_controller.rb
+++ taskManager/app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.defalt_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
