a503f95597a16ce21e86e4361863969432340c4d
Yutaka Kamei <kamei@yykamei.me>
2021-05-24 11:43:39 +0900

    Add User
    `User` is responsible for storing **User**'s information: `name` and
    `password`.
    
    So far, authentication has not implemented yet. This patch just adds a
    new model and applied some validations to it.

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_082235) do
+ActiveRecord::Schema.define(version: 2021_05_24_015331) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2021_05_21_082235) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_tasks_on_name"
     t.index ["status"], name: "index_tasks_on_status"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
