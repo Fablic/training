870d536da76b6f1e1db68d32d4ca8a31703a971d
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-06-22 16:55:36 +0900

    [add] installed bootstrap5.0

--- todo_app/app/frontend/packs/application.js
+++ todo_app/app/frontend/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+import '../js/bootstrap_js_files.js'
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
