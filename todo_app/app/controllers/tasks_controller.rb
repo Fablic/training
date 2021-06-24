2932dabec09155d47f513536ec9ea859de11d447
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-06-24 16:55:53 +0900

    [fix] miss

--- todo_app/app/controllers/tasks_controller.rb
+++ todo_app/app/controllers/tasks_controller.rb
@@ -14,6 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
     if @task.save
       flash[:success] = I18n.t(:'message.registered_task')
       redirect_to root_path
