88f6bd6b164f769bc06892ce77ca747aec3c86da
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-06-24 15:14:20 +0900

    [add] Rails application error handling setting

--- todo_app/app/controllers/application_controller.rb
+++ todo_app/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def render_404(e = nil)
+    logger.info "Rendering 404 with exception: #{e.message}" if e
+
+    render file: Rails.root.join('public/404.html'), status: 404
+  end
+
+  def render_500(e = nil)
+    logger.info "Rendering 500 with exception: #{e.message}" if e
+
+    render file: Rails.root.join('public/500.html'), status: 500
+  end
 end
