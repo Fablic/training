30ddb7ccc0c550b3a850ce0a0631c98ddc20ef2b
Yasir Taher <yasir.taher@rakuten.com>
2021-06-25 21:17:17 +0900

    [STEP16] added user model

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  # get 'sessions/login'
+  get 'welcome', to: 'sessions#welcome'
+  # get 'users/new'
+  # get 'users/create'
   root 'tasks#index'
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_052520) do
+ActiveRecord::Schema.define(version: 2021_06_25_120923) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2021_06_16_052520) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "due_date"
+    t.integer "user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
