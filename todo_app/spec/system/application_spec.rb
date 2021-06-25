d53ebd5be410ace92b62856a2069ec178f2a96e4
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-06-25 12:09:28 +0900

    [fix] Review fix, Consider exception orders, 500 error test case

--- todo_app/spec/system/application_spec.rb
+++ todo_app/spec/system/application_spec.rb
@@ -13,7 +13,13 @@ RSpec.describe 'Application', type: :system do
     it 'Record Not Found Error' do
       visit task_path(0)
 
-      expect(page).to have_content '誠に申し訳ありません。ページに何らかのエラーが起きました。'
+      expect(page).to have_content 'ご指定のページが見つかりませんでした。'
+    end
+
+    it '500 Error' do
+      visit root_path
+
+      allow_any_instance_of(ApplicationController).to receive(:render_500).and_raise StandardError
     end
   end
 end
