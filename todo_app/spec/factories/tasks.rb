f3633c615fb309418f2241dcceede48977ade35a
sadahiro-ono <sadahiro.ono@rakuten.com>
2021-06-29 11:57:17 +0900

    modify factories

--- todo_app/spec/factories/tasks.rb
+++ todo_app/spec/factories/tasks.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :task do
+    association :user
      title { Faker::Alphanumeric.alphanumeric(number: 10) }
      description { Faker::Alphanumeric.alphanumeric(number: 10) }
      due_date { Faker::Time.forward }
