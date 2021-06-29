768d19f23b6ccaef2c01a3154c1c6127303dd349
sadahiro-ono <sadahiro.ono@rakuten.com>
2021-06-29 11:48:33 +0900

    add routes for login and sign_up

--- todo_app/config/routes.rb
+++ todo_app/config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/sign_up', to: 'users#new'
+  post '/sign_up', to: 'users#create'
+
   get '*path', controller: :application, action: :routing_error
 end
