62b2521cc4176d7661b11611d5152f1a6ad4b6a7
Yasir Taher <yasir.taher@rakuten.com>
2021-06-29 15:31:51 +0900

    [STEP17] fixed login screen

--- myapp/app/controllers/sessions_controller.rb
+++ myapp/app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
 
   # post method for login
   def create
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
       log_in @user
       redirect_to @user
     else
