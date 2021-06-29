48e4e740e229819433e1d370168d9ad12dedc821
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-06-29 13:31:51 +0900

    [add] implements users model and test code

--- todo_app/app/models/user.rb
+++ todo_app/app/models/user.rb
@@ -1,7 +1,20 @@
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 class User < ApplicationRecord
   has_secure_password
   include IdGenerator
+  validates :username,
+    presence: true,
+    length: { maximum: 20 }
+
+  validates :email,
+    presence: true,
+    length: { maximum: 254 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+
+  validates :icon,
+    length: { maximum: 2000 }
   enum role: [:normal, :admin]
   has_many :tasks
-
 end
--- todo_app/spec/models/user_spec.rb
+++ todo_app/spec/models/user_spec.rb
@@ -1,5 +1,104 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    subject { build(:admin_user, params) }
+    let(:params) { { username: username, email: email, icon: icon, role: role, password_digest: password_digest } }
+
+    describe 'valid' do
+      context 'valid all property' do
+        let(:username) { 'admin-user' }
+        let(:email) { 'admin-user@example.com' }
+        let(:role) { :admin }
+        let(:icon) { nil }
+        let(:password_digest) { 'xxxxyyyy1234' }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'valid role' do
+        let(:username) { 'normal-user' }
+        let(:email) { 'normal-user@example.com' }
+        let(:role) { :normal }
+        let(:icon) { nil }
+        let(:password_digest) { 'xxxxyyyy1234' }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'valid icon' do
+        let(:username) { 'admin-user' }
+        let(:email) { 'admin-user@example.com' }
+        let(:role) { :admin }
+        let(:icon) { 'https://xxx.yyy.com' }
+        let(:password_digest) { 'xxxxyyyy1234' }
+
+        it { is_expected.to be_valid }
+      end
+    end
+
+    describe 'invalid' do
+      context 'invalid username nil' do
+        let(:username) { nil }
+        let(:email) { 'admin-user@example.com' }
+        let(:role) { :admin }
+        let(:icon) { nil }
+        let(:password_digest) { 'xxxxyyyy1234' }
+
+        it { is_expected.to_not be_valid }
+      end
+
+      context 'invalid username length' do
+        let(:username) { Faker::Alphanumeric.alpha(number: 21) }
+        let(:email) { 'admin-user@example.com' }
+        let(:role) { :admin }
+        let(:icon) { nil }
+        let(:password_digest) { 'xxxxyyyy1234' }
+
+        it { is_expected.to_not be_valid }
+      end
+
+      context 'invalid email nil' do
+        let(:username) { 'admin-user' }
+        let(:email) { nil }
+        let(:role) { :admin }
+        let(:icon) { nil }
+        let(:password_digest) { 'xxxxyyyy1234' }
+
+        it { is_expected.to_not be_valid }
+      end
+
+      context 'invalid email length' do
+        let(:username) { Faker::Alphanumeric.alpha(number: 248) + '@a.com' }
+        let(:email) { 'admin-user@example.com' }
+        let(:role) { :admin }
+        let(:icon) { nil }
+        let(:password_digest) { 'xxxxyyyy1234' }
+
+        it { is_expected.to_not be_valid }
+      end
+
+      context 'invalid email regex error' do
+        let(:username) { 'admin-user' }
+        let(:email) { 'admin-user@admin' }
+        let(:role) { :admin }
+        let(:icon) { nil }
+        let(:password_digest) { 'xxxxyyyy1234' }
+
+        it { is_expected.to_not be_valid }
+      end
+
+      context 'invalid icon length' do
+        let(:username) { 'admin-user' }
+        let(:email) { 'admin-user@example.com' }
+        let(:role) { :admin }
+        let(:icon) { Faker::Alphanumeric.alpha(number: 5001) }
+        let(:password_digest) { 'xxxxyyyy1234' }
+
+        it { is_expected.to_not be_valid }
+      end
+    end
+  end
 end
