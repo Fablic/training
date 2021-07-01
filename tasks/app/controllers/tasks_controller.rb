8276e5dc3d54353bfe51a83993db20018e1f168c
connectShinyaOno-0039 <shinya.ono@connec10.co.jp>
2021-07-01 15:08:54 +0900

    作成日時の降順で一覧を表示

--- tasks/app/controllers/tasks_controller.rb
+++ tasks/app/controllers/tasks_controller.rb
@@ -1,26 +1,24 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_task, only: %i[show edit update destroy]
 
   def index
-    @tasks = Task.without_deleted
+    @tasks = Task.without_deleted.created_at_desc
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @task = Task.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     create_params = task_params.merge(status_id: MasterTaskStatus::NOT_STARTED)
     @task = Task.new(create_params)
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: "タスクを作成しました。" }
+        format.html { redirect_to @task, notice: 'タスクを作成しました。' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -32,7 +30,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: "タスクを更新しました。" }
+        format.html { redirect_to @task, notice: 'タスクを更新しました。' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -46,18 +44,19 @@ class TasksController < ApplicationController
     now = Time.current
     @task.update(deleted_at: now)
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "タスクを削除しました。" }
+      format.html { redirect_to tasks_url, notice: 'タスクを削除しました。' }
       format.json { head :no_content }
     end
   end
 
   private
-    # 共通処理
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    def task_params
-      params.require(:task).permit(:task_name, :status_id, :priority_id, :label, :limit_date, :detail)
-    end
+  # 共通処理
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:task_name, :status_id, :priority_id, :label, :limit_date, :detail)
+  end
 end
--- tasks/app/models/task.rb
+++ tasks/app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
-    belongs_to :priority, class_name: "MasterTaskPriority"
-    belongs_to :status, class_name: "MasterTaskStatus"
-    scope :without_deleted, -> { where(deleted_at: nil) }
+  belongs_to :priority, class_name: 'MasterTaskPriority'
+  belongs_to :status, class_name: 'MasterTaskStatus'
+  scope :without_deleted, -> { where(deleted_at: nil) }
+  scope :created_at_desc, -> { order(created_at: :desc) }
 end
