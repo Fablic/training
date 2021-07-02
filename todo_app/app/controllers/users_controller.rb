42ff3e25a40add2db7d689cf5b00108f9e94854f
sadahiro-ono <sadahiro.ono@rakuten.com>
2021-07-02 13:12:09 +0900

    fix typo

--- todo_app/app/controllers/users_controller.rb
+++ todo_app/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  skip_before_action :authencate_user, only: %i[new create]
+  skip_before_action :authenticate_user, only: %i[new create]
 
   def new
     redirect_to root_path if user_signed_in?
