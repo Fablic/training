db0fd7d863b7b90629c74e81180d29fb9837c16d
sadahiro-ono <sadahiro.ono@rakuten.com>
2021-07-02 13:32:27 +0900

    rubocop spec/

--- todo_app/spec/factories/tasks.rb
+++ todo_app/spec/factories/tasks.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :task do
     association :user
-     title { Faker::Alphanumeric.alphanumeric(number: 10) }
-     description { Faker::Alphanumeric.alphanumeric(number: 10) }
-     due_date { Faker::Time.forward }
+    title { Faker::Alphanumeric.alphanumeric(number: 10) }
+    description { Faker::Alphanumeric.alphanumeric(number: 10) }
+    due_date { Faker::Time.forward }
   end
 end
