a68332ac603b51349e5d92c91d2496216a831e89
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-07-05 17:19:08 +0900

    [fix] ci

--- todo_app/spec/rails_helper.rb
+++ todo_app/spec/rails_helper.rb
@@ -70,7 +70,11 @@ RSpec.configure do |config|
   config.include_context 'set_user', :require_login
   config.before(:each) do |example|
     if example.metadata[:type] == :system
-      driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+      if example.metadata[:js]
+        driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+      else
+        driven_by :rack_test
+      end
     end
   end
 end
