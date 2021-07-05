47f0ad7a1b9b87dd6fba7590f3895f4329db0b98
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-07-05 16:23:38 +0900

    [fix] ci

--- todo_app/spec/rails_helper.rb
+++ todo_app/spec/rails_helper.rb
@@ -7,7 +7,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -23,7 +23,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -34,10 +34,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.before(:each) do |example|
-    # `describe 〜, type: :system do` のspecを対象にする
-    driven_by :selenium_chrome_headless, screen_size: [1600, 1600] if example.metadata[:type] == :system
-  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include Rails.application.routes.url_helpers
--- todo_app/spec/spec_helper.rb
+++ todo_app/spec/spec_helper.rb
@@ -16,6 +16,8 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'capybara/rspec'
+require 'selenium-webdriver'
+
 RSpec.configure do |config|
   config.include Capybara::DSL
   # rspec-expectations config goes here. You can use an alternate
@@ -89,4 +91,20 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+  # Capybara自体の設定、ここではどのドライバーを使うかを設定しています
+  Capybara.configure do |capybara_config|
+    capybara_config.default_driver = :selenium_chrome
+    capybara_config.default_max_wait_time = 10 # 一つのテストに10秒以上かかったらタイムアウトするように設定しています
+  end
+  # Capybaraに設定したドライバーの設定をします
+  Capybara.register_driver :selenium_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('headless') # ヘッドレスモードをonにするオプション
+    options.add_argument('--disable-gpu') # 暫定的に必要なフラグとのこと
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
+  Capybara.javascript_driver = :selenium_chrome
+
 end
+
