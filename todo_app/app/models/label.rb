68cf8e320c7669bef2fa9f2fb564cbfc0309979e
sadahiro-ono <sadahiro.ono@rakuten.com>
2021-07-06 10:38:09 +0900

    add association

---
+++ todo_app/app/models/label.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Label < ApplicationRecord
+  has_many :task_labels, dependent: :destroy
+  has_many :tasks, through: :task_labels
+
+  validates :name,
+            presence: true,
+            length: { maximum: 10 },
+            uniqueness: { case_sensitive: true }
+end
--- todo_app/app/models/task.rb
+++ todo_app/app/models/task.rb
@@ -4,6 +4,8 @@ class Task < ApplicationRecord
   enum status: { waiting: 1, work_in_progress: 2, completed: 3 }
 
   belongs_to :user
+  has_many :task_labels, dependent: :destroy
+  has_many :labels, through: :task_labels
 
   validates :title, presence: true
   validates :description, presence: true
