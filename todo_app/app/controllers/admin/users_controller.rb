a4d07ad71c1c2d25545dccf9a4ebfabfe6fe8b36
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-07-06 16:52:09 +0900

    [add] admin user show, edit, delete, new

--- todo_app/app/controllers/admin/users_controller.rb
+++ todo_app/app/controllers/admin/users_controller.rb
@@ -18,22 +18,40 @@ module Admin
     def edit; end
 
     def create
-
+      @user = User.new(users_param)
+      if @user.save
+        redirect_to admin_users_path, { flash: { success: I18n.t(:'message.registered_user') } }
+      else
+        flash.now[:error] = I18n.t(:'message.registered_is_failed')
+        render :new
+      end
     end
 
     def update
-
+      if @user.update(users_param)
+        flash[:success] = I18n.t(:'message.registered_user')
+        redirect_to admin_users_path
+      else
+        flash.now[:error] = I18n.t(:'message.edited_is_faild')
+        render :edit
+      end
     end
 
     def destroy
-
-    end
-
-    def tasks
-
+      if @user.destroy
+        flash[:success] = 'Deleted user'
+        redirect_to admin_users_path
+      else
+        flash.now[:error] = I18n.t(:'message.deleted_is_faild')
+        render admin_users_path
+      end
     end
 
     private
+
+    def users_param
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :icon, :role)
+    end
 
     def find_user
       @user = User.joins(:tasks).find(params[:id])
