7a359cfb6a40b967f6cab0b9c30bc907b471da2c
Yasir Taher <yasir.taher@rakuten.com>
2021-07-07 15:31:02 +0900

    fixed some codes using rubocop

--- myapp/app/controllers/sessions_controller.rb
+++ myapp/app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   before_action :logged_in_user, only: [:destroy]
 
@@ -8,7 +10,7 @@ class SessionsController < ApplicationController
   # post method for login
   def create
     @user = User.find_by(email: params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
+    if @user&.authenticate(params[:session][:password])
       log_in @user
       redirect_to root_path
     else
