d789718455e7d81a5076de04dd316270171dac27
sadahiro-ono <sadahiro.ono@rakuten.com>
2021-07-07 13:00:32 +0900

    modify system spec

--- todo_app/spec/system/labels_spec.rb
+++ todo_app/spec/system/labels_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Label, type: :system do
+
   describe '#index', :require_login do
     before {
       create(:label)
@@ -15,72 +16,77 @@ RSpec.describe Label, type: :system do
   end
 
   describe '#new', :require_login do
-    let(:label_name) { Faker::Alphanumeric.alphanumeric(number: 5) }
-    before {
+    subject {
       visit new_label_path
-    }
-    it 'success create label and redirect to labels_path' do
       fill_in :label_name, with: label_name
+      click_button I18n.t('common.action.create')
+    }
 
-      expect do
-        click_button I18n.t('common.action.create')
-      end.to change(Label, :count).by(1)
-      expect(current_path).to eq labels_path
-      expect(page).to have_content(label_name)
-      expect(page).to have_content(I18n.t('users.flash.success.create'))
+    context 'fill in label_name' do
+      let(:label_name) { Faker::Alphanumeric.alphanumeric(number: 5) }
+      it 'success create label and redirect to labels_path' do
+        expect { subject }.to change(Label, :count).by(1)
+        expect(current_path).to eq labels_path
+        expect(page).to have_content(label_name)
+        expect(page).to have_content(I18n.t('users.flash.success.create'))
+      end
     end
-    it 'error occurred and render new' do
-      fill_in :label_name, with: nil
 
-      expect do
-        click_button I18n.t('common.action.create')
-      end.to change(Label, :count).by(0)
-      expect(page).to have_content('名前を入力してください')
-      expect(page).to have_content(I18n.t('labels.flash.error.create'))
+    context 'do not fill label_name' do
+      let(:label_name) { nil }
+      it 'error occurred and render new' do
+        expect { subject }.to change(Label, :count).by(0)
+        expect(page).to have_content('名前を入力してください')
+        expect(page).to have_content(I18n.t('labels.flash.error.create'))
+      end
     end
   end
+
   describe '#edit', :require_login do
-    let(:label) { create(:label) }
-    let(:new_label_name) { Faker::Alphanumeric.alphanumeric(number: 5) }
-    before {
+    let!(:label) { create(:label) }
+    subject {
       visit edit_label_path(label)
+      fill_in :label_name, with: new_label_name
+      click_button I18n.t('common.action.update')
     }
 
-    it 'success create user and redirect to admin_root_path' do
-      fill_in :label_name, with: new_label_name
-
-      expect do
-        click_button I18n.t('common.action.update')
-      end.to change(Label, :count).by(0)
-      expect(current_path).to eq labels_path
-      expect(page).to have_content(new_label_name)
-      expect(page).to have_content(I18n.t('labels.flash.success.update'))
-      expect(page).not_to have_content(label.name)
+    context 'fill in label_name' do
+      let(:new_label_name) { Faker::Alphanumeric.alphanumeric(number: 5) }
+      it 'success create user and redirect to admin_root_path' do
+        expect { subject }.to change(Label, :count).by(0)
+        expect(current_path).to eq labels_path
+        expect(page).to have_content(new_label_name)
+        expect(page).to have_content(I18n.t('labels.flash.success.update'))
+        expect(page).not_to have_content(label.name)
+      end
     end
-    it 'error occurred and render new' do
-      fill_in :label_name, with: nil
 
-      expect do
-        click_button I18n.t('common.action.update')
-      end.to change(Label, :count).by(0)
-      expect(page).to have_content('名前を入力してください')
-      expect(page).to have_content(I18n.t('labels.flash.error.update'))
+    context 'do not fill label_name' do
+      let(:new_label_name) { nil }
+
+      it 'error occurred and render new' do
+        expect { subject }.to change(Label, :count).by(0)
+        expect(page).to have_content('名前を入力してください')
+        expect(page).to have_content(I18n.t('labels.flash.error.update'))
+      end
     end
   end
 
   describe '#destroy', :require_login do
     let!(:label) { create(:label) }
-    it 'sucess destroy and redirect to labels_path' do
+    subject {
       visit labels_path
+      find("a[href='#{label_path(label)}']").click
+    }
 
-      expect do
-        find("a[href='#{label_path(label)}']").click
-      end.to change(Label, :count).by(-1)
-
-      expect(current_path).to eq labels_path
-      expect(page).to have_content(I18n.t('labels.flash.success.destroy'))
-      expect(page).to_not have_content(label.id)
-      expect(page).to_not have_content(label.name)
+    context 'click destroy button' do
+      it 'sucess destroy and redirect to labels_path' do
+        expect { subject }.to change(Label, :count).by(-1)
+        expect(current_path).to eq labels_path
+        expect(page).to have_content(I18n.t('labels.flash.success.destroy'))
+        expect(page).to_not have_content(label.id)
+        expect(page).to_not have_content(label.name)
+      end
     end
   end
 end
