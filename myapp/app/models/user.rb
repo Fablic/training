f0f1b7450dd8766e14853a4f595f720dc1d55e23
Yasir Taher <yasir.taher@rakuten.com>
2021-07-07 11:02:52 +0900

    [STEP16] added validations

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence:true, uniqueness: true
+  validates :name, presence: true
+  validates :password_digest, presence: true, length: { in: 6..20 }
 end
