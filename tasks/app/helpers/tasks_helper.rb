4eefa46950481111d81228764d3fb045514c61f8
connectShinyaOno-0039 <shinya.ono@connec10.co.jp>
2021-07-08 17:18:09 +0900

    レビュー修正

--- tasks/app/helpers/tasks_helper.rb
+++ tasks/app/helpers/tasks_helper.rb
@@ -1,6 +1,6 @@
 module TasksHelper
   def sort_order(column, title)
-    direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
+    direction = sort_column == column && sort_direction == 'asc' ? 'desc' : 'asc'
     link_to title, root_path({ sort: column, direction: direction })
   end
 end
