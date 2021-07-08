d2858d0877a9f503c174a4d181bf74531c6b2c82
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-07-08 17:25:23 +0900

    [add] label list & crud

--- todo_app/app/controllers/labels_controller.rb
+++ todo_app/app/controllers/labels_controller.rb
@@ -2,6 +2,7 @@
 
 class LabelsController < ApplicationController
   skip_before_action :authenticate_user, only: %i[new create]
+  before_action :find_item, only: %i[edit update destroy]
 
   def new
     @label = Label.new
@@ -14,14 +15,38 @@ class LabelsController < ApplicationController
   def create
     @label = Label.new(label_param)
     if @label.save
-      redirect_to root_path, { flash: { success: I18n.t(:'message.registered_user') } }
+      redirect_to labels_path, { flash: { success: I18n.t(:'message.registered_user') } }
     else
       flash.now[:error] = I18n.t(:'message.registered_is_failed')
       render :new
     end
   end
 
+  def edit; end
+
+  def update
+    if @label.update(label_param)
+      redirect_to labels_path, { flash: { success: 'label success' } }
+    else
+      flash.now[:error] = I18n.t(:'message.edited_is_faild')
+      render :edit
+    end
+  end
+
+  def destroy
+    if @label.destroy
+      redirect_to labels_path, { flash: { success: 'label success' } }
+    else
+      flash[:error] = I18n.t(:'message.deleted_is_failed')
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
   private
+
+  def find_item
+    @label = Label.find(params[:id])
+  end
 
   def label_param
     params.require(:label).permit(:name, :color)
