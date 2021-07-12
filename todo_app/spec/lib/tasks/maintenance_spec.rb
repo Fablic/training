1280d8924acb6f263058b32a8bd66c62becc94e3
yoshiki-ohashi <yoshiki.ohashi@rakuten.com>
2021-07-12 14:27:29 +0900

    [add] maintenance spec

---
+++ todo_app/spec/lib/tasks/maintenance_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rake'
+
+RSpec.describe 'maintenance' do
+  let(:file) { Rails.root.join(Constants::MAINTENANCE) }
+
+  before(:all) do
+    @rake = Rake::Application.new
+    Rake.application = @rake
+    Rake.application.rake_require 'tasks/maintenance'
+    Rake::Task.define_task(:environment)
+  end
+
+  before(:each) do
+    @rake[task_name].reenable
+  end
+
+  describe 'start' do
+    let(:task_name) { 'maintenance:start' }
+
+    context 'test start' do
+      it 'create file' do
+        @rake[task_name].invoke
+        visit login_path
+
+        expect(File).to exist file
+        expect(page).to have_content '誠に申し訳ありません。ただいまメンテナンス中です。'
+      end
+    end
+  end
+
+  describe 'finish' do
+    let(:task_name) { 'maintenance:end' }
+
+    context 'delete' do
+      it 'delete file' do
+        @rake[task_name].invoke
+        visit login_path
+
+        expect(File).not_to exist file
+        expect(page).to have_content 'TODO App'
+      end
+    end
+  end
+end
