1be3707cb88d4cc51cece902dd5e5700fbc13a8a
k-kiosk <kyosuke.b.kubota@rakuten.com>
2021-07-28 14:55:07 +0900

    add create, update, delete

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -32,4 +32,46 @@ RSpec.describe "Tasks", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "POST create" do
+    it "creates a task" do
+      expect do
+        post tasks_path, params: { task: attributes_for(:task) }
+      end.to change(Task, :count).by(1)
+    end
+
+    it "returns 302" do
+      post tasks_path, params: { task: attributes_for(:task) }
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "PUT update" do
+    let(:task) { create(:task) }
+
+    it "updates a task" do
+      put task_path(task), params: { task: attributes_for(:task, name: "updated task") }
+      expect(task.reload.name).to eq "updated task"
+    end
+
+    it "returns 302" do
+      put task_path(task), params: { task: attributes_for(:task, name: "updated") }
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "DELETE destroy" do
+    let!(:task) { create(:task) }
+
+    it "deletes a task" do
+      expect do
+        delete task_path(task)
+      end.to change(Task, :count).by(-1)
+    end
+
+    it "returns 302" do
+      delete task_path(task)
+      expect(response).to have_http_status(302)
+    end
+  end
 end
