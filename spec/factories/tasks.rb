7704ddc5290beedeba22f43c7583c7eaa5ed7f9c
k-kiosk <kyosuke.b.kubota@rakuten.com>
2021-07-28 12:15:15 +0900

    add model spec

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  description :text(65535)
+#  name        :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+FactoryBot.define do
+  factory :task do
+    name { "do homework" }
+    description { "Deadline is August 31." }
+  end
+end
