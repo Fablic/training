f350f7c229028b6cbc1b7fd7c1aa15609f92b938
k-kiosk <kyosuke.b.kubota@rakuten.com>
2021-08-11 15:13:45 +0900

    add scope spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -41,4 +41,33 @@ RSpec.describe Task, type: :model do
       expect(task).to be_valid
     end
   end
+
+  describe 'scope' do
+    let!(:important_task) { create(:task, name: '[important]book flight tickets', status: 'ToDo') }
+    let!(:completed_task) { create(:task, name: 'completed task', status: 'Done') }
+
+    describe 'by_name' do
+      let(:search_name) { 'important' }
+
+      it 'returns a task' do
+        expect(Task.by_name(search_name).size).to eq 1
+      end
+
+      it 'returns tasks which name include search name' do
+        expect(Task.by_name(search_name).sample.name).to include(search_name)
+      end
+    end
+
+    describe 'by_status' do
+      let(:search_status) { 'Done' }
+
+      it 'returns a task' do
+        expect(Task.by_status(search_status).size).to eq 1
+      end
+
+      it 'returns tasks which status equal search status' do
+        expect(Task.by_status(search_status).sample.status).to eq (search_status)
+      end
+    end
+  end
 end
