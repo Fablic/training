75595e6a5169f6ce3278e61f3fc76b86399bb17c
Daisuke Hirabayashi <daisuk.a.hirabayshi@rakuten.com>
2021-08-17 15:53:16 +0900

    enumによるpriorityとprogressカラムの導入

--- task_manager/app/models/task.rb
+++ task_manager/app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+    enum priority: {
+        low: 0, #優先度低
+        normal: 1, #優先度中
+        high: 2, #優先度高
+    }
+
+    enum progress: {
+        Todo: 0,
+        InProgress: 1,
+        Done: 2,
+    }
 end
