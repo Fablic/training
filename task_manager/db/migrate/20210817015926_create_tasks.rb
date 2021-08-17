5b401e302e7e1aea143cff7ef9b4d234bd0e8f97
Daisuke Hirabayashi <daisuk.a.hirabayshi@rakuten.com>
2021-08-17 15:16:53 +0900

    rails g modelによるtaskモデルの作成

---
+++ task_manager/db/migrate/20210817015926_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name, :null => false
+      t.text :description
+      t.datetime :due_at, :null => false
+      t.integer :priority, :null => false
+      t.integer :progress, :null => false
+
+      t.timestamps
+    end
+  end
+end
