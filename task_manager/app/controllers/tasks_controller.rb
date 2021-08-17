99bcb2299195e204e13181a31bb46bd332640186
Daisuke Hirabayashi <daisuk.a.hirabayshi@rakuten.com>
2021-08-17 22:35:08 +0900

    新規タスクの作成

--- task_manager/app/controllers/tasks_controller.rb
+++ task_manager/app/controllers/tasks_controller.rb
@@ -8,9 +8,19 @@ class TasksController < ApplicationController
     end
   
     def new
+        @task = Task.new
     end
   
     def create
+        @task = Task.new(task_params)
+
+        if @task.save
+          flash[:success] = 'タスクが投稿されました'
+          redirect_to @task
+        else
+          flash[:danger] = 'タスクが投稿されません'
+          render :new
+        end
     end
   
     def edit
@@ -21,4 +31,10 @@ class TasksController < ApplicationController
   
     def destroy
     end  
+
+    private
+
+    def task_params
+        params.require(:task).permit(:name,:due_at,:priority,:progress)
+    end
 end
