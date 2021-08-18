8451716b2d0ac147167dd21d8859691ed233bb3f
Daisuke Hirabayashi <daisuk.a.hirabayshi@rakuten.com>
2021-08-18 18:44:47 +0900

    Revert "rubocop -a による修正"
    This reverts commit 9f853b06d6b544a11a20792259b91d278aa90b39.

--- task_manager/app/controllers/tasks_controller.rb
+++ task_manager/app/controllers/tasks_controller.rb
@@ -1,57 +1,57 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  def index
-    @tasks = Task.all
-  end
-
-  def show
-    @task = Task.find(params[:id])
-  end
-
-  def new
-    @task = Task.new
-  end
-
-  def create
-    @task = Task.new(task_params)
-
-    if @task.save
-      flash[:success] = 'タスクが投稿されました'
-      redirect_to @task
-    else
-      flash[:danger] = 'タスクが投稿されません'
-      render :new
+    def index
+        @tasks = Task.all
     end
-  end
-
-  def edit
-    @task = Task.find(params[:id])
-  end
-
-  def update
-    @task = Task.find(params[:id])
-
-    if @task.update(task_params)
-      flash[:success] = 'タスクが編集されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'タスクが編集されませんでした'
-      render :new
+  
+    def show
+        @task = Task.find(params[:id])
     end
-  end
+  
+    def new
+        @task = Task.new
+    end
+  
+    def create
+        @task = Task.new(task_params)
 
-  def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
+        if @task.save
+          flash[:success] = 'タスクが投稿されました'
+          redirect_to @task
+        else
+          flash[:danger] = 'タスクが投稿されません'
+          render :new
+        end
+    end
+  
+    def edit
+        @task = Task.find(params[:id])
+    end
+  
+    def update
+        @task = Task.find(params[:id])
 
-    flash[:success] = 'タスクが削除されました'
-    redirect_to tasks_path
-  end
+        if @task.update(task_params)
+            flash[:success] = 'タスクが編集されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'タスクが編集されませんでした'
+            render :new
+        end
+    end
 
-  private
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+      
+        flash[:success] = 'タスクが削除されました'
+        redirect_to tasks_path
+    end
 
-  def task_params
-    params.require(:task).permit(:name, :due_at, :priority, :progress)
-  end
+    private
+
+    def task_params
+        params.require(:task).permit(:name,:due_at,:priority,:progress)
+    end
 end
