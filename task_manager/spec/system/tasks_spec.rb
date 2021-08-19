8f40ed4a0ebb27b86eae0e3eb519dbb07b9ebd7a
Daisuke Hirabayashi <daisuk.a.hirabayshi@rakuten.com>
2021-08-19 15:26:33 +0900

    contextを用いたテストコードをわかりやすくするための修正

--- task_manager/spec/system/tasks_spec.rb
+++ task_manager/spec/system/tasks_spec.rb
@@ -24,48 +24,54 @@ RSpec.describe 'Tasks', type: :system do
     expect(page).to have_content 'InProgress'
   end
 
-  it '編集が行われているかの確認' do
+  context '編集が行われているかの確認' do
     # Task編集画面を開く
-    visit edit_task_path(task)
+    before { visit edit_task_path(task) }
+    
+    it '既存のタスク内容が書いている' do
+      expect(page).to have_field 'メモ', with: 'Memo'
+    end
+    
+    it 'タスクを編集できる' do
+      # メモに"Memo"が入力されていることを検証する
+      # メモを再入力
+      fill_in 'メモ', with: 'MyText'
 
-    # メモに"Memo"が入力されていることを検証する
-    expect(page).to have_field 'メモ', with: 'Memo'
+      # 更新実行
+      click_button '投稿'
 
-    # メモを再入力
-    fill_in 'メモ', with: 'MyText'
-
-    # 更新実行
-    click_button '投稿'
-
-    # 画面を検証する
-    expect(page).to have_content 'タスクが編集されました'
-    expect(page).to have_content 'MyString'
-    expect(page).to have_content 'MyText'
+      # 画面を検証する
+      expect(page).to have_content 'タスクが編集されました'
+      expect(page).to have_content 'MyString'
+      expect(page).to have_content 'MyText'
+    end
   end
 
-  it '新規作成が行われるかの確認' do
+  context '新規作成できるかの確認' do
     # Task新規作成画面を開く
-    visit new_task_path
+    before { visit new_task_path }
 
-    # タスクを入力
-    fill_in 'タスク', with: 'Task'
-    # メモを入力
-    fill_in 'メモ', with: 'Memo'
-    # 締め切りを入力
-    fill_in '締め切り', with: '2021-08-17 10:59:26'
-    # 優先順位を入力
-    select '中', from: '優先順位'
-    # 進捗状況を入力
-    select 'InProgress', from: '進捗状況'
-    # 更新実行
-    click_button '投稿'
+    it '全ての項目を入力して成功する' do
+      # タスクを入力
+      fill_in 'タスク', with: 'Task'
+      # メモを入力
+      fill_in 'メモ', with: 'Memo'
+      # 締め切りを入力
+      fill_in '締め切り', with: '2021-08-17 10:59:26'
+      # 優先順位を入力
+      select '中', from: '優先順位'
+      # 進捗状況を入力
+      select 'InProgress', from: '進捗状況'
+      # 更新実行
+      click_button '投稿'
 
-    # 画面を検証する
-    expect(page).to have_content 'タスクが投稿されました'
-    expect(page).to have_content 'Task'
-    expect(page).to have_content 'Memo'
-    expect(page).to have_content 'normal'
-    expect(page).to have_content 'InProgress'
+      # 画面を検証する
+      expect(page).to have_content 'タスクが投稿されました'
+      expect(page).to have_content 'Task'
+      expect(page).to have_content 'Memo'
+      expect(page).to have_content 'normal'
+      expect(page).to have_content 'InProgress'
+    end
   end
 
   it '削除の確認' do
