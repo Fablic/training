997959980a1635ccef4c12ce3c0f9adb2534e2b6
Daisuke Hirabayashi <daisuk.a.hirabayshi@rakuten.com>
2021-08-19 17:31:22 +0900

    rubocopによる修正

--- task_manager/config/application.rb
+++ task_manager/config/application.rb
@@ -23,6 +23,6 @@ module TaskManager
 
     config.i18n.default_locale = :ja
 
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
   end
 end
