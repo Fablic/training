806824536ba04b69d1edbc3c4fe725e1e0e28e72
Daisuke Hirabayashi <daisuk.a.hirabayshi@rakuten.com>
2021-08-22 14:01:57 +0900

    名前等を昇順に変更

--- task_manager/app/helpers/application_helper.rb
+++ task_manager/app/helpers/application_helper.rb
@@ -4,7 +4,11 @@ module ApplicationHelper
     def sortable(column, title = nil)
         title ||= column.titleize
         css_class = (column == sort_column) ? "current #{sort_direction}" : nil
-        direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+        if sort_column == 'created_at' #初期降順
+            direction = (column == sort_column && sort_direction == "desc") ? "asc" : "desc"
+        else #初期昇順
+            direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+        end
         link_to title, {:sort => column, :direction => direction}, {:class => css_class}
     end
 end
