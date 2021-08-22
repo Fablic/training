d687cbb6dc4dfe51392673b51be7cadce990bc0a
Daisuke Hirabayashi <daisuk.a.hirabayshi@rakuten.com>
2021-08-22 14:52:39 +0900

    初期値を作成びの降順にし、その後の検索は昇順になるように修正

--- task_manager/app/helpers/application_helper.rb
+++ task_manager/app/helpers/application_helper.rb
@@ -3,11 +3,7 @@
 module ApplicationHelper
   def sortable(column, title)
     css_class = column == sort_column ? "current #{sort_direction}" : nil
-    direction = if sort_column == 'created_at' # 初期降順
-                  column == sort_column && sort_direction == 'desc' ? 'asc' : 'desc'
-                else # 初期昇順
-                  column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-                end
+    direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
     link_to title, { sort: column, direction: direction }, { class: css_class }
   end
 end
