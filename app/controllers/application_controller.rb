827835a51eebb21cbc044eb449004ff0250d7f56
k-kiosk <kyosuke.b.kubota@rakuten.com>
2021-08-23 08:38:47 +0900

    available only to logging in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@
 
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  before_action :login_required
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+  end
+
+  def login_required
+    redirect_to login_path unless current_user
   end
 end
