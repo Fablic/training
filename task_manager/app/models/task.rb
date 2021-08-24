f9370e9e6903722f0bbd26d579f84b848df502de
DaisukeHirabayashi <daisuke@pluslab.org>
2021-08-24 14:05:57 +0900

    rubocopによる修正

--- task_manager/app/models/task.rb
+++ task_manager/app/models/task.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  
   enum priority: {
     low: 0, # 優先度低
     normal: 1, # 優先度中
@@ -19,17 +18,20 @@ class Task < ApplicationRecord
   validate :due_at_is_valid_datetime
   validates :priority, inclusion: { in: Task.priorities.keys }
   validates :progress, inclusion: { in: Task.progresses.keys }
-  
+
   with_options if: :due_at? do
     validate :due_at_start_check
   end
 
   def due_at_is_valid_datetime
-    errors.add(:due_at, :invalid_datetime) if ((due_at.in_time_zone rescue ArgumentError) == ArgumentError)
+    errors.add(:due_at, :invalid_datetime) if begin
+      due_at.in_time_zone
+    rescue StandardError
+      ArgumentError
+    end == ArgumentError
   end
 
   def due_at_start_check
-    errors.add(:due_at, :start_check) if due_at < Time.now
+    errors.add(:due_at, :start_check) if due_at < Time.current
   end
-
 end
--- task_manager/spec/rails_helper.rb
+++ task_manager/spec/rails_helper.rb
@@ -63,7 +63,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  RSpec.configure do |config|
-    config.include ActiveSupport::Testing::TimeHelpers
-  end
+  config.include ActiveSupport::Testing::TimeHelpers
 end
