6d7c63f7ec9886baf2f3aa74cad62e3a07bb12e5
DaisukeHirabayashi <daisuke@pluslab.org>
2021-08-24 09:54:07 +0900

    締め切り及び名前に対してvalidationの追加

--- task_manager/app/models/task.rb
+++ task_manager/app/models/task.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  
   enum priority: {
     low: 0, # 優先度低
     normal: 1, # 優先度中
@@ -12,4 +13,18 @@ class Task < ApplicationRecord
     InProgress: 1,
     Done: 2,
   }
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :due_at_is_valid_datetime
+  validate :due_at_start_check
+
+  def due_at_is_valid_datetime
+    errors.add(:due_at, :invalid_datetime) if ((due_at.in_time_zone rescue ArgumentError) == ArgumentError)
+  end
+
+  def due_at_start_check
+    errors.add(:due_at, :start_check) if due_at < Time.now
+  end
+
 end
