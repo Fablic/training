2a8ce115d6a3e89397fe2f36a424b0ac3e4dfb2f
k-kiosk <kyosuke.b.kubota@rakuten.com>
2021-08-27 10:40:37 +0900

    show user's tasks

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,23 @@
 ActiveAdmin.register User do
   permit_params :name, :email, :password
 
+  show do
+    attributes_table do
+      row :name
+      row :email
+      row :created_at
+      row :updated_at
+    end
+
+    panel 'タスク一覧' do
+      table_for user.tasks do
+        column :name
+        column :description
+        column :status
+      end
+    end
+  end
+
   form do |f|
     f.semantic_errors(*f.object.errors.keys)
     f.inputs do
