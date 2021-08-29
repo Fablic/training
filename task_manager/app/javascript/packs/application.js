bef08f1c6aa44cd381aa8ad4235a0145dc86ade6
DaisukeHirabayashi <daisuke@pluslab.org>
2021-08-29 22:22:03 +0900

    編集画面をmodal化

--- task_manager/app/javascript/packs/application.js
+++ task_manager/app/javascript/packs/application.js
@@ -7,10 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "bootstrap/scss/bootstrap.scss"
 
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 
 Rails.start()
 Turbolinks.start()
