24ea43576a42d52971d6c3e97a9612c0cb96fbf0
DaisukeHirabayashi <daisuke@pluslab.org>
2021-08-31 14:08:16 +0900

    userControllerの追加

---
+++ task_manager/app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  before_action :set_user_by_id, only: %i[show edit update destroy]
+  def index # rubocop:disable Metrics/AbcSize
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    return unless @user.save
+
+    flash[:success] = I18n.t 'tasks.flash.create.success'
+    redirect_to @user
+
+  end
+
+  def edit
+  end
+
+  def update
+    return unless @user.update(user_params)
+
+    flash[:success] = I18n.t 'users.flash.update.success'
+    redirect_to @task
+  end
+
+  def destroy
+    @task.destroy
+
+    flash[:success] = I18n.t 'users.flash.destroy.success'
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_user_by_id
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
