aa4ed634facc1b46ef34a37784699a321a98c90d
DaisukeHirabayashi <daisuke@pluslab.org>
2021-08-31 11:34:29 +0900

    サンプルデータの追加

--- task_manager/db/seeds.rb
+++ task_manager/db/seeds.rb
@@ -6,3 +6,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "root",
+  email: "root@railstutorial.org",
+  password: "password",
+  password_confirmation: "password")
+
+5.times do
+  task = Task.create!(
+    name: Faker::Hobby.activity,
+    description: "memo",
+    due_at: Faker::Date.between(from: Time.current + 1.day, to: Time.current + 1.year ),
+    priority: rand(3),
+    progress: rand(3),
+    user_id: User.last.id,
+  )
+end
