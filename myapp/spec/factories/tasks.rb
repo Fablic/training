9cdba778bd4867857ceee9a4c38590cff9c881c3
Amiko Hosoya <ts-amiko.hosoya@rakuten.com>
2021-08-31 16:32:07 +0900

    step12 終了期限を追加

--- myapp/spec/factories/tasks.rb
+++ myapp/spec/factories/tasks.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :task do
     sequence(:title) { |n| "test_title#{n}" }
     content { 'test_content' }
-    sequence(:created_at) { |n| Time.current + n }
+    sequence(:deadline) { |n| Time.current.next_month - n.days }
+    sequence(:created_at) { |n| Time.current + n.days }
   end
 end
