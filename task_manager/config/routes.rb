4ce4fff45c55e402cdaecd33739d1388c607c4b2
DaisukeHirabayashi <daisuke@pluslab.org>
2021-09-01 10:21:29 +0900

    簡易的なログイン機能の実装

--- task_manager/config/routes.rb
+++ task_manager/config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
 
   resources :tasks
-  
   resources :users
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
 end
