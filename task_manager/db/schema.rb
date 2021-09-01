4fd2616b32e3bc33ffb5fdac76b49b7c4084cf0b
DaisukeHirabayashi <daisuke@pluslab.org>
2021-09-01 18:05:03 +0900

    [edit]dbのカラム修正

--- task_manager/db/schema.rb
+++ task_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_091020) do
+ActiveRecord::Schema.define(version: 2021_09_01_090047) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 2021_08_31_091020) do
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.string "password_digest"
-    t.string "email"
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
