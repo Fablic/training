762c6a0db3347770771a32b5d428e2a16076b73d
DaisukeHirabayashi <daisuke@pluslab.org>
2021-09-03 16:21:45 +0900

    [edadd]タスクの数を取得して表示

--- task_manager/app/controllers/admin/users_controller.rb
+++ task_manager/app/controllers/admin/users_controller.rb
@@ -3,7 +3,9 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user_by_id, only: %i[show edit update destroy]
   def index
-    @users = User.search_name(params[:keyword]).or(User.search_email(params[:keyword])).page(params[:page]).per(5)
+    @users = User.all.includes(:tasks)
+    .search_name(params[:keyword]).or(User.search_email(params[:keyword]))
+    .page(params[:page]).per(5)
   end
 
   def show
