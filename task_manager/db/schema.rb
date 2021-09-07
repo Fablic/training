d36abf3f2efe8912df33ea068d4f0de66533e876
DaisukeHirabayashi <daisuke@pluslab.org>
2021-09-07 17:43:55 +0900

    [edit]nameカラムにuniqueを追加及びindexの追加

--- task_manager/db/schema.rb
+++ task_manager/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_020314) do
+ActiveRecord::Schema.define(version: 2021_09_07_084114) do
 
   create_table "labels", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_labels_on_name", unique: true
   end
 
   create_table "task_labels", charset: "utf8mb4", force: :cascade do |t|
