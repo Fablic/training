1e9a41fb3553e5bd6ec0667691cc5529ecd003a1
DaisukeHirabayashi <daisuke@pluslab.org>
2021-09-07 16:29:14 +0900

    [edit]改行の追加及び誤字の修正

--- task_manager/app/controllers/tasks_controller.rb
+++ task_manager/app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   before_action :set_task_by_id, only: %i[show edit update destroy]
   before_action :confirm_permission, only: %i[show edit update destroy]
 
-  def index # rubocop:disable Metrics/AbcSizeå
+  def index # rubocop:disable Metrics/AbcSize
     @tasks = Task.includes(:labels).search_user_id(session[:user_id])
       .sort_column_direction(params[:sort], params[:direction])
       .search_name(params[:keyword_name]).search_progress(params[:keyword_progress])
