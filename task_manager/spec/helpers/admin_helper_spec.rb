616d765feb3a84629df802521c721c762e8b3137
DaisukeHirabayashi <daisuke@pluslab.org>
2021-09-08 17:44:15 +0900

    [add]helperのテスト追加

---
+++ task_manager/spec/helpers/admin_helper_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AdminHelper, type: :helper do
+  describe 'method' do
+    describe 'admin_screen' do
+      let(:request) { double('request', fullpath: fullpath) }
+      before  { allow(helper).to receive(:request).and_return(request) }
+      subject{ admin_screen? }
+
+      context 'admin以外のページにリクエストを送る' do
+        let!(:fullpath) { '/users/2' }
+        it { is_expected.to be_falsey }
+      end
+
+      context 'adminページにリクエストを送る' do
+        let!(:fullpath) { '/admin/users/2' }
+        it { is_expected.to be_truthy }
+      end
+    end
+  end
+end
