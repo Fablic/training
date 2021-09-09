dcf5e53f96e40b1b85088050ebcbbe7c814743e1
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-09-09 10:39:58 +0900

    step 6

--- OreNoTask/db/migrate/20210907081903_create_task.rb
+++
@@ -1,8 +0,0 @@
-class CreateTask < ActiveRecord::Migration[6.1]
-  def change
-    create_table :tasks do |t|
-
-      t.timestamps
-    end
-  end
-end
--- OreNoTask/db/schema.rb
+++ OreNoTask/db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_09_07_082552) do
+
+  create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 50, null: false
+    t.text "description"
+    t.integer "status", limit: 1, default: 0, null: false
+    t.datetime "start_at", null: false
+    t.datetime "due_date_at", null: false
+    t.integer "deleted", limit: 1, default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
