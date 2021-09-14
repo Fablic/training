84fbf60ba99cd6d2ce7cb0ef2f01b3aef9e99761
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-09-14 18:52:27 +0900

    step7

---
+++ OreNoTask/app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.where(deleted: 0)
+  end
+
+  def new
+  end
+
+  def edit
+    id = params[:id]
+    @task = Task.find_by(id: id, deleted:0)
+  end
+
+  def update
+     @task = Task.find(params[:id])
+     @task.update(name: params[:task][:name], description: params[:task][:description], start_at_date: params[:task][:start_at_date], start_at_hour: params[:task][:start_at_hour], start_at_minute: params[:task][:start_at_minute], due_date_at_date: params[:task][:due_date_at_date], due_date_at_hour: params[:task][:due_date_at_hour], due_date_at_minute: params[:task][:due_date_at_minute])
+  end
+
+  def show
+    id = params[:id]
+    @task = Task.find_by(id: id, deleted:0)
+  end
+
+  def create
+    @task = Task.new(name: params[:name], description: params[:description], start_at: params[:start_at_date] << ' ' << params[:start_at_hour] << ':' << params[:start_at_minute], due_date_at: params[:due_date_at_date] << ' ' << params[:due_date_at_hour] << ':' << params[:due_date_at_minute])
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.update(deleted: 1)
+    redirect_to tasks_path
+  end
+
+end
--- OreNoTask/app/models/task.rb
+++ OreNoTask/app/models/task.rb
@@ -1,2 +1,21 @@
 class Task < ApplicationRecord
+	include DatetimeIntegratable
+		REGISTRABLE_ATTRIBUTES = %i(
+		    name
+		    description
+		    status
+		    start_at_date start_at_hour start_at_minute
+		    due_date_at_date due_date_at_hour due_date_at_minute
+		    deleted
+	    )
+
+	integrate_datetime_fields(:due_date_at, :start_at)
+
+	validates :start_at_date, presence: true
+	validates :start_at_hour, presence: true
+	validates :start_at_minute, presence: true
+
+	validates :due_date_at_date, presence: true
+	validates :start_at_hour, presence: true
+	validates :start_at_minute, presence: true
 end
