9836b50377efd0dc042245579bd4d6866c2e20d0
khanh le <khanh.leduy@rakuten.com>
2021-09-14 11:24:03 +0900

    add search spec test

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
-  factory :task do
+  factory :task, class: Task do
     name { 'task 1' }
     description { 'task 1 description' }
     priority { 0 } #low
+    status { 0 } #todo
+  end
+
+  factory :other_task, class: Task do
+    name { 'other task' }
+    description { 'other task description' }
+    priority { 0 } #low
+    status { 2 } #done
   end
 end
