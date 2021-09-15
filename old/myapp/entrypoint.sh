28ec0efbefa2c27abceede328b512e9e459ce845
u-Hashimoto <yu.a.hashimoto@rakuten.com>
2021-09-15 14:59:56 +0900

    docker上で動くようにする

---
+++ old/myapp/entrypoint.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# エラーが発生するとスクリプトを終了
+set -e
+
+# remove a potentially pre-existing server.pid for rails
+# railsのpidが存在している場合は削除する
+rm -f /myapp/tmp/pids/server.pid
+
+# Then exec the containers main process
+# DockerfileのCMDで渡されたコマンド（Railsのサーバー起動）を実行
+exec "$@"
