12308801f65c8cfaae1811cf217beefd0f94f797
u-Hashimoto <yu.a.hashimoto@rakuten.com>
2021-09-17 17:56:07 +0900

    step6-active

---
+++ myapp/db/migrate/20210917080322_labels.rb
@@ -0,0 +1,4 @@
+class Labels < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,6 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_09_17_080322) do
+
+  create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "name"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "mail_address"
+    t.integer "role_id"
+    t.datetime "last_login_date"
+    t.datetime "regist_date"
+    t.integer "regist_user"
+    t.datetime "update_date"
+    t.integer "update_user"
+    t.boolean "del_flag"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
