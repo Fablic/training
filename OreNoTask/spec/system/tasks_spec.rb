fece3829053f5ec1bacacb039ea5a017bf1b2447
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-09-21 17:19:11 +0900

    corrected probrems for rubocop result

--- OreNoTask/spec/system/tasks_spec.rb
+++ OreNoTask/spec/system/tasks_spec.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 describe 'タスク管理機能', type: :system do
   before do
-    FactoryBot.create(:task, name: "最初のタスク", description: '説明文', start_at: '2021/09/01 10:00', due_date_at: '2021/09/02 11:00')
-    FactoryBot.create(:task, name: "２番目のタスク", description: '説明文２', start_at: '2021/09/02 10:00', due_date_at: '2021/09/03 11:00')
+    FactoryBot.create(:task, name: '最初のタスク', description: '説明文', start_at: '2021/09/01 10:00', due_date_at: '2021/09/02 11:00')
+    FactoryBot.create(:task, name: '２番目のタスク', description: '説明文２', start_at: '2021/09/02 10:00', due_date_at: '2021/09/03 11:00')
   end
 
   context 'タスク一覧を表示する' do
     before do
       visit tasks_path
     end
+
     it '一覧画面の表示を確認する' do
       # 既存のタスクが表示されている
       expect(page).to have_content '最初のタスク'
@@ -22,6 +25,7 @@ describe 'タスク管理機能', type: :system do
     before do
       visit tasks_path
     end
+
     it '詳細画面に遷移し、内容を確認する' do
       click_link '最初のタスク'
       expect(page).to have_content '最初のタスク'
@@ -76,7 +80,6 @@ describe 'タスク管理機能', type: :system do
       expect(page).to have_content '２番目のタスク'
       expect(page).to have_content '2021-09-02 10:00:00 +0900 〜 2021-09-03 11:00:00 +0900'
 
-
       # 詳細画面で編集したタスクの内容を確認
       click_link '最初のタスクを編集'
       expect(page).to have_content '最初のタスクを編集'
@@ -91,13 +94,12 @@ describe 'タスク管理機能', type: :system do
       click_button '削除', match: :first
 
       # 作成されたタスクが表示されてない
-      expect(page).to_not have_content '最初のタスク'
-      expect(page).to_not have_content '2021-09-01 10:00:00 +0900 〜 2021-09-02 11:00:00 +0900'
+      expect(page).not_to have_content '最初のタスク'
+      expect(page).not_to have_content '2021-09-01 10:00:00 +0900 〜 2021-09-02 11:00:00 +0900'
 
       # 既存のデータに影響がない
       expect(page).to have_content '２番目のタスク'
       expect(page).to have_content '2021-09-02 10:00:00 +0900 〜 2021-09-03 11:00:00 +0900'
     end
   end
-
 end
