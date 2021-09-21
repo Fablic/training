94fc6988dacfbb0c4899b8b9a6707c7465f9539b
khanh le <khanh.leduy@rakuten.com>
2021-09-21 10:25:34 +0900

    change password to password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_061328) do
+ActiveRecord::Schema.define(version: 2021_09_21_012341) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "user_id"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_09_15_061328) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
