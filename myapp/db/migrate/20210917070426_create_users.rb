dcd99eaf79f82b0a16981c9c8c752282f24f83df
u-Hashimoto <yu.a.hashimoto@rakuten.com>
2021-09-24 17:33:44 +0900

    rubocopの導入とmigrationファイルの修正

--- myapp/db/migrate/20210917070426_create_users.rb
+++ myapp/db/migrate/20210917070426_create_users.rb
@@ -3,11 +3,11 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :name
       t.string :mail_address
-      t.integer :role_id,limit:1
+      t.integer :role_id, limit: 1
       t.datetime :last_login_date
       t.integer :regist_user
       t.integer :update_user
-      t.boolean :del_flag, null:false, default:false
+      t.boolean :del_flag, null: false, default: false
       t.timestamps
     end
   end
--- myapp/db/migrate/20210917072357_create_tasks.rb
+++ myapp/db/migrate/20210917072357_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :name
       t.text :description
-      t.references :user, foreign_key:true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
