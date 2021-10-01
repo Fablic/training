223b88425d7875b281b36c2fe22c8414e0a18e25
khanh le <khanh.leduy@rakuten.com>
2021-10-01 16:18:31 +0900

    change method name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   before_action :logged_in_user
-  before_action :check_maintenance, if: :is_maintenance_mode?
+  before_action :render_maintenance_page, if: :is_maintenance_mode?
 
   private
     def logged_in_user
       redirect_to login_url unless logged_in?
     end
 
-    def check_maintenance
+    def render_maintenance_page
       render file: Rails.root.join('public/503.html'), layout: false, status: 503
     end
 
