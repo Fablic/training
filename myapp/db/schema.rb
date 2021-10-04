d0b577304f88c3cc2d63a46d28690d8c4ea46446
taito.a.ito <taito.a.ito@rakuten.com>
2021-10-04 18:37:02 +0900

    Step10 テーブルに論理削除用カラムの追加

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_053717) do
+ActiveRecord::Schema.define(version: 2021_10_04_064644) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "deleted", limit: 1, default: 0, null: false
   end
 
 end
