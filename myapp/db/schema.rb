943f13cb042d2162e2150ef3bda74d8fccfd39ab
u-Hashimoto <yu.a.hashimoto@rakuten.com>
2021-10-04 09:20:36 +0900

    タスクテーブルの変更

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_075922) do
+ActiveRecord::Schema.define(version: 2021_10_01_061731) do
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "name"
     t.bigint "user_id"
-    t.integer "regist_user"
-    t.integer "update_user"
+    t.integer "regist_user_id"
+    t.integer "update_user_id"
     t.boolean "del_flag", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 2021_09_17_075922) do
   end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
+    t.string "name", limit: 256, comment: "タスク名"
+    t.string "description", limit: 1024, comment: "コメント"
     t.bigint "user_id"
+    t.datetime "period_date", comment: "終了期限"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
@@ -37,8 +38,8 @@ ActiveRecord::Schema.define(version: 2021_09_17_075922) do
     t.string "mail_address"
     t.integer "role_id", limit: 1
     t.datetime "last_login_date"
-    t.integer "regist_user"
-    t.integer "update_user"
+    t.integer "regist_user_id"
+    t.integer "update_user_id"
     t.boolean "del_flag", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
