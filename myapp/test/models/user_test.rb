57a4db6fca64f21f66e678e2d6ab895cdb0f8e69
mvincent.yap <mvincent.yap@rakuten.com>
2021-10-07 11:38:47 +0900

    add data fixtures and simple unit test for user

--- myapp/test/models/user_test.rb
+++ myapp/test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "basic user authentication" do
+    assert User.find_by(username: 'mvincent.yap@rakuten.com')&.authenticate('test1234')   
+    assert_not User.find_by(username: 'mvincent.yap@rakuten.com')&.authenticate('notright')   
+    U = User.find_by(username: 'mvincent.yap@rakuten.com')&.authenticate('test1234')
+    assert_equal "mvincent", U.name
+    assert_equal 1, U.id
+  end
+
 end
