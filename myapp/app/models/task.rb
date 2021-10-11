a6cfa536e706552a910434fd55dfd09a0633c2f5
taito.a.ito <taito.a.ito@rakuten.com>
2021-10-11 14:31:21 +0900

    Step12 add due_date and minimum validation

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
-  validates :title, { presence: true, length: { maximum: 50 } }
-  validates :description, { length: { maximum: 255 } }
+  validates :title, presence: true, length: { maximum: 50 } 
+  validates :description, length: { maximum: 255 }
+  validates :due_date, presence: true, date: true
+  # validates :due_date_check?
 end
+
+# def due_date_check
+#   errors.add(:due_date, "は現在日時よりも遅い日時を入力してください") if self.due_date < self.created_at
+#   print(self.due_date)
+#   print(self.created_at)
+# end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_081447) do
+ActiveRecord::Schema.define(version: 2021_10_10_091238) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", limit: 50, null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_10_05_081447) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "deleted", limit: 1, default: 0, null: false
+    t.datetime "due_date", null: false
   end
 
 end
