031c7c2af8e8676062ef1eabd081a5568c0886f5
yuta.saito <yuta.d.saito@rakuten.com>
2021-10-12 14:57:10 +0900

    Add rubocop

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,6 +1,4 @@
 class Task < ApplicationRecord
-
-    validates :title, presence: true, length: { minimum:3 , maximum: 20 }
-    validates :detail, length: {  maximum: 1000 }
-  
+  validates :title, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :detail, length: { maximum: 1000 }
 end
