df3700a8dcebe2ad71eb86734b235e8d6a153092
mvincent.yap <mvincent.yap@rakuten.com>
2021-10-13 15:20:51 +0900

    add enum to task

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -5,4 +5,11 @@ class Task < ApplicationRecord
   validates :name, { presence: true, length: { maximum: 75 } }
   validates :description, { length: { maximum: 1000 } }
   validates :finished_at, { presence: true }
+  # validates :status, { presence: true, numericality: { only_integer: true } }
+
+  enum status: {
+    pending: 0,
+    started: 1,
+    finished: 2,
+  }
 end
