25c382eda08d65603a212e9283cb5017f27ef672
yuta.saito <yuta.d.saito@rakuten.com>
2021-10-13 18:13:15 +0900

    spec system/tasks modifi

--- myapp/spec/system/task_spec.rb
+++ myapp/spec/system/task_spec.rb
@@ -4,32 +4,28 @@ RSpec.describe TasksController, type: :system do
   let!(:task) { FactoryBot.create(:task) }
 
   describe 'Check the screen transitions' do
-    subject { visit root_path }
+    before { visit root_path }
     it 'check show page' do
-      subject
       click_link task.title
       expect(current_path).to eq task_path(task.id)
     end
     it 'check new page' do
-      subject
       click_link 'Register a task'
       expect(current_path).to eq new_task_path
     end
   end
 
   describe 'Check the index page' do
-    subject { visit root_path }
+    before { visit root_path }
     it 'deital' do
-      subject
       expect(page).to have_content('Tasks (1)')
       expect(page).to have_content(task.title)
     end
   end
  
   describe 'Check the show page' do
-    subject { visit task_path(task.id) }
+    before { visit task_path(task.id) }
     it 'deital' do
-      subject
       expect(page).to have_content(task.title)
       expect(page).to have_content(task.detail)
       expect(page).to have_link 'EDIT', href: edit_task_path(task)
@@ -38,14 +34,13 @@ RSpec.describe TasksController, type: :system do
   end
  
   describe 'Check the new task' do
-    subject { visit new_task_path }
+    before { visit new_task_path }
 
     it 'Can create tasks' do
       title = 'new Title'
       detail = 'new Detail'
       flush = 'Regist Success!'
 
-      subject
       fill_in 'Title', with: title
       fill_in 'Detail', with: detail
       select 'low', from: 'Priority'
@@ -66,7 +61,6 @@ RSpec.describe TasksController, type: :system do
       detail = 'new Detail'
       flush = 'error'
 
-      subject
       fill_in 'Title', with: title
       fill_in 'Detail', with: detail
       select 'low', from: 'Priority'
@@ -81,14 +75,13 @@ RSpec.describe TasksController, type: :system do
 
 
   describe 'Check the edit task' do
-    subject { visit edit_task_path(task) }
+    before { visit edit_task_path(task) }
 
     it 'Can edit tasks' do
       title = 'edit Title'
       detail = 'edit Detail'
       flush = 'Edit Success!'
 
-      subject
       fill_in 'Title', with: title
       fill_in 'Detail', with: detail
       fill_in 'Due date', with: Time.zone.now.strftime("%Y-%m-%d")
@@ -105,7 +98,6 @@ RSpec.describe TasksController, type: :system do
       detail = 'edit Detail'
       flush = 'error'
 
-      subject
       fill_in 'Title', with: title
       fill_in 'Detail', with: detail
       fill_in 'Due date', with: Time.zone.yesterday.strftime("%Y-%m-%d")
@@ -117,9 +109,8 @@ RSpec.describe TasksController, type: :system do
   end
 
   describe 'Check the delete' do
-    subject { visit task_path(task.id) }
+    before { visit task_path(task.id) }
     it 'Can delete tasks' do
-      subject
       click_link 'DELETE'
 
       expect(page).to have_current_path( root_path )
