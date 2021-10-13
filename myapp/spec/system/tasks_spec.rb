cc58d7b1968a38088102511f1dbba07ab4ee0506
taito.a.ito <taito.a.ito@rakuten.com>
2021-10-13 17:17:39 +0900

    Step12 fix configuration of spec/system

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -6,32 +6,32 @@ RSpec.describe 'Tasks', type: :system do
   describe 'Index Page' do
     before { visit root_path }
 
-    context 'normal action confirmation' do
-      it 'display list view, success' do
+    context 'display task list' do
+      it 'display task list, success' do
         expect(page).to have_title 'Taskun'
         expect(page).to have_content(task_list.last.title)
       end
+    end
 
-      it 'move to create page, success' do
-        click_on '作成'
-        expect(page).to have_content 'Tasks#new'
-      end
-
-      it 'sort by created_at order by desc, success' do
+    context 'default sort' do
+      it 'default sort by created_at order by desc, success' do
         expect(find('tr:nth-child(2)')).to have_content I18n.l task_list.last.created_at
         expect(find('tr:nth-child(3)')).to have_content I18n.l task_list[2].created_at
         expect(find('tr:nth-child(4)')).to have_content I18n.l task_list[1].created_at
         expect(find('tr:nth-child(5)')).to have_content I18n.l task_list.first.created_at
       end
-
-      it 'sort by due_date order by ace, success' do
+    end
+    context 'sort by due_date asc' do
+      it 'sort by due_date asc, success' do
         click_on '期日で並び替え' # 1回押すと昇順
         expect(find('tr:nth-child(2)')).to have_content I18n.l task_list.first.due_date
         expect(find('tr:nth-child(3)')).to have_content I18n.l task_list[1].due_date
         expect(find('tr:nth-child(4)')).to have_content I18n.l task_list[2].due_date
         expect(find('tr:nth-child(5)')).to have_content I18n.l task_list.last.due_date
       end
+    end
 
+    context 'sort by due_date desc' do
       it 'sort by due_date order by desc, success' do
         click_on '期日で並び替え' # 1回押すと昇順
         click_on '期日で並び替え' # 2回押すと降順
@@ -40,12 +40,21 @@ RSpec.describe 'Tasks', type: :system do
         expect(find('tr:nth-child(4)')).to have_content I18n.l task_list[1].due_date
         expect(find('tr:nth-child(5)')).to have_content I18n.l task_list.first.due_date
       end
-
+    end
+    
+    context 'move to create page' do
+      it 'move to create page, success' do
+        click_on '作成'
+        expect(page).to have_content 'Tasks#new'
+      end
+    end
+    context 'move to edit page' do
       it 'move to edit page, success' do
         all('table tr')[1].click_on '編集'
         expect(page).to have_content 'Tasks#edit'
       end
-
+    end
+    context 'move to detail page' do
       it 'move to detail page, success' do
         find('tr:nth-child(2)').click_on task_list.last.title
         expect(page).to have_content 'Tasks#detail'
@@ -56,7 +65,7 @@ RSpec.describe 'Tasks', type: :system do
   describe 'Create Page' do
     before { visit new_task_path }
 
-    context 'normal action confirmation' do
+    context 'create task' do
       # https://github.com/faker-ruby/faker#usage
       it 'create task, success' do
         fill_in 'task[title]',       with: 'new task'
@@ -66,7 +75,9 @@ RSpec.describe 'Tasks', type: :system do
         expect(page).to have_content 'Successfully created'
         expect(page).to have_content 'Tasks#list'
       end
+    end
 
+    context 'move to list page' do
       it 'back to index page, success' do
         click_on '戻る'
         expect(page).to have_content 'Tasks#list'
@@ -77,17 +88,19 @@ RSpec.describe 'Tasks', type: :system do
   describe 'Edit Page' do
     before { visit edit_task_path(task_list.first.id) }
 
-    context 'normal action confirmation' do
-      let(:params) { { title: 'title for edit', description: 'description for edit' } }
-
-      it 'list task detail' do
+    context 'list task contents' do
+      it 'list task contents, success' do
         expect(page).to have_field '件名', with: task_list.first.title
         expect(page).to have_field '詳細', with: task_list.first.description
+        expect(page).to have_field '期日', with: task_list.first.due_date.strftime('%Y-%m-%dT%H:%M:%S')
       end
+    end
 
-      it 'edit and resister task' do
-        fill_in 'task[title]',       with: params[:title]
-        fill_in 'task[description]', with: params[:description]
+    context 'edit and resister task' do
+      it 'edit and resister task, success' do
+        fill_in 'task[title]',       with: 'title for edit'
+        fill_in 'task[description]', with: 'description for edit'
+        fill_in 'task[due_date]',    with: Faker::Time.forward(days: 23, period: :morning)
         click_button '送信'
         expect(page).to have_content 'Successfully updated'
         expect(page).to have_content 'Tasks#list'
@@ -98,15 +111,26 @@ RSpec.describe 'Tasks', type: :system do
   describe 'Detail Page' do
     before { visit task_path(task_list.first.id) }
 
-    context 'normal action confirmation' do
-      it 'list task details' do
+    context 'list task contents' do
+      it 'list task contents, success' do
         expect(page).to have_content task_list.first.title
         expect(page).to have_content task_list.first.description
+        expect(page).to have_content task_list.first.created_at
+        expect(page).to have_content task_list.first.due_date
       end
+    end
 
-      it 'back to list page' do
+    context 'move to list page' do
+      it 'move to list page, success' do
         click_on '戻る'
         expect(page).to have_content 'Tasks#list'
+      end
+    end
+
+    context 'move to edit page' do
+      it 'move to edit page, success' do
+        click_on '編集'
+        expect(page).to have_content 'Tasks#edit'
       end
     end
   end
