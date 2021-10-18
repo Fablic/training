b3eee0a25d67aff3dfe56f86fee983ecba7383b5
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-10-18 14:40:19 +0900

    fixed bug in spec and tweaked css

--- OreNoTask/spec/factories/tasks.rb
+++ OreNoTask/spec/factories/tasks.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     sequence(:name, "test_task_1")
     sequence(:start_at, Date.new(2020, 3, 1))
     sequence(:due_date_at, Date.new(2020, 3, 2))
-    sequence(:_at, Date.new(2020, 3, 2))
+    sequence(:created_at, Date.new(2020, 3, 2))
     description { 'description' }
     status { 'not_started' }
   end
