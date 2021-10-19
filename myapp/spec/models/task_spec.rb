de7daaf31a6042220bfb0fa76e95182f529d3b32
yuta.saito <yuta.d.saito@rakuten.com>
2021-10-19 14:18:49 +0900

    Changed the way task_spec is written.

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -1,105 +1,140 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  let(:task) { build(:task) }
   describe '#title' do
-    it 'with nil is not valid' do
-      task = build(:task, title: nil)
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:title)
+    context 'If nil is set' do
+      it 'Invalidated and returns an error message' do
+        task.title = nil
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:title)
+      end
     end
 
-    it 'with empty is not valid' do
-      task = build(:task, title: '')
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:title)
+    context 'If empty is set.' do
+      it 'Invalidated and returns an error message' do
+        task.title = ''
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:title)
+      end
     end
 
-    it 'Exceeds the specified number of characters' do
-      task = build(:task, title: 'a' * 21)
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:title)
+    context 'If the number of characters is set to be more than the specified number' do
+      it 'Invalidated and returns an error message' do
+        task.title = 'a' * 21
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:title)
+      end
     end
 
-    it 'Less than the specified number of characters' do
-      task = build(:task, title: 'a' * 2)
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:title)
+    context 'If the number of characters is set to less than the specified value' do
+      it 'Invalidated and returns an error message' do
+        task.title = 'a' * 2
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:title)
+      end
     end
   end
 
   describe '#detail' do
-    it 'with nil is valid' do
-      task = build(:task, detail: nil)
-      expect(task).to be_valid
-      expect(task.errors.messages).not_to include(:detail)
+    context 'If nil is set' do
+      it 'Invalidated and returns an error message' do
+        task.detail = nil
+        expect(task).to be_valid
+        expect(task.errors.messages).not_to include(:detail)
+      end
     end
 
-    it 'with empty is valid' do
-      task = build(:task, detail: '')
-      expect(task).to be_valid
-      expect(task.errors.messages).not_to include(:detail)
+    context 'If empty is set.' do
+      it 'Invalidated and returns an error message' do
+        task.detail = ''
+        expect(task).to be_valid
+        expect(task.errors.messages).not_to include(:detail)
+      end
     end
 
-    it 'Exceeds the specified number of characters' do
-      task = build(:task, detail: 'a' * 10_001)
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:detail)
+    context 'If the number of characters is set to be more than the specified number' do
+      it 'Invalidated and returns an error message' do
+        task.detail = 'a' * 10_001
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:detail)
+      end
     end
   end
 
   describe '#due_date' do
-    it 'with nil is valid' do
-      task = build(:task, due_date: nil)
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:due_date)
+    context 'If nil is set' do
+      it 'Invalidated and returns an error message' do
+        task.due_date = nil
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:due_date)
+      end
     end
 
-    it 'with empty is valid' do
-      task = build(:task, due_date: '')
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:due_date)
+    context 'If empty is set.' do
+      it 'Invalidated and returns an error message' do
+        task.due_date = ''
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:due_date)
+      end
     end
 
-    it 'Check the date now' do
-      task = build(:task, due_date: Time.zone.now)
-      expect(task).to be_valid
-      expect(task.errors.messages).not_to include(:due_date)
+    context 'If todays date is set' do
+      it 'Enabled, no error message returned' do
+        task.due_date = Time.zone.now
+        expect(task).to be_valid
+        expect(task.errors.messages).not_to include(:due_date)
+      end
     end
-    it 'Check the date tomorrow' do
-      task = build(:task, due_date: Time.zone.tomorrow)
-      expect(task).to be_valid
-      expect(task.errors.messages).not_to include(:due_date)
+
+    context 'If tomorrow date is set' do
+      it 'Invalidated and returns an error message' do
+        task.due_date = Time.zone.tomorrow
+        expect(task).to be_valid
+        expect(task.errors.messages).not_to include(:due_date)
+      end
     end
-    it 'Check the date yesterday' do
-      task = build(:task, due_date: Time.zone.yesterday)
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:due_date)
+
+    context 'If yesterday date is set' do
+      it 'Invalidated and returns an error message' do
+        task.due_date = Time.zone.yesterday
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:due_date)
+      end
     end
   end
 
   describe '#priority' do
-    it 'with nil is valid' do
-      task = build(:task, priority: nil)
-      expect(task).not_to be_valid
+    context 'If nil is set' do
+      it 'be invalidated' do
+        task.priority = nil
+        expect(task).not_to be_valid
+      end
     end
 
-    it 'If an out-of-specification value is received' do
-      task = build(:task, priority: [*0..100].delete_if { |n| Task.prioritys.values.include?(n) }.sample)
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:priority)
+    context 'If an unintended value is set' do
+      it 'Invalidated and returns an error message' do
+        task.priority = [*0..100].delete_if { |n| Task.prioritys.values.include?(n) }.sample
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:priority)
+      end
     end
   end
 
   describe '#status' do
-    it 'with nil is valid' do
-      task = build(:task, status: nil)
-      expect(task).not_to be_valid
+    context 'If nil is set' do
+      it 'be invalidated' do
+        task.status = nil
+        expect(task).not_to be_valid
+      end
     end
 
-    it 'If an out-of-specification value is received' do
-      task = build(:task, status: [*0..100].delete_if { |n| Task.statuses.values.include?(n) }.sample)
-      expect(task).not_to be_valid
-      expect(task.errors.messages).to include(:status)
+    context 'If an unintended value is set' do
+      it 'Invalidated and returns an error message' do
+        task.status = [*0..100].delete_if { |n| Task.statuses.values.include?(n) }.sample
+        expect(task).not_to be_valid
+        expect(task.errors.messages).to include(:status)
+      end
     end
   end
 end
