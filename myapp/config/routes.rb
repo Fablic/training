409516ead9cec53a2286c2c01f8788519ca13bb8
u-Hashimoto <yu.a.hashimoto@rakuten.com>
2021-10-19 16:57:03 +0900

    余分なコメントを削除

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
+  root "tasks#index"
   get 'users/login' => "users#new"
   get 'users/logout' => "users#destroy"
   post 'users/login' => "users#create"
-  #root
-  root "tasks#index"
-
-  #task.rb-path
   resources :tasks,:only => [:index, :edit, :new, :create, :update,:destroy]
 
   # error page
