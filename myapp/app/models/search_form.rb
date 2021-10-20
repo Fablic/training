52d1a761a10dd1916768aa7361164aeeb0be3217
u-Hashimoto <yu.a.hashimoto@rakuten.com>
2021-10-20 11:52:36 +0900

    search_formから移動したメソッドを消す

--- myapp/app/models/search_form.rb
+++ myapp/app/models/search_form.rb
@@ -11,20 +11,6 @@ class SearchForm
   attribute :sort, :string, default: 'created_at'
   attribute :order, :string, default: 'asc'
 
-  # taskの検索実行
-  def exec_search(page, user_id)
-    if valid?
-      tasks = Task.eager_load(:user)
-                  .where(user_id: user_id)
-                  .order("tasks.#{sort_value} #{order_value}")
-      tasks.where!('name LIKE ?', "%#{name}%") if name.present?
-      tasks.where!(status: status) if status.present?
-      tasks.page(page)
-    else
-      []
-    end
-  end
-
   def sort_value
     Task.column_names.include?(sort) ? sort : SORT_DEFAULT
   end
