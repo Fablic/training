ce9aebb3f96a040d72b9f0e8079dadf03d32a569
TaitoItou <taito.a.ito@rakuten.com>
2021-10-21 18:58:28 +0900

    Step12 Fix the way of rspec

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -2,25 +2,25 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Task Model(Post)', type: :model do
+RSpec.describe 'Task', type: :model do
   let(:title) { 'title for test' }
   let(:description) { 'description for test' }
   let(:due_date) { '2021-12-01 15:00:00' }
   let!(:task) { Task.new(title: title, description: description, due_date: due_date) }
 
-  describe 'Title' do
-    context 'when less than or equal 50' do
+  describe '#title' do
+    context 'when less than or equal 50 charcters' do
       let(:title) { 't' * 50 }
 
-      it 'create succesfully' do
+      it 'is valid' do
         expect(task).to be_valid
       end
     end
 
-    context 'when over 50 words' do
+    context 'when over 50 charcters' do
       let(:title) { 't' * 51 }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
@@ -28,43 +28,43 @@ RSpec.describe 'Task Model(Post)', type: :model do
     context 'when empty' do
       let(:title) { '' }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
   end
 
-  describe 'description' do
+  describe '#description' do
     context 'when empty' do
       let(:descripton) { '' }
 
-      it 'create success' do
+      it 'is valid' do
         expect(task).to be_valid
       end
     end
 
-    context 'when less than or equal to 256' do
+    context 'when less than or equal to 256 charcters' do
       let(:descripton) { 'd' * 255 }
 
-      it 'create success' do
+      it 'is valid' do
         expect(task).to be_valid
       end
     end
 
-    context 'when description over 255' do
+    context 'when description over 255 charcter' do
       let(:description) { 'd' * 256 }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
   end
 
-  describe 'due_date' do
-    context 'when due_date string' do
+  describe '#due_date' do
+    context 'when due_date type is string' do
       let(:due_date) { 'test' }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
@@ -72,7 +72,7 @@ RSpec.describe 'Task Model(Post)', type: :model do
     context 'when due_date empty' do
       let(:due_date) { '' }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -3,77 +3,59 @@
 require 'rails_helper'
 
 RSpec.describe 'tasks', type: :system do
-  describe 'Index Page' do
+  describe '#index' do
     let!(:task_list) { create_list(:task, 4) }
 
     before { visit root_path }
 
-    context 'when display task list' do
-      it 'display task list title, success' do
-        expect(page).to have_content task_list.first.title
+    context 'when open index page' do
+      context 'with displayed title' do
+        it 'display success' do
+          expect(page).to have_content task_list.first.title
+        end
       end
 
-      it 'display task list created_at, success' do
-        expect(page).to have_content I18n.l task_list.first.created_at
+      context 'with displayed created_at' do
+        it 'display success' do
+          expect(page).to have_content I18n.l task_list.first.created_at
+        end
       end
 
-      it 'display task list due_date, success' do
-        expect(page).to have_content I18n.l task_list.first.due_date
-      end
-    end
-
-    context 'when open list page' do
-      it 'default sort by created_at order by desc, success' do
-        expect(find('tr:nth-child(2)')).to have_content I18n.l task_list.last.created_at
-        expect(find('tr:nth-child(5)')).to have_content I18n.l task_list.first.created_at
-      end
-    end
-
-    context 'when push link to sort by due_date asc' do
-      it 'sort by due_date asc, success' do
-        click_on '期日で並び替え' # 1回押すと昇順
-        expect(find('tr:nth-child(2)')).to have_content I18n.l task_list.first.due_date
-        expect(find('tr:nth-child(5)')).to have_content I18n.l task_list.last.due_date
+      context 'with displayed due_date' do
+        it 'display success' do
+          expect(page).to have_content I18n.l task_list.first.due_date
+        end
       end
     end
 
-    context 'when push link to sort by due_date desc' do
-      it 'sort by due_date order by desc, success' do
-        click_on '期日で並び替え' # 1回押すと昇順
-        click_on '期日で並び替え' # 2回押すと降順
-        expect(find('tr:nth-child(2)')).to have_content I18n.l task_list.last.due_date
-        expect(find('tr:nth-child(5)')).to have_content I18n.l task_list.first.due_date
-      end
-    end
-
-    context 'when push create button' do
-      it 'move to create page, success' do
+    context 'when click create button' do
+      it 'move success' do
         click_on '作成'
         expect(page).to have_content 'Tasks#new'
       end
     end
 
-    context 'when push edit button' do
-      it 'move to edit page, success' do
+    context 'when click edit button' do
+      it 'move success' do
         all('table tr')[1].click_on '編集'
         expect(page).to have_content 'Tasks#edit'
       end
     end
 
-    context 'when push detail button' do
-      it 'move to detail page, success' do
+    context 'when click detail button' do
+      it 'move success' do
         find('tr:nth-child(2)').click_on task_list.last.title
         expect(page).to have_content 'Tasks#detail'
       end
     end
   end
 
-  describe 'Create Page' do
+  describe '#create' do
     before { visit new_task_path }
 
     context 'when create task' do
       # https://github.com/faker-ruby/faker#usage
-      it 'create task, success' do
+      it 'create success' do
         fill_in 'task[title]',       with: 'new task'
         fill_in 'task[description]', with: 'new description'
         fill_in 'task[due_date]',    with: Faker::Time.forward(days: 23, period: :morning)
@@ -82,7 +64,7 @@ RSpec.describe 'tasks', type: :system do
       end
     end
 
-    context 'when push back button' do
+    context 'when click back button' do
       it 'back to index page, success' do
         click_on '戻る'
         expect(page).to have_content 'Tasks#list'
@@ -90,22 +72,28 @@ RSpec.describe 'tasks', type: :system do
     end
   end
 
-  describe 'Edit Page' do
+  describe '#edit' do
     let!(:task) { create(:task) }
 
     before { visit edit_task_path(task.id) }
 
-    context 'when list task contents' do
-      it 'list task contents title, success' do
-        expect(page).to have_field '件名', with: task.title
+    context 'when open edit page' do
+      context 'with displayed title' do
+        it 'display success' do
+          expect(page).to have_field '件名', with: task.title
+        end
       end
 
-      it 'list task contents description, success' do
-        expect(page).to have_field '詳細', with: task.description
+      context 'with displayed created_at' do
+        it 'display success' do
+          expect(page).to have_field '詳細', with: task.description
+        end
       end
 
-      it 'list task contents due_date, success' do
-        expect(page).to have_field '期日', with: task.due_date.strftime('%Y-%m-%dT%H:%M:%S')
+      context 'with displayed due_date' do
+        it 'display success' do
+          expect(page).to have_field '期日', with: task.due_date.strftime('%Y-%m-%dT%H:%M:%S')
+        end
       end
     end
 
@@ -120,40 +108,78 @@ RSpec.describe 'tasks', type: :system do
     end
   end
 
-  describe 'Detail Page' do
+  describe '#show' do
     let(:task) { create(:task) }
 
     before { visit task_path(task.id) }
 
     context 'when open list page' do
-      it 'list task contents title, success' do
-        expect(page).to have_content task.title
+      context 'with displayed title' do
+        it 'display success' do
+          expect(page).to have_content task.title
+        end
       end
 
-      it 'list task contents description, success' do
-        expect(page).to have_content task.description
+      context 'with displayed description' do
+        it 'display success' do
+          expect(page).to have_content task.description
+        end
       end
 
-      it 'list task contents created_at, success' do
-        expect(page).to have_content task.created_at.strftime('%Y/%m/%d %H:%M:%S')
+      context 'with displayed created_at' do
+        it 'display success' do
+          expect(page).to have_content task.created_at.strftime('%Y/%m/%d %H:%M:%S')
+        end
       end
 
-      it 'list task contents due_date, success' do
-        expect(page).to have_content task.due_date.strftime('%Y/%m/%d %H:%M:%S')
+      context 'with displayed due_date' do
+        it 'display success' do
+          expect(page).to have_content task.due_date.strftime('%Y/%m/%d %H:%M:%S')
+        end
       end
     end
 
-    context 'when push back button' do
-      it 'move to list page, success' do
+    context 'when click back button' do
+      it 'move success' do
         click_on '戻る'
         expect(page).to have_content 'Tasks#list'
       end
     end
 
-    context 'when push edit button' do
-      it 'move to edit page, success' do
+    context 'when click edit button' do
+      it 'move success' do
         click_on '編集'
         expect(page).to have_content 'Tasks#edit'
+      end
+    end
+  end
+
+  describe 'sort function' do
+    let!(:task_list) { create_list(:task, 4) }
+
+    before { visit root_path }
+
+    context 'when open list page(sort by created_at order by desc)' do
+      it 'order success' do
+        expect(find('tr:nth-child(2)')).to have_content I18n.l task_list.last.created_at
+        expect(find('tr:nth-child(5)')).to have_content I18n.l task_list.first.created_at
+      end
+    end
+
+    context 'when click link to sort by due_date asc' do
+      it 'order success' do
+        click_on '期日で並び替え' # 1回押すと昇順
+        expect(find('tr:nth-child(2)')).to have_content I18n.l task_list.first.due_date
+        expect(find('tr:nth-child(5)')).to have_content I18n.l task_list.last.due_date
+      end
+    end
+
+    context 'when click link to sort by due_date desc' do
+      it 'order success' do
+        click_on '期日で並び替え' # 1回押すと昇順
+        click_on '期日で並び替え' # 2回押すと降順
+        expect(find('tr:nth-child(2)')).to have_content I18n.l task_list.last.due_date
+        expect(find('tr:nth-child(5)')).to have_content I18n.l task_list.first.due_date
       end
     end
   end
