3c7aca537d43f57a7a89a9030d6e9d53ce5eb5e3
yuta.saito <yuta.d.saito@rakuten.com>
2021-10-21 17:17:59 +0900

    Fixing the index

--- myapp/db/migrate/20211018025536_add_index_to_tasks_title_and_status.rb
+++ myapp/db/migrate/20211018025536_add_index_to_tasks_title_and_status.rb
@@ -1,6 +1,6 @@
 class AddIndexToTasksTitleAndStatus < ActiveRecord::Migration[6.0]
   def change
-    add_index :tasks, [:title,:status]
+    add_index :tasks, :title
     add_index :tasks, :status
   end
 end
