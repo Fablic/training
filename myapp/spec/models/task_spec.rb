329a7a27f7ff0536849418912377c67a7e09e91b
TaitoItou <taito.a.ito@rakuten.com>
2021-10-21 19:07:12 +0900

    Merge branch 'taito_ito_step12' into taito_ito_step13

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -2,26 +2,26 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Task Model(Post)', type: :model do
+RSpec.describe 'Task', type: :model do
   let(:title) { 'title for test' }
   let(:description) { 'description for test' }
   let(:due_date) { '2021-12-01 15:00:00' }
   let(:status) { 0 }
   let!(:task) { Task.new(title: title, description: description, due_date: due_date) }
 
-  describe 'Title' do
-    context 'when less than or equal 50' do
+  describe '#title' do
+    context 'when less than or equal 50 charcters' do
       let(:title) { 't' * 50 }
 
-      it 'create succesfully' do
+      it 'is valid' do
         expect(task).to be_valid
       end
     end
 
-    context 'when over 50 words' do
+    context 'when over 50 charcters' do
       let(:title) { 't' * 51 }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
@@ -29,43 +29,43 @@ RSpec.describe 'Task Model(Post)', type: :model do
     context 'when empty' do
       let(:title) { '' }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
   end
 
-  describe 'description' do
+  describe '#description' do
     context 'when empty' do
       let(:descripton) { '' }
 
-      it 'create success' do
+      it 'is valid' do
         expect(task).to be_valid
       end
     end
 
-    context 'when less than or equal to 256' do
+    context 'when less than or equal to 256 charcters' do
       let(:descripton) { 'd' * 255 }
 
-      it 'create success' do
+      it 'is valid' do
         expect(task).to be_valid
       end
     end
 
-    context 'when description over 255' do
+    context 'when description over 255 charcter' do
       let(:description) { 'd' * 256 }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
   end
 
-  describe 'due_date' do
-    context 'when input string' do
+  describe '#due_date' do
+    context 'when due_date type is string' do
       let(:due_date) { 'test' }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
@@ -73,7 +73,7 @@ RSpec.describe 'Task Model(Post)', type: :model do
     context 'when empty' do
       let(:due_date) { '' }
 
-      it 'create faild' do
+      it 'is invalid' do
         expect(task).not_to be_valid
       end
     end
