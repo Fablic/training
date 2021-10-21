a19f77a558094a0c151246030df1e54b2976321d
u-Hashimoto <yu.a.hashimoto@rakuten.com>
2021-10-21 12:06:50 +0900

    user.タスク検索の書き方を修正

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_secure_password
 
   def searched_tasks(search_form, page)
-    return [] if !search_form.is_a?(SearchForm) || !search_form&.valid?
+    return [] unless search_form.is_a?(SearchForm) and search_form&.valid?
     searched_tasks = tasks.order("tasks.#{search_form.sort_value} #{search_form.order_value}")
     searched_tasks.where!('name LIKE ?', "%#{search_form.name}%") if search_form.name.present?
     searched_tasks.where!(status: search_form.status) if search_form.status.present?
