1ee411eb294290fe2ffb6a56dd896694a199074e
yuta.saito <yuta.d.saito@rakuten.com>
2021-10-21 10:28:53 +0900

    Customizing Error Pages

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
+
   resources :tasks
   root 'tasks#index'
 end
