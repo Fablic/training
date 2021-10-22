72c41c340a7a61d64c1ebdba28b94234f04a11e5
u-Hashimoto <yu.a.hashimoto@rakuten.com>
2021-10-22 18:48:17 +0900

    中間テーブルにindexをはる

--- myapp/db/migrate/20211021001832_create_tasks_labels.rb
+++ myapp/db/migrate/20211021001832_create_tasks_labels.rb
@@ -1,7 +1,7 @@
 class CreateTasksLabels < ActiveRecord::Migration[6.0]
   def change
     create_table :labels_tasks do |t|
-      t.belongs_to :task, null: false, foreign_key: true , index: true
+      t.belongs_to :task, null: false, foreign_key: true, index: true
       t.belongs_to :label, null: false, foreign_key: true, index: true
     end
   end
