e5c026bf175cb1425c33cb4cc488db5fc0955b4d
yuta.saito <yuta.d.saito@rakuten.com>
2021-10-22 10:49:19 +0900

    Fixed a bug that made the show page invisible.

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :tasks
+  root 'tasks#index'
+
   get '*not_found' => 'application#routing_error'
   post '*not_found' => 'application#routing_error'
 
-  resources :tasks
-  root 'tasks#index'
 end
