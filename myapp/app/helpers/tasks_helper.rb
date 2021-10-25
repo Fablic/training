35fd91a33abdc73407b8594797d8cfaff64b8014
mvincent.yap <mvincent.yap@rakuten.com>
2021-10-25 14:32:50 +0900

    optimize helper

--- myapp/app/helpers/tasks_helper.rb
+++ myapp/app/helpers/tasks_helper.rb
@@ -2,9 +2,6 @@
 
 module TasksHelper
   def get_sort_direction(sort_name)
-    return 'DESC' if request.params[:sort].blank? || (request.params[:sort] != sort_name.to_s)
-    return 'DESC' if request.params[:sort] == sort_name.to_s && request.params[:direction] == 'ASC'
-
-    'ASC'
+    request.params[:direction] == 'ASC' ? 'DESC' : 'ASC'
   end
 end
