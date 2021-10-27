9fb93907d62cf41ee492941e64b6eaffd999ee64
mvincent.yap <mvincent.yap@rakuten.com>
2021-10-27 10:29:38 +0900

    simplify and rubocop fix

--- myapp/app/validators/date_validator.rb
+++ myapp/app/validators/date_validator.rb
@@ -4,30 +4,25 @@ class DateValidator < ActiveModel::Validator
   def validate(record)
     require 'date'
 
-    record.errors[:finished_at] << I18n.t('errors.messages.required') if record.finished_at.blank?
-
-    f = record.finished_at_before_type_cast
-
-    begin
-      Date.strptime(f, '%Y-%m-%d')
-    rescue StandardError
-      record.errors[:finished_at] << I18n.t('messages.invalid_date')
-      return
-    end
+    return record.errors[:finished_at] << I18n.t('errors.messages.required') if record.finished_at_before_type_cast.blank?
+    return record.errors[:finished_at] << I18n.t('messages.invalid_date') unless valid_date_format?(record.finished_at_before_type_cast)
 
     return if record.started_at_before_type_cast.blank?
-
-    s = record.started_at_before_type_cast
-
-    begin
-      Date.strptime(s, '%Y-%m-%d')
-    rescue StandardError
-      record.errors[:started_at] << I18n.t('messages.invalid_date')
-      return
-    end
+    return record.errors[:started_at] << I18n.t('messages.invalid_date') unless valid_date_format?(record.started_at_before_type_cast)
 
     return if record.finished_at >= record.started_at
 
     record.errors.add(:finished_at, I18n.t('earlier_date_error', start: Task.human_attribute_name(:started_at)))
+  end
+
+  private
+
+  def valid_date_format?(date)
+    begin
+      Date.strptime(date, '%Y-%m-%d')
+    rescue StandardError
+      return false
+    end
+    true
   end
 end
--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -18,17 +18,17 @@ RSpec.describe 'Taskモデルのテスト', type: :model do
   describe 'Normal case' do
     subject { task }
 
-    context 'all values filled' do
+    context 'with all values filled' do
       it { is_expected.to be_valid }
     end
 
-    context 'blank desc' do
+    context 'with blank desc' do
       let(:description) { '' }
 
       it { is_expected.to be_valid }
     end
 
-    context 'blank started_at' do
+    context 'with blank started_at' do
       let(:started_at) { '' }
 
       it { is_expected.to be_valid }
@@ -36,39 +36,38 @@ RSpec.describe 'Taskモデルのテスト', type: :model do
   end
 
   describe 'validation' do
-
     describe 'created_by' do
       subject { task }
 
-      context 'blank' do
+      context 'when blank' do
         let(:created_by) { '' }
+
         it { is_expected.not_to be_valid }
       end
 
-      context 'user does not exist' do
-        let(:created_by) { user.id + 12323 }
-        
-        it { is_expected.not_to be_valid }
+      context 'when user does not exist' do
+        let(:created_by) { user.id + 12_323 }
 
+        it { is_expected.not_to be_valid }
       end
     end
 
     describe 'name' do
       subject { task }
 
-      context 'blank' do
+      context 'when blank' do
         let(:name) { '' }
 
         it { is_expected.not_to be_valid }
       end
 
-      context '<= 75 len' do
+      context 'when <= 75 len' do
         let(:name) { 'a' * 75 }
 
         it { is_expected.to be_valid }
       end
 
-      context '>75 len' do
+      context 'when >75 len' do
         let(:name) { 'a' * 76 }
 
         it { is_expected.not_to be_valid }
@@ -78,13 +77,13 @@ RSpec.describe 'Taskモデルのテスト', type: :model do
     describe 'description' do
       subject { task }
 
-      context '<=1000 len' do
+      context 'when <=1000 len' do
         let(:description) { 'a' * 1000 }
 
         it { is_expected.to be_valid }
       end
 
-      context '>1000 len' do
+      context 'when >1000 len' do
         let(:description) { 'a' * 1001 }
 
         it { is_expected.not_to be_valid }
@@ -94,36 +93,35 @@ RSpec.describe 'Taskモデルのテスト', type: :model do
     describe 'status' do
       subject { task }
 
-      context 'valid values pending' do
+      context 'when pending' do
         let(:status) { 'pending' }
 
         it { is_expected.to be_valid }
       end
 
-      context 'valid values started' do
+      context 'when  started' do
         let(:status) { 'started' }
 
         it { is_expected.to be_valid }
       end
 
-      context 'valid values finished' do
+      context 'when  finished' do
         let(:status) { 'finished' }
 
         it { is_expected.to be_valid }
       end
-
     end
 
     describe 'started_at column' do
       subject { task }
 
-      context 'invalid date' do
+      context 'when invalid date' do
         let(:started_at) { '2021年09ー01 10:00' }
 
         it { is_expected.not_to be_valid }
       end
 
-      context 'impossible date' do
+      context 'when impossible date' do
         let(:started_at) { '2021/02/31' }
 
         it { is_expected.not_to be_valid }
@@ -133,19 +131,19 @@ RSpec.describe 'Taskモデルのテスト', type: :model do
     describe 'finished_at col' do
       subject { task }
 
-      context 'blank' do
+      context 'when blank' do
         let(:finished_at) { '' }
 
         it { is_expected.not_to be_valid }
       end
 
-      context 'invalid date' do
+      context 'when invalid date' do
         let(:finished_at) { '2021年09ー02 11:00' }
 
         it { is_expected.not_to be_valid }
       end
 
-      context 'impossible date' do
+      context 'when impossible date' do
         let(:finished_at) { '2021/02/31' }
 
         it { is_expected.not_to be_valid }
@@ -155,41 +153,40 @@ RSpec.describe 'Taskモデルのテスト', type: :model do
     describe 'priority' do
       subject { task }
 
-      context '<1' do
+      context 'when <1' do
         let(:priority) { -1 }
 
         it { is_expected.not_to be_valid }
       end
-      context '==0' do
+
+      context 'when ==0' do
         let(:priority) { 0 }
 
         it { is_expected.not_to be_valid }
       end
-      context '>10' do
+
+      context 'when >10' do
         let(:priority) { 11 }
 
         it { is_expected.not_to be_valid }
       end
 
-      context 'string' do
+      context 'when string' do
         let(:priority) { 'dsfef' }
 
         it { is_expected.not_to be_valid }
       end
-
     end
 
     describe 'finished earlier than start' do
       subject { task }
 
-      context 'start_at > due_date_at' do
+      context 'when start_at > due_date_at' do
         let(:started_at) { '2021/03/01' }
         let(:finished_at) { '2021/02/01' }
 
         it { is_expected.not_to be_valid }
       end
     end
-
-  
   end
 end
