b499c93aedf7afbb70c9698044326f05144714e5
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-10-27 14:26:46 +0900

    fixed migration

--- OreNoTask/db/migrate/20211014082027_add_index_tasks_user_id.rb
+++ OreNoTask/db/migrate/20211014082027_add_index_tasks_user_id.rb
@@ -3,10 +3,10 @@
 class AddIndexTasksUserId < ActiveRecord::Migration[6.1]
   def change
     change_table :tasks, bulk: true do |t|
-      t.add_index %i[user_id deleted]
-      t.add_index %i[user_id created_at deleted]
-      t.add_index %i[user_id due_date_at deleted]
-      t.add_index %i[user_id status deleted]
+      t.index %i[user_id deleted]
+      t.index %i[user_id created_at deleted]
+      t.index %i[user_id due_date_at deleted]
+      t.index %i[user_id status deleted]
     end
   end
 end
