e93363c19e7b6c75dd50eaf08dfc6774fcd87730
mvincent.yap <mvincent.yap@rakuten.com>
2021-10-28 08:15:48 +0900

    rubocop cleanup

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,74 +12,73 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_043142) do
-
-  create_table "cats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.string "color"
-    t.boolean "pet"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_211_026_043_142) do
+  create_table 'cats', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
+    t.string 'name'
+    t.string 'color'
+    t.boolean 'pet'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "created_by", null: false
-    t.string "name", limit: 256, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "color"
-    t.index ["created_by"], name: "fkeyLabelOwner"
+  create_table 'labels', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
+    t.bigint 'created_by', null: false
+    t.string 'name', limit: 256, null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'color'
+    t.index ['created_by'], name: 'fkeyLabelOwner'
   end
 
-  create_table "maintenances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "created_by", null: false
-    t.string "name", limit: 256, null: false
-    t.timestamp "started_at"
-    t.timestamp "finished_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["created_by"], name: "fkeyMaintenanceOwner"
+  create_table 'maintenances', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
+    t.bigint 'created_by', null: false
+    t.string 'name', limit: 256, null: false
+    t.timestamp 'started_at'
+    t.timestamp 'finished_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['created_by'], name: 'fkeyMaintenanceOwner'
   end
 
-  create_table "task_labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "task_id", null: false
-    t.bigint "label_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["label_id"], name: "fk_rails_7dc7cd6c47"
-    t.index ["task_id"], name: "fk_rails_fcbbc0fa3c"
+  create_table 'task_labels', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
+    t.bigint 'task_id', null: false
+    t.bigint 'label_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['label_id'], name: 'fk_rails_7dc7cd6c47'
+    t.index ['task_id'], name: 'fk_rails_fcbbc0fa3c'
   end
 
-  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "created_by", null: false
-    t.string "name", limit: 256, null: false
-    t.integer "status"
-    t.text "description"
-    t.timestamp "started_at"
-    t.timestamp "finished_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "priority", default: 5
-    t.index ["created_by"], name: "fkeyTaskOwner"
-    t.index ["name"], name: "index_tasks_on_name"
-    t.index ["priority"], name: "index_tasks_on_priority"
-    t.index ["status"], name: "index_tasks_on_status"
+  create_table 'tasks', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
+    t.bigint 'created_by', null: false
+    t.string 'name', limit: 256, null: false
+    t.integer 'status'
+    t.text 'description'
+    t.timestamp 'started_at'
+    t.timestamp 'finished_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'priority', default: 5
+    t.index ['created_by'], name: 'fkeyTaskOwner'
+    t.index ['name'], name: 'index_tasks_on_name'
+    t.index ['priority'], name: 'index_tasks_on_priority'
+    t.index ['status'], name: 'index_tasks_on_status'
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name", limit: 256, null: false
-    t.string "username", limit: 256, null: false
-    t.string "pw", limit: 256, null: false
-    t.boolean "first_run", default: true, null: false
-    t.boolean "admin", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["username"], name: "index_users_on_username", unique: true
+  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
+    t.string 'name', limit: 256, null: false
+    t.string 'username', limit: 256, null: false
+    t.string 'pw', limit: 256, null: false
+    t.boolean 'first_run', default: true, null: false
+    t.boolean 'admin', default: false, null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['username'], name: 'index_users_on_username', unique: true
   end
 
-  add_foreign_key "labels", "users", column: "created_by", name: "fkeyLabelOwner"
-  add_foreign_key "maintenances", "users", column: "created_by", name: "fkeyMaintenanceOwner"
-  add_foreign_key "task_labels", "labels"
-  add_foreign_key "task_labels", "tasks"
-  add_foreign_key "tasks", "users", column: "created_by", name: "fkeyTaskOwner"
+  add_foreign_key 'labels', 'users', column: 'created_by', name: 'fkeyLabelOwner'
+  add_foreign_key 'maintenances', 'users', column: 'created_by', name: 'fkeyMaintenanceOwner'
+  add_foreign_key 'task_labels', 'labels'
+  add_foreign_key 'task_labels', 'tasks'
+  add_foreign_key 'tasks', 'users', column: 'created_by', name: 'fkeyTaskOwner'
 end
