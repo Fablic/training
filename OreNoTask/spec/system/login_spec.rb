a2af046dfb71ec73b62c9059692a4a476c8b1791
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-10-28 13:51:08 +0900

    refactored login_spec.rb

--- OreNoTask/spec/system/login_spec.rb
+++ OreNoTask/spec/system/login_spec.rb
@@ -8,35 +8,40 @@ describe 'ログイン機能', type: :system do
     create(:task, name: '最初のタスク', status: 'not_started', start_at: '2021/09/01 10:00', due_date_at: '2021/09/02 11:00', user_id: user.id)
   end
 
-  describe 'ログイン画面' do
+  describe '画面表示' do
     before do
       visit tasks_path
     end
 
-    context 'ログイン画面からログインできる' do
-      it '正常ログイン' do
+    context 'ログインしていない時の画面表示' do
+      it 'ログイン画面が表示される' do
         expect(page).to have_content 'ログイン'
+      end
+    end
+  end
 
-        login_action 'YoshioRakuten'
+  describe 'ログイン処理' do
+    before do
+      visit tasks_path
+      fill_in 'ユーザー名', with: login_name
+      fill_in 'パスワード', with: 'rakuten'
+      click_button 'commit'
+    end
 
+    context 'ログイン画面からログインできる' do
+      let(:login_name) { 'YoshioRakuten' }
+
+      it '正常ログイン' do
         expect(page).to have_content '最初のタスク'
       end
     end
 
     context '誤った情報でログインしようとする' do
+      let(:login_name) { 'NotYoshioRakuten' }
+
       it 'ログインできない' do
-        expect(page).to have_content 'ログイン'
-
-        login_action 'NotYoshioRakuten'
-
         expect(page).to have_content 'ログインに失敗しました'
       end
     end
-  end
-
-  def login_action(login_name)
-    fill_in 'ユーザー名', with: login_name
-    fill_in 'パスワード', with: 'rakuten'
-    click_button 'commit'
   end
 end
