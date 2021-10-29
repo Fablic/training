481cbc29b29c0d0ad30e416965c2291b59add5b3
ichinoseken <ts-ken.a.ichinose@rakuten.com>
2021-10-29 10:18:56 +0900

    step10

--- myapp/spec/factories/tasks.rb
+++ myapp/spec/factories/tasks.rb
@@ -2,10 +2,8 @@ FactoryBot.define do
   factory :task, class: Task do
     sequence(:task_name) { |n| "Task_name#{n}" }
     sequence(:description) { |n| "Description#{n}" }
-#    task_name { 'TEST task' }
-#    description { 'TEST desc' }
-     sequence(:priority) { |n| "#{n}" }
-    start_date = Time.zone.yesterday.strftime('%Y/%m/%d %H:%M:$S')
-    end_date   = Time.zone.today.strftime('%Y/%m/%d %H:%M:$S')
+    sequence(:priority) { |n| "#{n}" }
+    start_date { Time.now }
+    end_date   { Time.now }
   end
 end
