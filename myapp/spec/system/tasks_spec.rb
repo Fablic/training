6798d19ba4b93e0196b1e87f3307670ba9354d93
ichinoseken <ts-ken.a.ichinose@rakuten.com>
2021-11-11 15:25:17 +0900

    pr

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -8,37 +8,45 @@ RSpec.describe 'tasks', type: :system do
   describe 'New task' do
     before { visit new_task_path() }
 
-    it 'show display' do
-      expect(page).to have_content I18n.t('tasks.new.title')
-      expect(page).to have_title 'Myapp'
-      expect(page).to have_content I18n.t('tasks.common.task_name')
-      expect(page).to have_content I18n.t('tasks.common.description') 
-      expect(page).to have_content I18n.t('tasks.common.status')
-      expect(page).to have_content I18n.t('tasks.common.priority')
-      expect(page).to have_content I18n.t('tasks.common.label') 
-      expect(page).to have_content I18n.t('tasks.common.start_date')
-      expect(page).to have_content I18n.t('tasks.common.end_date')
-    end
+    context 'Check each function on the new registration screen.' do
+      context 'Screen display confirmation' do
+        it 'show display' do
+          expect(page).to have_content I18n.t('tasks.new.title')
+          expect(page).to have_title 'Myapp'
+          expect(page).to have_content I18n.t('tasks.common.task_name')
+          expect(page).to have_content I18n.t('tasks.common.description') 
+          expect(page).to have_content I18n.t('tasks.common.status')
+          expect(page).to have_content I18n.t('tasks.common.priority')
+          expect(page).to have_content I18n.t('tasks.common.label') 
+          expect(page).to have_content I18n.t('tasks.common.start_date')
+          expect(page).to have_content I18n.t('tasks.common.end_date')
+        end
+      end
 
-    it 'registration' do
-      fill_in 'task_task_name', with: 'input Task'
-      fill_in 'task_description', with: 'input Description'
-      fill_in 'task_status', with: 'todo'
-      fill_in 'task_priority', with: 1
-      fill_in 'task_label', with: 'input Label'
-      fill_in 'task_start_date', with: Time.zone.yesterday.strftime('%Y-%m-%d') 
-      fill_in 'task_end_date', with: Time.zone.now.strftime('%Y-%m-%d')
+      context 'Registration Confirmation' do
+        it 'registration' do
+          fill_in 'task_task_name', with: 'input Task'
+          fill_in 'task_description', with: 'input Description'
+          fill_in 'task_status', with: 'todo'
+          fill_in 'task_priority', with: 1
+          fill_in 'task_label', with: 'input Label'
+          fill_in 'task_start_date', with: Time.zone.yesterday.strftime('%Y-%m-%d') 
+          fill_in 'task_end_date', with: Time.zone.now.strftime('%Y-%m-%d')
+    
+          click_button 'Create Task'
+    
+          expect(page).to have_current_path root_path, ignore_query: true
+          expect(page).to have_content I18n.t('tasks.flash.complete_task_registration')
+        end
+      end
 
-      click_button 'Create Task'
-
-      expect(page).to have_current_path root_path, ignore_query: true
-      expect(page).to have_content I18n.t('tasks.flash.complete_task_registration')
-    end
-
-    it "go to Task's list" do
-      click_on I18n.t('tasks.common.move_task_list')
-      expect(page).to have_current_path root_path, ignore_query: true
-    end
+      context 'Screen transition confirmation' do
+        it "go to Task's list" do
+          click_on I18n.t('tasks.common.move_task_list')
+          expect(page).to have_current_path root_path, ignore_query: true
+        end
+      end
+    end 
   end
 
   describe 'Show task' do
@@ -49,13 +57,19 @@ RSpec.describe 'tasks', type: :system do
       page.all('#click_show')[0].click
     }
 
-    it 'show display' do
-      expect(page).to have_content I18n.t('tasks.show.title')
-    end
+    context 'Check each function on the show screen.' do
+      context 'Screen display confirmation' do
+        it 'show display' do
+          expect(page).to have_content I18n.t('tasks.show.title')
+        end
+      end
 
-    it "go to Task's list" do
-      click_on I18n.t('tasks.common.move_task_list')
-      expect(page).to have_current_path root_path, ignore_query: true
+      context 'Screen transition confirmation' do
+        it "go to Task's list" do
+          click_on I18n.t('tasks.common.move_task_list')
+          expect(page).to have_current_path root_path, ignore_query: true
+        end
+      end
     end
   end
 
@@ -66,35 +80,48 @@ RSpec.describe 'tasks', type: :system do
       page.all('#click_edit')[0].click
     }
 
-    it 'show display' do
-      expect(page).to have_content I18n.t('tasks.edit.title')\
-    end
+    context 'Check each function on the edit screen.' do
+      context 'Screen display confirmation' do
+        it 'show display' do
+          expect(page).to have_content I18n.t('tasks.edit.title')\
+        end
+      end
 
-    it "go to Task's list" do
-      click_on I18n.t('tasks.common.move_task_list')
-      expect(page).to have_current_path root_path, ignore_query: true
-    end
+      context 'Screen transition confirmation' do
+        it "go to Task's list" do
+          click_on I18n.t('tasks.common.move_task_list')
+          expect(page).to have_current_path root_path, ignore_query: true
+        end
+      end
 
-    it 'update' do      
-      fill_in 'task_task_name', with: 'update_task_name'
+      context 'Update Confirmation' do     
+        it 'update' do
+          fill_in 'task_task_name', with: 'update_task_name'
 
-      click_button 'Update Task'
-      visit root_path
- 
-      expect(page).to have_content 'update_task_name'
+          click_button 'Update Task'
+          visit root_path
+    
+          expect(page).to have_content 'update_task_name'
+        end
+      end
     end
   end
 
   describe 'Delete task' do
+
     before {
       visit root_path
     }
 
-    it 'Destroy' do
-      page.all('#click_destroy')[0].click
-      
-      expect(page).to have_content I18n.t('tasks.index.title')
-      expect(page).to have_content I18n.t('tasks.flash.complete_task_destroy')
+    context 'Check each function on the delete screen.' do
+      context 'Screen display confirmation' do
+        it 'Destroy' do
+          page.all('#click_destroy')[0].click
+          
+          expect(page).to have_content I18n.t('tasks.index.title')
+          expect(page).to have_content I18n.t('tasks.flash.complete_task_destroy')
+        end
+      end
     end
   end
 
@@ -102,34 +129,40 @@ RSpec.describe 'tasks', type: :system do
 
     before { visit root_path }
 
-    it 'table colums check' do
-      within('#task_list') do
-        expect(page).to have_content I18n.t('tasks.common.task_name')
+    context 'Check each function on the index screen.' do
+      context 'Screen display confirmation' do
+        it 'table colums check' do
+          within('#task_list') do
+            expect(page).to have_content I18n.t('tasks.common.task_name')
+          end
+        end
       end
-    end
 
-    it 'Go to registration page' do
-      click_on I18n.t('tasks.index.move_new_task')
-      expect(page).to have_content I18n.t('tasks.new.title')
-    end
+      context 'Screen transition confirmation' do
+        it 'Go to registration page' do
+          click_on I18n.t('tasks.index.move_new_task')
+          expect(page).to have_content I18n.t('tasks.new.title')
+        end
 
-    it 'Go to Show page' do
-      #byebug
-      expect(page).to have_content task_list[0].task_name
-      page.all('#click_show')[0].click
-      expect(page).to have_content I18n.t('tasks.show.title')
-    end
+        it 'Go to Show page' do
+          #byebug
+          expect(page).to have_content task_list[0].task_name
+          page.all('#click_show')[0].click
+          expect(page).to have_content I18n.t('tasks.show.title')
+        end
 
-    it 'Go to Edit page' do
-      expect(page).to have_content task_list[0].task_name
-      page.all('#click_edit')[0].click
-      expect(page).to have_content I18n.t('tasks.edit.title')
-    end
+        it 'Go to Edit page' do
+          expect(page).to have_content task_list[0].task_name
+          page.all('#click_edit')[0].click
+          expect(page).to have_content I18n.t('tasks.edit.title')
+        end
 
-    it 'Show dialog of delete' do
-      expect(page).to have_content task_list[0].task_name
-      page.all('#click_destroy')[0].click
-      expect(page).to have_content I18n.t('tasks.index.title')
+        it 'Show dialog of delete' do
+          expect(page).to have_content task_list[0].task_name
+          page.all('#click_destroy')[0].click
+          expect(page).to have_content I18n.t('tasks.index.title')
+        end
+      end
     end
   end
 
