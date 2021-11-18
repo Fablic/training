a4044349b26c1658bedabf8d51a2d8a62672e960
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-11-18 17:19:38 +0900

    refactored maint_spec

--- OreNoTask/spec/lib/tasks/maint_spec.rb
+++ OreNoTask/spec/lib/tasks/maint_spec.rb
@@ -7,32 +7,28 @@ RSpec.describe 'メンテナンス切り替え' do # rubocop:disable RSpec/Descr
     File.delete('on_maint') if File.exist?('on_maint')
   end
 
-  let!(:start) { Rake.application['maint:start'] }
-  let!(:stop) { Rake.application['maint:stop'] }
-
   describe 'maint:start' do
-    subject { start }
+    let!(:start) { Rake.application['maint:start'] }
 
     before do
       File.delete('on_maint') if File.exist?('on_maint')
     end
 
     it 'on_maintが生成される' do
-      expect(File).not_to exist('on_maint')
       expect(start.invoke).to be_truthy
       expect(File).to exist('on_maint')
     end
   end
 
   describe 'maint:stop' do
-    subject { stop }
+    let!(:stop) { Rake.application['maint:stop'] }
 
     before do
-      start.invoke
+      require 'fileutils'
+      FileUtils.touch('on_maint') unless File.exist?('on_maint')
     end
 
     it 'on_maintが削除される' do
-      expect(File).to exist('on_maint')
       expect(stop.invoke).to be_truthy
       expect(File).not_to exist('on_maint')
     end
