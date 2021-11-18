ca1cbf24be81835d1574ca3c5bfcd147d1571bcd
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-11-18 10:54:46 +0900

    refactored label_spec

--- OreNoTask/spec/models/label_spec.rb
+++ OreNoTask/spec/models/label_spec.rb
@@ -5,34 +5,32 @@ require 'rails_helper'
 RSpec.describe Label, type: :model do
   let(:label_name) { 'ラベル名' }
 
-  describe '正常系' do
+  describe 'バリデーションのテスト' do
     subject { described_class.new(name: label_name) }
 
     context '全項目入力' do
       it { is_expected.to be_valid }
     end
-  end
 
-  describe 'バリデーションのテスト' do
-    subject { described_class.new(name: label_name) }
+    context '各項目' do
+      describe 'label_nameカラム' do
+        context '空欄' do
+          let(:label_name) { '' }
 
-    describe 'label_nameカラム' do
-      context '空欄' do
-        let(:label_name) { '' }
+          it { is_expected.not_to be_valid }
+        end
 
-        it { is_expected.not_to be_valid }
-      end
+        context '20文字以内' do
+          let(:label_name) { 'a' * 20 }
 
-      context '20文字以内' do
-        let(:label_name) { 'a' * 20 }
+          it { is_expected.to be_valid }
+        end
 
-        it { is_expected.to be_valid }
-      end
+        context '21文字' do
+          let(:label_name) { 'a' * 21 }
 
-      context '21文字' do
-        let(:label_name) { 'a' * 21 }
-
-        it { is_expected.not_to be_valid }
+          it { is_expected.not_to be_valid }
+        end
       end
     end
   end
