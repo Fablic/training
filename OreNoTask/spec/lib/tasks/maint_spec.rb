e8f2b26cacb839e452dd1f7bded400eacd1abc41
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-11-18 17:10:58 +0900

    maint_specの問題を修正

--- OreNoTask/spec/lib/tasks/maint_spec.rb
+++ OreNoTask/spec/lib/tasks/maint_spec.rb
@@ -3,20 +3,37 @@
 require 'rake_helper'
 
 RSpec.describe 'メンテナンス切り替え' do # rubocop:disable RSpec/DescribeClass
-  describe 'maint:start' do
-    let!(:task) { Rake.application['maint:start'] }
+  after do
+    File.delete('on_maint') if File.exist?('on_maint')
+  end
 
-    it 'on_maintが存在する' do
-      expect(task.invoke).to be_truthy
+  let!(:start) { Rake.application['maint:start'] }
+  let!(:stop) { Rake.application['maint:stop'] }
+
+  describe 'maint:start' do
+    subject { start }
+
+    before do
+      File.delete('on_maint') if File.exist?('on_maint')
+    end
+
+    it 'on_maintが生成される' do
+      expect(File).not_to exist('on_maint')
+      expect(start.invoke).to be_truthy
       expect(File).to exist('on_maint')
     end
   end
 
   describe 'maint:stop' do
-    let!(:task) { Rake.application['maint:stop'] }
+    subject { stop }
 
-    it 'on_maintが存在しない' do
-      expect(task.invoke).to be_truthy
+    before do
+      start.invoke
+    end
+
+    it 'on_maintが削除される' do
+      expect(File).to exist('on_maint')
+      expect(stop.invoke).to be_truthy
       expect(File).not_to exist('on_maint')
     end
   end
