db101f529e4bea2aba107f0ef97aa3eae2c1f216
Arai, Mio | Mio | ECID <mio.a.arai@rakuten.com>
2021-11-18 16:57:02 +0900

    Review Back (Describeのネスト)、rubocop -Aの実施

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -1,7 +1,32 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  context 'when at the register page,' do
+  describe '#index' do
+    let!(:tasks) { FactoryBot.create_list(:task, 2) }
+
+    before {
+      visit root_path()
+    }
+
+    example 'all tasks are displayed.' do
+      expect(page).to have_content tasks.last.title
+    end
+
+    example 'one task can be shown.' do
+      click_link tasks.first.title
+      expect(page).to have_current_path task_path(tasks.first)
+    end
+
+    example 'a task can be deleted.' do
+      expect {
+        page.first('[data-method="delete"]').click
+      }.to change(Task, :count).by(-1)
+    end
+  end
+
+  describe '#new' do
     before do
       visit new_task_path()
 
@@ -18,9 +43,9 @@ RSpec.describe 'Tasks', type: :system do
     end
   end
 
-  context 'when at the edit page,' do
-    let(:task) { FactoryBot.create(:task) }
-    let(:params) {
+  describe '#edit' do
+    let!(:task) { FactoryBot.create(:task) }
+    let!(:params) {
       {
         title: 'title for edit',
         description: 'description for edit',
@@ -35,26 +60,41 @@ RSpec.describe 'Tasks', type: :system do
       expect(page).to have_field 'task[title]', with: task.title
     end
 
-    example 'a task can be updated.' do
+    example 'Title can be updated.' do
       visit edit_task_path(task)
       fill_in 'task[title]', with: params[:title]
-      fill_in 'task[description]', with: params[:description]
-      select params[:status], from: 'Status'
-      select params[:priority], from: 'Priority'
-      fill_in 'task[expires_at]', with: params[:expires_at]
       click_button 'Update Task'
 
-      expect(page).to have_content 'Task was successfully updated.'
       expect(page).to have_content params[:title]
-      expect(page).to have_content params[:description]
+    end
+
+    example 'Status can be updated.' do
+      visit edit_task_path(task)
+      select params[:status], from: 'Status'
+      click_button 'Update Task'
+
       expect(page).to have_content params[:status]
+    end
+
+    example 'Priority can be updated.' do
+      visit edit_task_path(task)
+      fill_in 'task[title]', with: params[:priority]
+      click_button 'Update Task'
+
       expect(page).to have_content params[:priority]
+    end
+
+    example 'Expires_at can be updated.' do
+      visit edit_task_path(task)
+      fill_in 'task[title]', with: params[:expires_at]
+      click_button 'Update Task'
+
       expect(page).to have_content params[:expires_at]
     end
   end
 
-  context 'when deleting a task,' do
-    let(:task) { FactoryBot.create(:task) }
+  describe '#destroy' do
+    let!(:task) { FactoryBot.create(:task) }
 
     example 'a task can be deleted.' do
       visit task_path(task)
