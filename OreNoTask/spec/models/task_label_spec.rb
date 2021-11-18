2f31e715b19a2fa19498056c584f0305d314d32f
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-11-18 11:07:25 +0900

    refactored task_label_spec

--- OreNoTask/spec/models/task_label_spec.rb
+++ OreNoTask/spec/models/task_label_spec.rb
@@ -10,30 +10,28 @@ RSpec.describe TaskLabel, type: :model do
   let(:task_id) { task.id }
   let(:label_id) { label.id }
 
-  describe '正常系' do
+  describe 'バリデーションのテスト' do
     subject { described_class.new(task_id: task_id, label_id: label_id) }
 
     context '全項目入力' do
       it { is_expected.to be_valid }
     end
-  end
 
-  describe 'バリデーションのテスト' do
-    subject { described_class.new(task_id: task_id, label_id: label_id) }
+    context '各項目' do
+      describe 'task_idカラム' do
+        context '空欄' do
+          let(:task_id) { nil }
 
-    describe 'task_idカラム' do
-      context '空欄' do
-        let(:task_id) { nil }
-
-        it { is_expected.not_to be_valid }
+          it { is_expected.not_to be_valid }
+        end
       end
-    end
 
-    describe 'label_idカラム' do
-      context '空欄' do
-        let(:label_id) { nil }
+      describe 'label_idカラム' do
+        context '空欄' do
+          let(:label_id) { nil }
 
-        it { is_expected.not_to be_valid }
+          it { is_expected.not_to be_valid }
+        end
       end
     end
   end
