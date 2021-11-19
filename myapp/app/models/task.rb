651334c6f2216acd712e1fc93082feb24fc5acec
ichinoseken <91720515+ichinoseken@users.noreply.github.com>
2021-11-19 09:06:09 +0900

    Update task.rb

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -28,6 +28,7 @@ class Task < ApplicationRecord
     
     errors.add(:end_date, I18n.t('activerecord.errors.messages.earlier_date_error', start: I18n.t('tasks.common.end_date'))) unless
       self.start_date < self.end_date 
+  end
 
   scope :search_condition, lambda { |search_params|
                              return if search_params.blank?
