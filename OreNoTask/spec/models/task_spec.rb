7eb938bb3164ce8d493d0f16849e79abd32366ea
Ikebe-Ryo <ts-ryo.ikebe@rakuten.com>
2021-11-22 11:27:58 +0900

    spec修正

--- OreNoTask/spec/models/task_spec.rb
+++ OreNoTask/spec/models/task_spec.rb
@@ -17,132 +17,130 @@ RSpec.describe 'Taskモデルのテスト', type: :model do
       it { is_expected.to be_valid }
     end
 
-    context '各項目' do
-      describe 'nameカラム' do
-        context '空欄' do
-          let(:name) { '' }
+    describe 'nameカラム' do
+      context '空欄' do
+        let(:name) { '' }
 
-          it { is_expected.not_to be_valid }
-        end
-
-        context '50文字以内' do
-          let(:name) { 'a' * 50 }
-
-          it { is_expected.to be_valid }
-        end
-
-        context '51文字以上' do
-          let(:name) { 'a' * 51 }
-
-          it { is_expected.not_to be_valid }
-        end
+        it { is_expected.not_to be_valid }
       end
 
-      describe 'descriptionカラム' do
-        context '空欄' do
-          let(:description) { '' }
+      context '50文字以内' do
+        let(:name) { 'a' * 50 }
 
-          it { is_expected.to be_valid }
-        end
-
-        context '2000文字以内' do
-          let(:description) { 'a' * 2000 }
-
-          it { is_expected.to be_valid }
-        end
-
-        context '2000文字以上' do
-          let(:description) { 'a' * 2001 }
-
-          it { is_expected.not_to be_valid }
-        end
+        it { is_expected.to be_valid }
       end
 
-      describe 'statusカラム' do
-        context '許容される値 not_started' do
-          let(:status) { :not_started }
+      context '51文字以上' do
+        let(:name) { 'a' * 51 }
 
-          it { is_expected.to be_valid }
-        end
+        it { is_expected.not_to be_valid }
+      end
+    end
 
-        context '許容される値 wip' do
-          let(:status) { :wip }
+    describe 'descriptionカラム' do
+      context '空欄' do
+        let(:description) { '' }
 
-          it { is_expected.to be_valid }
-        end
-
-        context '許容される値 completed' do
-          let(:status) { :completed }
-
-          it { is_expected.to be_valid }
-        end
-
-        context '許容されない値' do
-          let(:status) { :pending }
-
-          it { is_expected.not_to be_valid }
-        end
+        it { is_expected.to be_valid }
       end
 
-      describe 'start_atカラム' do
-        context '空欄でないこと' do
-          let(:start_at) { '' }
+      context '2000文字以内' do
+        let(:description) { 'a' * 2000 }
 
-          it { is_expected.not_to be_valid }
-        end
-
-        context '日付のフォーマットが不正' do
-          let(:start_at) { '2021年09ー01 10:00' }
-
-          it { is_expected.not_to be_valid }
-        end
-
-        context '存在しない日付' do
-          let(:start_at) { '2021/02/99 10:00' }
-
-          it { is_expected.not_to be_valid }
-        end
-
-        context '存在しない時間' do
-          let(:start_at) { '2021/09/01 10:99' }
-
-          it { is_expected.not_to be_valid }
-        end
+        it { is_expected.to be_valid }
       end
 
-      describe 'due_date_atカラム' do
-        context '空欄でないこと' do
-          let(:due_date_at) { '' }
+      context '2000文字以上' do
+        let(:description) { 'a' * 2001 }
 
-          it { is_expected.not_to be_valid }
-        end
+        it { is_expected.not_to be_valid }
+      end
+    end
 
-        context '日付のフォーマットが不正' do
-          let(:due_date_at) { '2021年09ー02 11:00' }
+    describe 'statusカラム' do
+      context '許容される値 not_started' do
+        let(:status) { :not_started }
 
-          it { is_expected.not_to be_valid }
-        end
-
-        context '存在しない日付' do
-          let(:due_date_at) { '2021/09/99 11:00' }
-
-          it { is_expected.not_to be_valid }
-        end
-
-        context '存在しない時間' do
-          let(:due_date_at) { '2021/09/02 11:99' }
-
-          it { is_expected.not_to be_valid }
-        end
+        it { is_expected.to be_valid }
       end
 
-      describe '複合' do
-        context 'start_at > due_date_atでないこと' do
-          let(:start_at) { '2021/09/01 10:00' }
-          let(:due_date_at) { '2021/08/31 10:00' }
+      context '許容される値 wip' do
+        let(:status) { :wip }
 
-          it { is_expected.not_to be_valid }
-        end
+        it { is_expected.to be_valid }
+      end
+
+      context '許容される値 completed' do
+        let(:status) { :completed }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '許容されない値' do
+        let(:status) { :pending }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe 'start_atカラム' do
+      context '空欄でないこと' do
+        let(:start_at) { '' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context '日付のフォーマットが不正' do
+        let(:start_at) { '2021年09ー01 10:00' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context '存在しない日付' do
+        let(:start_at) { '2021/02/99 10:00' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context '存在しない時間' do
+        let(:start_at) { '2021/09/01 10:99' }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe 'due_date_atカラム' do
+      context '空欄でないこと' do
+        let(:due_date_at) { '' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context '日付のフォーマットが不正' do
+        let(:due_date_at) { '2021年09ー02 11:00' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context '存在しない日付' do
+        let(:due_date_at) { '2021/09/99 11:00' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context '存在しない時間' do
+        let(:due_date_at) { '2021/09/02 11:99' }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe '複合' do
+      context 'start_at > due_date_atでないこと' do
+        let(:start_at) { '2021/09/01 10:00' }
+        let(:due_date_at) { '2021/08/31 10:00' }
+
+        it { is_expected.not_to be_valid }
       end
     end
   end
--- OreNoTask/spec/models/user_spec.rb
+++ OreNoTask/spec/models/user_spec.rb
@@ -14,85 +14,83 @@ RSpec.describe 'Userモデルのテスト', type: :model do
       it { is_expected.to be_valid }
     end
 
-    context '各項目' do
-      describe 'nameカラム' do
-        context '空欄' do
-          let(:name) { '' }
+    describe 'nameカラム' do
+      context '空欄' do
+        let(:name) { '' }
 
-          it { is_expected.not_to be_valid }
-        end
-
-        context '20文字以内' do
-          let(:name) { 'a' * 20 }
-
-          it { is_expected.to be_valid }
-        end
-
-        context '21文字以上' do
-          let(:name) { 'a' * 21 }
-
-          it { is_expected.not_to be_valid }
-        end
-
-        context 'ユーザー名重複' do
-          before do
-            create(:user, name: 'HanakoRakuten', password: 'hanakopass', privilege: :user, deleted: 0)
-          end
-
-          let(:name) { 'HanakoRakuten' }
-
-          it { is_expected.not_to be_valid }
-        end
-
-        context 'deletedのユーザー名と重複' do
-          before do
-            create(:user, name: 'YoshioRakuten', password: 'yoshiopass', privilege: :user, deleted: 1)
-          end
-
-          let(:name) { 'YoshioRakuten' }
-
-          it { is_expected.to be_valid }
-        end
+        it { is_expected.not_to be_valid }
       end
 
-      describe 'passwordカラム' do
-        context '空欄' do
-          let(:password) { '' }
+      context '20文字以内' do
+        let(:name) { 'a' * 20 }
 
-          it { is_expected.not_to be_valid }
-        end
-
-        context '20文字以内' do
-          let(:password) { 'a' * 20 }
-
-          it { is_expected.to be_valid }
-        end
-
-        context '21文字以上' do
-          let(:password) { 'a' * 21 }
-
-          it { is_expected.not_to be_valid }
-        end
+        it { is_expected.to be_valid }
       end
 
-      describe 'privilegeカラム' do
-        context '許容される値(user)' do
-          let(:privilege) { :user }
+      context '21文字以上' do
+        let(:name) { 'a' * 21 }
 
-          it { is_expected.to be_valid }
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'ユーザー名重複' do
+        before do
+          create(:user, name: 'HanakoRakuten', password: 'hanakopass', privilege: :user, deleted: 0)
         end
 
-        context '許容される値(admin)' do
-          let(:privilege) { :admin }
+        let(:name) { 'HanakoRakuten' }
 
-          it { is_expected.to be_valid }
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'deletedのユーザー名と重複' do
+        before do
+          create(:user, name: 'YoshioRakuten', password: 'yoshiopass', privilege: :user, deleted: 1)
         end
 
-        context '許容されない値' do
-          let(:privilege) { :anonymous }
+        let(:name) { 'YoshioRakuten' }
 
-          it { is_expected.not_to be_valid }
-        end
+        it { is_expected.to be_valid }
+      end
+    end
+
+    describe 'passwordカラム' do
+      context '空欄' do
+        let(:password) { '' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context '20文字以内' do
+        let(:password) { 'a' * 20 }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '21文字以上' do
+        let(:password) { 'a' * 21 }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe 'privilegeカラム' do
+      context '許容される値(user)' do
+        let(:privilege) { :user }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '許容される値(admin)' do
+        let(:privilege) { :admin }
+
+        it { is_expected.to be_valid }
+      end
+
+      context '許容されない値' do
+        let(:privilege) { :anonymous }
+
+        it { is_expected.not_to be_valid }
       end
     end
   end
