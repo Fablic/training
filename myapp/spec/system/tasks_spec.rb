657af1d97b41286865330ceffdc29907f1f104bc
sho14rk <shogo.kinjo@rakuten.com>
2021-11-24 14:58:27 +0900

    テストコードの指摘を修正

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -1,53 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system, js: true do
-  # let!(:task) { create(:task) }
-  let!(:task_list) { create_list(:task,5) }
+  let(:task) { create(:task) }
+  let(:destroy_task) { create(:task) }
+
+  before do
+    task
+    destroy_task
+  end
 
   describe 'タスク一覧画面' do
     before do
       visit root_path
     end
 
-    it 'タスク一覧画面が表示される' do
-      expect(page).to have_content('Tasks#index')
-      expect(page).to have_content('タスク名')
-      expect(page).to have_content('説明')
-      expect(page).to have_content('編集')
-      expect(page).to have_content('削除')
-    end
-
-    it '登録してあるタスクが表示される' do
-      expect(page).to have_content(task_list[0].name)
-      expect(page).to have_content(task_list[1].name)
-      expect(page).to have_content(task_list[2].name)
-      expect(page).to have_content(task_list[3].name)
-      expect(page).to have_content(task_list[4].name)
-    end
-
-    it 'タスク作成画面に遷移できる' do
-      click_link 'Create task'
-      expect(page).to have_content('Tasks#new')
-    end
-
-    it '表示されてるタスク名を選択してタスク詳細画面に遷移できる' do
-      click_link task_list[0].name
-      expect(page).to have_content('Tasks#show')
-    end
-
-    it '表示されてるタスクのEditを選択してタスク編集画面に遷移できる' do
-      all('tbody tr')[0].click_link 'Edit'
-      expect(page).to have_content('Tasks#edit')
-    end
-
-    it '表示されてるタスクのDestroyを押すと削除確認ダイアログが出て削除ができる' do
-      page.dismiss_confirm("Are you sure?") do
-        click_link 'Destroy', match: :first
+    context 'タスク一覧画面に遷移した時' do
+      it 'タスク一覧タイトルが表示される' do
+        expect(page).to have_selector('h1', text: 'タスク一覧')
       end
-      page.accept_confirm do
-        click_link 'Destroy', match: :first
+
+      it '登録してあるタスクが表示される' do
+        expect(page).to have_content(task.name)
       end
-      expect(page).to have_content('タスク削除成功！')
+    end
+
+    context 'タスク作成リンクをクリックした時' do
+      it 'タスク作成画面に遷移できる' do
+        find('#click_new_task').click
+        expect(page).to have_selector('h1', text: 'タスク作成')
+      end
+    end
+
+    context '表示されてるタスク名を選択した時' do
+      it 'タスク詳細画面に遷移できる' do
+        find("#task_name_#{task.id}").click
+        expect(page).to have_selector('h1', text: 'タスク詳細')
+      end
+    end
+
+    context '表示されてるタスクのEditを選択した時' do
+      it 'タスク編集画面に遷移できる' do
+        find("#link_edit_task_#{task.id}").click
+        expect(page).to have_selector('h1', text: 'タスク編集')
+      end
+    end
+
+    context '表示されてるタスクのDestroyを押した時' do
+      context '削除確認ダイアログでOKを押した時' do
+        it 'タスクの削除ができる' do
+          page.accept_confirm do
+            find("#link_destroy_task_#{destroy_task.id}").click
+          end
+          expect(page).to have_content('タスク削除成功！')
+          expect(page.all("#task_name_#{destroy_task.id}").empty?).to eq true
+        end
+      end
+      context '削除確認ダイアログでキャンセルを押した時' do
+        it '削除をキャンセル出来る' do
+          page.dismiss_confirm do
+            find("#link_destroy_task_#{destroy_task.id}").click
+          end
+          expect(page.has_selector?("#task_name_#{destroy_task.id}")).to eq true
+        end
+      end
     end
   end
 
@@ -56,53 +71,64 @@ RSpec.describe 'Tasks', type: :system, js: true do
       visit new_task_path
     end
 
-    it 'タスク作成画面が表示される' do
-      expect(page).to have_content('Tasks#new')
+    context 'タスク作成画面に遷移した時' do
+      it 'タスク作成タイトルが表示される' do
+        expect(page).to have_selector('h1', text: 'タスク作成')
+      end
     end
 
-    it '新しくタスクが作成出来る' do
-      fill_in 'task_name', with: 'input Task'
-      fill_in 'task_description', with: 'input Description'
-      click_button 'Post'
-      expect(page).to have_content('Tasks#show')
-      expect(page).to have_content('タスク作成に成功しました！')
-      expect(page).to have_content('input Task')
-      expect(page).to have_content('input Description')
+    context '新しいタスクを作成した時' do
+      it '新しいタスクが作成される' do
+        fill_in 'task_name', with: 'input Task'
+        fill_in 'task_description', with: 'input Description'
+        find('#post_task_button').click
+        expect(page).to have_selector('h1', text: 'タスク詳細')
+        expect(page).to have_content('タスク作成に成功しました！')
+        expect(page).to have_content('input Task')
+        expect(page).to have_content('input Description')
+      end
     end
   end
 
   describe 'タスク詳細画面' do
     before do
-      visit task_path task_list[0]
+      visit task_path task
     end
 
-    it 'タスク詳細画面が表示される' do
-      expect(page).to have_content('Tasks#show')
-    end
+    context 'タスク詳細画面に遷移した時' do
+      it 'タスク詳細タイトルが表示される' do
+        expect(page).to have_selector('h1', text: 'タスク詳細')
+      end
 
-    it '選択したタスクの詳細情報が表示される' do
-      expect(page).to have_content(task_list[0].name)
-      expect(page).to have_content(task_list[0].description)
+      it '選択したタスクの詳細情報が表示される' do
+        expect(page).to have_content(task.name)
+        expect(page).to have_content(task.description)
+      end
     end
   end
 
   describe 'タスク編集画面' do
+    let(:edit_task) { create(:task) }
     before do
-      visit edit_task_path task_list[0]
+      visit edit_task_path edit_task
     end
 
-    it 'タスク編集画面が表示される' do
-      expect(page).to have_content('Tasks#edit')
+    context 'タスク編集画面に遷移した時' do
+      it 'タスク編集タイトルが表示される' do
+        expect(page).to have_selector('h1', text: 'タスク編集')
+      end
     end
 
-    it 'タスクを選択して情報更新が出来る' do
-      fill_in 'task_name', with: 'update Task'
-      fill_in 'task_description', with: 'update Description'
-      click_button 'Post'
-      expect(page).to have_content('Tasks#show')
-      expect(page).to have_content('タスク更新に成功しました！')
-      expect(page).to have_content('update Task')
-      expect(page).to have_content('update Description')
+    context 'タスクの情報を更新した時' do
+      it 'タスクの情報が変更されている' do
+        fill_in 'task_name', with: 'update Task'
+        fill_in 'task_description', with: 'update Description'
+        find('#post_task_button').click
+        expect(page).to have_selector('h1', text: 'タスク詳細')
+        expect(page).to have_content('タスク更新に成功しました！')
+        expect(page).to have_content('update Task')
+        expect(page).to have_content('update Description')
+      end
     end
   end
 end
