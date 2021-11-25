cbbd3a8af7fe77395fe1c015f93cceaa8bcd6b8c
sho14rk <shogo.kinjo@rakuten.com>
2021-11-25 13:46:14 +0900

    i18nの設定を導入

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -7,7 +7,15 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     config.load_defaults 6.0
+
+    # TimeZoneを日本（東京）に設定
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    # デフォルト言語を日本語に設定
+    config.i18n.default_locale = :ja
+
+    # 言語ファイルを階層ごとに設定するための記述
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
