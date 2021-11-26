95b5135d210a20530bcd2292ca3726ed6dbb175b
sho14rk <shogo.kinjo@rakuten.com>
2021-11-26 09:29:35 +0900

    エラーページをカスタマイズ

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   root 'tasks#index'
+
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
 end
