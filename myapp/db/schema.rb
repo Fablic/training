291d41c4eb5f91e8adab9ccdb6af0631c6650775
ichinoseken <ts-ken.a.ichinose@rakuten.com>
2021-11-26 16:40:50 +0900

    step20

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_023764) do
     t.timestamp "start_date"
     t.timestamp "end_date"
     t.boolean "deleted", default: false
+    t.integer "login_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
