8005edc0d6da6c12f7aca6d8b6fd0fda7828c1f5
sho14rk <shogo.kinjo@rakuten.com>
2021-11-29 13:25:43 +0900

    TaskモデルにValidateを追加し、テストを追加

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,5 +1,13 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { maximum: 1024 }
-  validates :description, presence: true
+  validates :deadline, presence: true
+  validate  :since_now
+
+  private
+    def since_now
+      unless deadline == nil
+        errors.add(:deadline, 'は、未来日時を登録して下さい') if deadline <= DateTime.now
+      end
+    end
 end
--- myapp/spec/model/tasks_spec.rb
+++ myapp/spec/model/tasks_spec.rb
@@ -6,26 +6,53 @@ RSpec.describe 'Tasks Model', type: :model do
     subject { task.valid? }
 
     context 'nameカラム' do
-      it '空欄でないこと' do
+      it '空欄でない事' do
         task.name = ''
         is_expected.to eq false
       end
 
-      it '255文字以下であること' do
+      it 'nilでない事' do
+        task.name = nil
+        is_expected.to eq false
+      end
+
+      it '255文字以下である事' do
         task.name = Faker::Lorem.characters(number:256)
         expect(task.valid?).to eq false
       end
     end
 
     context 'descriptionカラム' do
-      it '空欄でないこと' do
+      it '空欄でない事' do
         task.description = ''
         is_expected.to eq false
       end
 
-      it '1024文字以下であること' do
+      it 'nilでない事' do
+        task.description = nil
+        is_expected.to eq false
+      end
+
+      it '1024文字以下である事' do
         task.description = Faker::Lorem.characters(number:1025)
         expect(task.valid?).to eq false
+      end
+    end
+
+    context 'deadlineカラム' do
+      it '空欄でない事' do
+        task.deadline = ''
+        is_expected.to eq false
+      end
+
+      it 'nilでない事' do
+        task.deadline = nil
+        is_expected.to eq false
+      end
+
+      it '過去日時でない事' do
+        task.deadline = 1.hours.ago
+        is_expected.to eq false
       end
     end
   end
