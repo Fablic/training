299995ac86a6bde8960fb5607b5f546f7c562272
sho14rk <shogo.kinjo@rakuten.com>
2021-11-29 11:31:48 +0900

    一覧画面で各項目でソート出来るようにする

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,6 +1,9 @@
 class TasksController < ApplicationController
+
+  helper_method :sort_column, :sort_direction
+
   def index
-    @tasks = Task.order(created_at: "DESC")
+    @tasks = Task.order("#{sort_column} #{sort_direction}")
   end
 
   def show
@@ -43,7 +46,15 @@ class TasksController < ApplicationController
 
   private
 
-  def task_params
-    params.require(:task).permit(:name, :description, :deadline)
-  end
+    def task_params
+      params.require(:task).permit(:name, :description, :deadline)
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
+    end
+
+    def sort_column
+      Task.column_names.include?(params[:sort]) ? params[:sort] : 'created_at'
+    end
 end
