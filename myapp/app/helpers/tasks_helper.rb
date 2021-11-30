7491409fad33c2fc605381bb4174b2905bb65174
sho14rk <shogo.kinjo@rakuten.com>
2021-11-30 13:57:53 +0900

    TaskのSystemSpecにソートのテストを追加

--- myapp/app/helpers/tasks_helper.rb
+++ myapp/app/helpers/tasks_helper.rb
@@ -1,6 +1,6 @@
 module TasksHelper
   def sort_order(column, title)
     direction = (column == sort_column && sort_direction == 'asc') ? 'desc' : 'asc'
-    link_to title, { sort: column, direction: direction }
+    link_to title, { sort: column, direction: direction }, id: "tasks_#{column}_link"
   end
 end
--- myapp/spec/factories/tasks.rb
+++ myapp/spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     name { |i| "task_name_#{i}" }
     description { |i| "task_description_#{i}" }
-    deadline { Faker::Date.between(from: Date.today, to: 1.month.since) }
+    deadline { 1.hours.since }
     created_at { Date.today }
     updated_at { Date.today }
   end
