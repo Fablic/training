3433df7d42b858aa08172ffbbd4e94f134ad44a8
sho14rk <shogo.kinjo@rakuten.com>
2021-12-01 17:11:24 +0900

    system specのネストが深い部分を修正

--- myapp/spec/model/tasks_spec.rb
+++ myapp/spec/model/tasks_spec.rb
@@ -5,72 +5,66 @@ RSpec.describe 'Tasks Model', type: :model do
     let(:task) { create(:task) }
     let(:task_valid) { task.valid? }
 
-    describe 'nameカラム' do
-      context '空欄の時' do
-        it 'バリデーションがFalseになる事' do
-          task.name = ''
-          expect(task_valid).to eq false
-        end
-      end
-
-      context 'nilの時' do
-        it 'バリデーションがFalseになる事' do
-          task.name = nil
-          expect(task_valid).to eq false
-        end
-      end
-
-      context '256文字以上の時' do
-        it 'バリデーションがFalseになる事' do
-          task.name = Faker::Lorem.characters(number: 256)
-          expect(task_valid).to eq false
-        end
+    context 'nameの値が空欄の時' do
+      it 'バリデーションがFalseになる事' do
+        task.name = ''
+        expect(task_valid).to eq false
       end
     end
 
-    describe 'descriptionカラム' do
-      context '空欄の時' do
-        it 'バリデーションがFalseになる事' do
-          task.description = ''
-          expect(task_valid).to eq false
-        end
-      end
-
-      context 'nilの時' do
-        it 'バリデーションがFalseになる事' do
-          task.description = nil
-          expect(task_valid).to eq false
-        end
-      end
-
-      context '1025文字以上の時' do
-        it 'バリデーションがFalseになる事' do
-          task.description = Faker::Lorem.characters(number: 1025)
-          expect(task_valid).to eq false
-        end
+    context 'nameの値がnilの時' do
+      it 'バリデーションがFalseになる事' do
+        task.name = nil
+        expect(task_valid).to eq false
       end
     end
 
-    describe 'deadline_atカラム' do
-      context '空欄の時' do
-        it 'バリデーションがFalseになる事' do
-          task.deadline_at = ''
-          expect(task_valid).to eq false
-        end
+    context 'nameの値が256文字以上の時' do
+      it 'バリデーションがFalseになる事' do
+        task.name = Faker::Lorem.characters(number: 256)
+        expect(task_valid).to eq false
       end
+    end
 
-      context 'nilの時' do
-        it 'バリデーションがFalseになる事' do
-          task.deadline_at = nil
-          expect(task_valid).to eq false
-        end
+    context 'descriptionの値が空欄の時' do
+      it 'バリデーションがFalseになる事' do
+        task.description = ''
+        expect(task_valid).to eq false
       end
+    end
 
-      context '過去日時の時' do
-        it 'バリデーションがFalseになる事' do
-          task.deadline_at = 1.hour.ago
-          expect(task_valid).to eq false
-        end
+    context 'descriptionの値がnilの時' do
+      it 'バリデーションがFalseになる事' do
+        task.description = nil
+        expect(task_valid).to eq false
+      end
+    end
+
+    context 'descriptionの値が1025文字以上の時' do
+      it 'バリデーションがFalseになる事' do
+        task.description = Faker::Lorem.characters(number: 1025)
+        expect(task_valid).to eq false
+      end
+    end
+
+    context 'deadline_atの値が空欄の時' do
+      it 'バリデーションがFalseになる事' do
+        task.deadline_at = ''
+        expect(task_valid).to eq false
+      end
+    end
+
+    context 'deadline_atの値がnilの時' do
+      it 'バリデーションがFalseになる事' do
+        task.deadline_at = nil
+        expect(task_valid).to eq false
+      end
+    end
+
+    context 'deadline_atの値が過去日時の時' do
+      it 'バリデーションがFalseになる事' do
+        task.deadline_at = 1.hour.ago
+        expect(task_valid).to eq false
       end
     end
   end
