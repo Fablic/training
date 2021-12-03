f4123161cfc2c0b9666a974be33a80c2553ebf26
sho14rk <shogo.kinjo@rakuten.com>
2021-12-03 16:48:37 +0900

    rubocopでの自動修正

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -1,13 +1,13 @@
 99.times do |n|
-  name  = Faker::Types.rb_string
-  description = "これは説明です。#{n+1}"
+  name = Faker::Types.rb_string
+  description = "これは説明です。#{n + 1}"
   deadline_at = Faker::Time.between(from: DateTime.now + 1, to: DateTime.now + 30)
-  status = Task.statuses.keys.shuffle[0]
+  status = Task.statuses.keys.sample
   created_at = Faker::Time.between(from: DateTime.now - 30, to: DateTime.now)
   updated_at = Faker::Time.between(from: DateTime.now - 30, to: DateTime.now)
-  Task.create!(name:  name,
+  Task.create!(name: name,
                description: description,
-               deadline_at:              deadline_at,
+               deadline_at: deadline_at,
                status: status,
                created_at: created_at,
                updated_at: updated_at)
