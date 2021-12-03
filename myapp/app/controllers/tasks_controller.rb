52e96e5d40141fe7d81c2a98d565037b05ac7180
sho14rk <shogo.kinjo@rakuten.com>
2021-12-03 16:40:38 +0900

    タスク一覧画面にページネーションを追加

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-    @tasks = Task.order("#{sort_column} #{sort_direction}")
+    @tasks = Task.order("#{sort_column} #{sort_direction}").page(params[:page]).per(10)
     content = params[:content]
     status = params[:status]
     @tasks = @tasks.where('name LIKE ?', "%#{content}%") if content.present?
