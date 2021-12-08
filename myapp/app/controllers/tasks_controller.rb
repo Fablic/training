1efc8dd2258a3f045239e61f850277e38bb4a893
sho14rk <shogo.kinjo@rakuten.com>
2021-12-08 16:00:13 +0900

    デザインなどの軽微な修正

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -19,6 +19,9 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+    # 以下2行は次のStepでログインユーザーに変更するため、暫定処置
+    user = User.first
+    @task.user_id = user.id
     if @task.save
       flash[:success] = 'タスク作成に成功しました！'
       redirect_to @task
