ae834e4c8bfe2fa1fb7445ef303ea07eb3505ab7
Yoo, Jiseung | Jeff | ECID <jiseung.yoo@rakuten.com>
2021-12-16 17:34:09 +0900

    Add validation to model

--- todos-app/backend/task_service/app/models/task.rb
+++ todos-app/backend/task_service/app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  validates :user_id, presence: true
+  validates :title, length: {maximum: 20}, presence: true
+  validates :description, length: {maximum: 200}, allow_nil: true
+  validates :status, inclusion: {in: [0, 1, 2]}, presence: true
+  validates :priority, inclusion: {in: [0, 1, 2]}, presence: true
+  validates_datetime :due_datetime, allow_nil: true
 end
