e87742dfeac1b01963cbc0f54c8b8ea9c3b0af2f
sho14rk <shogo.kinjo@rakuten.com>
2021-12-16 09:48:38 +0900

    メールバリデーションをリファクタリング

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
 
