4a53b136921a69dd7e635b36afb6a5e276ab3da9
Yoo, Jiseung | Jeff | ECID <jiseung.yoo@rakuten.com>
2021-12-16 17:45:35 +0900

    Add label to each input

--- todos-app/frontend/src/views/TaskCreate.vue
+++ todos-app/frontend/src/views/TaskCreate.vue
@@ -2,18 +2,23 @@
 <template>
   <div>
     <h1>New Task</h1>
+    <span>Title</span><br/>
     <input v-model="newTask.title" type="text" placeholder="Input title" required><br/>
+    <span>Description</span><br/>
     <input v-model="newTask.description" placeholder="Input description"><br/>
+    <span>Priority</span><br/>
     <select v-model="newTask.priority">
       <option value="0">High</option>
       <option value="1">Medium</option>
       <option value="2">Low</option>
     </select><br/>
+    <span>Status</span><br/>
     <select v-model="newTask.status">
       <option value="0">Not Started</option>
       <option value="1">In Progress</option>
       <option value="2">Done</option>
     </select><br/>
+    <span>Due date</span><br/>
     <input v-model="newTask.due_datetime" type="datetime-local"><br/>
     <button @click="createTask(newTask)">Create Task</button>
   </div>
--- todos-app/frontend/src/views/TaskEdit.vue
+++ todos-app/frontend/src/views/TaskEdit.vue
@@ -2,18 +2,23 @@
 <template>
   <div>
     <h1>Edit Task</h1>
+    <span>Title</span><br/>
     <input v-model="task.title" type="text" placeholder="Input title" required><br/>
+    <span>Description</span><br/>
     <input v-model="task.description" placeholder="Input description"><br/>
+    <span>Priority</span><br/>
     <select v-model="task.priority">
       <option value="0">High</option>
       <option value="1">Medium</option>
       <option value="2">Low</option>
     </select><br/>
+    <span>Status</span><br/>
     <select v-model="task.status">
       <option value="0">Not Started</option>
       <option value="1">In Progress</option>
       <option value="2">Done</option>
     </select><br/>
+    <span>Due date</span><br/>
     <input v-model="task.due_datetime" type="datetime-local"><br/>
     <button @click="editTask(task)">Edit Task</button>
   </div>
