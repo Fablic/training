debbe97b505f0bc8d006b8971365432ae085d1fb
sho14rk <shogo.kinjo@rakuten.com>
2021-12-16 14:00:09 +0900

    不要なコメントを削除

--- myapp/app/helpers/sessions_helper.rb
+++ myapp/app/helpers/sessions_helper.rb
@@ -1,36 +1,29 @@
 module SessionsHelper
-  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
   def current_user?(user)
     user == current_user
   end
 
-  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
 
-  # 現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)
   end
 
-  # 記憶したURL (もしくはデフォルト値) にリダイレクト
   def redirect_back_or_default(default)
     redirect_to(session[:forwarding_url] || default)
     session.delete(:forwarding_url)
   end
 
-  # アクセスしようとしたURLを覚えておく
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
   end
