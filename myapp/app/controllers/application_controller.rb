6ef2286ba7cafb97b40203d87f1635f98672ddda
sho14rk <shogo.kinjo@rakuten.com>
2021-12-17 12:48:20 +0900

    404と500のエラー表示を開発環境のみデフォルトに変更

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-  rescue_from Exception,                      with: :_render_500
-  rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
-  rescue_from ActionController::RoutingError, with: :_render_404
 
+  unless Rails.env.development?
+    rescue_from Exception,                      with: :_render_500
+    rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
+    rescue_from ActionController::RoutingError, with: :_render_404
+  end
+  
   include SessionsHelper
 
   def routing_error
