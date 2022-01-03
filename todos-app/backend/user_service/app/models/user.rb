a945b574296642b1cf0980f50f331231208cc907
Yoo, Jiseung | Jeff | ECID <jiseung.yoo@rakuten.com>
2022-01-04 08:52:02 +0900

    Run rubocop

--- todos-app/backend/user_service/app/models/user.rb
+++ todos-app/backend/user_service/app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
   enum role: { admin: 0, user: 1 }
--- todos-app/backend/user_service/db/seeds.rb
+++ todos-app/backend/user_service/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,9 +10,9 @@
 
 5.times do
   User.create(
-    username: Faker::Internet::username,
-    email: Faker::Internet::email,
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
     role: Faker::Number.between(from: 0, to: 1),
-    password: Faker::Internet::password
+    password: Faker::Internet.password,
   )
 end
