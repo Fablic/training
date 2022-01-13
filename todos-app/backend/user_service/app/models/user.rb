fad4fad38e126da488b3acf12386fce9b80b8a25
Yoo, Jiseung | Jeff | ECID <jiseung.yoo@rakuten.com>
2022-01-13 14:16:25 +0900

    Set newly created user's role as user

--- todos-app/backend/user_service/app/models/user.rb
+++ todos-app/backend/user_service/app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   has_secure_password
   enum role: { admin: 0, user: 1 }
-  attribute :role, :integer, default: 0
+  attribute :role, :integer, default: 1
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
