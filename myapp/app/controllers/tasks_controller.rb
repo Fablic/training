9408eeb63b00160045b0bd0015c73c98c5114486
Kobayashi <ts-hidey.d.kobayashi@p51852.intra.rakuten.co.jp>
2022-02-18 14:28:10 +0900

    step7

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -9,17 +9,18 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+    @task.user_id = 1
   end
 
   def create
     @task = Task.new(task_params)
     if @task.save
       # 成功
-      # redirect task/index
       flash[:success] = "保存しました"
-      redirect_to @task
+      redirect_to @task # Tasks#showへ
     else
       # 失敗
+      flash.now[:alert] = "保存に失敗しました"
       render 'new'
     end
   end
@@ -32,19 +33,24 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     if @task.update(task_params)
       # 成功
+      flash[:success] = "保存しました"
       redirect_to @task
     else
       # 失敗
+      flash.now[:alert] = "保存に失敗しました"
       render 'edit'
     end
   end
 
   def destroy
+    Task.find(params[:id]).destroy
+    flash[:success] = "削除しました"
+    redirect_to tasks_url
   end
 
   private
 
     def task_params
-      params.require(:task).permit(:title, :body, :deadline, :priority, :label_id, :status)
+      params.require(:task).permit(:user_id, :title, :body, :deadline, :priority, :label_id, :status)
     end
 end
