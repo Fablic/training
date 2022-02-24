587d23a6b8d878d605c2751b0fd5bb8d11e9797f
Kobayashi <ts-hidey.d.kobayashi@p51852.intra.rakuten.co.jp>
2022-02-24 13:41:21 +0900

    test rename

--- myapp/test/controllers/tasks_controller_test.rb
+++ myapp/test/controllers/tasks_controller_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
-class TaskControllerTest < ActionDispatch::IntegrationTest
+class TasksControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get task_new_url
+    get tasks_new_url
     assert_response :success
   end
 
   test "should get index" do
-    get task_index_url
+    get tasks_index_url
     assert_response :success
   end
 
   test "should get show" do
-    get task_show_url
+    get tasks_show_url
     assert_response :success
   end
 
   test "should get edit" do
-    get task_edit_url
+    get tasks_edit_url
     assert_response :success
   end
 
