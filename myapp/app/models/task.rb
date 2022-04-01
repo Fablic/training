9a07ca9747614a68087faf3540b615b1c9e5151f
Kohei Sakurai <kohei.sakurai@rakuten.com>
2022-04-01 18:46:34 +0900

    add status column to tasks

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -2,4 +2,6 @@
 
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
+
+  enum status: %i[未着手 着手中 完了]
 end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_123017) do
+ActiveRecord::Schema.define(version: 2022_04_01_085910) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", limit: 50, null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0, null: false
   end
 
 end
