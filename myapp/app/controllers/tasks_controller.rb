0fb87d54a0c91b81c00982df6ddf6f0dccef2509
Kohei Sakurai <kohei.sakurai@rakuten.com>
2022-04-05 16:51:09 +0900

    change per value to 20

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@
 class TasksController < ApplicationController
   def index
     @q = Task.ransack(params[:q])
-    @tasks = @q.result.page(params[:page])
+    @tasks = @q.result.page(params[:page]).per(20)
   end
 
   def show
