c5d82d19996e6be0a38717eeb8204b4597bec741
Kohei Sakurai <kohei.sakurai@rakuten.com>
2022-04-06 15:53:53 +0900

    add reference to user

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -4,4 +4,6 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
 
   enum status: %i[未着手 着手中 完了]
+
+  belongs_to :user
 end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_041040) do
+ActiveRecord::Schema.define(version: 2022_04_06_051507) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", limit: 50, null: false
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2022_04_06_041040) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0, null: false
+    t.bigint "user_id", null: false
     t.index ["name"], name: "index_tasks_on_name"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
