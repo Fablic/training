854c0576cabf0a795d639acb2f6ca7b18afa1b21
Kohei Sakurai <kohei.sakurai@rakuten.com>
2022-04-06 13:19:57 +0900

    generate user model and migrate

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_043730) do
+ActiveRecord::Schema.define(version: 2022_04_06_041040) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", limit: 50, null: false
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2022_04_04_043730) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0, null: false
     t.index ["name"], name: "index_tasks_on_name"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
