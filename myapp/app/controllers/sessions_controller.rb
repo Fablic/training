d5b4114deeeb56833f1962e2df4b5895e7188590
Kohei Sakurai <kohei.sakurai@rakuten.com>
2022-04-11 14:29:47 +0900

    add flash message when login fails

--- myapp/app/controllers/sessions_controller.rb
+++ myapp/app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: t('.message_login')
     else
+      flash.now[:notice] = t('.message_login_failed')
       render :new
     end
   end
