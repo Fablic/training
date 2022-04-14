289de4f1b9b29c735d2f5e52c383376ad919b1c4
Kohei Sakurai <kohei.sakurai@rakuten.com>
2022-04-14 15:37:21 +0900

    add examples in `spec/models/label_spec.rb` to test `Label` model

--- myapp/spec/models/label_spec.rb
+++ myapp/spec/models/label_spec.rb
@@ -1,5 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Label, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with valid attributes' do
+      label = described_class.new(name: 'label1')
+      expect(label).to be_valid
+    end
+
+    it 'is invalid without name' do
+      label = described_class.new(name: nil)
+      expect(label).not_to be_valid
+    end
+  end
+
+  describe 'association' do
+    it 'has many tasks' do
+      r = described_class.reflect_on_association(:tasks)
+      expect(r.macro).to eq(:has_many)
+    end
+  end
 end
