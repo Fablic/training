398e497a607ca9c7136c41a4f5f86b5ed8925435
Kohei Sakurai <kohei.sakurai@rakuten.com>
2022-04-18 15:57:08 +0900

    add rspec to test `TaskLabel`

--- myapp/spec/models/task_label_spec.rb
+++ myapp/spec/models/task_label_spec.rb
@@ -3,5 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe TaskLabel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'association' do
+    it 'belongs to task' do
+      r = described_class.reflect_on_association(:task)
+      expect(r.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to label' do
+      r = described_class.reflect_on_association(:label)
+      expect(r.macro).to eq(:belongs_to)
+    end
+
+    it 'is successfully deleted when a parent task is deleted' do
+      task = FactoryBot.create(:task)
+      label = FactoryBot.create(:label)
+      task_label = described_class.create(task_id: task.id, label_id: label.id)
+      task.destroy!
+      expect(described_class.find_by(id: task_label.id)).to eq nil
+    end
+  end
 end
