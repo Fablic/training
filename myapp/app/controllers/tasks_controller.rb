e2d06aa347f2457c77f6ea20049482d83a936df3
kentakanbe <kenta.kanbe@connec10.co.jp>
2022-05-13 09:57:40 +0900

    rubocop対応(解析・自動修正漏れ)

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show edit update destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all_sort_by(:created_at, :desc)
@@ -14,9 +14,7 @@ class TasksController < ApplicationController
     render :index
   end
 
-  def show
-    
-  end
+  def show; end
 
   def new
     @task = Task.new()
@@ -29,29 +27,26 @@ class TasksController < ApplicationController
     @task.user_id = 0
 
     if @task.save
-      redirect_to @task, notice: t("tasks.flash.new")
+      redirect_to @task, notice: t('tasks.flash.new')
     else
       render :new
     end
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
     if @task.update(task_params)
-      redirect_to @task, notice: t("tasks.flash.update")
+      redirect_to @task, notice: t('tasks.flash.update')
     else
       render :edit
     end
   end
 
   def destroy
-
     return unless @task.destroy
     
-    redirect_to tasks_path, notice: t("tasks.flash.destroy")
+    redirect_to tasks_path, notice: t('tasks.flash.destroy')
   end
 
   private
