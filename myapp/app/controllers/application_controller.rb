81e7d52f69ea7e138439a121c22256b1d771b8b4
stakahashi-d <shohei.takahashi@rakuten.com>
2022-05-18 12:15:03 +0900

    rubocop

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,28 +1,27 @@
 class ApplicationController < ActionController::Base
-  rescue_from Exception,                      with: :_render_500
-  rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
-  rescue_from ActionController::RoutingError, with: :_render_404
+  rescue_from Exception,                      with: :render500
+  rescue_from ActiveRecord::RecordNotFound,   with: :render404
+  rescue_from ActionController::RoutingError, with: :render404
 
   def routing_error
     raise ActionController::RoutingError, params[:path]
   end
 
-  def t(key, options={})
-    if key[0] == '.'
-      key = controller_name + "_controller." + action_name + key
-    end
+  def t(key, options = {})
+    format('key = %<controller>s + _controller. + %<action>% + %<key>%', controller: controller_name,
+                                                                         action: action_name, key: key)
     super
   end
 
   private
 
-  def _render_404(e = nil)
-    logger.info "Rendering 404 with excaption: #{e.message}" if e
+  def render404(err = nil)
+    logger.info "Rendering 404 with excaption: #{err.message}" if err
     render 'errors/404.html', status: :not_found
   end
 
-  def _render_500(e = nil)
-    logger.error "Rendering 500 with excaption: #{e.message}" if e
+  def render500(err = nil)
+    logger.error "Rendering 500 with excaption: #{err.message}" if err
     render 'errors/500', status: :internal_server_error, layout: 'error'
   end
 end
