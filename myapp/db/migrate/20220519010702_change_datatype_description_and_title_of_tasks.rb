e6bbb1fd03df852d4f5510e6ff80c5a7ce66993d
stakahashi-d <shohei.takahashi@rakuten.com>
2022-05-19 10:18:15 +0900

    fix migrate

---
+++ myapp/db/migrate/20220519010702_change_datatype_description_and_title_of_tasks.rb
@@ -0,0 +1,6 @@
+class ChangeDatatypeDescriptionAndTitleOfTasks < ActiveRecord::Migration[6.0]
+  def change
+    change_column :tasks, :title, :string, null: false, limit: 50
+    change_column :tasks, :description, :text, null: true
+  end
+end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_065703) do
+ActiveRecord::Schema.define(version: 2022_05_19_010702) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
+    t.string "title", limit: 50, null: false
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
