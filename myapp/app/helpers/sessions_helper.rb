08a4351fd21efe742e3eef2148997c4a59af6e64
stakahashi-d <shohei.takahashi@rakuten.com>
2022-05-31 15:17:30 +0900

    fix by rubocop

--- myapp/app/helpers/sessions_helper.rb
+++ myapp/app/helpers/sessions_helper.rb
@@ -5,17 +5,11 @@ module SessionsHelper
 
   def log_out
     session.delete(:user_id)
-    @current_user = nil
   end
 
   # 現在ログイン中のユーザーを返却
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  # 受け取ったユーザーがログイン中のユーザーと一致するかどうか
-  def current_user?(user)
-    user == current_user
+    User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
