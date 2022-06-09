d5e0fc1c59d2f3240587e360b3972b674b59fdbe
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-09 17:14:30 +0900

    rubocopで整形

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :get_user_by_id, only: [:show, :edit, :update]
+  before_action :get_user_by_id, only: %i[show edit update]
   def index
     @tasks = Task.all
   end
@@ -16,7 +16,7 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     if @task.save
       # タスク作成を通知
-      flash[:success] = "Task created!"
+      flash[:success] = 'Task created!'
       redirect_to root_path
     else
       render 'new'
@@ -30,27 +30,27 @@ class TasksController < ApplicationController
   def update
     # @task = Task.find(params[:id])
     if @task.update(task_params)
-      flash[:success] = "Task updated!"
+      flash[:success] = 'Task updated!'
       redirect_to root_path
     else
-      render "edit"
+      render 'edit'
     end
   end
 
   def destroy
     Task.find(params[:id]).destroy
-    flash[:success] = "Task deleted!"
+    flash[:success] = 'Task deleted!'
     redirect_to root_path
   end
 
   private
-    def task_params
-      params.require(:task).permit(:title, :description, :priority, :status, :expire_at)
-    end
 
-    # 指定されたIDのユーザーを取得
-    def get_user_by_id
-      @task = Task.find(params[:id])
-    end
+  def task_params
+    params.require(:task).permit(:title, :description, :priority, :status, :expire_at)
+  end
 
+  # 指定されたIDのユーザーを取得
+  def get_user_by_id
+    @task = Task.find(params[:id])
+  end
 end
