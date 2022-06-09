875150917bb9abb22c6390d780bd7aa9145fa2a2
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-09 16:45:23 +0900

    Controller,Viewの最低限の部分を実装

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :get_user_by_id, only: [:show, :edit, :update]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -12,27 +13,44 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params[:task])
+    @task = Task.new(task_params)
     if @task.save
       # タスク作成を通知
       flash[:success] = "タスクを作成しました"
-      redirect_to @task
+      redirect_to root_path
     else
       render 'new'
     end
   end
 
   def edit
+    # @task = Task.find(params[:id])
   end
 
   def update
+    # @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = "Task updated!"
+      redirect_to root_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    Task.find(params[:id]).destroy
+    flash[:success] = "Task deleted!"
+    redirect_to root_path
   end
 
   private
     def task_params
-      params.require(:task).permit(:title, :description, :priority, :status)
+      params.require(:task).permit(:title, :description, :priority, :status, :expire_at)
     end
+
+    # 指定されたIDのユーザーを取得
+    def get_user_by_id
+      @task = Task.find(params[:id])
+    end
+
 end
