92ac0085c2274dd46caa602ed6f2ddcf369822fd
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-09 13:18:39 +0900

    2000-01-01しか保存されないのを修正しました

--- myapp/db/migrate/20220609030305_add_expired_at_to_tasks.rb
+++ myapp/db/migrate/20220609030305_add_expired_at_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddExpiredAtToTasks < ActiveRecord::Migration[6.0]
   def change
-    add_column :tasks, :expire_at, :time
+    add_column :tasks, :expire_at, :datetime, precision: 6
   end
 end
