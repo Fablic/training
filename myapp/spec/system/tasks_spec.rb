ac41fb42c7b0335b5297a577a4ee7ff8b837e52b
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-13 13:26:33 +0900

    タスクの更新、詳細、削除のテスト

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Tasks", type: :system do
     driven_by(:rack_test)
   end
 
-  # タスクの一覧が表示される
+  # タスクの一覧が表示
   it "show all tasks" do
     task = FactoryBot.create(:task)
     visit root_path
@@ -27,7 +27,6 @@ RSpec.describe "Tasks", type: :system do
       fill_in "Expire at", with: "2022-06-10 12:00:00"
       click_button "Save"
   
-      # flashメッセージが表示されるか
       expect(page).to have_content "Task created!"
       expect(page).to have_content "test title"
       expect(page).to have_content "test description"
@@ -39,34 +38,59 @@ RSpec.describe "Tasks", type: :system do
   it "can't create task if title is blank" do
     visit root_path
     click_link "Create Task"
-  
     fill_in "Description", with: "test description"
     fill_in "Expire at", with: "2022-06-10 12:00:00"
     click_button "Save"
-    
-    # エラーメッセージが表示されるか
-    expect(page).to have_content ("Title can't be blank")
 
+    expect(page).to have_content ("Title can't be blank")
   end
   
-  # titleを編集できる
-  it "edit title in the task"
+  # 編集して更新できる
+  it "update the task" do
+    task = FactoryBot.create(:task)
+    visit root_path
+    click_link "Edit"
+    fill_in "Title", with: "edited title"
+    fill_in "Description", with: "edited desc"
+    fill_in "Expire at", with: "2022-06-10 00:00:00"
+    click_button "Save"
+
+    expect(page).to have_content "Task updated!"
+    expect(page).to have_content "edited title"
+    expect(page).to have_content "edited desc"
+    expect(page).to have_content "2022-06-10 00:00:00"
+  end
 
   # titleが空欄だと更新できない
-  it "can't edit if title is blank"
+  it "can't update if the title is blank" do
+    task = FactoryBot.create(:task)
+    visit edit_task_path(task)
+    fill_in "Title", with: ""
+    click_button "Save"
 
-  # descriptionを編集できる
-  it "edit title in the task"
-
-  # expire_atを編集できる
-  it "edit expire_at in the task"
-
-  # 編集後にflashメッセージが表示される
-  it "show flash message after edit"
+    expect(page).to have_content ("Title can't be blank")
+  end
 
   # タスク詳細が表示される
-  it "show task detail"
+  it "show task detail" do
+    task = FactoryBot.create(:task)
+    visit root_path
+    click_link task.title
 
+    expect(page).to have_content "Task Detail"
+    expect(page).to have_content task.title
+    expect(page).to have_content task.description
+    expect(page).to have_content task.expire_at
+  end
 
+  # タスクが削除できる
+  it "delete task" do
+    expect{
+      task = FactoryBot.create(:task)
+      visit root_path
+      click_link "Delete"
+    }.to change(Task, :count).by(0)
+    expect(page).to have_content "Task deleted!"
+  end
 
 end
