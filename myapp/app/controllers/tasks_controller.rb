c37dc873fe671913ae1fcc88bc411997478ecebb
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-14 14:52:49 +0900

    Remove explanatory variable and fix them to one line

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -47,8 +47,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
+    Task.find(params[:id]).destroy
     flash[:success] = 'delete success'
     redirect_to tasks_path
   end
