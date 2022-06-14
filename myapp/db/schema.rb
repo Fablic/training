23705fb9b2c09b4ce88384c569bcc46db409f7b4
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-14 13:58:19 +0900

    fix migration file order error

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_015746) do
+ActiveRecord::Schema.define(version: 2022_06_14_041047) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -32,4 +32,5 @@ ActiveRecord::Schema.define(version: 2022_06_14_015746) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
