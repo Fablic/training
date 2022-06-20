d8184d2d3fc65865520ff6d2cae457f10c6cea09
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-20 09:03:31 +0900

    create initial test code

---
+++ myapp/spec/system/tasks_spec.rb
@@ -0,0 +1,213 @@
+require 'rails_helper'
+
+RSpec.describe 'Task', type: :system do
+  before do
+    User.create!(id: 1, name: 'test', email: 'test@mail', password: 'password')
+  end
+
+  describe '#index' do
+    before do
+      visit root_path
+    end
+
+    context 'when user open this page' do
+      it "display default item" do
+        expect(page).to have_link 'Add Task'
+      end
+
+      it 'go to New Task page' do
+        click_on 'Add Task'
+        expect(current_path).to eq new_task_path
+      end
+
+      context 'when user has tasks' do
+        before do
+          Task.create!(name: '散歩', description: '多摩川を歩く', user_id: 1)
+          visit current_path
+        end
+
+        it "return user's task" do # rubocop:disable RSpec/MultipleExpectations
+          expect(page).to have_link '散歩'
+          expect(page).to have_link 'Edit'
+          expect(page).to have_link 'Delete'
+        end
+
+      end
+
+    end
+
+  end
+
+  describe '#show' do
+    let(:task) {Task.create!(name: '散歩', description: '多摩川を歩く', priority: 1, status: 1, limit: "2022-6-20".to_date, user_id: 1)}
+    before do
+      visit task_path(task)
+    end
+
+    it 'return task info' do # rubocop:disable RSpec/MultipleExpectations
+      expect(page).to have_content task.name
+      expect(page).to have_content task.description
+      expect(page).to have_content task.priority
+      expect(page).to have_content task.limit
+    end
+
+    context 'when user click Edit' do
+      it 'return edit task form' do
+        click_on 'Edit'
+        expect(current_path).to eq edit_task_path(task)
+      end
+
+    end
+# -----------------------------------------
+    context 'when user click Delete' do
+      it 'display confirmation dialog' do
+        # click_on 'Delete'
+        # expect {
+        #   expect(page.accept_confirm).to eq 'Do you want to delete #{task.name}?'
+        #   expect(page).to have_content "delete success"
+        # }.to change {Task.count}.by(-1)
+      end
+    end
+# ------------------------------------------
+
+    context 'when user click Home' do
+      it 'go to roog page' do
+        click_on 'Home'
+        expect(current_path).to eq root_path
+      end
+    end
+
+  end
+
+  describe '#new' do
+    before do
+      visit new_task_path
+    end
+
+    context 'when user go to this page' do
+      it 'display blank task form' do # rubocop:disable RSpec/MultipleExpectations
+        expect(page).to have_field 'Task title'
+        expect(page).to have_field 'Description'
+        expect(page).to have_field 'Limit'
+        expect(page).to have_field 'Status'
+        expect(page).to have_field 'Priority'
+      end
+    end
+
+  end
+
+  describe '#edit' do
+    let(:task) {Task.create!(name: '散歩', description: '多摩川を歩く', priority: 1, status: 1, limit: "2022-6-20".to_date, user_id: 1)}
+    before do
+      visit edit_task_path(task)
+      puts current_path
+    end
+
+    context 'when user go to this page' do
+      it 'display task form' do # rubocop:disable RSpec/MultipleExpectations
+        expect(page).to have_field 'Task title', with: task.name
+        expect(page).to have_field 'Description', with: task.description
+        expect(page).to have_field 'Limit', with: task.limit
+        expect(page).to have_field 'Status', with: task.status
+        expect(page).to have_field 'Priority', with: task.priority
+      end
+    end
+  end
+
+  describe '#create' do
+    before do
+      visit new_task_path
+      puts current_path
+    end
+
+    context 'when user go to Add task page' do
+      it 'display brank form' do # rubocop:disable RSpec/MultipleExpectations
+        expect(page).to have_field 'Task title'
+        expect(page).to have_field 'Description'
+        expect(page).to have_field 'Limit'
+        expect(page).to have_field 'Status'
+        expect(page).to have_field 'Priority'
+      end
+    end
+
+    context 'when user input task' do
+      before do
+        fill_in 'Task title', with: '散歩'
+        fill_in 'Description', with: '多摩川を歩く'
+        fill_in 'Limit', with: '2022-06-20'
+        select 'TODO', from: 'Status'
+        select 'Low', from: 'Priority'
+      end
+
+      it 'display input task info' do # rubocop:disable RSpec/MultipleExpectations
+        expect(page).to have_field 'Task title', with: '散歩'
+        expect(page).to have_field 'Description', with: '多摩川を歩く'
+        expect(page).to have_field 'Limit', with: '2022-06-20'
+        expect(page).to have_field 'Status', with: 1
+        expect(page).to have_field 'Priority', with: 1
+      end
+
+      context 'when user click "Create Task" button' do
+        it 'go to Task detail page' do # rubocop:disable RSpec/MultipleExpectations
+          click_on 'Create Task'
+          expect(page).to have_content '散歩'
+          expect(page).to have_content '多摩川を歩く'
+          expect(page).to have_content '2022-06-20'
+          expect(page).to have_content 1
+          expect(page).to have_content 1
+        end
+      end
+    end
+
+  end
+
+  describe '#update' do
+    let(:task) {Task.create!(name: '散歩', description: '多摩川を歩く', priority: 1, status: 1, limit: "2022-6-20".to_date, user_id: 1)}
+    before do
+      visit edit_task_path(task)
+    end
+
+    context 'when user edit task form' do
+      before do
+        fill_in 'Task title', with: '運動'
+        fill_in 'Description', with: '多摩川を走る'
+        fill_in 'Limit', with: '2022-06-20'
+        select 'IN PROGRESS', from: 'Status'
+        select 'Normal', from: 'Priority'
+      end
+
+      it 'display new task info' do # rubocop:disable RSpec/MultipleExpectations
+        expect(page).to have_field 'Task title', with: '運動'
+        expect(page).to have_field 'Description', with: '多摩川を走る'
+        expect(page).to have_field 'Limit', with: '2022-06-20'
+        expect(page).to have_field 'Status', with: 2
+        expect(page).to have_field 'Priority', with: 2
+      end
+
+      context 'when user click "Update Task" button' do
+        it 'go to Task detail page' do # rubocop:disable RSpec/MultipleExpectations
+          click_on 'Update Task'
+          expect(page).to have_content '運動'
+          expect(page).to have_content '多摩川を走る'
+          expect(page).to have_content '2022-06-20'
+          expect(page).to have_content 2
+          expect(page).to have_content 2
+        end
+      end
+
+    end
+
+  end
+
+  describe '#destroy' do
+    let(:task) {Task.create!(name: '散歩', description: '多摩川を歩く', priority: 1, status: 1, limit: "2022-6-20".to_date, user_id: 1)}
+    before do
+      visit root_path
+    end
+
+    context 'when user click Delete' do
+
+    end
+  end
+
+end
