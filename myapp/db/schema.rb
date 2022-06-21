5a520c9e2a117e3ef1cea6d1b4ab08109269710d
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-21 12:07:42 +0900

    User モデルの追加

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_21_001454) do
+ActiveRecord::Schema.define(version: 2022_06_21_024647) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2022_06_21_001454) do
     t.datetime "expire_at", precision: 6
     t.integer "status", default: 0
     t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -1,8 +1,18 @@
-
+# タスク
 100.times do |i|
 	Task.create(
 		title: "Rails研修step#{i}",
 		description: '研修の説明',
 		status: rand(0..2)
+	)
+end
+
+# ユーザー
+5.times do |i| 
+	name = Faker::Name.name
+	User.create(
+		name: name,
+		email: "rails#{i}@gmail.com",
+		password: 'password'
 	)
 end
