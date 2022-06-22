ee3adf530d0382d77e8ef9c058046c39e796d7e9
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-22 16:01:52 +0900

    fix task can not be deleted if it is in progress

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -5,4 +5,14 @@ class Task < ApplicationRecord
   validates :name, presence: true
   enum priority: {Low: 1, Normal: 2, High: 3}
   enum status: {TODO: 1, IN_PROGRESS: 2, DONE: 3}
+
+  before_destroy :ensure_not_in_progress
+
+  private
+  def ensure_not_in_progress
+    if IN_PROGRESS?
+      errors.add(:base, 'task is in progress')
+      throw :abort
+    end
+  end
 end
