bb3420a27be6f8f3bb249d307517483040caf0d0
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-22 13:50:36 +0900

    seedデータのタスクにユーザーIDを追加

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -1,14 +1,18 @@
+TASK_NUM = 100
+USER_NUM = 10
+
 # タスク
-100.times do |i|
+TASK_NUM.times do |i|
 	Task.create(
 		title: "Rails研修step#{i}",
 		description: '研修の説明',
-		status: rand(0..2)
+		status: rand(0..2),
+		user_id: i % USER_NUM
 	)
 end
 
 # ユーザー
-5.times do |i| 
+USER_NUM.times do |i| 
 	name = Faker::Name.name
 	User.create(
 		name: name,
