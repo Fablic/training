53500b03db57510ffb38b14f83c21d72b83e07eb
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-23 11:54:19 +0900

    do rubocop

--- myapp/spec/factories/task.rb
+++ myapp/spec/factories/task.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :task do
-    name {"散歩"}
-    description {"多摩川を歩く"}
-    priority {1}
-    status {1}
-    limit {"2022-6-20".to_date}
-    user_id {1}
+    name { '散歩' }
+    description { '多摩川を歩く' }
+    priority { 1 }
+    status { 1 }
+    limit { '2022-6-20'.to_date }
+    user_id { 1 }
   end
 end
--- myapp/spec/factories/user.rb
+++ myapp/spec/factories/user.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    id {1}
-    name {"test"}
-    email {"test@email"}
-    password {"password"}
+    id { 1 }
+    name { 'test' }
+    email { 'test@email' }
+    password { 'password' }
   end
 end
