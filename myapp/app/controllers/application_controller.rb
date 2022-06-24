32c65d8c1c6f36d5b0c6b8fa1a7b22c8eb4f7665
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-24 17:55:16 +0900

    check rubocop

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -2,9 +2,9 @@
 
 class ApplicationController < ActionController::Base
   # error handle
-  rescue_from Exception, with: :_render_500
-  rescue_from ActiveRecord::RecordNotFound, with: :_render_404
-  rescue_from ActionController::RoutingError, with: :_render_404
+  rescue_from Exception, with: :_render500
+  rescue_from ActiveRecord::RecordNotFound, with: :_render404
+  rescue_from ActionController::RoutingError, with: :_render404
 
   def routing_error
     raise ActionController::RoutingError, params[:path]
@@ -12,12 +12,12 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def _render_404(error = nil)
+  def _render404(error = nil)
     logger.info "Rendering 404 with excaption: #{error.message}" if error
     render 'errors/404.html', status: :not_found
   end
 
-  def _render_500(error = nil)
+  def _render500(error = nil)
     logger.info "Rendering 500 with exception: #{error.message}" if error
     render 'errors/500.html', status: :internal_server_error
   end
