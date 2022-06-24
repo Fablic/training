c1120a8fca47ddd1a8abb13e5311c416539d63be
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-24 14:51:12 +0900

    add validates

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_secure_password
 
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password_digest, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 30, allow_blank: true }, uniqueness: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true, length: { minimum: 8 }, allow_nil: true
 end
