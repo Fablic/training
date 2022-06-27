da92486493723fa42adeb7cc19241e78fdaa5818
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-27 14:18:43 +0900

    tab->スペースに変更

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -9,9 +9,9 @@ USER_NUM.times do |i|
     password: 'password',
   )
   TASK_NUM.times do |j|
-	  user.tasks.create(
-    title: "#{user.name}-Rails研修step#{j}",
-    description: '研修の説明',
+    user.tasks.create(
+      title: "#{user.name}-Rails研修step#{j}",
+      description: '研修の説明',
       status: rand(0..2),
     )
   end
