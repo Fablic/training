52234eb93731b794c0c6991e6e755dc9913fcc5a
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-27 14:40:40 +0900

    add nil title test

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -2,6 +2,25 @@
 
 require 'rails_helper'
 
-RSpec.describe Task, type: model do
+RSpec.describe Task, type: :model do
+  before do
+    create(:user)
+  end
 
+  describe 'validation test' do
+    context 'when valid input' do
+      let(:task) { create(:task) }
+
+      it 'addition is success' do
+        expect(task).to be_valid
+      end
+    end
+
+    context 'when entering title column' do
+      it 'failure if nil' do
+        task = build(:task, name: nil)
+        expect(task.valid?).to be false
+      end
+    end
+  end
 end
