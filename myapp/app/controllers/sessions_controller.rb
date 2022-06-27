c0b09e039f973020ce8bbe7b2a330c67a4124b94
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-27 10:24:31 +0900

    login失敗時のルーティングを修正

--- myapp/app/controllers/sessions_controller.rb
+++ myapp/app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to root_path
     else
       flash.now[:danger] = 'Invalid email or password'
-      render new
+      render 'new'
     end
   end
 
