dccc5316bedf5e4348de0dfdc32a5d0db4eb6699
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-28 12:14:02 +0900

    delete comments

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :name, presence: true, length: { maximum: 30, allow_blank: true }, uniqueness: true # TODO: Rails/UniqueValidationWithoutIndex step11で直す
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } # TODO: Rails/UniqueValidationWithoutIndex step11で直す
-  validates :password_digest, presence: true, length: { minimum: 8 }, allow_nil: true
+  validates :name, presence: true, length: { maximum: 30, allow_blank: true }, uniqueness: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
