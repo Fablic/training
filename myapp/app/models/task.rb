0bbfb3bdcaa5e5f0abb69409194589f95a21bc73
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-28 17:19:31 +0900

    add scope for sort

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -5,4 +5,6 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2, maximum: 32 }
   enum priority: { Low: 1, Normal: 2, High: 3 }
   enum status: { TODO: 1, IN_PROGRESS: 2, DONE: 3 }
+
+  scope :sort_limit, -> { order(limit: 'ASC') }
 end
