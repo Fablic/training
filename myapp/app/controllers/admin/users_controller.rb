a6808d187a6b1fb3e08f66375a4a9a45db07f75b
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-29 13:57:11 +0900

    ユーザーのCRUDを実装

--- myapp/app/controllers/admin/users_controller.rb
+++ myapp/app/controllers/admin/users_controller.rb
@@ -11,21 +11,46 @@ class Admin::UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    return render 'new' unless @user.save
+
+    flash[:success] = 'User created'
+    redirect_to admin_users_path
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+
+    return render 'edit' unless @user.update(user_params)
+
+    flash[:success] = 'User updated!'
+    redirect_to admin_users_path
   end
 
   def destroy
+    # 自分を指定しても消せないようにする
+    return redirect_to admin_users_path if params[:id].to_i == session[:user_id]
+
+    User.find(params[:id]).destroy
+
+    flash[:success] = 'User deleted!'
+    redirect_to admin_users_path
   end
 
   private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+  end
+
   def admin_user
     redirect_to root_path unless admin?
   end
