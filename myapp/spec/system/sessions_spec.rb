7832ad3e6eb9d11246f4d1e2625cd18412aafc78
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-06-29 22:21:17 +0900

    rubocopによる整形と一部リファクタリング

--- myapp/spec/system/sessions_spec.rb
+++ myapp/spec/system/sessions_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :system do
-  let!(:user) {
+RSpec.describe 'Sessions', type: :system do
+  let!(:user) do
     FactoryBot.create(:user)
-  }
+  end
 
   describe 'ログイン画面', type: :system do
     context '正しいemailとpasswordの組み合わせのとき' do
@@ -48,8 +48,8 @@ RSpec.describe "Sessions", type: :system do
     context '2人のユーザーがタスクを作成した時' do
       example '自分のタスクのみが表示' do
         another_user = FactoryBot.create(:user, email: 'anothor@example.com')
-        task1 = FactoryBot.create(:task, title: 'title1', user_id: user.id)
-        task2 = FactoryBot.create(:task, title: 'title2', user_id: another_user.id)
+        FactoryBot.create(:task, title: 'title1', user_id: user.id)
+        FactoryBot.create(:task, title: 'title2', user_id: another_user.id)
         login(user)
         expect(page).to have_content 'title1'
         expect(page).not_to have_content 'title2'
