fe8180866fff98cef387a7786a4eceedd3820c7d
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-06-29 10:51:31 +0900

    rename password_digest to password

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :name, presence: true, length: { maximum: 30, allow_blank: true }, uniqueness: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password_digest, presence: true, length: { minimum: 8 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
 end
