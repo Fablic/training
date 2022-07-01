2f44501a282799f0da0ee21aa47134f3d70e46b2
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-01 15:04:14 +0900

    create search method

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -59,6 +59,11 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def search
+    @tasks = Task.where('name LIKE ?', "%#{params[:keyword]}%")
+    render 'index'
+  end
+
   private
 
   def task_params
