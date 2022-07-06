a5c4638b258b262d3f52b3abdcc544df5195d7af
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-06 13:45:10 +0900

    add test code for compound search by keyword and status

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -83,25 +83,38 @@ RSpec.describe 'Task', type: :system do
 
     context 'when use search function' do
       before do
-        create(:task, name: '洗濯', description: 'コインランドリーに行く', priority: 'Low', status: 'TODO', limit: Time.new(2022, 1, 10).in_time_zone, created_at: Time.new(2021, 12, 1).in_time_zone)
+        create(:task, name: '洗濯1', description: 'コインランドリーに行く', priority: 'Low', status: 'TODO', limit: Time.new(2022, 1, 10).in_time_zone, created_at: Time.new(2021, 12, 1).in_time_zone)
+        create(:task, name: '洗濯2', description: 'クリーニング屋に行く', priority: 'High', status: 'DONE', limit: Time.new(2022, 1, 10).in_time_zone, created_at: Time.new(2021, 12, 1).in_time_zone)
         create(:task, name: '掃除', description: '洗面所掃除する', priority: 'Normal', status: 'IN_PROGRESS', limit: Time.new(2022, 1, 2).in_time_zone, created_at: Time.new(2021, 12, 2).in_time_zone)
         create(:task, name: '買い物', description: '柔軟剤買う', priority: 'High', status: 'DONE',  limit: Time.new(2022, 1, 30).in_time_zone, created_at: Time.new(2021, 12, 3).in_time_zone)
         visit current_path
       end
 
-      it 'can search using the search form' do # rubocop:disable RSpec/MultipleExpectations
+      it 'can search using the keyword' do # rubocop:disable RSpec/MultipleExpectations
         fill_in 'keyword', with: '洗濯'
         click_on '検索'
-        expect(page).to have_content '洗濯'
+        expect(page).to have_content '洗濯1'
+        expect(page).to have_content '洗濯2'
         expect(page).not_to have_content '掃除'
         expect(page).not_to have_content '買い物'
       end
 
-      it 'can search using status button' do # rubocop:disable RSpec/MultipleExpectations
+      it 'can search using status' do # rubocop:disable RSpec/MultipleExpectations
         select '着手中', from: 'status'
         click_on '検索'
-        expect(page).not_to have_content '洗濯'
+        expect(page).not_to have_content '洗濯1'
+        expect(page).not_to have_content '洗濯2'
         expect(page).to have_content '掃除'
+        expect(page).not_to have_content '買い物'
+      end
+
+      it 'can search using keyword and status' do # rubocop:disable RSpec/MultipleExpectations
+        fill_in 'keyword', with: '洗濯'
+        select '未着手', from: 'status'
+        click_on '検索'
+        expect(page).to have_content '洗濯1'
+        expect(page).not_to have_content '洗濯2'
+        expect(page).not_to have_content '掃除'
         expect(page).not_to have_content '買い物'
       end
     end
