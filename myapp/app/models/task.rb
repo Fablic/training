3395c76c57087c84e25909d845b55407995d4070
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-06 10:57:36 +0900

    add new scope to task model

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -6,6 +6,9 @@ class Task < ApplicationRecord
   enum priority: { Low: 1, Normal: 2, High: 3 }
   enum status: { TODO: 1, IN_PROGRESS: 2, DONE: 3 }
 
+  scope :sorted, -> { order(created_at: 'DESC') }
   scope :sort_limit_asc, -> { order(limit: 'ASC') }
   scope :sort_limit_desc, -> { order(limit: 'DESC') }
+  scope :name_or_description, -> (keyword) { where('name LIKE ? OR description LIKE ?', "%#{keyword}%", "%#{keyword}%") }
+  scope :status, -> (status) { where(status: status) }
 end
