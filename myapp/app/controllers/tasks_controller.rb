28a974734fb159ed56a9378965ea78218705515f
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-07 14:23:53 +0900

    fix conflict

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -2,11 +2,7 @@
 
 class TasksController < ApplicationController
   def index
-<<<<<<< HEAD
-    @tasks = Task.sort_created_desc.page(params[:page])
-=======
     @tasks = Task.search(status: params[:status], keyword: params[:keyword], sort: params[:sort], direction: params[:direction])
->>>>>>> 9b03fd7e5149818bbca335777b2896adaebd0fff
   end
 
   def show
@@ -54,35 +50,6 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-<<<<<<< HEAD
-  def search # rubocop:disable all
-    sort = params[:sort]
-    tasks = if sort.present?
-              case sort
-              when 'limit'
-                Task.sort_limit_asc
-              when '-limit'
-                Task.sort_limit_desc
-              else
-                Task.sort_created_desc
-              end
-            else
-              Task.sort_created_desc
-            end
-
-    if params[:keyword].present?
-      tasks = tasks.name_or_description(params[:keyword])
-    end
-    if params[:status].present?
-      tasks = tasks.status(params[:status])
-    end
-
-    @tasks = tasks.page(params[:page])
-    render 'index'
-  end
-
-=======
->>>>>>> 9b03fd7e5149818bbca335777b2896adaebd0fff
   private
 
   def task_params
