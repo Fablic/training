41e232ba0ff63d23bcc34a3d6529556e6235827a
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-12 11:40:04 +0900

    add create and destroy method

--- myapp/app/controllers/sessions_controller.rb
+++ myapp/app/controllers/sessions_controller.rb
@@ -5,8 +5,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to login_path
   end
 end
