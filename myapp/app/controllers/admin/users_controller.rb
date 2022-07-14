20ac419301c8b21001171c09c149d8b3632406c5
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-14 16:21:57 +0900

    add create and edit fucntion

--- myapp/app/controllers/admin/users_controller.rb
+++ myapp/app/controllers/admin/users_controller.rb
@@ -7,9 +7,41 @@ class Admin::UsersController < ApplicationController # rubocop:disable Style/Cla
     @users = User.all.order(created_at: :desc)
   end
 
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def admin_user
     redirect_to(root_path) unless current_user.admin?
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
