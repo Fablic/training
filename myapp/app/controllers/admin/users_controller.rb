c9078dcfa643792765ed9622a137077b916731db
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-14 14:39:28 +0900

    add index method to admin/users_controller.rb

--- myapp/app/controllers/admin/users_controller.rb
+++ myapp/app/controllers/admin/users_controller.rb
@@ -1,2 +1,15 @@
-class Admin::UsersController < ApplicationController
+# frozen_string_literal: true
+
+class Admin::UsersController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
+  before_action :admin_user
+
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
+
+  private
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 end
