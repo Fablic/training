cff38b31845d42fdfa9dd04b79cbb4930f83622d
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-15 14:04:46 +0900

    add admin column and index

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_062236) do
+ActiveRecord::Schema.define(version: 2022_07_14_065827) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -31,7 +31,9 @@ ActiveRecord::Schema.define(version: 2022_07_01_062236) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name", "email"], name: "index_users_on_name_and_email", unique: true
+    t.boolean "admin"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "tasks", "users"
