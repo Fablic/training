aa81638a77582f66cd9040908785c2691f7fe0cb
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-20 14:54:22 +0900

    create label model and labels table

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_065827) do
+ActiveRecord::Schema.define(version: 2022_07_20_055136) do
+
+  create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
