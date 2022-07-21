b8b2af9c2040bcf822a07cda27ad3b34608589be
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-21 13:43:08 +0900

    add line break to @tasks

--- myapp/app/controllers/admin/users_controller.rb
+++ myapp/app/controllers/admin/users_controller.rb
@@ -10,7 +10,11 @@ module Admin
 
     def show
       @user = User.find(params[:id])
-      @tasks = Task.search(user_id: @user.id, status: params[:status], keyword: params[:keyword], sort: params[:sort], direction: params[:direction]).page(params[:page])
+      @tasks = Task.search(user_id: @user.id,
+                           status: params[:status],
+                           keyword: params[:keyword],
+                           sort: params[:sort],
+                           direction: params[:direction]).page(params[:page])
     end
 
     def new
