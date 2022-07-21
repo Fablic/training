949ec4ad9c3d0b91ebb4aeeb101801cd859db5ba
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-21 16:43:54 +0900

    add test case for users_controller.rb

--- myapp/spec/system/users_spec.rb
+++ myapp/spec/system/users_spec.rb
@@ -3,9 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
+  let(:user) { create(:user, name: 'test1', email: 'test1@gmail.com', admin: true) }
+
   before do
-    driven_by(:rack_test)
+    login(user: user)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe '#show' do
+    it 'display user info', :aggregate_failures do
+      visit user_path(user.id)
+      expect(page).to have_content 'test1'
+      expect(page).to have_content 'test1@gmail.com'
+    end
+  end
 end
