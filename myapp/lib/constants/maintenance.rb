055a300ee7299158ae8e539185f82de45b8154af
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-27 11:28:26 +0900

    create maintenance runner script

---
+++ myapp/lib/constants/maintenance.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Constants
+  class Maintenance
+    def self.on
+      maintenance_mode = Constant.find_by(key: 'maintenance_mode')
+      maintenance_mode.update(value: true)
+    end
+
+    def self.off
+      maintenance_mode = Constant.find_by(key: 'maintenance_mode')
+      maintenance_mode.update(value: false)
+    end
+  end
+end
