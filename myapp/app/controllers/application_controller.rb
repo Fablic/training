44a00dcf607f41d479bab351a6caf64fb7238880
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-27 14:47:18 +0900

    fix maintenance_mode? method to return false when maintenance_mode is false

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -34,6 +34,8 @@ class ApplicationController < ActionController::Base
   def maintenance_mode?
     maintenance_mode = Constant.find_by(key: 'maintenance_mode')
     return true if maintenance_mode.value
+
+    false
   end
 
   def _render503(error = nil)
