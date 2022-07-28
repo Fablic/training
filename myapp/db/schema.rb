5d826d9935bd6e422510098aec31c41adec4668d
Wataru Okamoto <wataru.okamoto@rakuten.com>
2022-07-28 15:02:16 +0900

    add constants table

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_21_064221) do
+ActiveRecord::Schema.define(version: 2022_07_27_060410) do
+
+  create_table "constants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "value", default: "0", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
