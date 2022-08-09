2e2467d8237edf3f9038192af1a1d0d62d810f64
nanae-kusakari <nanae.kusakari@connec10.co.jp>
2022-08-09 10:59:57 +0900

    Step7_一覧、詳細、登録画面作成

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,13 +1,33 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :delete]
+
   def index
+    @tasks = Task.all
   end
 
   def show
+
   end
 
   def new
+    @task = Task.new()
+  end
+
+  def create
+    task = Task.new(task_param)
+    task.save!
+    redirect_to tasks_url, notice: 'タスク「#{task.name}」を登録しました。'
   end
 
   def edit
   end
+
+  private
+    def task_param
+      params.require(:task).permit(:name, :detail, :status, :priority)
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
 end
