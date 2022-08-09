f41452dcb4365b1362597ebd9628730177a377e8
miu-tsuzuki <miu.tsuzuki@connec10.co.jp>
2022-08-09 14:43:27 +0900

    step6 制約追加

--- myapp/db/migrate/20220808084114_create_tasks.rb
+++ myapp/db/migrate/20220808084114_create_tasks.rb
@@ -1,11 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.string :title
+      t.string :title, limit: 128, null: false, default: ''
       t.text :description
-      t.integer :user_id
-      t.string :status
-      t.string :label
+      t.integer :user_id, limit: 8, null: false
+      t.string :status, limit: 1, null: false, default: '0'
+      t.string :label, limit: 64
       t.datetime :deleted_at
 
       t.timestamps
