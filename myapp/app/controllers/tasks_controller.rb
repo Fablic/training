f1b34928e21a8dba6b0110d7aa56dabc37c60dc4
nanae-kusakari <nanae.kusakari@connec10.co.jp>
2022-08-10 11:17:04 +0900

    Step7_編集、削除機能作成とその他画面の修正

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :delete]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-
   end
 
   def new
@@ -14,16 +13,41 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_param)
-    task.save!
-    redirect_to tasks_url, notice: 'タスク「#{task.name}」を登録しました。'
+    @task = Task.new(task_params)
+    @task.save!
+    
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。" }
+      format.json { render :show, status: :ok, location: @task }
+    end
   end
 
   def edit
   end
 
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to task_url, notice: "タスク「#{@task.name}」を更新しました。" }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json, @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。" }
+      format.json { head :no_content }
+    end
+  end
+
   private
-    def task_param
+    def task_params
       params.require(:task).permit(:name, :detail, :status, :priority)
     end
 
