3c1b4aa49e2288f05c0b5a11cc6618b6219c1886
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-08-10 11:45:47 +0900

    STEP7 更新機能の作成

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -11,10 +11,17 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def create
     @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
     redirect_to tasks_path
   end
 
