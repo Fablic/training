4b168ca032bc99f1ada2f1e62004b257e4a8c66d
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-08-10 10:52:38 +0900

    STEP7 作成画面の作成

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,13 +1,26 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
--- myapp/db/migrate/20220808071509_create_tasks.rb
+++ myapp/db/migrate/20220808071509_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :title, limit: 128, null: false, default: ''
       t.text :description, null: false
-      t.integer :user_id, limit: 8, null: false
+      t.integer :user_id, limit: 8
       t.string :status, limit: 1, null: false, default: '0'
       t.string :label, limit: 64
       t.datetime :deleted_at
