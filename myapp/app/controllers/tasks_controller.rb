39203f850f9fe388e41f10c691aae41ccde890da
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-08-19 12:24:28 +0900

    STEP7 再レビュー修正

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -18,38 +20,31 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.create(task_params)
+    @task = Task.new(task_params)
     @task.user_id = 1
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to tasks_url, notice: "タスク「#{@task.title}」を登録しました。" }
-      else
-        format.html { render :new }
-      end
+    if @task.save
+      redirect_to tasks_url, notice: "タスク「#{@task.title}」を登録しました。"
+    else
+      render :new
     end
   end
 
   def update
-    @task = Task.find(params[:id])
-    @task.update(task_params)
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to tasks_url, notice: "タスク「#{@task.title}」を更新しました。" }
-      else
-        format.html { render :new }
-      end
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク「#{@task.title}」を更新しました。"
+    else
+      render :new
     end
   end
 
   def destroy
     @task = Task.find(params[:id])
-    respond_to do |format|
-      if @task.destroy
-        format.html { redirect_to tasks_url, notice: "タスク「#{@task.title}」を削除しました。" }
-      else
-        format.html { render :show }
-      end
+
+    if @task.destroy
+      redirect_to tasks_url, notice: "タスク「#{@task.title}」を削除しました。"
+    else
+      render :show
     end
   end
 
@@ -57,5 +52,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
