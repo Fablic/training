960d4f725c66b1b9b2b14fe0506bbb11cf34219f
kohei-matsubara <110374166+kohei-matsubara@users.noreply.github.com>
2022-08-19 13:41:33 +0900

    ステップ7: タスクを登録・更新・削除できるようにしよう (#1362)
    * envへプロジェクト名記載
    
    * rails new実行
    
    * STEP3 datebase.yml 書き換え
    
    * STEP5 READMEにモデル図を作成
    
    * STEP6  model作成
    
    * STEP6 DBのカラム制約追加
    
    * STEP6 DB制約の修正
    
    * STEP7 一覧ページ作成、bootstrapの組み込み
    
    * STEP7 作成画面の作成
    
    * STEP7 更新機能の作成
    
    * STEP7 削除機能の作成
    
    * STEP7 作成画面のボタンデザインと一覧へ戻るリンクの位置修正
    
    * STEP7 詳細画面の作成
    
    * STEP7 flushメッセージの作成、デザイン成形
    
    * STEP7 削除ボタンにconfirm追加
    
    * STEP7 RuboCop実行
    
    * STEP7 bodyのマージン変更
    
    * 競合解消
    
    * STEP7修正 bootstrap該当箇所削除
    
    * STEP7 gemfileの修正
    
    * STEP7 ja.ymlの削除
    
    * STEP7 format .json{}の削除
    
    * STEP7 aタグをlink_toタグに修正
    
    * STEP7 create_tasksの修正戻し
    
    * STEP7 user_idに仮の値を追加する処理
    
    * STEP7 最後の行に改行を設定
    
    * STEP7 gemfile.lockのjbuilderを削除
    
    * STEP7 再レビュー修正
    
    * STEP7 レビュー修正 before_action追加によるfindの削除

---
+++ myapp/app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.user_id = 1
+
+    if @task.save
+      redirect_to tasks_url, notice: "タスク「#{@task.title}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク「#{@task.title}」を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if @task.destroy
+      redirect_to tasks_url, notice: "タスク「#{@task.title}」を削除しました。"
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
