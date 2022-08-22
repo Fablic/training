a9a08e257a1ea25403a6581bd95c9f77ea230b59
nanae-kusakari <nanae.kusakari@connec10.co.jp>
2022-08-22 09:39:43 +0900

    Step11_バリデーション追加

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
   enum status: { not_started: 1, in_progress: 2, completed: 3 }
   enum priority: { low: 1, middle: 2, high: 3 }
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validates :detail, presence: true
+  validates :detail, length: { maximum: 100 }
 end
---
+++ myapp/spec/models/task_spec.rb
@@ -0,0 +1,12 @@
+# describe 'Taskモデルテスト' do
+#     context 'タスク名が空欄' do
+#       before do
+
+
+#       end
+
+#       it '空欄だと登録できないこと' do
+#         expect(task_nil).to be_valid
+#       end
+#     end
+#   end
