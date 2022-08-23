a661fd356b33816a9545b2e87d92c9a9f5ec083b
nanae-kusakari <nanae.kusakari@connec10.co.jp>
2022-08-23 17:50:16 +0900

    Step11_モデルのテスト追加

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -1,12 +1,98 @@
-# describe 'Taskモデルテスト' do
-#     context 'タスク名が空欄' do
-#       before do
+require 'rails_helper'
 
+describe 'Taskモデル', type: :model do
+  describe 'バリデーション' do
+    describe 'タスク名カラム' do
+      context '30文字で入力されている場合' do
+        task = FactoryBot.create(:task, name: 'あいうえおあいうえおあいうえおあいうえおあいうえおあいうえお')
+        it '登録できる' do
+          expect(task).to be_valid
+        end
+      end
 
-#       end
+      context '空の場合' do
+        task = FactoryBot.build(:task, name: '')
+        it '無効である' do
+          expect(task).to be_invalid
+        end
+      end
 
-#       it '空欄だと登録できないこと' do
-#         expect(task_nil).to be_valid
-#       end
-#     end
-#   end
+      context 'nilの場合' do
+        task = FactoryBot.build(:task, name: nil)
+        it '無効である' do
+          expect(task).to be_invalid
+        end
+      end
+
+      context '31文字以上の場合' do
+        task = FactoryBot.build(:task, name: 'あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあ')
+        it '無効である' do
+          expect(task).to be_invalid
+        end
+      end
+    end
+
+    describe '詳細カラム' do
+      context '100文字で入力されている場合' do
+        it '有効である' do
+          task = FactoryBot.create(:task, detail: '１いうえおあいうえお２いうえおあいうえお３いうえおあいうえお４いうえお'\
+            'あいうえお５いうえおあいうえお６いうえおあいうえお７いうえおあいうえお８いうえおあいうえお９いうえおあいうえお０いうえおあいうえお')
+          expect(task).to be_valid
+        end
+      end
+
+      context '空の場合' do
+        it '無効である' do
+          task = FactoryBot.build(:task, detail: '')
+          expect(task).to be_invalid
+        end
+      end
+
+      context 'nilの場合' do
+        it '無効である' do
+          task = FactoryBot.build(:task, detail: nil)
+          expect(task).to be_invalid
+        end
+      end
+
+      context '101文字以上の場合' do
+        it '無効である' do
+          task = FactoryBot.build(:task, detail: '１いうえおあいうえお２いうえおあいうえお３いうえおあいうえお４いうえおあいうえお５いうえおあいうえお６いうえおあいうえお７いうえおあいうえお８いうえおあいうえお９いうえおあいうえお０いうえおあいうえお１')
+          expect(task).to be_invalid
+        end
+      end
+    end
+
+    describe 'ステータスカラム' do
+      context '入力されている場合' do
+        it '有効である' do
+          task = FactoryBot.create(:task)
+          expect(task).to be_valid
+        end
+      end
+
+      context 'nilの場合' do
+        it '有効である' do
+          task = FactoryBot.create(:task, status: nil)
+          expect(task).to be_valid
+        end
+      end
+    end
+
+    describe '優先度カラム' do
+      context '入力されている場合' do
+        task = FactoryBot.create(:task)
+        it '有効である' do
+          expect(task).to be_valid
+        end
+      end
+
+      context 'nilの場合' do
+        task = FactoryBot.create(:task, priority: nil)
+        it '有効である' do
+          expect(task).to be_valid
+        end
+      end
+    end
+  end
+end
