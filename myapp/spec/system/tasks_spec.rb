e18b8b0dc2ae05a312e8cd0c38f5450db95f2470
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-08-24 15:33:03 +0900

    STEP8:Rv修正

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -44,6 +44,25 @@ describe 'タスク管理機能', type: :system do
       end
     end
 
+    describe '削除機能' do
+      subject(:visit_task_a) { visit task_path(task_a) }
+      context '削除ボタンをクリックした場合' do
+        it 'タスクが正常に削除される' do
+          visit_task_a
+          # DBの該当データが削除される
+          expect { click_on('削除') }.to change(Task, :count).by(-1)
+          expect(Task.find_by(title: task_a.title)).to be_nil
+        end
+
+        it 'Flashメッセージが表示される' do
+          visit_task_a
+          click_on('削除')
+          # Flashメッセージが表示される
+          expect(page).to have_selector '.alert-success', text: "タスク「#{task_a.title}」を削除しました。"
+        end
+      end
+    end
+
     describe '画面遷移機能' do
       context '詳細ボタンをクリックした場合' do
         let!(:task_1) { task_a }
@@ -81,136 +100,114 @@ describe 'タスク管理機能', type: :system do
         end
       end
     end
+
+    describe '削除機能' do
+      context '削除ボタンをクリックした場合' do
+        it 'タスクが正常に削除される' do
+          visit_task_a
+          # DBの該当データが削除される
+          expect { click_on('削除') }.to change(Task, :count).by(-1)
+          expect(Task.find_by(title: task_a.title)).to be_nil
+        end
+
+        it 'Flashメッセージが表示される' do
+          visit_task_a
+          click_on('削除')
+          # Flashメッセージが表示される
+          expect(page).to have_selector '.alert-success', text: "タスク「#{task_a.title}」を削除しました。"
+        end
+      end
+    end
   end
 
   describe '新規登録機能' do
-    before do
-      visit new_task_path
-      fill_in 'textarea1', with: title
-      fill_in 'textarea2', with: description
-    end
+    subject(:visit_new_task){ visit new_task_path }
 
-    context 'タスクの内容を入力した場合' do
-      let(:title) { '新規作成のテスト' }
-      let(:description) { '新規作成のテストを書く' }
+    describe '登録機能' do
+      context 'タスクの内容を入力した場合' do
+        let(:name) { '新規作成のテスト' }
+        let(:description) { '新規作成のテストを書く' }
+        let(:status) { 'not_started' }
+        let(:label) { 'low' }
 
-      it 'タスクが正常に作成される' do
-        # DBに登録されている
-        expect { click_button 'submit' }.to change(Task, :count).by(1)
-        # 画面で入力された内容でDBに登録されている
-        task = Task.find_by(title: title)
-        expect(task.title).to eq(title)
-        expect(task.description).to eq(description)
-        # Flashメッセージが表示される
-        expect(page).to have_selector '.alert-success', text: '新規作成のテスト'
-        # タスク一覧画面が表示される
-        expect(current_path).to eq tasks_path
+        it 'タスクが正常に作成される' do
+          # タスク内容入力
+          visit_new_task
+          fill_in 'textarea1', with: title
+          fill_in 'textarea2', with: description
+          # DBに登録されている
+          expect { click_button 'submit' }.to change(Task, :count).by(1)
+          # 画面で入力された内容でDBに登録されている
+          expect(Task.find_by(title: title, description: description)).not_to be_nil
+        end
+
+        it 'Flashメッセージが表示される' do
+          # タスク内容入力
+          visit_new_task
+          fill_in 'textarea1', with: title
+          fill_in 'textarea2', with: description
+
+          visit_new_task
+          click_button 'submit'
+          expect(page).to have_selector '.alert-success', text: "タスク「#{title}」を登録しました。"
+        end
+
+        it '一覧画面が表示される' do
+          # タスク内容入力
+          visit_new_task
+          fill_in 'textarea1', with: title
+          fill_in 'textarea2', with: description
+
+          visit_new_task
+          click_button 'submit'
+          expect(page).to have_current_path tasks_path
+        end
       end
     end
   end
 
   describe '編集機能' do
-    before do
-      visit edit_task_path(task_a)
-    end
+    subject(:visit_task_a_edit){visit edit_task_path(task_a)}
 
-    context 'タスクの各項目を更新した場合' do
-      let(:title) { '新規作成のテスト２' }
-      let(:description) { '新規作成のテストを書く２' }
-
-      it 'タスクが正常に更新される' do
-        # 画面表示時に編集前のタスク内容が各項目に表示されている
-        expect(page).to have_field 'textarea1', with: task_a.title
-        expect(page).to have_field 'textarea2', with: task_a.description
-        # 更新処理
-        fill_in 'textarea1', with: title
-        fill_in 'textarea2', with: description
-        click_button 'submit'
-        # 画面で入力された内容でDBのデータが更新されている
-        task = Task.find_by(title: title)
-        expect(task.title).to eq(title)
-        expect(task.description).to eq(description)
-        # Flashメッセージが表示される
-        expect(page).to have_selector '.alert-success', text: '新規作成のテスト２'
-      end
-    end
-  end
-
-  describe '削除機能' do
-    before do
-      visit task_path(task_a)
-    end
-
-    context 'タスクを削除した場合' do
-      it 'タスクが正常に削除される' do
-        # DBの該当データが削除される
-        expect { click_on('削除') }.to change(Task, :count).by(-1)
-        expect(Task.find_by(title: task_a.title)).to be nil
-
-        # Flashメッセージが表示される
-        expect(page).to have_selector '.alert-success', text: '削除しました'
-      end
-    end
-  end
-
-  describe 'ページ遷移' do
-    context 'タスク一覧画面から詳細画面へ遷移' do
-      before do
-        visit tasks_path
-        click_link '詳細リンク', match: :first
-      end
-      it '詳細画面へ遷移できること' do
-        expect(current_path).to eq task_path(task_a)
+    describe '表示機能' do
+      context '画面を表示した場合' do
+        it '編集前のタスク名が表示される' do
+          visit_task_a_edit
+          expect(page).to have_field 'textarea1', with: task_a.title
+        end
+        it '編集前の詳細が表示される' do
+          visit_task_a_edit
+          expect(page).to have_field 'textarea2', with: task_a.description
+        end
       end
     end
 
-    context '一覧画面から新規登録画面へ遷移' do
-      before do
-        visit tasks_path
-        click_link '新規登録'
-      end
-      it '新規登録画面へ遷移できる' do
-        expect(current_path).to eq new_task_path
-      end
-    end
+    describe '更新機能' do
+      context 'タスクの各項目を更新した場合' do
+        let(:title) { '新規作成のテスト２' }
+        let(:description) { '新規作成のテストを書く２' }
+        let(:status) { 'in_progress' }
+        let(:label) { 'middle' }
 
-    context '新規登録画面からタスク一覧画面へ遷移' do
-      before do
-        visit new_task_path
-        click_link '一覧に戻る'
-      end
-      it '一覧画面へ遷移できる' do
-        expect(current_path).to eq tasks_path
-      end
-    end
+        it 'タスクが更新される' do
+          visit_task_a_edit
+          # 更新処理
+          fill_in 'textarea1', with: title
+          fill_in 'textarea2', with: description
+          click_button 'submit'
+          # 画面で入力された内容でDBのデータが更新されている
+          expect(Task.find_by(title: title, description: description)).not_to be_nil
+        end
 
-    context '詳細画面からタスク一覧画面へ遷移' do
-      before do
-        visit task_path(task_a)
-        click_link '一覧に戻る'
-      end
-      it '一覧画面へ遷移できる' do
-        expect(current_path).to eq tasks_path
-      end
-    end
-
-    context '詳細画面から編集画面へ遷移' do
-      before do
-        visit task_path(task_a)
-        click_link '編集'
-      end
-      it '編集画面へ遷移できる' do
-        expect(current_path).to eq edit_task_path(task_a)
-      end
-    end
-
-    context '編集画面から一覧画面へ遷移' do
-      before do
-        visit edit_task_path(task_a)
-        click_link '一覧に戻る'
-      end
-      it '詳細画面へ遷移できる' do
-        expect(current_path).to eq tasks_path
+        it 'Flashメッセージが表示される' do
+          visit_task_a_edit
+          # 更新処理
+          fill_in 'textarea1', with: title
+          fill_in 'textarea2', with: description
+          click_button 'submit'
+          # Flashメッセージが表示される
+          expect(page).to have_selector '.alert-success', text: "タスク「#{title}」を更新しました。"
+        end
       end
     end
   end
