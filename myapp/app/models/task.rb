77c544e273ba7cb2313f4104e1abc492cbd19cf1
kazuma.gushiken <kazuma.gushiken@connec10.co.jp>
2022-08-25 12:40:39 +0900

    Step11（モデルのバリデーションとテストを追加）

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 10 }
+  validates :description, presence: true
+  validates :description, length: { maximum: 50 }
 end
---
+++ myapp/spec/models/task_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+describe 'Taskモデル', type: :model do
+  describe 'バリデーション' do
+    describe 'タスク名' do
+      context '10文字で入力されている場合' do
+        task = FactoryBot.create(:task, name: 'あいうえおあいうえお')
+        it '登録できる' do
+          expect(task).to be_valid
+        end
+      end
+
+      context '空の場合' do
+        task = FactoryBot.build(:task, name: '')
+        it '無効である' do
+          expect(task).to be_invalid
+        end
+      end
+
+      context 'nilの場合' do
+        task = FactoryBot.build(:task, name: nil)
+        it '無効である' do
+          expect(task).to be_invalid
+        end
+      end
+
+      context '11文字以上の場合' do
+        task = FactoryBot.build(:task, name: 'あいうえおあいうえおあ')
+        it '無効である' do
+          expect(task).to be_invalid
+        end
+      end
+    end
+
+    describe '詳細' do
+      context '50文字で入力されている場合' do
+        it '有効である' do
+          task = FactoryBot.create(:task, description: 'あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお')
+          expect(task).to be_valid
+        end
+      end
+
+      context '空の場合' do
+        it '無効である' do
+          task = FactoryBot.build(:task, description: '')
+          expect(task).to be_invalid
+        end
+      end
+
+      context 'nilの場合' do
+        it '無効である' do
+          task = FactoryBot.build(:task, description: nil)
+          expect(task).to be_invalid
+        end
+      end
+
+      context '51文字以上の場合' do
+        it '無効である' do
+          task = FactoryBot.build(:task, description: 'あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあ')
+          expect(task).to be_invalid
+        end
+      end
+    end
+  end
+end
