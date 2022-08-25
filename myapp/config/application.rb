b6f9483803d323b60ae5315e9cf7926e5d78e16c
connect-yuma-nakamura <110370186+connect-yuma-nakamura@users.noreply.github.com>
2022-08-25 16:41:13 +0900

    ステップ9: アプリの各種設定を行いましょう (#1371)
    * プロジェクト名変更
    
    * rails new 実施
    
    * docsにシステム仕様記載
    
    * ReadMeへDB設計と画面設計を記載
    
    * Model生成
    
    * 一覧画面作成
    
    * コメント削除
    
    * 記述方法修正
    
    * 作成画面追加
    
    * 詳細画面追加
    
    * パス、コントローラーコミット
    
    * 不要ファイル削除
    
    * 初期データ登録
    
    * 一覧画面作成
    
    * 編集画面作成
    
    * gitignoreへ追加
    
    * メッセージエリア追加
    
    * 若干見やすくなるためscaffoldのcss追加
    
    * 削除機能追加
    
    * 不要なファイル削除
    
    * RuboCop実施
    
    * webdriversをGemFileから削除
    
    * バージョンアップデート
    
    * system spec 準備
    
    * タスク一覧画面の並び順に不具合があったため修正
    
    * ルートパスに明示的に名称を設定
    
    * rails_helperの設定が誤っていたため修正
    
    * テストコード製造
    
    * バージョンアップデート（自動）
    
    * 実装による変更箇所修正
    
    * 一覧画面をlistからindexへ変更
    
    * ReadMeの変更漏れ
    
    * selectで必要な項目のみ取得していた箇所をallで取得するよう修正
    
    * format.jsonを削除
    また、jbuilderをGemFileから削除
    
    * 引数なしのメソッドからカッコを削除
    
    * メソッドを用いて取得するよう修正
    
    * 基本的にシングルクォーテーションを使用するように修正
    
    * resources :tasks を使用するよう修正
    
    * fablicopの導入、対応
    
    * 最新を上にするよう並び順修正
    
    * ステップ7の指摘修正に伴う修正
    
    * gemfileに追加されたためコミット
    
    * factorybot追加
    
    * rubocop対応
    
    * 日本語リソース共通化準備
    
    * TimeZone追加
    
    * エラー画面対応※途中
    
    * all&firstではなくfind_byを使用するよう修正
    
    * user対応コメントアウト
    
    * ステータス対応コメントアウト
    
    * ステータス対応コメントアウト
    
    * ソースコードの改行を修正
    
    * html、head、bodyタグ削除
    インデントを4 → 2 へ変更
    
    * '/'ではなくroot_pathを使用
    
    * findを使用するように修正
    
    * routesの見直し、またformの共通化
    
    * gem(自動更新)
    
    * respond_toを削除
    
    * ステップ7の指摘修正に伴う修正
    
    * respond_to 削除に伴う修正
    
    * 日本語対応
    
    * ステップ7の指摘修正に伴い修正
    
    * fablicop導入
    
    * 未使用だが、日本語化の準備
    
    * エラーページ作成
    
    * 日本語化対応
    
    * rubocop対応
    
    * 不要な「type: :system」削除
    
    * User対応コメントアウト
    
    * Factory Botを使用するよう修正
    
    * expect をitの中に含めるよう修正
    
    * 遅延評価へ修正
    
    * DRYではなく、各条件のテスト毎に処理を記載するよう修正
    
    * specソース全体見直し
    
    * 作成機能のテスト修正
    
    * コード統一
    採番
    テスト修正
    
    * 画面から日付を確認できるよう修正
    
    * lメソッド呼び出しをI18n.l()へ修正
    
    * publicディレクトリ配下のエラーページを参照するよう修正

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -11,9 +11,14 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # タイムゾーン設定
+    config.time_zone = 'Tokyo'
+
+    # 日本語設定
+    config.i18n.default_locale = :ja
+    # ファイルパス設定
+    config.i18n.load_path += Dir[Rails.root.join('path/to')]
+    config.i18n.load_path += Dir[Rails.root.join('path/to', '**').to_s]
   end
 end
+
