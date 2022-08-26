1252fc65455488119b1fee8f918471723f2b0bec
nanae-kusakari <nanae.kusakari@connec10.co.jp>
2022-08-26 10:55:29 +0900

    Step9_レビュー修正

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -11,12 +11,16 @@ class ApplicationController < ActionController::Base
   private
 
     def _render_404(e = nil)
-      logger.info "Rendering 404 with excaption: #{e.message}" if e
+      if !e.nil?
+        logger.error "Rendering 404 with exception: #{e.message}"
+      end
       render 'errors/404.html', status: :not_found
     end
 
     def _render_500(e = nil)
-      logger.error "Rendering 500 with excaption: #{e.message}" if e
+      if !e.nil?
+        logger.error "Rendering 500 with exception: #{e.message}"
+      end
       render 'errors/500.html', status: :internal_server_error
     end
 end
