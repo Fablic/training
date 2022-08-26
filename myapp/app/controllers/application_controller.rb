c3f4493168b9d5e12bf7bd24549f21dade9fee96
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-08-26 18:02:27 +0900

    STEP9 Rv修正

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,20 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  rescue_from Exception,                      with: :_render_500
-  rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
-  rescue_from ActionController::RoutingError, with: :_render_404
-
-  def routing_error
-    raise ActionController::RoutingError, params[:path]
-  end
-
-  private
-
-  def _render_404(e = nil)
-      render :file => "#{Rails.root}/public/404", status: :not_found, :layout => false
-  end
-
-  def _render_500(e = nil)
-    render :file => "#{Rails.root}/public/500", status: :internal_server_error,  :layout => false
-  end
 end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
   resources :tasks
-
-  get '*not_found' => 'application#routing_error'
-  post '*not_found' => 'application#routing_error'
+  root to: 'tasks#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
