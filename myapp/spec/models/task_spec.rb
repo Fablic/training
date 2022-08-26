f0c4e15ddf7a62130dafa57884a59ef82226cdbb
nanae-kusakari <nanae.kusakari@connec10.co.jp>
2022-08-26 18:15:32 +0900

    Step11_モデルテスト修正

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -4,28 +4,32 @@ describe 'Taskモデル', type: :model do
   describe 'バリデーション' do
     describe 'タスク名カラム' do
       context '30文字で入力されている場合' do
-        task = FactoryBot.create(:task, name: 'あいうえおあいうえおあいうえおあいうえおあいうえおあいうえお')
+        let!(:task) { FactoryBot.create(:task, name: 'あいうえおあいうえおあいうえおあいうえおあいうえおあいうえお') }
+
         it '登録できる' do
           expect(task).to be_valid
         end
       end
 
       context '空の場合' do
-        task = FactoryBot.build(:task, name: '')
+        let!(:task) { FactoryBot.build(:task, name: '') }
+
         it '無効である' do
           expect(task).to be_invalid
         end
       end
 
       context 'nilの場合' do
-        task = FactoryBot.build(:task, name: nil)
+        let!(:task) { FactoryBot.build(:task, name: nil) }
+
         it '無効である' do
           expect(task).to be_invalid
         end
       end
 
       context '31文字以上の場合' do
-        task = FactoryBot.build(:task, name: 'あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあ')
+        let!(:task) { FactoryBot.build(:task, name: 'あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあ') }
+
         it '無効である' do
           expect(task).to be_invalid
         end
@@ -34,30 +38,35 @@ describe 'Taskモデル', type: :model do
 
     describe '詳細カラム' do
       context '100文字で入力されている場合' do
+        let!(:task) { FactoryBot.build(:task, detail: '１いうえおあいうえお２いうえおあいうえお３いうえおあいうえお４いうえお'\
+          'あいうえお５いうえおあいうえお６いうえおあいうえお７いうえおあいうえお８いうえおあいうえお９いうえおあいうえお０いうえおあいうえお') }
+
         it '有効である' do
-          task = FactoryBot.create(:task, detail: '１いうえおあいうえお２いうえおあいうえお３いうえおあいうえお４いうえお'\
-            'あいうえお５いうえおあいうえお６いうえおあいうえお７いうえおあいうえお８いうえおあいうえお９いうえおあいうえお０いうえおあいうえお')
           expect(task).to be_valid
         end
       end
 
       context '空の場合' do
+        let!(:task) { FactoryBot.build(:task, detail: '') }
+
         it '無効である' do
-          task = FactoryBot.build(:task, detail: '')
           expect(task).to be_invalid
         end
       end
 
       context 'nilの場合' do
+        let!(:task) { FactoryBot.build(:task, detail: nil) }
+
         it '無効である' do
-          task = FactoryBot.build(:task, detail: nil)
           expect(task).to be_invalid
         end
       end
 
       context '101文字以上の場合' do
+        let!(:task) { FactoryBot.build(:task, detail: '１いうえおあいうえお２いうえおあいうえお３いうえおあいうえお４いうえお'\
+          'あいうえお５いうえおあいうえお６いうえおあいうえお７いうえおあいうえお８いうえおあいうえお９いうえおあいうえお０いうえおあいうえお１')}
+
         it '無効である' do
-          task = FactoryBot.build(:task, detail: '１いうえおあいうえお２いうえおあいうえお３いうえおあいうえお４いうえおあいうえお５いうえおあいうえお６いうえおあいうえお７いうえおあいうえお８いうえおあいうえお９いうえおあいうえお０いうえおあいうえお１')
           expect(task).to be_invalid
         end
       end
@@ -65,15 +74,17 @@ describe 'Taskモデル', type: :model do
 
     describe 'ステータスカラム' do
       context '入力されている場合' do
+        let!(:task) { FactoryBot.build(:task) }
+
         it '有効である' do
-          task = FactoryBot.create(:task)
           expect(task).to be_valid
         end
       end
 
       context 'nilの場合' do
+        let!(:task) { FactoryBot.build(:task, status: nil) }
+
         it '有効である' do
-          task = FactoryBot.create(:task, status: nil)
           expect(task).to be_valid
         end
       end
@@ -81,14 +92,16 @@ describe 'Taskモデル', type: :model do
 
     describe '優先度カラム' do
       context '入力されている場合' do
-        task = FactoryBot.create(:task)
+        let!(:task) { FactoryBot.build(:task) }
+
         it '有効である' do
           expect(task).to be_valid
         end
       end
 
       context 'nilの場合' do
-        task = FactoryBot.create(:task, priority: nil)
+        let!(:task) { FactoryBot.create(:task, priority: nil) }
+
         it '有効である' do
           expect(task).to be_valid
         end
