22fba613739ef718399f4d6470eb1b8d1b580219
miu-tsuzuki <miu.tsuzuki@connec10.co.jp>
2022-08-29 14:11:46 +0900

    STEP8:Rv 再レビュー修正

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -9,8 +9,7 @@ describe 'タスク管理機能', type: :system do
       # タスクが表示される期待動作を共通化
 
       context 'タスクが1件存在する場合' do
-        # user = User.create!(name: 'name1')
-        let!(:task_1) { FactoryBot.create(:task, title: '最初のタスク', description: '最初のタスクを実施する', user_id: "1", status: "1", label: '1') }
+        let!(:task_1) { FactoryBot.create(:task, title: '最初のタスク', description: '最初のタスクを実施する', user_id: '1', status: '1', label: '1') }
 
         let(:tds){ all('tbody tr')[0].all('td') }
         it 'タスク名が表示される' do
@@ -25,9 +24,8 @@ describe 'タスク管理機能', type: :system do
       end
 
       context 'タスクが2件(複数)存在する場合' do
-        # user = User.create!(name: 'name1')
-        let!(:task_1) { FactoryBot.create(:task, title: '0 title', description: '最初のタスクを実施する', user_id: "1", status: "1", label: "0 label") }
-        let!(:task_2) { FactoryBot.create(:task, title: '1 title', description: '２つ目のタスクを実施する', user_id: "1", status: "1", label: "1 label") }
+        let!(:task_1) { FactoryBot.create(:task, title: '0 title', description: '最初のタスクを実施する', user_id: '1', status: '1', label: '0 label') }
+        let!(:task_2) { FactoryBot.create(:task, title: '1 title', description: '２つ目のタスクを実施する', user_id: '1', status: '1', label: '1 label') }
 
         let(:tds){ all('tbody tr')[1].all('td') }
         it 'タスク名が表示される' do
@@ -42,8 +40,7 @@ describe 'タスク管理機能', type: :system do
       end
 
       describe '画面遷移機能' do
-        # user = User.create!(name: 'name1')
-        let!(:task_a) { FactoryBot.create(:task, title: '0 title', description: '最初のタスクを実施する', user_id: "1", status: "1", label: "0 label") }
+        let!(:task_a) { FactoryBot.create(:task, title: '0 title', description: '最初のタスクを実施する', user_id: '1', status: '1', label: '0 label') }
         context '詳細ボタンをクリックした場合' do
           it '詳細画面へ遷移できる' do
             visit_tasks
@@ -72,8 +69,7 @@ describe 'タスク管理機能', type: :system do
   end
 
   describe '詳細表示機能' do
-    user = User.create!(name: 'name1')
-    let!(:task_a) { FactoryBot.create(:task, title: '0 title', description: '最初のタスクを実施する', user_id: "1", status: "1", label: "0 label") }
+    let!(:task_a) { FactoryBot.create(:task, title: '0 title', description: '最初のタスクを実施する', user_id: '1', status: '1', label: '0 label') }
     subject(:visit_task_a) { visit task_path(task_a) }
 
     describe '表示機能' do
@@ -128,7 +124,7 @@ describe 'タスク管理機能', type: :system do
     end
 
     describe '画面遷移機能' do
-      let!(:task_a) { FactoryBot.create(:task, title: '最初のタスク', description: '最初のタスクを実施する', user_id: "1", status: "1", label: 1) }
+      let!(:task_a) { FactoryBot.create(:task, title: '最初のタスク', description: '最初のタスクを実施する', user_id: '1', status: '1', label: '1') }
 
       context '新規登録ボタンをクリックした場合' do
         it '一覧画面へ遷移できる' do
@@ -149,8 +145,7 @@ describe 'タスク管理機能', type: :system do
   end
 
   describe '編集機能' do
-    user = User.create!(name: 'name1')
-    let!(:task_a) { FactoryBot.create(:task, title: '最初のタスク', description: '最初のタスクを実施する', user_id: "1", status: "1", label: 1) }
+    let!(:task_a) { FactoryBot.create(:task, title: '最初のタスク', description: '最初のタスクを実施する', user_id: '1', status: '1', label: '1') }
     subject(:visit_task_a_edit){visit edit_task_path(task_a)}
 
     describe '表示機能' do
@@ -164,33 +159,32 @@ describe 'タスク管理機能', type: :system do
           expect(page).to have_field 'textarea2', with: task_a.description
         end
       end
-    end
+      context 'タスクの各項目を更新した場合' do
+        let(:input_values) {
+          {
+            title: '最初のタスク',
+            description: '最初のタスクを実施する',
+            user_id: '1',
+          }
+        }
 
-    context 'タスクの各項目を更新した場合' do
-      let(:title) { '新規作成のテスト２' }
-      let(:description) { '新規作成のテストを書く２' }
-      it 'タスクが正常に更新される' do
-        visit_task_a_edit
-        # 更新処理
-        fill_in 'textarea1', with: title
-        fill_in 'textarea2', with: description
-
-        click_button 'submit'
-        # 画面で入力された内容でDBのデータが更新されている
-        task = Task.find_by(title: title)
-        expect(task.title).to eq(title)
-        expect(task.description).to eq(description)
-        # Flashメッセージが表示される
-        expect(page).to have_selector '.alert-success', text: 'Update Task Success!!'
-      end
-    end
-
-    describe '画面遷移機能' do
-      context '一覧へボタンをクリックした場合' do
-        it '一覧画面へ遷移できる' do
+        it 'タスクが正常に更新される' do
           visit_task_a_edit
-          click_link '一覧に戻る'
-          expect(page).to have_current_path tasks_path
+          # 更新処理
+          fill_in 'textarea1', with: input_values[:title]
+          fill_in 'textarea2', with: input_values[:description]
+          # 画面で入力された内容でDBのデータが更新されている
+          expect(Task.find_by(input_values)).to be_present
+        end
+      end
+
+      describe '画面遷移機能' do
+        context '一覧へボタンをクリックした場合' do
+          it '一覧画面へ遷移できる' do
+            visit_task_a_edit
+            click_link '一覧に戻る'
+            expect(page).to have_current_path tasks_path
+          end
         end
       end
     end
