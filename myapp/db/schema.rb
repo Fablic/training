14a1d391df6ca530e47d6b01191871422cd7121b
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-08-30 13:47:39 +0900

    終了期限カラム追加

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_085553) do
+ActiveRecord::Schema.define(version: 2022_08_30_025317) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.timestamp "finish_at"
   end
 
 end
