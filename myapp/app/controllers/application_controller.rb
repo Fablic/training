aed0537d4dcd7b166669ac46441dba3aaa6ba524
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-30 17:46:31 +0900

    sessions_helperを利用するように修正

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,35 +1,6 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
   before_action :login_user
   before_action :re_login
-
-  def login(user)
-    token = User.create_login_token
-    cookies.permanent[:login_token] = token
-    user.update(login_token: User.encrypt_login_token(token))
-    @login_user = user
-  end
-
-  def logout
-    cookies.delete(:login_token)
-    @login_user = nil
-  end
-
-  def login?
-    @login_user.present?
-  end
-
-  def login_user
-    if login?
-      @login_user
-    else
-      token = User.encrypt_login_token(cookies[:login_token])
-      @login_user ||= User.find_by(login_token: token)
-    end
-  end
-
-  private
-
-  def re_login
-    redirect_to(login_path) if !login?
-  end
 end
