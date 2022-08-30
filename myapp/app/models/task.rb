448acdde6660c5229a1d555a6f2f1e5ce187fdbc
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-08-30 18:14:31 +0900

    今日分コミット

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -3,6 +3,8 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :body,  presence: true
+  validates :finish_at,  presence: true
+  
   scope :desc, -> { order(created_at: :desc) }
   scope :finish_de, -> { order(finish_at: :desc) }
   scope :asc, -> { order(created_at: :asc) }
