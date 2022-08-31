bfa3b44733ba83824db2e7ac950c5f9f8385a38b
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-08-31 11:32:53 +0900

    sessionのsystem spec 追加

---
+++ myapp/spec/system/sessions_spec.rb
@@ -0,0 +1,92 @@
+require 'rails_helper'
+
+describe 'Sessions', type: :system do
+
+  describe '#new' do
+    describe 'ログイン' do
+      let(:user) { FactoryBot.create(:user) }
+      let(:task) { FactoryBot.create(:task, user_id: user.id) }
+      context 'ログイン成功' do
+        it 'ログインできること' do
+          visit login_path
+          fill_in 'session[email]', with: user.email
+          fill_in 'session[password]', with: 'password'
+          click_on I18n.t('button.login')
+          expect(page).to have_current_path root_path
+        end
+
+        it 'ログイン後タスク作成画面へ遷移できること' do
+          visit login_path
+          fill_in 'session[email]', with: user.email
+          fill_in 'session[password]', with: 'password'
+          click_on I18n.t('button.login')
+          visit new_task_path
+          expect(page).to have_current_path new_task_path
+        end
+
+        it 'ログイン後タスク詳細画面へ遷移できること' do
+          visit login_path
+          fill_in 'session[email]', with: user.email
+          fill_in 'session[password]', with: 'password'
+          click_on I18n.t('button.login')
+          visit task_path(task)
+          expect(page).to have_current_path task_path(task)
+        end
+
+        it 'ログイン後タスク編集画面へ遷移できること' do
+          visit login_path
+          fill_in 'session[email]', with: user.email
+          fill_in 'session[password]', with: 'password'
+          click_on I18n.t('button.login')
+          visit edit_task_path(task)
+          expect(page).to have_current_path edit_task_path(task)
+        end
+      end
+
+      context 'ログイン失敗' do
+        it 'ログインできないこと' do
+          visit login_path
+          fill_in 'session[email]', with: user.email
+          fill_in 'session[password]', with: 'password_failed'
+          click_on I18n.t('button.login')
+          expect(page).to have_current_path login_path
+        end
+
+        it 'ログイン失敗時エラーメッセージが表示されること' do
+          visit login_path
+          fill_in 'session[email]', with: user.email
+          fill_in 'session[password]', with: 'password_failed'
+          click_on I18n.t('button.login')
+          expect(page).to have_content 'ログインに失敗しました'
+        end
+
+        it 'ログイン後タスク作成画面へ遷移できないこと' do
+          visit login_path
+          fill_in 'session[email]', with: user.email
+          fill_in 'session[password]', with: 'password_failed'
+          click_on I18n.t('button.login')
+          visit new_task_path
+          expect(page).to have_current_path login_path
+        end
+
+        it 'ログイン後タスク詳細画面へ遷移できないこと' do
+          visit login_path
+          fill_in 'session[email]', with: user.email
+          fill_in 'session[password]', with: 'password_failed'
+          click_on I18n.t('button.login')
+          visit task_path(task)
+          expect(page).to have_current_path login_path
+        end
+
+        it 'ログイン後タスク編集画面へ遷移できないこと' do
+          visit login_path
+          fill_in 'session[email]', with: user.email
+          fill_in 'session[password]', with: 'password_failed'
+          click_on I18n.t('button.login')
+          visit edit_task_path(task)
+          expect(page).to have_current_path login_path
+        end
+      end
+    end
+  end
+end
