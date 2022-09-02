9caeaddba6b3efc369d53a10b65e7f695bdb723b
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-09-02 16:57:53 +0900

    letをbeforeへ修正

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -13,10 +13,12 @@ describe 'Tasks', type: :system do
     end
 
     describe '検索エリア' do
-      let!(:task_A1) { FactoryBot.create(:task, title: 'titleA1', status: 'not_started') }
-      let!(:task_A2) { FactoryBot.create(:task, title: 'titleA2', status: 'in_progress') }
-      let!(:task_B1) { FactoryBot.create(:task, title: 'titleB1', status: 'not_started') }
-      let!(:task_B2) { FactoryBot.create(:task, title: 'titleB2', status: 'in_progress') }
+      before do
+        FactoryBot.create(:task, title: 'titleA1', status: 'not_started')
+        FactoryBot.create(:task, title: 'titleA2', status: 'in_progress')
+        FactoryBot.create(:task, title: 'titleB1', status: 'not_started')
+        FactoryBot.create(:task, title: 'titleB2', status: 'in_progress')
+      end
 
       context '条件なし検索' do
         let(:conditions) { { title: '', status: '' } }
@@ -274,11 +276,13 @@ describe 'Tasks', type: :system do
 
     describe 'ページングエリア' do
       context 'ページングなし' do
-        let!(:task_one) { FactoryBot.create(:task) }
-        let!(:task_two) { FactoryBot.create(:task) }
-        let!(:task_three) { FactoryBot.create(:task) }
-        let!(:task_four) { FactoryBot.create(:task) }
-        let!(:task_five) { FactoryBot.create(:task) }
+        before do
+          FactoryBot.create(:task, title: 'title_one')
+          FactoryBot.create(:task, title: 'title_two')
+          FactoryBot.create(:task, title: 'title_three')
+          FactoryBot.create(:task, title: 'title_four')
+          FactoryBot.create(:task, title: 'title_five')
+        end
 
         it 'ページングが表示されないこと ページ番号1' do
           visit root_path
@@ -302,17 +306,19 @@ describe 'Tasks', type: :system do
       end
 
       context 'ページングあり' do
-        let!(:task_one) { FactoryBot.create(:task, title: 'titleOne') }
-        let!(:task_two) { FactoryBot.create(:task, title: 'titleTwo') }
-        let!(:task_three) { FactoryBot.create(:task, title: 'titleThree') }
-        let!(:task_four) { FactoryBot.create(:task, title: 'titleFour') }
-        let!(:task_five) { FactoryBot.create(:task, title: 'titleFive') }
-        let!(:task_six) { FactoryBot.create(:task, title: 'titleSix') }
-        let!(:task_seven) { FactoryBot.create(:task, title: 'titleSeven') }
-        let!(:task_eight) { FactoryBot.create(:task, title: 'titleEight') }
-        let!(:task_nine) { FactoryBot.create(:task, title: 'titleNine') }
-        let!(:task_ten) { FactoryBot.create(:task, title: 'titleTen') }
-        let!(:task_eleven) { FactoryBot.create(:task, title: 'titleEleven') }
+        before do
+          FactoryBot.create(:task, title: 'title_one')
+          FactoryBot.create(:task, title: 'title_two')
+          FactoryBot.create(:task, title: 'title_three')
+          FactoryBot.create(:task, title: 'title_four')
+          FactoryBot.create(:task, title: 'title_five')
+          FactoryBot.create(:task, title: 'title_six')
+          FactoryBot.create(:task, title: 'title_seven')
+          FactoryBot.create(:task, title: 'title_eight')
+          FactoryBot.create(:task, title: 'title_nine')
+          FactoryBot.create(:task, title: 'title_ten')
+          FactoryBot.create(:task, title: 'title_eleven')
+        end
 
         it 'ページングが表示されること ページ番号1' do
           visit root_path
@@ -337,33 +343,33 @@ describe 'Tasks', type: :system do
         it 'ページ番号2を押下すると次ページの要素が表示されること' do
           visit root_path
           click_on '2'
-          expect(page).to have_content 'titleSix'
+          expect(page).to have_content 'title_six'
         end
 
         it '次ページボタンを押下すると次ページの要素が表示されること' do
           visit root_path
           click_on 'Next'
-          expect(page).to have_content 'titleSix'
+          expect(page).to have_content 'title_six'
         end
 
         it '最終ページボタンを押下すると最終ページの要素が表示されること' do
           visit root_path
           click_on 'Last'
-          expect(page).to have_content 'titleOne'
+          expect(page).to have_content 'title_one'
         end
 
         it '最初のページボタンを押下すると最初のページの要素が表示されること' do
           visit root_path
           click_on 'Last'
           click_on 'First'
-          expect(page).to have_content 'titleEleven'
+          expect(page).to have_content 'title_eleven'
         end
 
         it '前のページボタンを押下すると最初のページの要素が表示されること' do
           visit root_path
           click_on 'Last'
           click_on 'Previous'
-          expect(page).to have_content 'titleSix'
+          expect(page).to have_content 'title_six'
         end
       end
     end
