afabe5a64339d3cf87a8c0f031327f1fc5e8cbb7
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-09-02 15:22:29 +0900

    不具合修正

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -8,14 +8,11 @@ class Task < ApplicationRecord
   scope :desc, -> { order(created_at: :desc) }
   scope :finish_desc, -> { order(finish_at: :desc) }
   scope :asc, -> { order(created_at: :asc) }
-  scope :finish_a, -> { order(finish_at: :asc) }
+  scope :finish_asc, -> { order(finish_at: :asc) }
 
   enum status: {
     untouched: 0,
     in_progress: 1,
     completion: 2,
-    # '未着手': 0,
-    # '着手中': 1,
-    # '完了': 2,
   }
 end
