f31f623daf1e82e5d7a0916221c3921ab91758ba
nanae-kusakari <nanae.kusakari@connec10.co.jp>
2022-09-02 15:27:31 +0900

    Step14_レビュー修正

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all.page(params[:page]).per(5)
+    @tasks = Task.all.page(params[:page])
   end
 
   def show
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   end
 
   def search
-    @tasks = Task.name_like(params[:name]).status_equal(Task.statuses[params[:status]]).page(params[:page]).per(5)
+    @tasks = Task.name_like(params[:name]).status_equal(Task.statuses[params[:status]]).page(params[:page])
     render :index
   end
 
