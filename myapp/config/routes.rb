ca76e5c6d4b91c70219d814e11e8d6d2965a5a8c
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-09-05 10:33:06 +0900

    STEP17 ログインページ作成

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new', as: :login
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :tasks
   root to: 'tasks#index'
 
