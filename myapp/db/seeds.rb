c7cf6e4774a1d08b6dfe1dab5bf1a22e57798316
kazuma.gushiken <kazuma.gushiken@connec10.co.jp>
2022-09-05 15:59:30 +0900

    seed_５ユーザ登録

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -6,4 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: '具志堅')
+
+# user = User.new
+# user.name = '一真'
+# user.save
+
+5.times do |i|
+    User.create(name: "一真#{i + 1}")
+end
