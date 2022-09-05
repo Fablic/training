ba8ce6dd076bff6ad3c53d77b4a975dcf1e8b327
kazuma.gushiken <kazuma.gushiken@connec10.co.jp>
2022-09-05 17:30:41 +0900

    ユーザIDインデックス追加

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_05_072419) do
+ActiveRecord::Schema.define(version: 2022_09_05_082040) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", limit: 10, null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_09_05_072419) do
     t.integer "status"
     t.integer "user_id"
     t.index ["name", "status"], name: "index_tasks_on_name_and_status"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
