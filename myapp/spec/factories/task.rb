d993b31012087a8aff19a98c8a54c0e56a7d8e62
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-09-05 11:12:10 +0900

    model spec 修正

--- myapp/spec/factories/task.rb
+++ myapp/spec/factories/task.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :task, class: 'Task' do
     title    { 'title' }
     content  { 'content' }
-    label    { 'label' }
     status   { 'not_started' }
     user_id  { FactoryBot.create(:user).id }
   end
