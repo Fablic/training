d89e75ace3c2c34dd97013ff3e3449e09c2086b8
miu-tsuzuki <miu.tsuzuki@connec10.co.jp>
2022-09-05 12:37:18 +0900

    STEP11 ヴァリデーションの実装

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,18 +1,6 @@
 class Task < ApplicationRecord
   # 結合キー
-  # belongs_to :user
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :description, presence: true, length: { maximum: 100 }
 
-  # ステータスEnum
-  enum status: {
-    not_started: '1',
-    in_progress: '2',
-    closed: '3'
-  }
-
-  # ステータス変換用Enum
-  STATUS_VIEW = {
-    Task.statuses[:not_started] => '未着手',
-    Task.statuses[:in_progress] => '着手中',
-    Task.statuses[:closed] => '完了'
-  }.freeze
 end
