2c346c99dfeba1e576a1dbba0b39e2ea1216dd1b
kazuma.gushiken <kazuma.gushiken@connec10.co.jp>
2022-09-05 15:44:58 +0900

    seed_ユーザ登録

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_01_085322) do
+ActiveRecord::Schema.define(version: 2022_09_05_063239) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", limit: 10, null: false
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2022_09_01_085322) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status"
     t.index ["name", "status"], name: "index_tasks_on_name_and_status"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
