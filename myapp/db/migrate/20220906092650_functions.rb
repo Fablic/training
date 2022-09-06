87591899a73c541594f177f41bd29b62f477baad
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-09-06 18:29:49 +0900

    テーブル functions 追加

---
+++ myapp/db/migrate/20220906092650_functions.rb
@@ -0,0 +1,11 @@
+class Functions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :functions do |t|
+      t.string :name
+      t.string :status, limit: 1, null: false, default: '1'
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+  end
+end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_024655) do
+ActiveRecord::Schema.define(version: 2022_09_06_092650) do
+
+  create_table "functions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "status", limit: 1, default: "1", null: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", limit: 64, default: "not setting", null: false
