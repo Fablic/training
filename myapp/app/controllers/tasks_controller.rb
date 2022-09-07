a7bae274741a95c9ab855d319e485fcbead2f15f
h-aqua25 <ts-aqua.hara@rakuten.com>
2022-09-07 15:44:36 +0900

    i18nで共通の文言を設定

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -19,7 +19,8 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      redirect_to task_url(@task), notice: 'タスクが正常に作成されました。'
+      redirect_to task_url(@task),
+                  notice: I18n.t('crud_messages.create', model_name: I18n.t('activerecord.models.task'))
     else
       render :new, status: :unprocessable_entity
     end
@@ -27,7 +28,8 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to task_url(@task), notice: 'タスクが正常に更新されました。'
+      redirect_to task_url(@task),
+                  notice: I18n.t('crud_messages.update', model_name: I18n.t('activerecord.models.task'))
     else
       render :edit, status: :unprocessable_entity
     end
@@ -35,7 +37,8 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: 'タスクが正常に削除されました。'
+
+    redirect_to tasks_url, notice: I18n.t('crud_messages.destroy', model_name: I18n.t('activerecord.models.task'))
   end
 
   private
