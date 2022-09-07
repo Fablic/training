3bbb4232bcfcb61cf933b33936ccc125f9964a2f
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-09-07 11:07:23 +0900

    migrate修正

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2022_09_06_075918) do
     t.date "finish_at", null: false
     t.integer "status", default: 0, null: false
     t.string "user_id", null: false
-    t.index ["title", "status"], name: "index_tasks_on_title_and_status"
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
