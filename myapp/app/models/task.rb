0fbb71d10779b5f112092e92e3ea8568fc25e96e
h-aqua25 <ts-aqua.hara@rakuten.com>
2022-09-07 16:10:30 +0900

    default値をApp側で持つように変更

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  DEFAULT_PRIORITY_VALUE = 1
+  DEFAULT_STATUS_VALUE = 0
+
+  after_initialize :set_default_values
+
   validates :name, presence: true, length: { maximum: 255 }
 
   enum :priority, {
@@ -14,4 +19,9 @@ class Task < ApplicationRecord
     touched: 1,   # 着手中
     completed: 2  # 完了
   }, prefix: true
+
+  def set_default_values
+    self.priority ||= DEFAULT_PRIORITY_VALUE
+    self.status   ||= DEFAULT_STATUS_VALUE
+  end
 end
