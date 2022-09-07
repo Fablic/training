15a4f07785d21d64806d70a262dd3af76c4baacd
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-09-07 18:01:06 +0900

    パスワード関連のspecは明示的にパスワードを設定するよう修正

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Tasks', type: :system do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { FactoryBot.create(:user, password_digest: 'password') }
 
   before do
     login(user, 'password')
