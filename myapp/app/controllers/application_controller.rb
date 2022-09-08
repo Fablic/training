d8b74925573e4d50e5bc8e06d606902573c455ba
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-09-08 14:37:26 +0900

    修正忘れ

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
     raise ActionController::RoutingError, params[:path]
   end
 
-  # unless Rails.env.production?
-  #   rescue_from Exception,                        with: :_render500
-  #   rescue_from ActiveRecord::RecordNotFound,     with: :_render404
-  #   rescue_from ActionController::RoutingError,   with: :_render404
-  # end
+  unless Rails.env.production?
+    rescue_from Exception,                        with: :_render500
+    rescue_from ActiveRecord::RecordNotFound,     with: :_render404
+    rescue_from ActionController::RoutingError,   with: :_render404
+  end
 
   private
 
