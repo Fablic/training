e5e76ff35c0904f16f084c581a56ebddafbdf8ef
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-09-08 09:42:10 +0900

    STEP13 migrate Fileの修正とyarnの更新

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_30_015011) do
+ActiveRecord::Schema.define(version: 2022_09_08_003930) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", limit: 128, default: "", null: false
