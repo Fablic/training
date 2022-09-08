a6e3f816ac5bd7577b5232159820b70869108009
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-09-08 10:46:40 +0900

    helperに定義しているメソッドをApplicationControllerへ移動

--- myapp/app/helpers/functions_helper.rb
+++ myapp/app/helpers/functions_helper.rb
@@ -1,12 +1,2 @@
 module FunctionsHelper
-  def check_system_started
-    if Function.is_stopped(Function::FUNC_ID_SYSTEM)
-      render(
-        file: Rails.public_path.join("503.html"),
-        content_type: "text/html",
-        layout: false,
-        status: :service_unavailable,
-      )
-    end
-  end
 end
