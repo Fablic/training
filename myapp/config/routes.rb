3dcff2dde44b67d66c15bdea05b6412212e8326d
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-09-08 18:14:48 +0900

    ログイン用ページ実装

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'task_schedule#index'
   resources :task_schedule
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '*not_found' => 'application#routing_error'
   post '*not_found' => 'application#routing_error'
