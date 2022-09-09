c173a66877f25e1f1a4a3d428c62e6e8b96ba8a6
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-09-09 17:00:03 +0900

    STEP21 メンテナンス機能追加

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
     include SessionsHelper
 
     before_action :login_user
+    before_action :check_maintenance
+
+    def check_maintenance
+      maintenance = Maintenance.find_by(content_id: ALL_MENTE)
+      if maintenance.maintenance_flg
+        render(
+          file: Rails.public_path.join("503.html"),
+          content_type: "text/html",
+          layout: false,
+          status: :service_unavailable,
+        )
+      end
+    end
   end
---
+++ myapp/lib/tasks/maintenances.rb
@@ -0,0 +1,24 @@
+module Tasks
+  class Maintenaner
+    def self.start_maintenance
+      switch_maintenance(true, 'メンテナンス開始')
+    end
+
+    def self.end_maintenance
+      switch_maintenance(false, 'メンテナンス終了')
+    end
+
+    private
+
+    def self.switch_maintenance(maintenance_flg, type)
+      maintenance = Maintenance.find_by(content_id: TASK)
+      if maintenance.present? && maintenance.update(maintenance_flg: maintenance_flg)
+        # 動作確認のため出力
+        puts "#{type}"
+      else
+        # 動作確認のため出力
+        puts "#{type}に失敗しました"
+      end
+    end
+  end
+end
