e13b85d0dc6be25daa211507a3fe649bc71547c7
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-09-12 13:18:36 +0900

    とりあえずコミット

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'task_schedule#index'
   resources :task_schedule
+  scope '/admin' do
+    resources :users
+  end
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
