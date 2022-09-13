ae958dedc28a8815fbfe028ba7ed4b06f832d026
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-09-13 11:46:20 +0900

    レビュー指摘対応

--- myapp/db/migrate/20220912014806_create_task_labels.rb
+++ myapp/db/migrate/20220912014806_create_task_labels.rb
@@ -1,8 +1,8 @@
 class CreateTaskLabels < ActiveRecord::Migration[6.0]
   def change
     create_table :task_labels do |t|
-      t.belongs_to :task
-      t.belongs_to :label
+      t.references :task, index: true, foreign_key: true
+      t.references :label, index: true, foreign_key: true
 
       t.timestamps
     end
