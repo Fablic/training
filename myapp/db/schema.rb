27e6a1638a4609ca3782a699e1f8367795fc74f4
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-09-13 19:32:19 +0900

    DB修正

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_09_09_044145) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "personal_id", null: false
     t.string "password_digest", null: false
   end
 
