391acf324807ac1e498577d0684765b21244436a
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-09-14 09:21:14 +0900

    rspec不具合修正

--- myapp/spec/models/user_spec.rb
+++ myapp/spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = create(:user)
+  end
+
+  it 'check user validates' do
+    expect(@user).to be_valid
+
+    @user = build(:user, name: '')
+    expect(@user.valid?).to eq false
+
+    @user = build(:user, personal_id: '')
+    expect(@user.valid?).to eq false
+
+    @user = build(:user, password: '')
+    expect(@user.valid?).to eq false
+  end
+
 end
--- myapp/spec/system/task_schedule_spec.rb
+++ myapp/spec/system/task_schedule_spec.rb
@@ -133,7 +133,7 @@ RSpec.describe 'TaskSchedule', type: :system do
   context 'edit systems check' do
     let!(:task) { create(:task, title: 'showタスク', body: 'showボディ', user_id: user.id) }
     before do
-      create(:user, name: 'user 二郎')
+      create(:user, name: 'user 二郎', personal_id: 'editID')
       fill_in 'personal_id', with: 'MyUserID'
       fill_in 'password', with: 'pass'
       click_button 'ログイン'
