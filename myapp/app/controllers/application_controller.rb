d9d831854e7234b13ffede8803d057ec485927e8
h-aqua25 <ts-aqua.hara@rakuten.com>
2022-09-16 17:56:23 +0900

    stub化を廃止と404ケースを各ファイルに記述

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from Exception, with: :render_500
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from Exception, with: :render_500
 
   def render_404
-    render file: 'public/404', layout: false, status: 404, content_type: 'text/html'
+    render file: 'public/404.html', layout: false, status: 404
   end
 
   def render_500
-    render file: 'public/500', layout: false, status: 500, content_type: 'text/html'
+    render file: 'public/500.html', layout: false, status: 500
   end
 end
--- myapp/spec/features/tasks/edit_spec.rb
+++ myapp/spec/features/tasks/edit_spec.rb
@@ -56,4 +56,22 @@ RSpec.feature '/task/:id/edit' do
       expect(page).to have_content 'タスク名を入力してください'
     end
   end
+
+  feature '#not_found' do
+    scenario 'correctly displays 404' do
+      visit '/aqua'
+
+      expect(page.status_code).to eq 404
+      expect(page).to have_content '404なので僕のせいじゃないっす'
+      expect(page).to have_content '多分アドレスとか違うっす'
+    end
+
+    scenario 'correctly displays 404' do
+      visit '/tasks/999999999/edit'
+
+      expect(page.status_code).to eq 404
+      expect(page).to have_content '404なので僕のせいじゃないっす'
+      expect(page).to have_content '多分アドレスとか違うっす'
+    end
+  end
 end
