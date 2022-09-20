2e2264949d9f49d48fb86e7c6da654c6a2b7c808
yuma.nakamura <yuma.nakamura@connec10.co.jp>
2022-09-20 18:03:24 +0900

    referencesの時点でindexが貼られるため、index: true 削除

--- myapp/db/migrate/20220912014806_create_task_labels.rb
+++ myapp/db/migrate/20220912014806_create_task_labels.rb
@@ -1,8 +1,8 @@
 class CreateTaskLabels < ActiveRecord::Migration[6.0]
   def change
     create_table :task_labels do |t|
-      t.references :task, index: true
-      t.references :label, index: true
+      t.references :task
+      t.references :label
 
       t.timestamps
     end
