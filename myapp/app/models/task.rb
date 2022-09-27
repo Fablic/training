fe89e6382551f2602e6b9c283615c85d3851f0d4
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-09-27 15:40:19 +0900

    STEP20 モデル作成

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
-  has_many :labels, dependent: :destroy
+  has_many :task_labels
+  has_many :labels, through: :task_labels
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 100 }
@@ -16,7 +17,9 @@ class Task < ApplicationRecord
     scope :where_title, -> (title) { where('title like ?', "%#{title}%") if title.present? }
     scope :where_status, -> (status) { where(status: status) if status.present? }
     scope :where_user_id, -> (user_id) { where(user_id: user_id) if user_id.present? }
-    scope :where_label, -> (label) { where('tasks.id IN( SELECT task_id FROM labels WHERE labels.name like ? )', "%#{label}%") if label.present? }
+    scope :where_label, -> (label_id) { where(labels: { id: label_id }) if label_id.present? }
+    # scope :where_label, -> (label) { where('tasks.id IN( SELECT task_id FROM labels WHERE labels.name like ? )', "%#{label}%") if label.present? }
+    scope :get_ids, -> (title, label, status) { eager_load(:labels).where_title(title).where_status(status).where_label(label).select('tasks.id') }
 
   # ページ内要素数
   paginates_per 5
