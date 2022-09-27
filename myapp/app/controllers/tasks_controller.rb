529f179998a06a43a0550f466c702c724bbe2f0d
kohei.matsubara <kohei.matsubara@connec10.co.jp>
2022-09-27 15:39:53 +0900

    STEP20 コントローラ作成

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show edit update destroy]
 
   def index
     # タスク一覧オブジェクト取得
-    @tasks = login_user.tasks.where_title(params[:title]).where_status(params[:status]).order('tasks.created_at desc').page(params[:page])
+    @tasks = login_user.tasks.preload(:labels).where(id: login_user.tasks.get_ids(params[:title], params[:label], params[:status])).order('tasks.created_at desc').page(params[:page])
   end
 
   def show
-    @task = login_user.tasks.eager_load(:labels).find(params[:id])
-    @label1 = @task.labels[0].name if @task.labels.size > 0
-    @label2 = @task.labels[1].name if @task.labels.size > 1
-    @label3 = @task.labels[2].name if @task.labels.size > 2
-    @label4 = @task.labels[3].name if @task.labels.size > 3
-    @label5 = @task.labels[4].name if @task.labels.size > 4
+    @task = login_user.tasks.find(params[:id])
   end
 
   def new
@@ -22,16 +16,13 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
-    @is_status = true
-    @task_form = TaskForm.new
-    @task_form.setting(params[:id])
+    @task = login_user.tasks.find(params[:id])
   end
 
   def create
-    @task_form = TaskForm.new(task_params)
+    @task = login_user.tasks.new(task_params)
 
-    if @task_form.save
+    if @task.save
       redirect_to(root_path, notice: 'タスクを登録しました')
     else
       render(:new, status: :unprocessable_entity)
@@ -39,32 +30,23 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task_form = TaskForm.new(task_params)
+    @task = login_user.tasks.find(params[:id])
 
-    if @task_form.update(params[:id])
-      redirect_to(root_path, notice: 'タスクを更新しました')
+    if @task.update(task_params)
+      redirect_to(root_path, notice: 'タスク更新成功')
     else
-      @task = Task.find(params[:id])
       render(:edit, status: :unprocessable_entity)
     end
   end
 
   def destroy
-    if @task.destroy
-      redirect_to tasks_url, notice: "タスク「#{@task.title}」を削除しました。"
-    else
-      render :show
-    end
+    @task = login_user.tasks.find(params[:id])
+    redirect_to(root_path, notice: 'タスク削除成功') if @task.destroy
   end
 
   private
 
   def task_params
-    params.require(:task_form)[:user_id] = login_user.id
-    params.require(:task_form).permit(:title, :description, :label1, :label2, :label3, :label4, :label5, :user_id, :status)
-  end
-
-  def set_task
-    @task = Task.find(params[:id])
+    params.require(:task).permit(:title, :description, :status, { label_ids: [] })
   end
 end
