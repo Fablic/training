500b9ec6958c31a98e1788210aac8fc5cd9e0fb9
ichinorakuma <hayataka.ichino+rakuma@connec10.co.jp>
2022-09-28 20:15:26 +0900

    指摘対応

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2022_09_15_062559) do
   end
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
