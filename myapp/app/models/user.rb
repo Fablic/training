83a79c6a4681df3003a757e9229dc5d19910bbdc
h-aqua25 <ts-aqua.hara@rakuten.com>
2022-10-04 18:00:02 +0900

    validation対応

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
 end
