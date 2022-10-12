7abb622c917e69eb992949596d05cb1a4c04cfed
h-aqua25 <ts-aqua.hara@rakuten.com>
2022-10-12 17:47:47 +0900

    レビュー対応

--- myapp/db/migrate/20221006043610_add_role_to_users.rb
+++ myapp/db/migrate/20221006043610_add_role_to_users.rb
@@ -2,6 +2,7 @@
 
 class AddRoleToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :role, :integer, null: false, comment: '役割: 0:一般(ordinary), 1:管理者(admin)'
+    add_column :users, :role, :integer, after: :password_digest, null: false, default: 0,
+                                        comment: '役割: 0:一般(ordinary), 1:管理者(admin)'
   end
 end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -30,9 +30,9 @@ ActiveRecord::Schema[7.0].define(version: 20_221_006_043_610) do
     t.string 'name', null: false, comment: 'タスク名'
     t.string 'email', null: false, comment: 'Eメール'
     t.string 'password_digest', null: false, comment: '暗号化されたパスワード'
+    t.integer 'role', default: 0, null: false, comment: '役割: 0:一般(ordinary), 1:管理者(admin)'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.integer 'role', null: false, comment: '役割: 0:一般(ordinary), 1:管理者(admin)'
     t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
