5084c26d7de09b5dbe1a13b772b83eecddb8303e
h-aqua25 <ts-aqua.hara@rakuten.com>
2022-10-12 15:00:15 +0900

    routing, view spec追加

---
+++ myapp/spec/views/labels/edit.html.erb_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'labels/edit', type: :view do
+  let!(:label) { assign(:label, create(:label)) }
+
+  it 'renders the edit label form' do
+    render
+
+    assert_select 'form[action=?][method=?]', label_path(label), 'post' do
+      assert_select 'input[name=?]', 'label[name]'
+    end
+  end
+end
