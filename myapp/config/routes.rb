7fd08f27b4b8799cebce662e9bcc504fad76e31a
KumoRaku <jian.zhao@rakuten.com>
2022-12-02 15:36:14 +0900

    add error pages

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root 'tasks#index'
 
   resources :tasks
+
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
 end
