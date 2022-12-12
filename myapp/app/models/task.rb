a1a4a5cc4199f302add8d5f4a20d3b767200690a
anne ju <anne.ju@rakuten.com>
2022-12-12 13:56:50 +0900

    anne_ju_step6: add gem annotate

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  title       :string(255)
+#  description :string(255)
+#  priority    :integer
+#  status      :integer
+#  due_date    :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Task < ApplicationRecord
 end
