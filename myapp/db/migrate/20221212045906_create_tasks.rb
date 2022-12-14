9ae28224aec78701b61abbe85cb4be986c6ab992
Satoru-Aihara <ts-satoru.aihara@rakuten.com>
2022-12-14 09:49:17 +0900

    indexを削除

--- myapp/db/migrate/20221212045906_create_tasks.rb
+++ myapp/db/migrate/20221212045906_create_tasks.rb
@@ -14,10 +14,3 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     end
   end
 end
-
-
-class AddIndexToTasks < ActiveRecord::Migration[6.0]
-  def change
-    add_index :tasks, :title, :content
-  end
-end
