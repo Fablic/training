6cdd139607c6f7dcd7c555e27a8b8d806e5e03ec
Satoru-Aihara <ts-satoru.aihara@rakuten.com>
2022-12-20 16:48:36 +0900

    ステップ7: タスクの登録・更新・削除

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :tag
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
+  validates :due_date, presence: true
+
 end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 一覧
+  get "/", to: "tasks#index"
+  get "/tasks", to: "tasks#index"
+  get "/tasks/index", to: "tasks#index"
+  # 作成
+  post "/tasks", to: "tasks#create"
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  # 編集
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  # 詳細
+  get "/tasks/:id", to: "tasks#show", as: "task"
+  # 更新
+  patch "/tasks/:id", to: "tasks#update"
+  # 削除
+  delete "/tasks/:id", to: "tasks#destroy"
 end
