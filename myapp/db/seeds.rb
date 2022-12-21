500bf927ca7f4823a7cfd1f512f3b1280294f77c
KumoRaku <jian.zhao@rakuten.com>
2022-12-21 10:37:11 +0900

    add User

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: 'admin@admin.com',
+  name: 'admin',
+  password: 'admin'
+)
+
+25.times do |n|
+  Task.create!(
+    title: "test#{n + 1}",
+    description: "テストテストテストテスト#{n + 1}",
+    status: n % 3,
+    user_id: 1
+  )
+end
