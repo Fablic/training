955177cdfdce1a4655a2c5b02c97ad0a2dfbacc5
anne ju <anne.ju@rakuten.com>
2022-12-21 13:47:57 +0900

    anne_ju_step11: add model validation specs

---
+++ myapp/spec/models/tasks_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Task, type: :model do
+  describe 'validations' do
+    subject { build(:task) }
+
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+  end
+end
