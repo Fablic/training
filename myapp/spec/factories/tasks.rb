9d4b188235048f433ac65f9c261d8a37c6ca0186
anne ju <anne.ju@rakuten.com>
2022-12-21 12:01:50 +0900

    anne_ju_step11: add migration for null false

--- myapp/spec/factories/tasks.rb
+++ myapp/spec/factories/tasks.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  description :string(255)      not null
+#  due_date    :datetime
+#  priority    :integer
+#  status      :integer
+#  title       :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 FactoryBot.define do
   factory :task do
     title { 'Buy chair' }
