3ba2188b581c74423e99b9548876afac071a8b1c
anne ju <anne.ju@rakuten.com>
2022-12-28 16:28:59 +0900

    anne_ju_step16: add gem bullet

--- myapp/config/environments/development.rb
+++ myapp/config/environments/development.rb
@@ -62,4 +62,13 @@ Rails.application.configure do
 
   # turn on web sonsole for debug
   config.web_console.permissions = '172.20.0.1'
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
--- myapp/spec/rails_helper.rb
+++ myapp/spec/rails_helper.rb
@@ -99,4 +99,15 @@ RSpec.configure do |config|
   end
 
   config.include FactoryBot::Syntax::Methods
+
+  if Bullet.enable?
+    config.before(:each) do
+      Bullet.start_request
+    end
+
+    config.after(:each) do
+      Bullet.perform_out_of_channel_notifications if Bullet.notification?
+      Bullet.end_request
+    end
+  end
 end
