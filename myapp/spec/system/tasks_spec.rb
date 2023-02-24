e798ead1b0bcba2703dbc89224bc9a2e18aff50a
Take-st <s.takewuw@gmail.com>
2023-02-24 18:06:42 +0900

    [add] i18n test case

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -13,4 +13,29 @@ RSpec.describe "Tasks", type: :system do
     end
   end
 
+  describe 'I18n' do
+    context 'Default locale' do
+      it 'shows Japanese pages'do
+        visit '/'
+        expect(page).to have_content 'タスク 一覧'
+      end
+    end
+
+    context 'English locale' do
+      it 'shows English pages'do
+        visit '/?locale=en'
+        expect(page).to have_content 'task'
+      end
+    end
+
+    context 'Moving another page when English locale' do
+      it 'takes over the locale setting and shows English pages'do
+        visit '/?locale=en'
+        expect(page).to have_content 'task list'
+        click_link('add')
+        expect(page).to have_content 'task new'
+      end
+    end
+  end
+
 end
