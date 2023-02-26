e9c22715a78582547bad175f4cfb4e68145074a1
Take-st <s.takewuw@gmail.com>
2023-02-27 07:46:41 +0900

    [add] locale test case

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -14,25 +14,33 @@ RSpec.describe "Tasks", type: :system do
   end
 
   describe 'I18n' do
-    context 'Default locale' do
-      it 'shows Japanese pages'do
+    context 'Specify nothing' do
+      it 'shows Default(Japanese) pages'do
         visit '/'
         expect(page).to have_content 'タスク 一覧'
       end
     end
 
-    context 'English locale' do
+    context 'Specify Japanese locale' do
+      it 'shows Japanese pages'do
+        visit '/?locale=ja'
+        expect(page).to have_content 'タスク 一覧'
+      end
+    end
+
+    context 'Specify English locale' do
       it 'shows English pages'do
         visit '/?locale=en'
         expect(page).to have_content 'task'
       end
     end
 
-    context 'Moving another page when English locale' do
+    context 'Moving another page from English page' do
       it 'takes over the locale setting and shows English pages'do
         visit '/?locale=en'
         expect(page).to have_content 'task list'
         click_link('add')
+        expect(current_url).to include 'locale=en'
         expect(page).to have_content 'task new'
       end
     end
