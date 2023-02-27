79418e9e415685e6d9b7b65e6dae2034f77ce5ee
Take-st <s.takewuw@gmail.com>
2023-02-27 11:26:17 +0900

    [add] E2E test

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -7,9 +7,99 @@ RSpec.describe 'Tasks', type: :system do
 
   # TODO: バリデーションの実装は後で行うので、異常系のテストも後回し
   describe 'GET /' do
-    it 'renders a successful response' do
+    before do
       visit '/'
+    end
+    it 'renders tasks list page' do
       expect(page).to have_content 'タスク 一覧'
+    end
+  end
+
+  describe 'GET /tasks' do
+    before do
+      create(:task, name: 'sample_task')
+      visit '/tasks'
+    end
+    it 'renders a successful response' do
+      expect(page).to have_content 'タスク 一覧'
+      expect(page).to have_content 'sample_task'
+    end
+  end
+
+  describe 'GET /tasks/:id' do
+    before do
+      @tasks = create(:task, name: 'sample_task')
+      visit "/tasks/#{@tasks.id}"
+    end
+    it 'renders a successful response' do
+      expect(page).to have_content 'タスク 詳細'
+      expect(page).to have_content 'sample_task'
+    end
+  end
+
+  describe 'GET /tasks/new' do
+    before do
+      visit '/tasks/new'
+    end
+    it 'renders tasks list page' do
+      expect(page).to have_content 'タスク 新規'
+    end
+  end
+
+  describe 'GET /tasks/:id/edit' do
+    before do
+      @tasks = create(:task, name: 'sample_task')
+      visit "/tasks/#{@tasks.id}/edit"
+    end
+    it 'renders a successful response' do
+      expect(page).to have_content 'タスク 編集'
+      expect(page).to have_selector 'input[value="sample_task"]'
+    end
+  end
+
+  describe 'Creating a new task successfully' do
+    before do
+      visit "/tasks"
+      click_link('追加')
+    end
+    it 'successfully create a task' do
+      expect(Task.all.length).to eq 0
+      fill_in 'task[name]', with: 'sample_task'
+      find('input[type="submit"]').click
+      expect(page).to have_content 'タスクが正常に登録されました。'
+      expect(page).to have_content 'タスク 詳細'
+      expect(page).to have_content 'sample_taask'
+      expect(Task.all.length).to eq 1
+    end
+  end
+
+  describe 'Updating a task successfully' do
+    before do
+      @tasks = create(:task, name: 'sample_task')
+      visit "/tasks/#{@tasks.id}/edit"
+    end
+    it 'successfully update a task' do
+      fill_in 'task[name]', with: 'hoge_task'
+      find('input[type="submit"]').click
+      expect(page).to have_content 'タスクが正常に更新されました。'
+      expect(page).to have_content 'タスク 詳細'
+      expect(page).to have_content 'hoge_task'
+      expect(Task.all.length).to eq 1
+    end
+  end
+
+  describe 'Deleting a task successfully' do
+    before do
+      @tasks = create(:task, name: 'sample_task')
+      visit "/tasks"
+    end
+    it 'successfully update a task' do
+      expect(Task.all.length).to eq 1
+      click_link('削除')
+      expect(page).to have_content 'タスクが正常に削除されました。'
+      expect(page).to have_content 'タスク 一覧'
+      expect(page).not_to have_content 'hoge_task'
+      expect(Task.all.length).to eq 0
     end
   end
 
