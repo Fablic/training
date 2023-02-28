2e9cb1c01707d725e987ea7bae2450b0c91df4c4
Take-st <s.takewuw@gmail.com>
2023-02-28 17:22:29 +0900

    [refactor] use let! instead of instance variables

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -136,26 +136,24 @@ RSpec.describe 'Tasks', type: :system do
   end
 
   describe 'Sorting by created_at' do
-    before do
-      @task1 = create(:task, name: 'hoge_task', created_at: Time.current.yesterday)
-      @task2 = create(:task, name: 'fuga_task', created_at: Time.current)
-    end
+    let!(:task1) { create(:task, name: 'hoge_task', created_at: Time.current.yesterday) }
+    let!(:task2) { create(:task, name: 'fuga_task', created_at: Time.current) }
 
     context 'Sorting asc => desc' do
       it 'sorts successfully' do
         visit '/tasks?sort_direction=asc&sort_key=created_at'
-        expect(page).to have_content %r{#{@task1.name}[\s\S]*#{@task2.name}}
+        expect(page).to have_content %r{#{task1.name}[\s\S]*#{task2.name}}
         click_link('作成日時')
-        expect(page).to have_content %r{#{@task2.name}[\s\S]*#{@task1.name}}
+        expect(page).to have_content %r{#{task2.name}[\s\S]*#{task1.name}}
       end
     end
 
     context 'Sorting desc => asc' do
       it 'sorts successfully' do
         visit '/tasks?sort_direction=desc&sort_key=created_at'
-        expect(page).to have_content %r{#{@task2.name}[\s\S]*#{@task1.name}}
+        expect(page).to have_content %r{#{task2.name}[\s\S]*#{task1.name}}
         click_link('作成日時')
-        expect(page).to have_content %r{#{@task1.name}[\s\S]*#{@task2.name}}
+        expect(page).to have_content %r{#{task1.name}[\s\S]*#{task2.name}}
       end
     end
   end
