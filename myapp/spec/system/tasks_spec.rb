e2992c91edf38456bd8450e6d2990dae3999c231
Take-st <s.takewuw@gmail.com>
2023-02-28 10:55:48 +0900

    [fix] use before do instead of let!

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe 'Tasks', type: :system do
     end
 
     it 'renders a successful response' do
-      visit '/tasks'
       expect(page).to have_content 'タスク 一覧'
       expect(page).to have_content 'sample_task'
     end
@@ -94,7 +93,6 @@ RSpec.describe 'Tasks', type: :system do
     end
 
     it 'successfully update a task' do
-      visit '/tasks'
       expect(Task.all.length).to eq 1
       click_link('削除')
       expect(page).to have_content 'タスクが正常に削除されました。'
