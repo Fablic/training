b10e187b6b0e4152b1753ac5306098094d4fe2b7
Take-st <s.takewuw@gmail.com>
2023-03-02 10:21:07 +0900

    [fix] arrangement of nest

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Task', type: :model do
-  describe 'validation' do
+  describe 'normal case' do
     context 'all valid parameters' do
       let(:task) {
         Task.new(name: 'sample_task',
@@ -14,58 +14,62 @@ RSpec.describe 'Task', type: :model do
         expect(task.errors.count).to eq 0
       end
     end
+  end
 
-    context 'invalid name, length is 0' do
-      let(:task) {
-        Task.new(name: '',
-                 description: 'hoge fuga',
-                 deadline_at: '2023-01-01T00:00')
-      }
+  describe 'validation' do
+    describe 'name' do
+      context 'length is 0' do
+        let(:task) {
+          Task.new(name: '',
+                  description: 'hoge fuga',
+                  deadline_at: '2023-01-01T00:00')
+        }
 
-      it 'has name validation error' do
-        task.valid?
-        expect(task.errors.count).to eq 1
-        expect(task.errors[:name].present?).to be true
+        it 'has name validation error' do
+          task.valid?
+          expect(task.errors.count).to eq 1
+          expect(task.errors[:name].present?).to be true
+        end
       end
-    end
 
-    context 'invalid name, length is 1' do
-      let(:task) {
-        Task.new(name: 'a',
-                 description: 'hoge fuga',
-                 deadline_at: '2023-01-01T00:00')
-      }
+      context 'length is 1' do
+        let(:task) {
+          Task.new(name: 'a',
+                  description: 'hoge fuga',
+                  deadline_at: '2023-01-01T00:00')
+        }
 
-      it 'has no error' do
-        task.valid?
-        expect(task.errors.count).to eq 0
+        it 'has no error' do
+          task.valid?
+          expect(task.errors.count).to eq 0
+        end
       end
-    end
 
-    context 'invalid name, length is 255' do
-      let(:task) {
-        Task.new(name: 'a' * 255,
-                 description: 'hoge fuga',
-                 deadline_at: '2023-01-01T00:00')
-      }
+      context 'length is 255' do
+        let(:task) {
+          Task.new(name: 'a' * 255,
+                  description: 'hoge fuga',
+                  deadline_at: '2023-01-01T00:00')
+        }
 
-      it 'has no error' do
-        task.valid?
-        expect(task.errors.count).to eq 0
+        it 'has no error' do
+          task.valid?
+          expect(task.errors.count).to eq 0
+        end
       end
-    end
 
-    context 'invalid name, over length limit' do
-      let(:task) {
-        Task.new(name: 'a' * 256,
-                 description: 'hoge fuga',
-                 deadline_at: '2023-01-01T00:00')
-      }
+      context 'length is 256' do
+        let(:task) {
+          Task.new(name: 'a' * 256,
+                  description: 'hoge fuga',
+                  deadline_at: '2023-01-01T00:00')
+        }
 
-      it 'has name validation error' do
-        task.valid?
-        expect(task.errors.count).to eq 1
-        expect(task.errors[:name].present?).to be true
+        it 'has name validation error' do
+          task.valid?
+          expect(task.errors.count).to eq 1
+          expect(task.errors[:name].present?).to be true
+        end
       end
     end
   end
