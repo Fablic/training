aa277e2f104625ef4d89bdc182f653c981fccaa7
Take-st <s.takewuw@gmail.com>
2023-03-02 18:27:02 +0900

    [fix] mistake

--- myapp/app/helpers/task_helper.rb
+++ myapp/app/helpers/task_helper.rb
@@ -1,6 +1,6 @@
 module TaskHelper
   def options_for_select_of_statuses
-    Task.statuses.map { |i| [I18n.t(i[0], scope: [:activerecord, :attributes, :task, :statuses]), i[1]] }
+    Task.statuses.map { |i| [I18n.t(i[0], scope: [:activerecord, :enums, :task, :status]), i[1]] }
   end
 
   def value_with_i18n(task, column_name)
