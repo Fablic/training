738fcd45d4fdf7dbdd28b4fc4b24d1b100313cb2
Take-st <s.takewuw@gmail.com>
2023-03-06 16:28:06 +0900

    [add] index of status columns of tasks table

---
+++ myapp/db/migrate/20230306072646_add_index_to_status_of_tasks.rb
@@ -0,0 +1,5 @@
+class AddIndexToStatusOfTasks < ActiveRecord::Migration[7.0]
+  def change
+    add_index :tasks, :status
+  end
+end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_005646) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_072646) do
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_005646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
