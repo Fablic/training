e5fd7d0368dd985df0a0cc6af30b5181446115b4
Take-st <s.takewuw@gmail.com>
2023-03-09 15:50:07 +0900

    [add] seed file

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ActiveRecord::Base.transaction do
+  user = User.create!(name: 'taro', email: 'taro2@hoge.hoge', password: 'password', password_confirmation: 'password')
+  user.tasks.create!(name: 'sample_task')
+end
