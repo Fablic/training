c49729aa9404ca74a670154639c4c652fda06a45
Take-st <s.takewuw@gmail.com>
2023-03-10 15:36:52 +0900

    [add] CRUD of user

---
+++ myapp/app/controllers/users_controller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+  before_action :fetch_user_by_params_id, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = I18n.t('flash.user.create.success')
+      return redirect_to @user
+    end
+
+    flash.now[:danger] = I18n.t('flash.user.create.failure')
+    render 'new', status: :unprocessable_entity
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = I18n.t('flash.user.update.success')
+      return redirect_to @user
+    end
+
+    flash.now[:danger] = I18n.t('flash.user.update.failure')
+    render 'edit', status: :unprocessable_entity
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = I18n.t('flash.user.delete.success')
+    else
+      flash.now[:danger] = I18n.t('flash.user.delete.failure')
+    end
+    redirect_to users_url
+  end
+
+  private
+
+  def fetch_user_by_params_id
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
