ba2a3514b27b06297dce385e91fa0ed0376b2837
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-10 13:18:02 +0900

    bulletの初期設定

--- myapp/config/environments/development.rb
+++ myapp/config/environments/development.rb
@@ -67,4 +67,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.after_initialize do
+    Bullet.enable = true # Bulletを有効化する
+    Bullet.alert = true # JavaScriptのポップアップアラートを表示する
+    Bullet.bullet_logger = true # log/bullet.logに出力
+    Bullet.console = true # ブラウザのconsole.logに出力
+    Bullet.rails_logger = true # Railsのログに結果を出力
+    Bullet.add_footer = true # ページの左下に結果を表示
+  end
 end
