31d7dbe374d2f3ad2ff24761227b537174ae9535
Take-st <s.takewuw@gmail.com>
2023-03-13 10:07:01 +0900

    [add] login 画面

---
+++ myapp/app/controllers/session_controller.rb
@@ -0,0 +1,14 @@
+class SessionController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def delete
+
+  end
+end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
   resources :users, path: '/admin/users'
+
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
 end
