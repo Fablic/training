42ef86ba79e1e8c136f31cef9644b7438476827a
Take-st <s.takewuw@gmail.com>
2023-03-13 10:57:53 +0900

    [add] login flow

--- myapp/app/controllers/session_controller.rb
+++ myapp/app/controllers/session_controller.rb
@@ -1,14 +1,29 @@
 class SessionController < ApplicationController
 
   def new
-
   end
 
   def create
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
 
+      # TODO: session につめる
+
+      flash.now[:danger] = I18n.t('flash.session.login.success')
+      return redirect_to tasks_path
+    else
+      flash.now[:danger] = I18n.t('flash.session.login.failure')
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def delete
 
+  end
+
+  private
+
+  def session_params
+    params.require('session').permit(:email, :password)
   end
 end
