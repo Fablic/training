07ce7c6a567913bbedd2747fa507b7d6af7c7e5f
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-16 15:22:41 +0900

    記述ルールのミスを修正

--- myapp/app/controllers/sessions_controller.rb
+++ myapp/app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path, flash: { success: I18n.t('sessions.flash.login.success') }
     else
-      flash.now[:danger] = I18n.t('sessions.flash.login.success'))
+      flash.now[:danger] = I18n.t('sessions.flash.login.success')
       render :new
     end
   end
