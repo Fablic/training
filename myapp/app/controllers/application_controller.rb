331c6d8a24aae9813cf51fb41e5a7d9eec6d8495
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-16 14:34:50 +0900

    ログイン機能の実装

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -3,8 +3,22 @@ class ApplicationController < ActionController::Base
   
   add_flash_types :success, :info, :warning, :danger
   rescue_from Exception, with: :render_500
+  rescue_from ActionController::BadRequest, with: :render_400
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
+
+  helper_method :current_user
+  before_action :login_required
+
+  def render_400(e = nil)
+    logger.error "Rendering 400 with excaption: #{e.message}" if e
+    
+    if request.format.to_sym == :json
+      render json: { error: '400 Bad Request' }, status: :not_found
+    else
+      render file: 'public/400.html', status: 404, layout: false, content_type: 'text/html'
+    end
+  end
 
   def render_404(e = nil)
     logger.error "Rendering 404 with excaption: #{e.message}" if e
@@ -25,4 +39,15 @@ class ApplicationController < ActionController::Base
       render file: 'public/404.html', status: 404, layout: false, content_type: 'text/html'
     end
   end
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    redirect_to login_url unless current_user
+  end
+
 end
--- myapp/app/controllers/sessions_controller.rb
+++ myapp/app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
-  def new
+  skip_before_action :login_required
+
+  def new; end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, flash: { success: I18n.t('sessions.flash.login.success') }
+    else
+      flash.now[:danger] = I18n.t('あああ')
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, flash: { success: I18n.t('sessions.flash.logout.success') }
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
