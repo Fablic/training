ee4a5aee0f631c4faa0850a01336c156ebc23a7a
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-16 15:23:27 +0900

    logoutパスのdeleteをgetに変更

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :controllers
   resources :users
   resources :tasks
-  
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/logout',  to: 'sessions#destroy'
   root 'tasks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
