9ce5453acb19e789a556649a83cf85a88b2ec4d3
Take-st <s.takewuw@gmail.com>
2023-03-17 11:44:13 +0900

    [fix] schema, add relation between users and tags

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :delete_all
+  has_many :tags, dependent: :delete_all
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_16_045810) do
   create_table "tags", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "name"], name: "index_tags_on_user_id_and_name", unique: true
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "task_tags", charset: "utf8mb4", force: :cascade do |t|
@@ -49,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_045810) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tags", "users"
   add_foreign_key "task_tags", "tags"
   add_foreign_key "task_tags", "tasks"
   add_foreign_key "tasks", "users"
