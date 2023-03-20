af753fb157a7c8558e1161e50dd04b016033a4f8
Take-st <s.takewuw@gmail.com>
2023-03-20 16:00:07 +0900

    [add] maintenance mode view and logic

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :render_maintenance, if: :maintenance_mode?
   around_action :switch_locale
   include SessionsHelper
 
@@ -14,5 +15,15 @@ class ApplicationController < ActionController::Base
 
   def render_404
     render file: Rails.public_path.join('404.html'), status: :not_found, layout: false, content_type: 'text/html'
+  end
+
+  def render_maintenance
+    render file: Rails.public_path.join('503.html'), status: :service_unavailable, layout: false, content_type: 'text/html'
+  end
+
+  private
+
+  def maintenance_mode?
+    File.exist?('tmp/maintenance.txt')
   end
 end
