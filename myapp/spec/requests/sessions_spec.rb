c7bbeffa1ec2a137061b4bb0f889f3dd037329a2
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-22 10:38:36 +0900

    session controllerのテストを追加

--- myapp/spec/requests/sessions_spec.rb
+++ myapp/spec/requests/sessions_spec.rb
@@ -8,4 +8,46 @@ RSpec.describe 'Sessions', type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'POST /login' do
+    let(:user) { create(:user) }
+
+    subject { post '/login', params: }
+
+    context 'valid params' do
+      let(:params) do 
+        { session: { email: user.email, password: user.password } }
+      end
+
+      it 'returns http success' do
+        subject
+
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'invalid params' do
+      let(:params) do 
+        { session: { email: 'no.user@rakuten.com', password: 'xxxx'} }
+      end
+
+      it 'returns http fail' do
+        subject
+
+        # expect(response).to be_a_new Session
+        expect(flash[:danger])
+      end
+    end
+  end
+
+  describe 'GET /logout' do
+    it 'returns http success' do
+      get '/logout'
+
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(root_path)
+    end
+  end
 end
+
