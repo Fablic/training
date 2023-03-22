738e0f41e58dffd3b23797fdcbce41e8072d7bd8
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-22 13:43:56 +0900

    メールアドレスのユニークキー制約対応

--- myapp/spec/factories/users.rb
+++ myapp/spec/factories/users.rb
@@ -19,6 +19,6 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |i| "name_#{i}" }
     password { '123' }
-    email { 'takasawa@rakuten.com' }
+    email { |i| "takasawa#{1}@rakuten.com" }
   end
 end
