f77ed836ff9a79820b44fe26bf13185b37b6b539
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-22 13:45:17 +0900

    bad request時のテストを追加

--- myapp/spec/requests/task_spec.rb
+++ myapp/spec/requests/task_spec.rb
@@ -344,4 +344,23 @@ RSpec.describe 'Tasks', type: :request do
       end
     end
   end
+  describe 'bad_request' do
+    let(:another_user) { create(:user, email: "another_user@rakuten.com") }
+    let(:task) { create(:task, user_id: another_user.id) }
+
+    it 'show url' do
+      get task_url(task)
+      expect(response).to have_http_status :bad_request
+    end
+
+    it 'update url' do
+      put task_url(task)
+      expect(response).to have_http_status :bad_request
+    end
+
+    it 'destroy url' do
+      delete task_url(task)
+      expect(response).to have_http_status :bad_request
+    end
+  end
 end
