87e159e9b7091076756aab4289841ff446082cd2
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-27 09:56:12 +0900

    インデントずれを修正

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :system do
   let(:user) { create(:user, name: 'test_kun') }
   describe 'GET /index' do
-    
     context "When tasks and task.user_id data rendered" do
       let!(:task) do
         create(
@@ -15,12 +14,12 @@ RSpec.describe "Tasks", type: :system do
           user_id: user.id
         )
       end
-        it 'exist user name' do
-          get(tasks_url)
-          expect(response.body).to include 'test_title'
-          expect(response.body).to include '低'
-          expect(response.body).to include '未着手'
-          expect(response.body).to include 'test_kun'
+      it 'exist user name' do
+        get(tasks_url)
+        expect(response.body).to include 'test_title'
+        expect(response.body).to include '低'
+        expect(response.body).to include '未着手'
+        expect(response.body).to include 'test_kun'
       end
     end
   end
