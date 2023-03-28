c9250df05cdee63ddba2d50734138caea6380e1f
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-28 18:39:14 +0900

    railsの機能を試していたときに書いた不要な記述を削除

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :controllers
   resources :users
   resources :tasks
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get    '/logout',  to: 'sessions#destroy'
-  
+
   root   'tasks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
