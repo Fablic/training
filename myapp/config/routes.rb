6f08aa7c6312852c49d05e8337cd964e6f77e8ce
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-28 16:58:09 +0900

    スペースを揃える

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  get '/logout',  to: 'sessions#destroy'
-  root 'tasks#index'
+  get    '/logout',  to: 'sessions#destroy'
+  
+  root   'tasks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
