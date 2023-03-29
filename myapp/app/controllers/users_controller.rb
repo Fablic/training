3cd2c8d8670972bbcd3ad5922aa70eba6f62f32a
tomohiro-takasawa <tomohiro.takasawa@rakuten.com>
2023-03-29 15:28:23 +0900

    permitにroleを追加

--- myapp/app/controllers/users_controller.rb
+++ myapp/app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:id, :name, :email, :password, :created_at, :updated_at)
+    params.require(:user).permit(:id, :name, :email, :password, :role, :created_at, :updated_at)
   end
 
   def search_params
