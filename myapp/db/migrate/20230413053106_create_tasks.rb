5f6289775638d535edea6264355bb58c70e5f8ed
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-04-13 14:57:43 +0900

    Revert "Taskモデル作成"
    This reverts commit b8fa36a914ac4a0d0c1f665448e3cacc0959cf43.

--- myapp/db/migrate/20230413053106_create_tasks.rb
+++
@@ -1,9 +0,0 @@
-class CreateTasks < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tasks do |t|
-      t.string  :title, null: false
-      t.string  :outline, null: false
-      t.timestamps
-    end
-  end
-end
