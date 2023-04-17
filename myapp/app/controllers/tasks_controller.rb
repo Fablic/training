0075549778ee609e7fa5f78b43bae04d79b6be2b
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-04-17 11:54:52 +0900

    タスク新規登録機能

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,8 +1,25 @@
 class TasksController < ApplicationController
    def index
+      @task = Task.all
    end
 
    def new
       @task = Task.new
    end
+
+   def create
+      @task = Task.new(task_params)
+      if @task.valid?
+         @task.save
+         redirect_to tasks_path
+      else
+         render :new
+      end
+   end
+
+   private
+   def task_params
+      params.require(:task).permit(:title, :content)
+   end
+
 end
