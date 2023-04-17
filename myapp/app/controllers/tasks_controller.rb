883be1a9ae2763ebbb25fa9e8452a5c46877d3fe
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-04-17 13:59:59 +0900

    update処理の追加

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+   before_action :set_task, only: [:edit, :update]
    def index
       @task = Task.all
    end
@@ -18,10 +19,21 @@ class TasksController < ApplicationController
    end
 
    def edit
-      @task = Task.find(params[:id])
+   end
+
+   def update
+      if @task.update(task_params)
+         redirect_to tasks_path
+      else
+         render :edit
+      end
    end
 
    private
+   def set_task
+      @task = Task.find(params[:id])
+   end
+
    def task_params
       params.require(:task).permit(:title, :content)
    end
