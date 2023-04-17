78c980ce15b3737a26c5bcfc06be51f8f11ff587
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-04-17 15:03:00 +0900

    rubocopの自動修正

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,51 +1,50 @@
 class TasksController < ApplicationController
-   before_action :set_task, only: [:edit, :update, :destroy]
-   def index
-      @task = Task.all
-   end
+  before_action :set_task, only: %i[edit update destroy]
+  def index
+    @task = Task.all
+  end
 
-   def new
-      @task = Task.new
-   end
+  def new
+    @task = Task.new
+  end
 
-   def create
-      @task = Task.new(task_params)
-      if @task.valid?
-         @task.save
-         flash[:notice] = "登録に成功しました。"
-         redirect_to tasks_path
-      else
-         flash[:alret] = "登録に失敗しました。"
-         render :new
-      end
-   end
-
-   def edit
-   end
-
-   def update
-      if @task.update(task_params)
-         flash[:notice] = "更新に成功しました。"
-         redirect_to tasks_path
-      else
-         flash[:alret] = "更新に失敗しました。"
-         render :edit
-      end
-   end
-
-   def destroy
-      @task.destroy
-      flash[:notice] = "削除に成功しました。"
+  def create
+    @task = Task.new(task_params)
+    if @task.valid?
+      @task.save
+      flash[:notice] = '登録に成功しました。'
       redirect_to tasks_path
-   end
+    else
+      flash[:alret] = '登録に失敗しました。'
+      render :new
+    end
+  end
 
-   private
-   def set_task
-      @task = Task.find(params[:id])
-   end
+  def edit; end
 
-   def task_params
-      params.require(:task).permit(:title, :content)
-   end
+  def update
+    if @task.update(task_params)
+      flash[:notice] = '更新に成功しました。'
+      redirect_to tasks_path
+    else
+      flash[:alret] = '更新に失敗しました。'
+      render :edit
+    end
+  end
 
+  def destroy
+    @task.destroy
+    flash[:notice] = '削除に成功しました。'
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
