e1079b3a14ef553b78a365ad17c471b69716ad4a
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-04-25 14:53:38 +0900

    timezoneコメント追加

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -16,7 +16,11 @@ module Myapp
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
+    # タイムゾーンを日本に設定
     config.time_zone = 'Tokyo'
+    # 取得した時間をローカルタイムとして扱う
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
