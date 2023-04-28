f8db3cbaf724e4f65e856b7e48a7f4e387004cd1
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-04-28 15:58:56 +0900

    タスクのステータスカラム追加

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -49,6 +49,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :task_status_id)
   end
 end
