a94c6868064fabf25be7f03b4114e46e66bc43fd
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-08 09:19:31 +0900

    destroyメソッドエラーハンドリング

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,14 +1,17 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[edit update destroy show]
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
     @tasks = Task.all.order(created_at: 'DESC')
   end
 
+  def show; end
+
   def new
     @task = Task.new
   end
+
+  def edit; end
 
   def create
     @task = Task.new(task_params)
@@ -19,8 +22,6 @@ class TasksController < ApplicationController
     end
   end
 
-  def edit; end
-
   def update
     if @task.update(task_params)
       redirect_to tasks_path, notice: t('messages.update', model_name: t('activerecord.models.task'))
@@ -30,20 +31,17 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task.destroy
-    redirect_to tasks_path, notice: t('messages.delete', model_name: t('activerecord.models.task'))
+    if @task.destroy
+      redirect_to tasks_path, notice: t('messages.delete', model_name: t('activerecord.models.task'))
+    else
+      render :index
+    end
   end
-
-  def show; end
 
   private
 
   def set_task
     @task = Task.find(params[:id])
-  end
-
-  def record_not_found
-    redirect_to root_path, alert: t('error.messages.record_not_found', model_name: t('activerecord.models.task'))
   end
 
   def task_params
