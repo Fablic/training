012769fefd4bae55b774f4de1a3058425d456bc3
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-08 09:40:40 +0900

    例外処理のエラーログを出力

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -8,7 +8,11 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  def record_not_found
+  def record_not_found(e = nil)
+    if e
+      logger.info "ActiveRecord::RecordNotFound!: #{e.message}"
+      logger.info e.backtrace.join("\n")
+    end
     redirect_to root_path, alert: t('error.messages.record_not_found')
   end
 end
