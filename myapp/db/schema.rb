a66eba4ac02068af9eea6a9dd3b4871b95efffa5
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-09 09:28:40 +0900

    deadlineカラムの追加

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_13_074916) do
+ActiveRecord::Schema.define(version: 2023_05_08_235119) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", limit: 30, null: false
     t.string "content"
+    t.date "deadline", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
