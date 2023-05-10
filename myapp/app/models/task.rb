b455903785400dce56d3ab596aec39ddaa850f8d
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-10 10:29:50 +0900

    Taskモデルにバリテーション、検索処理を追記

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,6 +1,16 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
+  validates :status, inclusion: { in: ['未着手','着手中','完了済'] }
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :task_status
+  def self.looks(search, word)
+    if search == 'not_started_task'
+      @tasks = Task.where('title LIKE? AND status LIKE?', "%#{word}%", "未着手")
+    elsif search == 'started_task'
+      @tasks = Task.where("title LIKE? AND status LIKE?", "%#{word}%", "着手中")
+    elsif search == 'completed_task'
+      @tasks = Task.where('title LIKE? AND status LIKE?', "%#{word}%", "完了済")
+    else
+      @tasks = Task.all
+    end
+  end
 end
