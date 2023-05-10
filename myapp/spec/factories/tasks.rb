c313cc2a722762f941f2f7fd4dcfabcee368b083
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-10 13:11:39 +0900

    終了期限のモデルテスト

--- myapp/spec/factories/tasks.rb
+++ myapp/spec/factories/tasks.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :task do
     title { Faker::Lorem.word }
     content { Faker::Lorem.word }
+    status  { '着手中' }
   end
 end
--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'バリデーションのテスト' do
-    context 'タスク名と詳細が入力されている時' do
+    context 'タスク名、概要、終了期限が入力されている時' do
       it 'タスクを登録できる' do
         task = build(:task)
 
@@ -35,12 +35,21 @@ RSpec.describe Task, type: :model do
       end
     end
 
-    context 'タスク名が30文字以上の場合' do
+    context 'タスク名が30文字以上の時' do
       it 'タスクを登録できず、タスク名は30文字以内で入力してくださいと表示' do
         task = build(:task, title: 'a' * 31)
 
         expect(task).to be_invalid
         expect(task.errors.full_messages).to include('タスク名は30文字以内で入力してください')
+      end
+    end
+
+    context '終了期限を選択していない時' do
+      it 'タスクを登録できず、ステータスは一覧にありませんと表示' do
+        task = build(:task, status: '------')
+
+        expect(task).to be_invalid
+        expect(task.errors.full_messages).to include('ステータスは一覧にありません')
       end
     end
   end
