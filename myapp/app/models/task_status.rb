d83e45323bc24e307a79b50a8d8302ce9e0ba729
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-10 10:26:43 +0900

    ActiveHashかselectに方式に変更

--- myapp/app/models/task_status.rb
+++
@@ -1,7 +0,0 @@
-class TaskStatus < ActiveHash::Base
-  self.data = [
-    { id: 1, name: '未着手' },
-    { id: 2, name: '着手中' },
-    { id: 3, name: '完了済' }
-  ]
-end
---
+++ myapp/db/migrate/20230509042420_add_task_status_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddTaskStatusToTasks < ActiveRecord::Migration[6.0]
+  def change
+    add_column :tasks, :status, :string, :null => false
+  end
+end
