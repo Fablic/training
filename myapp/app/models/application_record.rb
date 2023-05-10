0cacde0c83561913de3db80850ffc22d89202b26
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-10 13:35:16 +0900

    scope記載位置の変更

--- myapp/app/models/application_record.rb
+++ myapp/app/models/application_record.rb
@@ -1,5 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  scope :created, -> { order(deadline: :asc) }
-  scope :updated, -> { order(deadline: :desc) }
 end
--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :deadline, presence: true
+
+  scope :deadleine_asc, -> { order(deadline: :asc) }
+  scope :deadline_desc, -> { order(deadline: :desc) }
 end
