d211bc2005663b5d652aaec2d2fa1dc5eb17bdce
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-10 10:45:05 +0900

    rubocop修正

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,16 +1,16 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
-  validates :status, inclusion: { in: ['未着手','着手中','完了済'] }
+  validates :status, inclusion: { in: ['未着手', '着手中', '完了済'] }
 
   def self.looks(search, word)
-    if search == 'not_started_task'
-      @tasks = Task.where('title LIKE? AND status LIKE?', "%#{word}%", "未着手")
-    elsif search == 'started_task'
-      @tasks = Task.where("title LIKE? AND status LIKE?", "%#{word}%", "着手中")
-    elsif search == 'completed_task'
-      @tasks = Task.where('title LIKE? AND status LIKE?', "%#{word}%", "完了済")
-    else
-      @tasks = Task.all
-    end
+    @tasks = if search == 'not_started_task'
+               Task.where('title LIKE? AND status LIKE?', "%#{word}%", '未着手')
+             elsif search == 'started_task'
+               Task.where('title LIKE? AND status LIKE?', "%#{word}%", '着手中')
+             elsif search == 'completed_task'
+               Task.where('title LIKE? AND status LIKE?', "%#{word}%", '完了済')
+             else
+               Task.all
+             end
   end
 end
--- myapp/db/migrate/20230509042420_add_task_status_id_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddTaskStatusIdToTasks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tasks, :task_status_id, :integer, :null => false
-  end
-end
