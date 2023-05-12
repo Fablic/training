c08686d1da00ea24ffe26203fcf173999e40cced
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-12 14:34:01 +0900

    rubocop

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :deadline, presence: true
 
-  enum status: {not_started: 0, start: 1, completed: 2}
+  enum status: { not_started: 0, start: 1, completed: 2 }
 
   scope :where_title, -> (title) { where('title like ?', "%#{title}%") if title.present? }
   scope :where_status, -> (status) { where(status: status) if status.present? }
