193756453ee40e38ed0aa9cdf9e8974c8a8bf7c4
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-16 15:48:37 +0900

    引数名を変更

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :deadline, presence: true
 
-  scope :deadline_order, -> (v){ order(deadline: v) }
+  scope :deadline_order, -> (sort){ order(deadline: sort) }
 end
