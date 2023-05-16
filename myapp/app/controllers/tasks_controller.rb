7e8afc74233d98ffde5ec288e5ff0d2931154495
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-16 15:45:29 +0900

    終了期限ソートのスコープメソッド名を統合

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -2,10 +2,8 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[edit update destroy show]
 
   def index
-    @tasks = if params[:deadline_asc]
-               Task.deadline_asc.page(params[:page]).per(5)
-             elsif params[:deadline_desc]
-               Task.deadline_desc.page(params[:page]).per(5)
+    @tasks = if params[:deadline_order]
+               Task.deadline_order(params[:deadline_order]).page(params[:page]).per(5)
              else
                Task.all.order(created_at: 'DESC').page(params[:page]).per(5)
              end
