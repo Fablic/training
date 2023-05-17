9c4f745ca8e27e384474643b40a1024060cc8f3d
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-17 09:55:34 +0900

    条件式を変更

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[edit update destroy show]
 
   def index
-    @tasks = if params[:deadline_order]
+    @tasks = if params[:deadline_order] == 'asc' || params[:deadline_order] == 'desc'
                Task.deadline_order(params[:deadline_order]).page(params[:page]).per(5)
              else
                Task.all.order(created_at: 'DESC').page(params[:page]).per(5)
