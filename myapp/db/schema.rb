531374e48419cefd08186ff7cc5227b6d5951375
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-18 16:26:37 +0900

    履歴

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_18_043938) do
+ActiveRecord::Schema.define(version: 2023_05_18_050006) do
+
+  create_table "task_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_task_users_on_task_id"
+    t.index ["user_id", "task_id"], name: "index_task_users_on_user_id_and_task_id"
+    t.index ["user_id"], name: "index_task_users_on_user_id"
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", limit: 30, null: false
@@ -23,12 +33,13 @@ ActiveRecord::Schema.define(version: 2023_05_18_043938) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name", limit: 191, null: false
-    t.string "email", limit: 191, null: false
-    t.string "password_digest", limit: 191, null: false
-    t.string "password_token", limit: 191
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "task_users", "tasks"
+  add_foreign_key "task_users", "users"
 end
