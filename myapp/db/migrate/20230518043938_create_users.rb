1c8fddfd9a0c4fad118952c65c4ffff6da1e87cc
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-18 16:25:57 +0900

    userテーブル

--- myapp/db/migrate/20230518043938_create_users.rb
+++ myapp/db/migrate/20230518043938_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string  :name, null: false, limit: 191
-      t.string  :email, null: false, limit: 191
-      t.string  :password_digest, null: false, limit: 191
-      t.string  :password_token, limit: 191
+      t.string  :name, null: false
+      t.string  :email, null: false
+      t.string  :password_digest, null: false
+
       t.timestamps
     end
   end
