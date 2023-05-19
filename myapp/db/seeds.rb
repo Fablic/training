509beba5157857966bb6b02e32487c8419efa52a
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-19 14:19:09 +0900

    初期データ修正

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -1,5 +1,21 @@
-User.create!(
-  name: 'テスト太郎',
-  email: 'test@test.com',
-  password_digest: 'hogehoge'
-)
+USER_NUM = 3
+TASK_NUM = 3
+
+USER_NUM.times do |i|
+  name = Faker::Name.name
+  user = User.create!(
+    name: name,
+    email: "test#{i}@gmail.com",
+    password_digest: 'password',
+  )
+  TASK_NUM.times do |j|
+    deadline = Faker::Date.backward
+    content = Faker::Lorem.word
+    user.tasks.create(
+      title: "#{user.name}-test#{j}",
+      content: content,
+      status: rand(0..2),
+      deadline: deadline,
+    )
+  end
+end
