6308d02bb8ae22b13603444bba7684a6b5b7ada2
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-19 16:04:50 +0900

    テスト修正

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: User::VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }, format: { with: User::VALID_EMAIL_REGEX }
   has_secure_password
 end
--- myapp/spec/models/user_spec.rb
+++ myapp/spec/models/user_spec.rb
@@ -1,53 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user_valid) {
-    FactoryBot.build(:user, id: 1)
+  let(:user) {
+    build(:user, id: 1)
   }
 
   context 'emailのバリデーション' do
     it 'emailの長さが256文字以上だと無効' do
-      user_valid.email = 'a' * 256
-      expect(user_valid).not_to be_valid
+      user.email = 'a' * 256
+      expect(user).not_to be_valid
     end
 
     it '同じものが2つ以上あると無効' do
-      user_exist = FactoryBot.create(:user, email: 'fuga@example.com')
-      user_valid.email = user_exist.email
-      expect(user_valid).not_to be_valid
+      user_exist = create(:user, email: 'fuga@example.com')
+      user.email = user_exist.email
+      expect(user).not_to be_valid
     end
 
     it '@から始まるものは無効' do
-      user_valid.email = '@example.com'
-      expect(user_valid).not_to be_valid
+      user.email = '@example.com'
+      expect(user).not_to be_valid
     end
 
     it '@がないものは無効' do
-      user_valid.email = 'hugaexample.com'
-      expect(user_valid).not_to be_valid
+      user.email = 'hugaexample.com'
+      expect(user).not_to be_valid
     end
 
     it '2次ドメインが入っていないものは無効' do
-      user_valid.email = 'huga@.com'
-      expect(user_valid).not_to be_valid
+      user.email = 'huga@.com'
+      expect(user).not_to be_valid
     end
 
     it 'ドットがないものは無効' do
-      user_valid.email = 'huga@examplecom'
-      expect(user_valid).not_to be_valid
+      user.email = 'huga@examplecom'
+      expect(user).not_to be_valid
     end
 
     it 'トップレベルドメインがないものは無効' do
-      user_valid.email = 'huga@example.'
-      expect(user_valid).not_to be_valid
+      user.email = 'huga@example.'
+
+      expect(user).not_to be_valid
     end
   end
 
   context '外部キー制約' do
     it 'ユーザーを削除した時にタスクも削除される' do
-      user = FactoryBot.create(:user)
-      task = FactoryBot.create(:task, user_id: user.id)
+      user = create(:user)
+      task = create(:task, user_id: user.id)
+
       User.destroy(user.id)
+
       expect(Task.find_by(id: task.id)).to be_nil
     end
   end
