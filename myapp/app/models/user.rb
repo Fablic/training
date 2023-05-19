8b2358644d1a8dcee0e876003587dad185837a1a
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-19 14:14:11 +0900

    アソシエーションを変更

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  has_many :task_users, dependent: :destroy
-  has_many :tasks, through: :task_users
+  has_many :tasks, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: User::VALID_EMAIL_REGEX }
+  has_secure_password
 end
