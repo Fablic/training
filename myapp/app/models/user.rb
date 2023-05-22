8a98830e9da8558d1d183b7a53da56449f92bdb1
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-22 14:19:00 +0900

    nameのバリデーションを追加

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
+  validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }, format: { with: User::VALID_EMAIL_REGEX }
   has_secure_password
