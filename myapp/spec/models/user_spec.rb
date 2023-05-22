cfae5ef385c0b50914d8f989774bf2f49e7d40fd
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-22 11:49:20 +0900

    エラーメッセージの取得比較するテストを追加

--- myapp/spec/models/user_spec.rb
+++ myapp/spec/models/user_spec.rb
@@ -1,57 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) {
-    build(:user, id: 1)
-  }
+  describe 'バリデーションのテスト' do
+    let(:user) {
+      build(:user, id: 1)
+    }
+    context '全て入力したとき' do
+      it '正常に登録できる' do
 
-  context 'emailのバリデーション' do
-    it 'emailの長さが256文字以上だと無効' do
-      user.email = 'a' * 256
-      expect(user).not_to be_valid
+        expect(user).to be_valid
+      end
     end
 
-    it '同じものが2つ以上あると無効' do
-      user_exist = create(:user, email: 'fuga@example.com')
-      user.email = user_exist.email
-      expect(user).not_to be_valid
-    end
+    context 'emailのバリデーション' do
+      it 'emailの長さが256文字以上だと無効' do
+        text = 'a' * 256
+        user.email = "#{text}" + '@example.com'
 
-    it '@から始まるものは無効' do
-      user.email = '@example.com'
-      expect(user).not_to be_valid
-    end
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include('Emailは255文字以内で入力してください')
+      end
 
-    it '@がないものは無効' do
-      user.email = 'hugaexample.com'
-      expect(user).not_to be_valid
-    end
+      it '同じものが2つ以上あると無効' do
+        user_exist = create(:user, email: 'fuga@example.com')
+        user.email = user_exist.email
 
-    it '2次ドメインが入っていないものは無効' do
-      user.email = 'huga@.com'
-      expect(user).not_to be_valid
-    end
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include('Emailはすでに存在します')
+      end
 
-    it 'ドットがないものは無効' do
-      user.email = 'huga@examplecom'
-      expect(user).not_to be_valid
-    end
+      it '@から始まるものは無効' do
+        user.email = '@example.com'
 
-    it 'トップレベルドメインがないものは無効' do
-      user.email = 'huga@example.'
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include('Emailは不正な値です')
+      end
 
-      expect(user).not_to be_valid
+      it '@がないものは無効' do
+        user.email = 'hugaexample.com'
+
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include('Emailは不正な値です')
+      end
+
+      it '2次ドメインが入っていないものは無効' do
+        user.email = 'huga@.com'
+
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include('Emailは不正な値です')
+      end
+
+      it 'ドットがないものは無効' do
+        user.email = 'huga@examplecom'
+
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include('Emailは不正な値です')
+      end
+
+      it 'トップレベルドメインがないものは無効' do
+        user.email = 'huga@example.'
+
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include('Emailは不正な値です')
+      end
     end
   end
 
-  context '外部キー制約' do
-    it 'ユーザーを削除した時にタスクも削除される' do
-      user = create(:user)
-      task = create(:task, user_id: user.id)
+  describe '外部キー制約のテスト' do
+    context 'ユーザーを削除したとき' do
+      it '関連タスクも削除される' do
+        user = create(:user)
+        task = create(:task, user_id: user.id)
 
-      User.destroy(user.id)
+        User.destroy(user.id)
 
-      expect(Task.find_by(id: task.id)).to be_nil
+        expect(Task.find_by(id: task.id)).to be_nil
+      end
     end
   end
 end
