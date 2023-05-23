1e2d1a7757cf78df11c106e4dc2ab6c1e9dfaeb0
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-05-23 16:27:06 +0900

    user、sessionのルーティング

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
   root to: 'tasks#index'
   resources :tasks do
     get :search, on: :collection
   end
+  resources :users, only: %i[new create]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
