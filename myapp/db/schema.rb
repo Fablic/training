93a9357188a28795ec1fa17e78f0748e81ac107e
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-06-01 09:41:42 +0900

    ラベル用テーブル作成

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_26_055737) do
+ActiveRecord::Schema.define(version: 2023_06_01_001222) do
+
+  create_table "labellings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "task_id"
+    t.bigint "label_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["label_id"], name: "index_labellings_on_label_id"
+    t.index ["task_id", "label_id"], name: "index_labellings_on_task_id_and_label_id", unique: true
+    t.index ["task_id"], name: "index_labellings_on_task_id"
+  end
+
+  create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", limit: 30, null: false
@@ -33,5 +49,7 @@ ActiveRecord::Schema.define(version: 2023_05_26_055737) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "labellings", "labels"
+  add_foreign_key "labellings", "tasks"
   add_foreign_key "tasks", "users"
 end
