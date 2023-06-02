b99ae6012e08f4b131b7a2f7a77d3c339b398554
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-06-02 13:01:55 +0900

    enumのvalueを変更

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 8 }
 
-  enum role: { general: false, admin: true }
+  enum role: { general: 0, admin: 1 }
 end
