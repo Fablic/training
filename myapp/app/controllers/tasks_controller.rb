9049c7d1bd73622554f86b313c55a28e575979f9
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-06-06 14:43:09 +0900

    検索時の値をフォームにセット

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -52,7 +52,7 @@ class TasksController < ApplicationController
     @title = params[:title]
     @status = params[:status]
     @deadline_order = params[:deadline_order]
-    @label_id = params[:label_id]
+    @label = params[:label]
     render :index
   end
 
