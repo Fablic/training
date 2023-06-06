e6abfff8f7f537e74f952e026d7cf39a39cda7f4
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-06-06 14:52:20 +0900

    labelのモデルテスト

--- myapp/spec/models/label_spec.rb
+++ myapp/spec/models/label_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe Label, type: :model do
 
         expect(label).to be_valid
       end
+
+      it 'nameが20文字以内なら正常に登録できる' do
+        label.name = 'a' * 20
+
+        expect(label).to be_valid
+      end
     end
 
     context 'nameのバリデーション' do
@@ -20,12 +26,34 @@ RSpec.describe Label, type: :model do
         expect(label.errors.full_messages).to include('ラベルを入力してください')
       end
 
-      it '同じものが2つ以上あると無効' do
+      it 'nameが21文字以上だと無効' do
+        label.name = 'a' * 21
+
+        expect(label).not_to be_valid
+        expect(label.errors.full_messages).to include('ラベルは20文字以内で入力してください')
+      end
+
+      it '同じラベル名が2つ以上あると無効' do
         label_exist = create(:label, name: 'foo')
         label.name = label_exist.name
 
         expect(label).not_to be_valid
         expect(label.errors.full_messages).to include('ラベルはすでに存在します')
+      end
+    end
+  end
+
+  describe '外部キー制約のテスト' do
+    let!(:user) { create(:user) }
+    let!(:label) { create(:label, user: user) }
+    let!(:task) { create(:task, user_id: user.id, labels: [label]) }
+
+    context 'ユーザーを削除したとき' do
+      it '関連ラベルも削除される' do
+        User.destroy(user.id)
+
+        expect(Task.find_by(id: task.id)).to be_nil
+        expect(Label.find_by(id: label.id)).to be_nil
       end
     end
   end
