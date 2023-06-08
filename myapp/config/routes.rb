8ba5b63c45eb88fcadc4efc3bf4a259c57f476c4
coffee-stain-nako <ts-tatsuya.koyama@rakuten.com>
2023-06-08 16:24:39 +0900

    ルーティング修正

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -10,6 +10,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get 'maintenances/index', as: 'maintenance_index'
   get '/maintenance' => 'maintenances#index', as: 'maintenance'
 end
