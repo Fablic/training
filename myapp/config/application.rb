5a1e2e0e1a8e730ac02f6448cc909c982eaab38a
Kami-naga <yuji.kaminaga@rakuten.com>
2023-06-21 09:56:22 +0900

    configure rails application time zone

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -13,6 +13,8 @@ module Myapp
     I18n.available_locales = [:en, :jp]
     I18n.default_locale = :en
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
