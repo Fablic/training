9273e6a9824fc0d81f4542f74c6982367714f374
Kami-naga <yuji.kaminaga@rakuten.com>
2023-06-27 11:25:35 +0900

    refactor the params sanitization for better reuse

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -5,9 +5,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    sort_column = Task.column_names.include?(task_list_params[:sort_column]) ? task_list_params[:sort_column] : 'created_at'
-    sort_direction = %w[ASC DESC].include?(task_list_params[:sort_direction]) ? task_list_params[:sort_direction] : 'ASC'
-    @tasks = Task.all.order("#{sort_column} #{sort_direction}")
+    @tasks = Task.all.order("#{task_list_params[:sort_column]} #{task_list_params[:sort_direction]}")
   end
 
   def show
@@ -54,5 +52,9 @@ class TasksController < ApplicationController
 
   def task_list_params
     params.permit(:sort_column, :sort_direction)
+    {
+      sort_column: Task.column_names.include?(params[:sort_column]) ? params[:sort_column] : 'created_at',
+      sort_direction: %w[ASC DESC].include?(params[:sort_direction]) ? params[:sort_direction] : 'ASC',
+    }
   end
 end
