c9ed782a71767579fe0018df7f82b1d70afc8141
Kami-naga <yuji.kaminaga@rakuten.com>
2023-06-30 16:55:22 +0900

    add model tests

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -92,4 +92,32 @@ RSpec.describe Task do
       expect(task.errors[:priority]).to eq ['is not a number']
     end
   end
+
+  describe 'check_scope' do
+    before do
+      @task1 = create(:task, name: 'Task1', priority: Task.priority_types[:High], status: Task.status_types[:Doing], expired_date: '2023-06-30')
+      @task2 = create(:task, name: 'Task2', priority: Task.priority_types[:Low], status: Task.status_types[:Done], expired_date: '2024-06-30')
+      @task3 = create(:task, name: 'Task3', priority: Task.priority_types[:Medium], status: Task.status_types[:Todo], expired_date: '2023-07-30')
+    end
+
+    it 'search by name successfully', :aggregate_failures do
+      tasks = Task.search_by_name("2")
+      expect(tasks).to include @task2
+      expect(tasks).not_to include @task1
+      expect(tasks).not_to include @task3
+    end
+
+    it 'search by status successfully', :aggregate_failures do
+      tasks = Task.search_by_status(Task.status_types[:Doing])
+      expect(tasks).to include @task1
+      expect(tasks).not_to include @task2
+      expect(tasks).not_to include @task3
+    end
+
+    it 'get correct order by sorting', :aggregate_failures do
+      tasks = Task.sort_by_column('expired_date', 'DESC')
+      expect(tasks).to match_array [@task2, @task3, @task1]
+    end
+
+  end
 end
