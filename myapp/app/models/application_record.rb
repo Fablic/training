7eb9d16151158b0459e49c98edb3d26a0d3e62d9
Kami-naga <yuji.kaminaga@rakuten.com>
2023-06-30 16:44:45 +0900

    add i18n support for enum type(status&priority) and search&sort button

--- myapp/app/models/application_record.rb
+++ myapp/app/models/application_record.rb
@@ -3,4 +3,16 @@
 # some comments for application record
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def enum_i18n(enum_name)
+    return nil if self.send(enum_name).nil?
+
+    I18n.t!("enums.#{self.model_name.i18n_key}.#{enum_name}.#{self.send(enum_name)}")
+  end
+
+  def self.enums_i18n(enum_name)
+    self.send(enum_name.to_s.pluralize).transform_keys do |key|
+      I18n.t!("enums.#{self.model_name.i18n_key}.#{enum_name}.#{key}")
+    end
+  end
 end
