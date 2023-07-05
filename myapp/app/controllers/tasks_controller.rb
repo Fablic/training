10ddef0b950f84e1f109f547dba240ca52b78b64
Kami-naga <yuji.kaminaga@rakuten.com>
2023-07-05 14:37:54 +0900

    Revert "Revert "enable search & sort together and keep the search condition when sorting""
    This reverts commit 5e091c429ce9a12a92e7b6be014ff0ea56fac29e.

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -3,11 +3,12 @@
 # some comments here for task controller
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :get_index_page_params, only: [:index]
 
   def index
-    @tasks = Task.all.search_by_name(search_params[:search_name])
-                 .search_by_status(search_params[:search_status])
-                 .sort_by_column(sort_params[:sort_column], sort_params[:sort_direction])
+    @tasks = Task.search_by_name(@search_name)
+                 .search_by_status(@search_status)
+                 .sort_by_column(@sort_column, @sort_direction)
   end
 
   def show
@@ -52,34 +53,19 @@ class TasksController < ApplicationController
     params.require(:task).permit(:name, :description, :priority, :expired_date, :status)
   end
 
-  def sort_params
-    params.permit(sort: [:column, :direction])
-    if params[:sort].present?
-      {
-        sort_column: Task.column_names.include?(params[:sort][:column]) ? params[:sort][:column] : 'created_at',
-        sort_direction: %w[ASC DESC].include?(params[:sort][:direction]) ? params[:sort][:direction] : 'ASC',
-      }
-    else
-      {
-        sort_column: 'created_at',
-        sort_direction: 'ASC',
-      }
-    end
-  end
-
-  def search_params
-    params.permit(search: [:name, :status])
-
+  def get_index_page_params
+    params.permit(search: [:name, :status, :column, :direction])
     if params[:search].present?
-      {
-        search_name: params[:search][:name],
-        search_status: params[:search][:status],
-      }
+      @search_name = params[:search][:name]
+      @search_status = params[:search][:status]
+      @sort_column = params[:search][:column]
+      @sort_direction = params[:search][:direction]
     else
-      {
-        search_name: nil,
-        search_status: nil,
-      }
+      @search_name = ""
+      @search_status = ""
+      @sort_column = 'created_at'
+      @sort_direction = 'ASC'
     end
   end
+
 end
