8c9ec0449c518a5a77b62a00b67088a45c21e976
Kami-naga <yuji.kaminaga@rakuten.com>
2023-07-10 14:01:32 +0900

    add bullet config for N+1 check to avoid it

--- myapp/config/environments/development.rb
+++ myapp/config/environments/development.rb
@@ -62,4 +62,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.file_watcher = ActiveSupport::FileUpdateChecker
+
+  config.after_initialize do
+    Bullet.enable = true # Bulletを有効化する
+    Bullet.alert = true # JavaScriptのポップアップアラートを表示する
+    Bullet.bullet_logger = true # log/bullet.logに出力
+    Bullet.console = true # ブラウザのconsole.logに出力
+    Bullet.rails_logger = true # Railsのログに結果を出力
+    Bullet.add_footer = true # ページの左下に結果を表示
+  end
 end
