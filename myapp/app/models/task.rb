f2918bce71734290fd0fe6b30ca7475bbd1a13ec
Zhao Zhao <zhao.zhao@rakuten.com>
2023-07-19 12:40:15 +0900

    add models

---
+++ myapp/app/models/task.rb
@@ -0,0 +1,10 @@
+class Task < ApplicationRecord
+  STATUSES = ["Not Started", "In Progress", "Completed"].freeze
+  belongs_to :user
+  has_many :task_labels
+  has_many :labels, through: :task_labels
+
+  validates :name, presence: true
+  validates :status, presence: true, inclusion: { in: STATUSES }
+  validates :priority, presence: true, numericality: { only_integer: true }
+end
