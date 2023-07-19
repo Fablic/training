8a8b955af19471e1eb7d4cbdd583e481df1df51b
Kami-naga <yuji.kaminaga@rakuten.com>
2023-07-19 10:57:17 +0900

    add role column(enum type) to users

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -3,8 +3,15 @@
 # some comments for user model
 class User < ApplicationRecord
   has_secure_password
+
+  enum role: {
+    admin: 0,
+    normal: 1,
+  }
+
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: { maximum: 255 }
   validates :password, presence: true, length: { maximum: 255 }, on: :create
   validates :description, length: { maximum: 1000 }
+  validates :role, presence: true
 end
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_003413) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_054642) do
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_003413) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
