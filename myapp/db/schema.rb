e08488df9802557e6896c6dd9d8fffba344247c9
Kami-naga <yuji.kaminaga@rakuten.com>
2023-07-20 14:46:58 +0900

    change schema after adding after in add column script

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -28,9 +28,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_054642) do
     t.string "name", null: false
     t.string "password_digest", null: false
     t.text "description"
+    t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
   end
 
 end
