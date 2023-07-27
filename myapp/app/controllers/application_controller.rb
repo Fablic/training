a56902be3aa72e8d91df317d13e2b18a04114580
Zhao Zhao <zhao.zhao@rakuten.com>
2023-07-27 14:45:25 +0900

    add error handle pages

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  # ...
+  # Custom 500 Internal Server Error handling
+  rescue_from Exception, with: :render_internal_server_error
+
+  # Custom 404 Not Found error handling
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActionController::RoutingError, with: :render_not_found
+  rescue_from ActionController::UnknownFormat, with: :route_not_found
+
+
+
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
+  # Custom method to render the 404 Not Found error page
+  def render_not_found
+    render template: 'errors/404.html', status: :not_found
+  end
+
+  # Custom method to render the 500 Internal Server Error page
+  def render_internal_server_error(exception)
+    # Log the exception for debugging purposes
+    byebug
+    Rails.logger.error "500 Internal Server Error: #{exception.message}"
+    render template: 'errors/500.html', status: :internal_server_error
+  end
 end
