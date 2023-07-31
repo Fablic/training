6f7ee4958a02db910cf4bf05f79764ccf08889e3
Zhao Zhao <zhao.zhao@rakuten.com>
2023-07-31 09:45:06 +0900

    add i18n setting for ja and en

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
+
   def routing_error
     raise ActionController::RoutingError, params[:path]
   end
