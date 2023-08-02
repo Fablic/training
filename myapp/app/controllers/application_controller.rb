95371a489bc938988e23f54a54b710944f2306dd
Zhao Zhao <zhao.zhao@rakuten.com>
2023-08-02 11:49:16 +0900

    add robocop and remove empty lines

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -14,7 +14,6 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-
   def routing_error
     raise ActionController::RoutingError, params[:path]
   end
@@ -30,5 +29,4 @@ class ApplicationController < ActionController::Base
     Rails.logger.error "500 Internal Server Error: #{exception.message}"
     render template: 'errors/500.html', status: :internal_server_error
   end
-
 end
