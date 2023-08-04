bc5d102af223b749fda13d893fe12a84d9a92c9e
Zhao Zhao <zhao.zhao@rakuten.com>
2023-08-04 12:16:10 +0900

    fix models spec

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   before do
-    @user = create(:user)
+    create(:user)
   end
   describe "validations" do
     context "when valid" do
--- myapp/spec/models/user_spec.rb
+++ myapp/spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "associations" do
+  describe "validations" do
     context "when valid" do
       it "is valid with valid attributes" do
         user = build(:user)
