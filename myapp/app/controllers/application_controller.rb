df088c6b71129de7a4be1695f84418f11839614a
Md Alauddin Al-Amin <shakeelcse09@gmail.com>
2023-08-08 11:10:12 +0900

    Configured Custom Error Handling settings

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+  unless Rails.env.development?
+    rescue_from Exception,                        with: :_render_500
+    rescue_from ActiveRecord::RecordNotFound,     with: :_render_404
+    rescue_from ActionController::RoutingError,   with: :_render_404
+  end
+
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
+  private
+    def _render_404(e = nil)
+      Rails.logger.info "Rendering 404 with exception: #{e.message}" if e
+
+      render 'errors/404', status: :not_found
+    end
+
+    def _render_500(e = nil)
+      Rails.logger.error "Rendering 500 with exception: #{e.message}" if e
+
+      render 'errors/500', status: :internal_server_error
+    end
 end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   resources :task
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Custom Error pages handling routes
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
 end
