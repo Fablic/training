43cbc6ed2f8a1b6a763c0b2d54c7f79a2de10656
Zhao Zhao <zhao.zhao@rakuten.com>
2023-08-09 13:21:44 +0900

    add system spec for task searching

--- myapp/spec/system/tasks_searching_spec.rb
+++ myapp/spec/system/tasks_searching_spec.rb
@@ -1,28 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
-  describe "search_by_name_and_status" do
-    # Create a user
-    let(:user) { create(:user) }
-    # Create tasks with different creation and dealine timestamps for testing sorting
-    let!(:task1) { create(:task, name: "Task 1", status: "Not Started", user: user) }
-    let!(:task2) { create(:task, name: "Task 2", status: "In Progress", user: user) }
-    let!(:task3) { create(:task, name: "Another Task", status: "Done", user: user) }
+RSpec.describe 'Task Search', type: :system do
+  before do
+    # Create some tasks for testing
+    create(:user)
+    create(:task, name: 'Task 1', status: 'Not Started')
+    create(:task, name: 'Task 2', status: 'In Progress')
+    create(:task, name: 'Task 3', status: 'Done')
+    visit tasks_path
+  end
 
-    it "returns tasks that match the search by name and status" do
-      expect(Task.search_by_name_and_status("Task", "In Progress")).to match_array([task2])
-    end
+  it 'allows users to search by name and status' do
+    fill_in 'Name', with: 'Task 1'
+    select 'Not Started', from: 'Status'
+    click_button 'Search'
 
-    it "returns tasks that match the search by name" do
-      expect(Task.search_by_name_and_status("Task 1", nil)).to match_array([task1])
-    end
+    expect(page).to have_content('Task 1')
+    expect(page).not_to have_content('Task 2')
+    expect(page).not_to have_content('Task 3')
+  end
 
-    it "returns tasks that match the search by status" do
-      expect(Task.search_by_name_and_status(nil, "Done")).to match_array([task3])
-    end
+  it 'displays "No tasks found" message when no search results' do
+    fill_in 'Name', with: 'Non-existent Task'
+    select 'Done', from: 'Status'
+    click_button 'Search'
 
-    it "returns all tasks when no search parameters are provided" do
-      expect(Task.search_by_name_and_status(nil, nil)).to match_array([task1, task2, task3])
-    end
+    expect(page).to have_content('No tasks found')
   end
 end
