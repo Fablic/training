2db0e8a3c111a5dc913b7402ede8e4bd7e9207d5
Zhao Zhao <zhao.zhao@rakuten.com>
2023-08-21 16:28:32 +0900

    refactor sorting funtion by adding helpers

--- myapp/app/helpers/tasks_helper.rb
+++ myapp/app/helpers/tasks_helper.rb
@@ -1,2 +1,10 @@
 module TasksHelper
+  def link_sort_order(params, sort_by)
+    sort_order(params, sort_by) == 'asc' ? 'asc' : 'desc'
+  end
+
+  def sort_order(params, sort_by)
+    return nil unless params[:sort_by] == sort_by 
+    params[:sort_order] == 'asc' ? 'desc' : 'asc'
+  end
 end
