3941498d70131f8ca9593510c9182e87849456be
Zhao Zhao <zhao.zhao@rakuten.com>
2023-08-24 13:44:18 +0900

    add role to user

--- myapp/app/controllers/admin/users_controller.rb
+++ myapp/app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
   before_action :find_user, only: [:update, :show, :destroy]
+  before_action :require_admin
 
   def index
     @users = User.all
@@ -37,6 +38,11 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
+    if @user.admin? && User.where(role: "admin").count <= 1
+      flash[:alert] = "You cannot delete the only admin user."
+      redirect_to admin_users_path
+      return
+    end
     @user.tasks.destroy_all
     @user.destroy
     redirect_to admin_users_path, notice: 'User and associated tasks deleted.'
@@ -45,10 +51,17 @@ class Admin::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :role)
   end
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def require_admin
+    unless current_user && current_user.admin?
+      flash[:alert] = "You are not authorized to do that."
+      redirect_to root_path
+    end
   end
 end
