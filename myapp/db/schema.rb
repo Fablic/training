80b6d2fef9589d1fd7e9835b7d375c0839d7c4f3
Meghana-Reddy2 <meghanareddy.sareddy@rakuten.com>
2024-02-21 13:05:10 +0900

    Added the task model

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2024_02_21_035323) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.string "priority", null: false
+    t.string "status", null: false
+    t.date "duedate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
