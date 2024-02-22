1dd6a043019100c58f6061fbdb549a72f255ec57
Meghana-Reddy2 <meghanareddy.sareddy@rakuten.com>
2024-02-22 18:19:36 +0900

    implement the task CRUD controller

---
+++ myapp/app/controllers/tasks_controller.rb
@@ -0,0 +1,57 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+     def index
+      @tasks = Task.all
+     end
+
+      def show
+          @task = Task.find(params[:id])
+      end
+
+      def new
+          @task = Task.new
+      end
+
+      def edit
+          @task = Task.find(params[:id])
+      end
+
+      def create
+          @task = Task.new(task_params)
+          if @task.save
+            flash[:success] = "Task created successfully."
+            redirect_to task_path(@task)
+          else
+           render :new, status: :unprocessable_entity
+          end
+      end
+
+      def update
+          @task = Task.find(params[:id])
+          if @task.update(task_params)
+            flash[:success] = "Task updated successfully."
+            redirect_to task_path(@task)
+          else
+            render :edit, status: :unprocessable_entity
+          end
+      end
+
+      def destroy
+          @task = Task.find(params[:id])
+          @task.destroy
+          flash[:success] = "Task deleted successfully."
+          redirect_to tasks_path
+      end
+
+      private
+
+      def set_task
+          @task = Task.find(params[:id])
+      end
+
+      def task_params
+          params.require(:task).permit(:name, :description, :priority, :status, :duedate)
+        end
+end
--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+    STATUSES = ["Not Started", "In Progress", "Done"].freeze
+    PRIORITY = ["High" , "Medium", "Low"].freeze
+    validates :name, presence: true
+    validates :status, presence: true, inclusion: { in: STATUSES }
+    validates :priority, presence: true, inclusion: { in: PRIORITY }
 end
