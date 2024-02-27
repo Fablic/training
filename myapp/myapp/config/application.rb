a6f0172152e69d772379499223fe3dcfa641f09a
Meghana-Reddy2 <meghanareddy.sareddy@rakuten.com>
2024-02-27 15:09:55 +0900

    setting time zone to Japan(Tokyo)

--- myapp/myapp/config/application.rb
+++ myapp/myapp/config/application.rb
@@ -18,5 +18,8 @@ module Myapp
     config.i18n.available_locales[:en, :ja]
     config.i18n.default_locale = :en
     config.i18n.fallbacks =true
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
