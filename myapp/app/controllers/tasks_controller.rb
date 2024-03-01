2e7c4a0838d128aaa6f0c89f926a72fb2ff8ecf0
Meghana-Reddy2 <159000498+Meghana-Reddy2@users.noreply.github.com>
2024-03-01 17:17:09 +0900

    Update tasks_controller.rb

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -54,6 +54,7 @@ class TasksController < ApplicationController
   def set_task
     @task = Task.find(params[:id])
   end
+  
 
   def task_params
     params.require(:task).permit(:name, :description, :priority, :status, :duedate)
