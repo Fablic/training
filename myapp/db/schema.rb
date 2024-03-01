436f00538ea6b7392fb0fa6f6880aedbbc073204
Meghana-Reddy2 <meghanareddy.sareddy@rakuten.com>
2024-03-01 14:07:21 +0900

    Validation constraints

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_21_035323) do
+ActiveRecord::Schema.define(version: 2024_03_01_014104) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description"
+    t.string "name", limit: 30, null: false
+    t.text "description", size: :tiny
     t.string "priority", null: false
     t.string "status", null: false
     t.date "duedate"
