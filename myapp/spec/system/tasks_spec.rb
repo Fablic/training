f79a82cfa6bb6249a36897e31a7fd211eda555ef
Meghana-Reddy2 <meghanareddy.sareddy@rakuten.com>
2024-03-14 20:07:30 +0900

    modified the tasks_spec file

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'Tasks', type: :system do
       login
     end
 
-    let!(:user) { User.last }
+    let!(:user) { create(:user) }
 
     it 'homepage should be the task list page' do
       visit '/'
@@ -28,10 +28,8 @@ RSpec.describe 'Tasks', type: :system do
 
     it 'task list page should be shown' do
       create(:task, name: "test_task" , user: user)
-      create(:task, name: 'test_task')
       visit '/tasks'
       expect(page).to have_content 'Tasks'
-      expect(page).to have_link 'test_task'
     end
 
     it 'task detail page should be shown' do
@@ -41,7 +39,7 @@ RSpec.describe 'Tasks', type: :system do
     end
 
     it 'task edit page should be shown' do
-      task = create(:task, user: user)
+      task = create(:task)
       visit "/tasks/#{task.id}/edit"
       expect(page).to have_content 'Edit task'
     end
@@ -58,89 +56,25 @@ RSpec.describe 'Tasks', type: :system do
       login
     end  
     it 'task created successfully & show flash message when task created', :aggregate_failure do
-        visit '/'
-        click_link('New task')
-        # expect(Task.all.length).to eq 0
-        fill_in 'task[name]', with: 'a_new_task'
-        fill_in 'task[description]', with: 'new task description'
-        select 'Done', from: 'Status'
-        select 'Low', from: 'Priority'
-        fill_in 'task[duedate]', with: '2024-08-08'
-        click_on "Update"
-        expect(page).to have_content "a_new_task"
-        expect(page).to have_link href: /\/tasks\/\d+/
-        expect(Task.last.name).to eq 'a_new_task'
-        # expect(Task.all.length).to eq 1
-      end
-    end
-  
-    describe 'task update' do
-      before do
-        user = create_user
-        create(:task, name: "task_before_edit", description: "description before", user: user)
-        login
-      end
-  
-      it 'task updated successfully' do
-        visit '/'
-        click_on('Edit')
-        fill_in 'task[name]', with: 'task_after_edit'
-        fill_in 'task[description]', with: 'description after'
-        click_on "Update"
-        expect(page).to have_content 'task_after_edit'
-        expect(Task.last.name).to eq 'task_after_edit'
-        # expect(Task.all.length).to eq 1
-      end
-    end
-  
-    describe 'task deletion' do
-      before do
-        user = create_user
-        create(:task, name: 'task_should_be_deleted', user: user)
-        login
-      end
-  
-      it 'task deleted successfully & show flash message when task deleted', :aggregate_failures do
-        visit '/'
-        # expect(Task.all.length).to eq 1
-        click_on "Delete"
-        # expect(Task.all.length).to eq 0
-        expect(page).to have_content 'Task deleted successfully'
-      end
-    end
-  
-    describe 'pagination' do
-      before do
-        user = create_user
-        login
-        10.times do |i|
-          create(:task, name: "Task#{i + 1}", priority: :Low, status: :Done, user: user)
-        end
-      create(:user, id: 1, username: 'UserTest')
-    end
-
-    it 'task created successfully & show flash message when task created', :aggregate_failure do
       visit '/'
       click_link('New task')
-      # expect(Task.all.length).to eq 0
-      fill_in 'task[user_id]', with: 1
       fill_in 'task[name]', with: 'a_new_task'
       fill_in 'task[description]', with: 'new task description'
       select 'Done', from: 'Status'
       select 'Low', from: 'Priority'
       fill_in 'task[duedate]', with: '2024-08-08'
-      click_on 'Update'
-      expect(page).to have_content 'a_new_task'
-      expect(page).to have_link href: %r{/tasks/\d+}
+      click_on "Update"
+      expect(page).to have_content "a_new_task"
+      expect(page).to have_link href: /\/tasks\/\d+/
       expect(Task.last.name).to eq 'a_new_task'
-      # expect(Task.all.length).to eq 1
     end
   end
-
+  
   describe 'task update' do
     before do
-      create(:user, id: 1, username: 'UserTest')
-      create(:task, name: 'task_before_edit', description: 'description before')
+      user = create_user
+      create(:task, name: "task_before_edit", description: "description before", user: user)
+      login
     end
 
     it 'task updated successfully' do
@@ -148,31 +82,32 @@ RSpec.describe 'Tasks', type: :system do
       click_on('Edit')
       fill_in 'task[name]', with: 'task_after_edit'
       fill_in 'task[description]', with: 'description after'
-      click_on 'Update'
+      click_on "Update"
       expect(page).to have_content 'task_after_edit'
       expect(Task.last.name).to eq 'task_after_edit'
-      # expect(Task.all.length).to eq 1
     end
   end
-
+  
   describe 'task deletion' do
     before do
-      create(:task, name: 'task_should_be_deleted')
+      user = create_user
+      create(:task, name: 'task_should_be_deleted', user: user)
+      login
     end
 
     it 'task deleted successfully & show flash message when task deleted', :aggregate_failures do
       visit '/'
-      # expect(Task.all.length).to eq 1
       click_on 'Delete'
-      # expect(Task.all.length).to eq 0
       expect(page).to have_content 'Task deleted successfully'
     end
   end
-
+  
   describe 'pagination' do
     before do
+      user = create_user
+      login
       10.times do |i|
-        create(:task, name: "Task#{i + 1}", priority: :Low, status: :Done)
+        create(:task, name: "Task#{i + 1}", priority: :Low, status: :Done, user: user)
       end
     end
 
@@ -182,63 +117,64 @@ RSpec.describe 'Tasks', type: :system do
         expect(page.body).to have_link "Task#{i + 1}"
       end
       5.times do |i|
-        expect(page.body).not_to have_link "Task#{i + 11}"
+        expect(page.body).not_to have_link "Task#{i + 6}"
       end
     end
   end
 
-  # describe 'show tasks list ordered by specific column in ascending/descending order' do
-  #   before do
-  #     create(:task, name: 'Task1', priority: 'High', status: 'Done', duedate: '2024-02-10',created_at: Time.now- 3.days)
-  #     create(:task, name: 'Task2', priority: 'Low', status: 'In Progress' , duedate: '2024-02-23',created_at: Time.now- 2.days)
-  #     create(:task, name: 'Task3', priority: 'Medium', status: 'Not Started', duedate: '2024-02-12', created_at: Time.now- 1.days)
-  #   end
+  describe 'show tasks list ordered by specific column in ascending/descending order' do
+    before do
+      user = create_user
+      login
+      create(:task, name: 'Task1', priority: 'High', status: 'Done', duedate: '2024-09-10',created_at: Time.now- 3.days, user: user)
+      create(:task, name: 'Task2', priority: 'Low', status: 'In Progress' , duedate: '2024-09-23',created_at: Time.now- 2.days, user: user)
+      create(:task, name: 'Task3', priority: 'Medium', status: 'Not Started', duedate: '2024-09-12', created_at: Time.now- 1.days, user: user)
+    end
 
-  #   it '3 tasks should be created' do
-  #     visit '/'
-  #     # expect(Task.all.length).to eq 3
-  #   end
+    it '3 tasks should be created' do
+      visit '/'
+    end
 
-  #   it 'by created_time desc/asc' do
-  #     visit '/'
-  #     click_link('created_at')
-  #     expect(page.body.index('Task3')).to be < page.body.index('Task2')
-  #     expect(page.body.index('Task2')).to be < page.body.index('Task1')
-  #     click_link('created_at')
-  #     expect(page.body.index('Task1')).to be < page.body.index('Task2')
-  #     expect(page.body.index('Task2')).to be < page.body.index('Task3')
-  #   end
+    it 'by created_time desc/asc' do
+      visit '/'
+      click_link('created_at')
+      expect(page.body.index('Task3')).to be < page.body.index('Task2')
+      expect(page.body.index('Task2')).to be < page.body.index('Task1')
+      click_link('created_at')
+      expect(page.body.index('Task1')).to be < page.body.index('Task2')
+      expect(page.body.index('Task2')).to be < page.body.index('Task3')
+    end 
 
-  #   it 'by priority asc' do
-  #     visit '/'
-  #     click_link('Priority')
-  #     expect(page.body.index('Task1')).to be < page.body.index('Task3')
-  #     expect(page.body.index('Task3')).to be < page.body.index('Task2')
-  #     click_link('Priority')
-  #     expect(page.body.index('Task2')).to be < page.body.index('Task3')
-  #     expect(page.body.index('Task3')).to be < page.body.index('Task1')
-  #   end
+    it 'by priority asc' do
+      visit '/'
+      click_link('Priority')
+      expect(page.body.index('Task1')).to be < page.body.index('Task3')
+      expect(page.body.index('Task3')).to be < page.body.index('Task2')
+      click_link('Priority')
+      expect(page.body.index('Task2')).to be < page.body.index('Task3')
+      expect(page.body.index('Task3')).to be < page.body.index('Task1')
+    end
 
-  #   it 'by status asc' do
-  #     visit '/'
-  #     click_link('Status')
-  #     expect(page.body.index('Task1')).to be < page.body.index('Task2')
-  #     expect(page.body.index('Task2')).to be < page.body.index('Task3')
-  #     click_link('Status')
-  #     expect(page.body.index('Task3')).to be < page.body.index('Task2')
-  #     expect(page.body.index('Task2')).to be < page.body.index('Task1')
-  #   end
+    it 'by status asc' do
+      visit '/'
+      click_link('Status')
+      expect(page.body.index('Task1')).to be < page.body.index('Task2')
+      expect(page.body.index('Task2')).to be < page.body.index('Task3')
+      click_link('Status')
+      expect(page.body.index('Task3')).to be < page.body.index('Task2')
+      expect(page.body.index('Task2')).to be < page.body.index('Task1')
+    end
 
-  #     it 'by duedate asc' do
-  #       visit '/'
-  #       click_link('Duedate')
-  #       expect(page.body.index('Task1')).to be < page.body.index('Task3')
-  #       expect(page.body.index('Task3')).to be < page.body.index('Task2')
-  #       click_link('Duedate')
-  #       expect(page.body.index('Task2')).to be < page.body.index('Task3')
-  #       expect(page.body.index('Task3')).to be < page.body.index('Task1')
-  #     end
-  # end
+      it 'by duedate asc' do
+        visit '/'
+        click_link('Duedate')
+        expect(page.body.index('Task1')).to be < page.body.index('Task3')
+        expect(page.body.index('Task3')).to be < page.body.index('Task2')
+        click_link('Duedate')
+        expect(page.body.index('Task2')).to be < page.body.index('Task3')
+        expect(page.body.index('Task3')).to be < page.body.index('Task1')
+      end
+  end
 
   it 'valid if task has all required contents' do
     task = create(:task)
