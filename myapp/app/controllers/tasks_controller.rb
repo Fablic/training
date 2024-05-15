cc71a5244fd4a681b6815a06854f372b8a9a95d6
shihao xu <shihao.a.xu@rakuten.com>
2024-05-15 14:17:18 +0900

    Add update page, enable deleting task and add flash message

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -4,13 +4,13 @@ class TasksController < ApplicationController
   end
 
   def new
-    
   end
 
   def create
     @new_task = Task.new(params.require(:task).permit(:title, :description))
     @new_task.save
-
+      
+    flash[:success] = "New task was created successfully!"
     redirect_to action: "index"
   end
 
@@ -19,5 +19,22 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params.require(:task).permit(:title, :description))
+
+    flash[:notice] = "Edit task successfully!"
+    redirect_to action: "index"
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:alert] = "Deleted task successfully!"
+    redirect_to action: "index"
   end
 end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   scope(path_name: {index: ""}) do
-    resources :tasks, only: [:index, :new, :create, :show, :edit], path: "/"
+    resources :tasks, path: "/"
   end
 end
