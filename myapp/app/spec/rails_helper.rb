1ec34ff64e20c5fd33c3c1fe4eac3c00370e3a5b
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-15 12:24:21 +0900

    rubocop auto correction

--- myapp/app/spec/rails_helper.rb
+++ myapp/app/spec/rails_helper.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Capybara.register_driver :remote_chrome do |app|
-  hub_url = 'https://chrome:4444/wd/hub'
+  hub_url = "https://chrome:4444/wd/hub"
   chrome_capabilities = ::Selenium::WebDriver::Remote::Capabilities.chrome(
-    'goog:chromeOptions' => {
-      'args' => %w[no-sandbox headless disable-gpu window-size=1680,1050],
+    "goog:chromeOptions" => {
+      "args" => %w[no-sandbox headless disable-gpu window-size=1680,1050],
     },
   )
   Capybara::Selenium::Driver.new(app, browser: :remote, url: hub_url, desired_capabilities: chrome_capabilities)
