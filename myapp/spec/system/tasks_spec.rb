23f60a19ad5e83b2e48dd0378d5e49e7cb2baf14
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-16 14:05:02 +0900

    temporary commit

---
+++ myapp/spec/system/tasks_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe 'Tasks', type: :system do
+  before do
+    @task = Task.create!(title: 'test task title', description: 'test task description')
+  end
+
+  it 'create task' do
+    # Task編集画面を開く
+    visit new_task_path
+
+    # titleを入力
+    fill_in 'task_title', with: 'task title'
+
+    # fill in description
+    fill_in 'task_description', with: 'task description'
+
+    # 更新実行
+    click_button 'Create Task'
+
+    # 正しく更新されていること（＝画面の表示が正しいこと）を検証する
+    expect(page).to have_content 'Task created.'
+    expect(page).to have_content 'task title'
+    expect(page).to have_content 'task description'
+  end
+
+  it 'read task' do
+    # Task編集画面を開く
+    visit task_path(@task)
+
+    # titleに"test task title"が入力されていることを検証する
+    expect(page).to have_content 'task title'
+    expect(page).to have_content 'task description'
+  end
+
+  it 'update task' do
+    # Task編集画面を開く
+    visit edit_task_path(@task)
+
+    # Check if the title and the description are correct
+    expect(page).to have_field 'task_title', with: 'test task title'
+    expect(page).to have_field 'task_description', with: 'test task description'
+
+    # fill in title and description
+    fill_in 'task_title', with: 'test task title changed'
+    fill_in 'task_description', with: 'test task description changed'
+
+    # 更新実行
+    click_button 'Update Task'
+
+    # 正しく更新されていること（＝画面の表示が正しいこと）を検証する
+    expect(page).to have_content 'test task title changed'
+    expect(page).to have_content 'test task description changed'
+  end
+
+  it 'delete task', js: true do
+    # Task編集画面を開く
+    visit tasks_path()
+
+    # expect(button['data-confirm']).to eq 'Are you sure?'
+
+    # Delte task
+    all('tr')[1].click_button 'delete'
+    # accept_confirm('Are you sure?')
+    accept_confirm 'Are you sure?' do
+      click_link 'OK'
+    end    
+
+    # 正しく更新されていること（＝画面の表示が正しいこと）を検証する
+    expect(page).to_not have_content 'test task title'
+  end
+end
