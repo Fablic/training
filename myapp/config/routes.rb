8a62094bb6627d05784e9ac6dbdd22e687e8e73a
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-17 14:25:38 +0900

    step 9 done

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   resources :tasks
   get "/", to: "tasks#index"
+  get '*path', controller: 'application', action: 'render_404'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
