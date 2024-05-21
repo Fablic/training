20ac983a4aeaebb555f2a512a2e62d7eb0ff0256
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-21 17:40:10 +0900

    added contexts

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "Tasks", type: :system do
 
   let!(:task) { tasks.first }
 
-  describe "check order by creation date" do
+  context "check order by creation date" do
     before do
       visit tasks_path
     end
@@ -25,69 +25,97 @@ RSpec.describe "Tasks", type: :system do
     end
   end
 
-  it "create task" do
-    visit new_task_path
+  context "create task" do
+    before do
+      visit new_task_path
+    end
 
-    fill_in "task_title", with: "task title"
-    fill_in "task_description", with: "task description"
+    # fill_in "task_title", with: "task title"
+    # fill_in "task_description", with: "task description"
 
-    click_button I18n.t('tasks.new.create_button')
+    # click_button I18n.t('tasks.new.create_button')
 
-    expect(page).to have_content I18n.t('tasks.create.notice')
-    expect(page).to have_content "task title"
-    expect(page).to have_content "task description"
+    it 'expect creating a success' do
+      fill_in "task_title", with: "task title"
+      fill_in "task_description", with: "task description"
+
+      click_button I18n.t('tasks.new.create_button')
+
+      expect(page).to have_content I18n.t('tasks.create.notice')
+      expect(page).to have_content "task title"
+      expect(page).to have_content "task description"
+    end
   end
 
-  it "read task" do
-    visit task_path(task)
+  context "read task" do
+    before do
+      visit task_path(task)
+    end
 
-    expect(page).to have_content "test title 1"
-    expect(page).to have_content "test description 1"
+    it 'expect reading the task' do
+      expect(page).to have_content "test title 1"
+      expect(page).to have_content "test description 1"
+    end
   end
 
-  it "update task" do
-    visit edit_task_path(task)
+  context "update task" do
+    before do
+      visit edit_task_path(task)
+    end
 
-    expect(page).to have_field "task_title", with: "test title 1"
-    expect(page).to have_field "task_description", with: "test description 1"
+    it 'check the contents before update' do
+      expect(page).to have_field "task_title", with: "test title 1"
+      expect(page).to have_field "task_description", with: "test description 1"
+    end
 
-    fill_in "task_title", with: "test task title changed"
-    fill_in "task_description", with: "test task description changed"
+    it 'check the contents after update' do
+      fill_in "task_title", with: "test task title changed"
+      fill_in "task_description", with: "test task description changed"
 
-    click_button I18n.t('tasks.edit.update_button')
+      click_button I18n.t('tasks.edit.update_button')
 
-    expect(page).to have_content "test task title changed"
-    expect(page).to have_content "test task description changed"
+      expect(page).to have_content "test task title changed"
+      expect(page).to have_content "test task description changed"
+    end
   end
 
-  it "delete task" do
-    visit tasks_path
+  context "delete task" do
+    before do
+      visit tasks_path
+    end
 
-    all("tr")[1].click_button I18n.t('tasks.index.delete')
-
-    expect(page).to_not have_content "test title 1"
+    it 'check if the task is deleted' do
+      all("tr")[1].click_button I18n.t('tasks.index.delete')
+      expect(page).to_not have_content "test title 1"
+    end
   end
 
-  it "validation check empty" do
-    visit new_task_path
-
-    fill_in "task_title", with: ""
-
-    click_button I18n.t('tasks.new.create_button')
+  context "validation check empty" do
+    before do
+      visit new_task_path
+    end
     
-    expect(page).to have_content 'タイトルを入力してください'
+    it 'check the warning' do
+      fill_in "task_title", with: ""
+
+      click_button I18n.t('tasks.new.create_button')
+      expect(page).to have_content 'タイトルを入力してください'
+    end
   end
 
-  it "validation check too many characters" do
-    visit new_task_path
+  context "validation check too many characters" do
+    before do
+      visit new_task_path
+    end
 
-    fill_in "task_title", with: "X"*300
-    fill_in "task_description", with: "Y"*30010
+    it 'check the warnings' do
+      fill_in "task_title", with: "X"*300
+      fill_in "task_description", with: "Y"*30010
 
-    click_button I18n.t('tasks.new.create_button')
+      click_button I18n.t('tasks.new.create_button')
 
-    expect(page).to have_content 'タイトルは255文字以内で入力してください'
-    expect(page).to have_content '説明は30000文字以内で入力してください'
-
+      expect(page).to have_content 'タイトルは255文字以内で入力してください'
+      expect(page).to have_content '説明は30000文字以内で入力してください'
+    end
   end
 end
