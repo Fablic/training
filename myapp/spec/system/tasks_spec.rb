876440c07dabdb0eef00ef5b3f963f1cfe70b145
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-21 15:58:14 +0900

    used let in rspec

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -3,24 +3,25 @@
 require "rails_helper"
 
 RSpec.describe "Tasks", type: :system do
-  before do
-    # Create dummy data
-    @tasks = []
-    # @tasks are sorted by descending order 
-    for i in 1..9
-      @tasks.append({ title: "test title #{i}",
-                     description: "test description #{i}",
-                     created_at: i.days.ago })
+  
+  # Create dummy data
+  let!(:tasks) do
+    (1..9).map do |i|
+      Task.create(title: "test title #{i}", description: "test description #{i}", created_at: i.days.ago)
     end
-    @taskRecords = Task.create!(@tasks)
-    @task = @taskRecords.first
   end
 
-  it "check order by creation date" do
-    visit tasks_path
+  let!(:task) { tasks.first }
 
-    for i in 1..9
-      expect(page.all("tr")[i]).to have_content @tasks[i - 1][:created_at].strftime("%Y-%m-%d %H:%M:%S")
+  describe "check order by creation date" do
+    before do
+      visit tasks_path
+    end
+
+    it "expect return desceding order" do
+      for i in 1..9
+        expect(page.all("tr")[i]).to have_content tasks[i - 1][:created_at].strftime("%Y-%m-%d %H:%M:%S")
+      end
     end
   end
 
@@ -30,22 +31,22 @@ RSpec.describe "Tasks", type: :system do
     fill_in "task_title", with: "task title"
     fill_in "task_description", with: "task description"
 
-    click_button "Create Task"
+    click_button '登録'
 
-    expect(page).to have_content "Task created."
+    expect(page).to have_content "作成"
     expect(page).to have_content "task title"
     expect(page).to have_content "task description"
   end
 
   it "read task" do
-    visit task_path(@task)
+    visit task_path(task)
 
     expect(page).to have_content "test title 1"
     expect(page).to have_content "test description 1"
   end
 
   it "update task" do
-    visit edit_task_path(@task)
+    visit edit_task_path(task)
 
     expect(page).to have_field "task_title", with: "test title 1"
     expect(page).to have_field "task_description", with: "test description 1"
@@ -53,7 +54,7 @@ RSpec.describe "Tasks", type: :system do
     fill_in "task_title", with: "test task title changed"
     fill_in "task_description", with: "test task description changed"
 
-    click_button "Update Task"
+    click_button "更新"
 
     expect(page).to have_content "test task title changed"
     expect(page).to have_content "test task description changed"
@@ -62,7 +63,7 @@ RSpec.describe "Tasks", type: :system do
   it "delete task" do
     visit tasks_path
 
-    all("tr")[1].click_button "delete"
+    all("tr")[1].click_button "削除"
 
     expect(page).to_not have_content "test title 1"
   end
