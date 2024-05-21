dbf1238116a9883ae6bddb5b4f0f7209d0999faf
shihao xu <shihao.a.xu@rakuten.com>
2024-05-21 16:28:31 +0900

    Specify GET request when routing 404 and 500 status

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
 
-  match '/404', to: 'errors#not_found', via: :all
-  match '/500', to: 'errors#internal_server_error', via: :all
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_server_error'
 end
