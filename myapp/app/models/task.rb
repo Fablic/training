4ce4c0fc7030ea54631c3986d91a6efd23db2de5
shihao xu <shihao.a.xu@rakuten.com>
2024-05-22 17:25:57 +0900

    Validate all attributes of Task to be not blank

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  validates :title, presence: { message: 'Title must not be blank!' }
+  validates :description, presence: { message: 'Description must not be blank!' }
+  validates :due, presence: { message: 'Due must be specified!' }
 end
