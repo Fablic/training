ab8b7b0e277115d640e55c8d7065e1a2b839f13e
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-22 16:07:56 +0900

    temporary commit

--- myapp/app/models/application_record.rb
+++ myapp/app/models/application_record.rb
@@ -3,7 +3,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def human_enum_name(enum_name, enum_value)
+  def self.human_enum_name(enum_name, enum_value)
+    return '' if enum_value.nil?
     I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}")
   end
 end
--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -4,4 +4,7 @@ class Task < ApplicationRecord
   enum status: [:not_started, :in_progress, :completed]
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 30000 }
+
+  def enum_options_for_select(enum_name)
+  end
 end
