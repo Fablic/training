70620f5a9db3133feaf82fdf97b367ce4362d087
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-22 14:04:05 +0900

    added status

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  enum status: [:not_started, :in_progress, :completed]
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 30000 }
 end
