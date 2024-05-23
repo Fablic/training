c535467aa20bc8f96664ed6633f3dca836a10987
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-23 15:02:53 +0900

    added seeds

--- myapp/db/seeds.rb
+++ myapp/db/seeds.rb
@@ -6,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "firstuser", email: "user@example.com", password: "123")
+
+Task.create(title: "title 1", description: "description 1", user_id: 1)
+Task.create(title: "title 2", description: "description 2", user_id: 1)
+Task.create(title: "title 3", description: "description 3", user_id: 1)
