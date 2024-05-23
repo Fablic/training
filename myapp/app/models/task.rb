509bc0128c18e67e452faa7656fb50e52afbb0ca
shihao xu <shihao.a.xu@rakuten.com>
2024-05-23 18:13:36 +0900

    Add test cases for validation in edit page

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -5,18 +5,18 @@ class Task < ApplicationRecord
   validates :title, length: { in: 5..30, too_short: 'please input more than %{count} characters', too_long: '%{count} characters is the maximum allowed' }
   validates :description, length: { in: 10..300, too_short: 'please input more than %{count} characters', too_long: '%{count} characters is the maximum allowed' }
   validates :due, presence: { message: 'must be specified!' }
-  
+
   validate :due_cannot_be_in_the_past_on_create, on: :create
   validate :due_cannot_be_changed_to_past_on_update, on: :update
 
   def due_cannot_be_in_the_past_on_create
-    if due.present? && due < Time.now
+    if due.present? && due < Time.zone.now.beginning_of_minute
       errors.add(:due, "can't be in the past")
     end
   end
 
   def due_cannot_be_changed_to_past_on_update
-    if due_changed? && due < Time.now
+    if due.present? && due_changed? && due < Time.zone.now.beginning_of_minute
       errors.add(:due, "can't be in the past")
     end
   end
--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Tasks', type: :system do 
   describe 'index page' do 
     it 'show page title, list and delete buttons' do 
-      Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.now + 5)
-      Task.create!(title: 'Test title 2', description: 'Test Description 2', due: Time.now + 5)
+      Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.zone.now + 5)
+      Task.create!(title: 'Test title 2', description: 'Test Description 2', due: Time.zone.now + 5)
 
       visit tasks_path
 
@@ -18,7 +18,7 @@ RSpec.describe 'Tasks', type: :system do
 
     context 'delete a task' do
       it "delete the task with button" do 
-        task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.now + 5)
+        task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.zone.now + 5)
 
         visit tasks_path
         click_on task.id.to_s
@@ -30,9 +30,9 @@ RSpec.describe 'Tasks', type: :system do
     end
 
     it 'show tasks in due date order' do 
-      Task.create!(title: 'Test title 1', description: 'Test Description 1', due: '2024-04-01 20:00:00')
-      Task.create!(title: 'Test title 2', description: 'Test Description 2', due: '2024-04-02 20:00:00')
-      Task.create!(title: 'Test title 3', description: 'Test Description 3', due: '2024-04-03 20:00:00')
+      Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.zone.now + 5)
+      Task.create!(title: 'Test title 2', description: 'Test Description 2', due: Time.zone.now + 10)
+      Task.create!(title: 'Test title 3', description: 'Test Description 3', due: Time.zone.now + 15)
       
       visit tasks_path
 
@@ -58,7 +58,8 @@ RSpec.describe 'Tasks', type: :system do
 
       fill_in 'Title', with: 'New Task Title'
       fill_in 'Description', with: 'New Task Description'
-      fill_in 'Due', with: Time.now + 5
+      new_time = Time.zone.now + 5
+      fill_in 'Due', with: new_time
 
       click_on 'Submit Task'
 
@@ -70,6 +71,7 @@ RSpec.describe 'Tasks', type: :system do
 
       expect(page).to have_content('New Task Title')
       expect(page).to have_content('New Task Description')
+      expect(page).to have_content(I18n.l new_time)
     end
 
     context 'input invalid value' do 
@@ -77,7 +79,7 @@ RSpec.describe 'Tasks', type: :system do
         visit new_task_path 
 
         fill_in 'Description', with: 'New Task Description'
-        fill_in 'Due', with: Time.now + 5
+        fill_in 'Due', with: Time.zone.now + 5
 
         click_on 'Submit Task'
 
@@ -89,7 +91,7 @@ RSpec.describe 'Tasks', type: :system do
         visit new_task_path 
 
         fill_in 'Title', with: 'New Task Title'
-        fill_in 'Due', with: Time.now + 5
+        fill_in 'Due', with: Time.zone.now + 5
 
         click_on 'Submit Task'
 
@@ -114,7 +116,7 @@ RSpec.describe 'Tasks', type: :system do
 
         fill_in 'Title', with: 'New Task Title'
         fill_in 'Description', with: 'New Task Description'
-        fill_in 'Due', with: Time.now - 5
+        fill_in 'Due', with: Time.zone.now - 60
 
         click_on 'Submit Task'
 
@@ -126,7 +128,8 @@ RSpec.describe 'Tasks', type: :system do
 
   describe 'detail page' do
     it 'show the detail of the task' do 
-      task = Task.create!(title: 'Test title 1', description: 'Test Description 1')
+      new_time = Time.zone.now + 5
+      task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: new_time)
       
       visit task_path(task)
 
@@ -134,15 +137,14 @@ RSpec.describe 'Tasks', type: :system do
 
       expect(page).to have_content('Title')
       expect(page).to have_content('Test title 1')
-
-      expect(page).to have_content('Description')
-      expect(page).to have_content('Test Description 1')
+      expect(page).to have_content(I18n.l(new_time))
     end
   end
 
   describe 'edit task page' do 
-    it 'show page title' do 
-      task = Task.create!(title: 'Test title 1', description: 'Test Description 1')
+    it 'show fileds with values inside' do 
+      previous_time = Time.zone.now + 5
+      task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: previous_time)
       
       visit edit_task_path(task)
 
@@ -150,17 +152,20 @@ RSpec.describe 'Tasks', type: :system do
 
       expect(page).to have_field('Title', with: 'Test title 1')
       expect(page).to have_field('Description', with: 'Test Description 1')
+      expect(page).to have_field('Due', with: I18n.l(previous_time, format: :field))
 
       expect(page).to have_selector("input[type='submit'][value='Update Task']")
     end
 
     it 'redirect to index page after submit' do 
-      task = Task.create!(title: 'Test title 1', description: 'Test Description 1')
+      task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.zone.now + 5)
 
       visit edit_task_path(task)
 
       fill_in 'Title', with: 'Edit Task Title'
       fill_in 'Description', with: 'Edit Task Description'
+      edit_time = Time.zone.now + 5
+      fill_in 'Due', with: edit_time
 
       click_on 'Update Task'
 
@@ -171,6 +176,74 @@ RSpec.describe 'Tasks', type: :system do
 
       expect(page).to have_content('Edit Task Title')
       expect(page).to have_content('Edit Task Description')
+      expect(page).to have_content(I18n.l(edit_time))
+    end
+
+    context 'edit with invalid inputs' do 
+      it 'should show error message for empty title' do 
+        task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.zone.now + 5)
+
+        visit edit_task_path(task)
+
+        fill_in 'Title', with: ''
+
+        click_on 'Update Task'
+
+        expect(page).to have_content('You have some invalid inputs!')
+        expect(page).to have_content('Title must not be blank!')
+      end
+
+      it 'should show error message for empty description' do 
+        task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.zone.now + 5)
+
+        visit edit_task_path(task)
+
+        fill_in 'Description', with: ''
+
+        click_on 'Update Task'
+
+        expect(page).to have_content('You have some invalid inputs!')
+        expect(page).to have_content('Description must not be blank!')
+      end
+
+      it 'should show error message for empty due date' do 
+        task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.zone.now + 5)
+
+        visit edit_task_path(task)
+
+        fill_in 'Due', with: ''
+
+        click_on 'Update Task'
+
+        expect(page).to have_content('You have some invalid inputs!')
+        expect(page).to have_content('Due must be specified!')
+      end
+
+      it 'should show error message for past due date' do 
+        task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.zone.now + 5)
+
+        visit edit_task_path(task)
+
+        fill_in 'Due', with: Time.zone.now.prev_day
+
+        click_on 'Update Task'
+
+        expect(page).to have_content('You have some invalid inputs!')
+        expect(page).to have_content("Due can't be in the past")
+      end
+
+      it 'should NOT show error message if not change due date' do 
+        task = Task.create!(title: 'Test title 1', description: 'Test Description 1', due: Time.zone.now + 5)
+
+        visit edit_task_path(task)
+
+        fill_in 'Title', with: 'New Task Title'
+        fill_in 'Description', with: 'New Task Description'
+
+        click_on 'Update Task'
+
+        expect(current_path).to eq(tasks_path)
+      end
     end
   end
 end
