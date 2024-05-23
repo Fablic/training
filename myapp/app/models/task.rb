725f406ea851450d5032221ee1cde97c1312d911
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-23 09:45:20 +0900

    temporary commit

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -5,7 +5,8 @@ class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 30000 }
 
-  def self.enum_options_for_select
-    return self.statuses.map{ |status, i| self.human_attribute_name(status) }
+  def self.enum_options_for_select_status
+    puts t("activerecord.attributes.task.title")
+    return self.statuses.map{ |status, i| [self.human_attribute_name(".statuses.#{status}"), status] }
   end
 end
