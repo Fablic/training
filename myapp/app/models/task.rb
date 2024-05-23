8538bad62dc4dda97957b74f13fdcfa9ba1e0fba
shihao xu <shihao.a.xu@rakuten.com>
2024-05-23 13:19:08 +0900

    Change the validation rules of Task

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -2,12 +2,21 @@ require 'time'
 
 class Task < ApplicationRecord
   validates :title, :description, presence: { message: 'must not be blank!' }
+  validates :title, length: { in: 5..30, too_short: 'please input more than %{count} characters', too_long: '%{count} characters is the maximum allowed' }
+  validates :description, length: { in: 10..300, too_short: 'please input more than %{count} characters', too_long: '%{count} characters is the maximum allowed' }
   validates :due, presence: { message: 'must be specified!' }
+  
+  validate :due_cannot_be_in_the_past_on_create, on: :create
+  validate :due_cannot_be_changed_to_past_on_update, on: :update
 
-  validate :due_cannot_be_in_the_past
-
-  def due_cannot_be_in_the_past
+  def due_cannot_be_in_the_past_on_create
     if due.present? && due < Time.now
+      errors.add(:due, "can't be in the past")
+    end
+  end
+
+  def due_cannot_be_changed_to_past_on_update
+    if due_changed? && due < Time.now
       errors.add(:due, "can't be in the past")
     end
   end
