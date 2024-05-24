20213bf8ce46f6fcca1ded39b1d6a24f6e9aab63
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-24 13:59:08 +0900

    temporary commit

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_24_030024) do
+ActiveRecord::Schema.define(version: 2024_05_24_044922) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.string "title", limit: 100, null: false
