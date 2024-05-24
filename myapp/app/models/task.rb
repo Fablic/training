8d476a1e3c7be4d469b7378e7376ad99e9671273
shihao xu <shihao.a.xu@rakuten.com>
2024-05-24 16:29:17 +0900

    Remove redundant validation in task model

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,23 +1,15 @@
 require 'time'
 
 class Task < ApplicationRecord
-  validates :title, :description, presence: { message: 'must not be blank!' }
   validates :title, length: { in: 5..30, too_short: 'please input more than %{count} characters', too_long: '%{count} characters is the maximum allowed' }
   validates :description, length: { in: 10..300, too_short: 'please input more than %{count} characters', too_long: '%{count} characters is the maximum allowed' }
   validates :due, presence: { message: 'must be specified!' }
 
-  validate :due_cannot_be_earlier_than_now_on_create, on: :create
-  validate :due_cannot_be_changed_to_be_earlier_than_now_on_update, on: :update, if: :due_changed?
+  validate :due_cannot_be_earlier_than_now, if: :due_changed?
 
-  def due_cannot_be_earlier_than_now_on_create
+  def due_cannot_be_earlier_than_now
     if due.present? && due < Time.zone.now.beginning_of_minute
       errors.add(:due, "can't be earlier than now!")
-    end
-  end
-
-  def due_cannot_be_changed_to_be_earlier_than_now_on_update
-    if due.present? && due < Time.zone.now.beginning_of_minute
-      errors.add(:due, "can't be earlier than now if you want to change!")
     end
   end
 end
--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -229,7 +229,7 @@ RSpec.describe 'Tasks', type: :system do
         click_on 'Update Task'
 
         expect(page).to have_content('You have some invalid inputs!')
-        expect(page).to have_content("Due can't be earlier than now if you want to change!")
+        expect(page).to have_content("Due can't be earlier than now!")
       end
 
       it 'should NOT show error message if not change due date' do 
