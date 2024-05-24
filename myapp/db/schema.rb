777d33b5a2f94b9f764358196279cee940843e7e
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-24 12:03:07 +0900

    set the default value on status in tasks

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_23_053524) do
+ActiveRecord::Schema.define(version: 2024_05_24_030024) do
+
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.string "title", limit: 100, null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "expiration_date"
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["title"], name: "index_tasks_on_title"
   end
 
@@ -30,4 +29,5 @@ ActiveRecord::Schema.define(version: 2024_05_23_053524) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
