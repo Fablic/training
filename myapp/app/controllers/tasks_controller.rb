8e260981d44339967d75d2a576388849c978ed08
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-27 08:34:34 +0900

    fixed the sorting function bug

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -5,16 +5,7 @@ class TasksController < ApplicationController
   TASKS_PER_PAGE = 5
 
   def index
-    # if params[:search]
-    #   @tasks = Task.search_title(params[:search])
-    # elsif params[:status] && Task.statuses.include?(params[:status])
-    #   @tasks = Task.filter_status(params[:status])
-    # else
-    #   @tasks = Task.all
-    # end
-
-    puts params[:status].blank?
-
+    # For search function
     if params[:search] && !params[:status].blank?
       @tasks = Task.filter_status(params[:status])
       @tasks = @tasks.search_title(params[:search])
@@ -23,27 +14,17 @@ class TasksController < ApplicationController
     else
       @tasks = Task.all
     end
-
-    # session[:is_order_desc] = !session.fetch(:is_order_desc, false)
-    # sort_column = params[:sort].presence_in(Task.column_names) ? params[:sort] : "created_at"
-    # sort_direction = session[:is_order_desc] ? "DESC" : "ASC"
-    # @tasks = @tasks.order("#{sort_column} #{sort_direction}")
-
+    
+    # For sorting function
     if params[:sort].presence_in(Task.column_names)
       puts params[:is_order_desc]
       sort_column = params[:sort]
       sort_direction = params[:is_order_desc] == 'true' ? "DESC" : "ASC"
-      # params[:is_order_desc] = !params[:is_order_desc]
     else
-      puts 'YYYYYYYYYYYYYYYYY'
       sort_column = "created_at"
       sort_direction = "DESC"
     end
-    puts 'sort_direction'
-    puts sort_direction
-    puts 'sort_direction'
-    @tasks.order("#{sort_column} #{sort_direction}")
-    # @tasks = @tasks.sort_by( |task| task.expiration_date,  )
+    @tasks = @tasks.order("#{sort_column} #{sort_direction}")
 
     @tasks = @tasks.page(params[:page]).per(TASKS_PER_PAGE)
     @tasks
