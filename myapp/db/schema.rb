0f172924976dd819476c3b0916cf65c2d99b4ab9
fanfannb <siqi.fan@rakuten.com>
2024-05-27 15:40:44 +0900

    add_null_constraint_to_tasks_title

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_27_052714) do
+ActiveRecord::Schema.define(version: 2024_05_27_063426) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
