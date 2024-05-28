c5f26831aafc5670b1b32c2cb79317271af6d700
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-28 14:30:24 +0900

    modified [:XXX] to .XXX when I access attributes

--- myapp/spec/models/tasks_spec.rb
+++ myapp/spec/models/tasks_spec.rb
@@ -7,49 +7,49 @@ RSpec.describe "Tasks", type: :model do
     context "create a task" do
       let!(:user1) { User.create(name: "user 1", email: "user1@example.com", password: "123") }
       context "maximum length" do
-        let!(:task) { Task.create(title: "X" * 100, description: "Y" * 30000, user_id: user1[:id]) }
+        let!(:task) { Task.create(title: "X" * 100, description: "Y" * 30000, user_id: user1.id) }
         it "valid" do
           expect(task).to be_valid
         end
       end
 
       context "empty title" do
-        let!(:task_empty_title) { Task.create(title: "", user_id: user1[:id]) }
+        let!(:task_empty_title) { Task.create(title: "", user_id: user1.id) }
         it "no valid" do
           expect(task_empty_title).to_not be_valid
         end
       end
 
       context "appropriate length title" do
-        let!(:task_empty_title) { Task.create(title: "task title", user_id: user1[:id]) }
+        let!(:task_empty_title) { Task.create(title: "task title", user_id: user1.id) }
         it "valid" do
           expect(task_empty_title).to be_valid
         end
       end
 
       context "too long title" do
-        let!(:task_too_long_title) { Task.create(title: "X" * 110, user_id: user1[:id]) }
+        let!(:task_too_long_title) { Task.create(title: "X" * 110, user_id: user1.id) }
         it "no valid" do
           expect(task_too_long_title).to_not be_valid
         end
       end
 
       context "empty description" do
-        let!(:task_empty_description) { Task.create(title: "X", description: "", user_id: user1[:id]) }
+        let!(:task_empty_description) { Task.create(title: "X", description: "", user_id: user1.id) }
         it "valid" do
           expect(task_empty_description).to be_valid
         end
       end
 
       context "appropriate length description" do
-        let!(:task_empty_title) { Task.create(title: "X", description: "task description", user_id: user1[:id]) }
+        let!(:task_empty_title) { Task.create(title: "X", description: "task description", user_id: user1.id) }
         it "valid" do
           expect(task_empty_title).to be_valid
         end
       end
 
       context "too long description" do
-        let!(:task_empty_description) { Task.create(title: "X", description: "", user_id: user1[:id]) }
+        let!(:task_empty_description) { Task.create(title: "X", description: "", user_id: user1.id) }
         it "no valid" do
           expect(task_empty_description).to be_valid
         end
@@ -60,11 +60,11 @@ RSpec.describe "Tasks", type: :model do
   describe "search" do
     let!(:user1) { User.create(name: "user 1", email: "user1@example.com", password: "123") }
     context "title" do
-      let!(:task1) { Task.create(title: "test title 1", user_id: user1[:id]) }
-      let!(:task2) { Task.create(title: "test title 2", user_id: user1[:id]) }
-      let!(:task3) { Task.create(title: "test title 3", user_id: user1[:id]) }
+      let!(:task1) { Task.create(title: "test title 1", user_id: user1.id) }
+      let!(:task2) { Task.create(title: "test title 2", user_id: user1.id) }
+      let!(:task3) { Task.create(title: "test title 3", user_id: user1.id) }
       it "find a task with its title 'test title 1'" do
-        expect(Task.search_title("1")[0][:title]).to eq "test title 1"
+        expect(Task.search_title("1")[0].title).to eq "test title 1"
       end
     end
   end
@@ -72,11 +72,11 @@ RSpec.describe "Tasks", type: :model do
   describe "filter" do
     let!(:user1) { User.create(name: "user 1", email: "user1@example.com", password: "123") }
     context "status" do
-      let!(:task1) { Task.create(title: "test title 1", status: "not_started", user_id: user1[:id]) }
-      let!(:task2) { Task.create(title: "test title 2", status: "in_progress", user_id: user1[:id]) }
-      let!(:task3) { Task.create(title: "test title 3", status: "completed", user_id: user1[:id]) }
+      let!(:task1) { Task.create(title: "test title 1", status: "not_started", user_id: user1.id) }
+      let!(:task2) { Task.create(title: "test title 2", status: "in_progress", user_id: user1.id) }
+      let!(:task3) { Task.create(title: "test title 3", status: "completed", user_id: user1.id) }
       it "find a task with its status 'in_progress'" do
-        expect(Task.filter_status("in_progress")[0][:status]).to eq "in_progress"
+        expect(Task.filter_status("in_progress")[0].status).to eq "in_progress"
       end
     end
   end
@@ -84,9 +84,9 @@ RSpec.describe "Tasks", type: :model do
   describe "user" do
     context "associated" do
       let!(:user1) { User.create(name: "user 1", email: "user1@example.com", password: "123") }
-      let!(:task1) { Task.create(title: "title 1", description: "description 1", user_id: user1[:id]) }
+      let!(:task1) { Task.create(title: "title 1", description: "description 1", user_id: user1.id) }
       it "find user 1" do
-        expect(task1[:user_id]).to eq user1[:id]
+        expect(task1.user_id).to eq user1.id
       end
     end
   end
--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "Tasks", type: :system do
                     description: "test description #{i}",
                     created_at: i.days.ago,
                     expiration_date: Time.now.since(i.days),
-                    user_id: user1[:id]
+                    user_id: user1.id
                    )
       end
     end
@@ -26,14 +26,14 @@ RSpec.describe "Tasks", type: :system do
 
       it "expect created_at descending order" do
         tasks.each_with_index do |tsk, idx|
-          expect(page.all("tr")[idx + 1]).to have_content tsk[:created_at].strftime("%Y-%m-%d %H:%M:%S")
+          expect(page.all("tr")[idx + 1]).to have_content tsk.created_at.strftime("%Y-%m-%d %H:%M:%S")
         end
       end
 
       it "expect expiration_date ascending order" do
         click_link I18n.t("activerecord.attributes.task.expiration_date")
         tasks.each_with_index do |tsk, idx|
-          expect(page.all("tr")[-idx - 1]).to have_content tsk[:expiration_date].strftime("%Y-%m-%d %H:%M:%S")
+          expect(page.all("tr")[-idx - 1]).to have_content tsk.expiration_date.strftime("%Y-%m-%d %H:%M:%S")
         end
       end
     end
@@ -125,9 +125,9 @@ RSpec.describe "Tasks", type: :system do
 
   describe "search" do
     let!(:user1) { User.create(name: "user 1", email: "user1@example.com", password: "123") }
-    let!(:task1) { Task.create(title: "test title 1", user_id: user1[:id]) }
-    let!(:task2) { Task.create(title: "test title 2", user_id: user1[:id]) }
-    let!(:task3) { Task.create(title: "test title 3", user_id: user1[:id]) }
+    let!(:task1) { Task.create(title: "test title 1", user_id: user1.id) }
+    let!(:task2) { Task.create(title: "test title 2", user_id: user1.id) }
+    let!(:task3) { Task.create(title: "test title 3", user_id: user1.id) }
     before do
       visit tasks_path
     end
@@ -135,16 +135,16 @@ RSpec.describe "Tasks", type: :system do
       it "find a task with its title 'test title 1'" do
         fill_in "search", with: "test title 1"
         click_button I18n.t("tasks.index.filter")
-        expect(Task.search_title("1")[0][:title]).to eq "test title 1"
+        expect(Task.search_title("1")[0].title).to eq "test title 1"
       end
     end
   end
 
   describe "filter" do
     let!(:user1) { User.create(name: "user 1", email: "user1@example.com", password: "123") }
-    let!(:task1) { Task.create(title: "test title 1", status: "not_started", user_id: user1[:id]) }
-    let!(:task2) { Task.create(title: "test title 2", status: "in_progress", user_id: user1[:id]) }
-    let!(:task3) { Task.create(title: "test title 3", status: "completed", user_id: user1[:id]) }
+    let!(:task1) { Task.create(title: "test title 1", status: "not_started", user_id: user1.id) }
+    let!(:task2) { Task.create(title: "test title 2", status: "in_progress", user_id: user1.id) }
+    let!(:task3) { Task.create(title: "test title 3", status: "completed", user_id: user1.id) }
     before do
       visit tasks_path
     end
@@ -152,7 +152,7 @@ RSpec.describe "Tasks", type: :system do
       it "find a task with its status 'in_progress'" do
         find("option[value='in_progress']").select_option
         click_button I18n.t("tasks.index.filter")
-        expect(Task.filter_status("in_progress")[0][:status]).to eq "in_progress"
+        expect(Task.filter_status("in_progress")[0].status).to eq "in_progress"
       end
     end
   end
@@ -163,7 +163,7 @@ RSpec.describe "Tasks", type: :system do
       (1..20).map do |i|
         Task.create(title: "test title #{i}",
                     description: "test description #{i}",
-                    user_id: user1[:id])
+                    user_id: user1.id)
       end
     end
     before do
@@ -172,22 +172,22 @@ RSpec.describe "Tasks", type: :system do
     context "next page" do
       it "find tasks in the page 2" do
         tasks[0..4].each_with_index do |tsk, idx|
-          expect(page.all("tr")[idx + 1]).to have_content tsk[:created_at].strftime("%Y-%m-%d %H:%M:%S")
+          expect(page.all("tr")[idx + 1]).to have_content tsk.created_at.strftime("%Y-%m-%d %H:%M:%S")
         end
         click_link I18n.t("views.pagination.next").tr(" &rsaquo;", "")
         tasks[5..9].each_with_index do |tsk, idx|
-          expect(page.all("tr")[idx + 1]).to have_content tsk[:created_at].strftime("%Y-%m-%d %H:%M:%S")
+          expect(page.all("tr")[idx + 1]).to have_content tsk.created_at.strftime("%Y-%m-%d %H:%M:%S")
         end
       end
     end
     context "last page" do
       it "find tasks in the last page" do
         tasks[0..4].each_with_index do |tsk, idx|
-          expect(page.all("tr")[idx + 1]).to have_content tsk[:created_at].strftime("%Y-%m-%d %H:%M:%S")
+          expect(page.all("tr")[idx + 1]).to have_content tsk.created_at.strftime("%Y-%m-%d %H:%M:%S")
         end
         click_link I18n.t("views.pagination.next").tr(" &rsaquo;", "")
         tasks[15..19].each_with_index do |tsk, idx|
-          expect(page.all("tr")[idx + 1]).to have_content tsk[:created_at].strftime("%Y-%m-%d %H:%M:%S")
+          expect(page.all("tr")[idx + 1]).to have_content tsk.created_at.strftime("%Y-%m-%d %H:%M:%S")
         end
       end
     end
