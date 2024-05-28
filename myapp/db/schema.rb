2670ab3c7c232efcf89b7be2bad767642ac635ce
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-28 09:25:56 +0900

    modified db schema

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -26,6 +24,7 @@ ActiveRecord::Schema.define(version: 2024_05_27_071554) do
     t.index ["label_id"], name: "index_labels_tasks_on_label_id"
     t.index ["task_id"], name: "index_labels_tasks_on_task_id"
   end
+
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.string "title", limit: 100, null: false
     t.text "description"
