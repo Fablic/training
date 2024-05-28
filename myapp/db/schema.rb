b053a7a1bb081e2456f6b7ebd368594ca04d6874
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-28 09:15:57 +0900

    reset db schema

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2024_05_24_044922) do
+
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.string "title", limit: 100, null: false
     t.text "description"
@@ -20,8 +19,8 @@ ActiveRecord::Schema.define(version: 2024_05_24_044922) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "expiration_date"
     t.integer "status", default: 0, null: false
-    t.index ["title"], name: "index_tasks_on_title"
     t.bigint "user_id", null: false
+    t.index ["title"], name: "index_tasks_on_title"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
