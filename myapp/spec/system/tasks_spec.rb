862d3a98a2045a941cd25b4acce6828d1e34d035
fanfannb <siqi.fan@rakuten.com>
2024-05-29 16:21:08 +0900

    rubocop

--- myapp/spec/system/tasks_spec.rb
+++ myapp/spec/system/tasks_spec.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Task management", type: :system do
+RSpec.describe 'Task management', type: :system do
   before do
     driven_by(:rack_test)
   end
 
   it 'enables me to create tasks' do
     visit '/tasks/new'
-    
+
     fill_in 'Title', with: 'My Task'
     fill_in 'Description', with: 'This is my task.'
     click_on 'Create Task'
-    
+
     expect(page).to have_content('Task was successfully created.')
     expect(page).to have_content('My Task')
     expect(page).to have_content('This is my task.')
@@ -20,7 +22,7 @@ RSpec.describe "Task management", type: :system do
   it 'displays a task' do
     task = Task.create(title: 'Existing Task', description: 'This is an existing task.')
     visit "/tasks/#{task.id}"
-    
+
     expect(page).to have_content('Existing Task')
     expect(page).to have_content('This is an existing task.')
   end
@@ -28,11 +30,11 @@ RSpec.describe "Task management", type: :system do
   it 'enables me to edit tasks' do
     task = Task.create(title: 'Edit Me', description: 'Edit this task.')
     visit "/tasks/#{task.id}/edit"
-    
+
     fill_in 'Title', with: 'Edited Task'
     fill_in 'Description', with: 'This task has been edited.'
     click_on 'Update Task'
-    
+
     expect(page).to have_content('Task was successfully updated.')
     expect(page).to have_content('Edited Task')
     expect(page).to have_content('This task has been edited.')
@@ -41,9 +43,9 @@ RSpec.describe "Task management", type: :system do
   it 'enables me to delete tasks' do
     task = Task.create(title: 'Delete Me', description: 'Delete this task.')
     visit "/tasks/#{task.id}"
-    
+
     click_button 'Delete'
-    
+
     expect(page).to have_content('Task was successfully deleted.')
     expect(page).not_to have_content('Delete Me')
     expect(page).not_to have_content('Delete this task.')
