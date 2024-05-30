d48225f5ce88b3e1a3caa7612aa1a62b845dca4f
kodai <30628299+kodai-t@users.noreply.github.com>
2024-05-30 18:15:18 +0900

    added a test for destroy user

--- myapp/spec/models/users_spec.rb
+++ myapp/spec/models/users_spec.rb
@@ -34,4 +34,21 @@ RSpec.describe "Users", type: :model do
       end
     end
   end
+
+  describe "delete" do
+    context "delete a user having 3 tasks" do
+      let!(:user1) { create(:user1) }
+      let!(:user2) { create(:user2) }
+      before do
+        create(:task1, user_id: user1.id)
+        create(:task2, user_id: user1.id)
+        create(:task3, user_id: user1.id)
+        create(:task4, user_id: user2.id)
+      end
+      it "tasks deleted" do
+        user1.destroy
+        expect(Task.count).to eq(1)
+      end
+    end
+  end
 end
