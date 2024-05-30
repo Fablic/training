a0aea73f989e66af9c43077694ff1621c72450ad
shihao xu <shihao.a.xu@rakuten.com>
2024-05-30 14:45:41 +0900

    Refine modle spec

--- myapp/spec/models/tasks_spec.rb
+++ myapp/spec/models/tasks_spec.rb
@@ -89,169 +89,167 @@ RSpec.describe Task, type: :model do
     end
   end
 
-  describe 'class method' do 
-    context '.search_with_sort' do 
-      it 'when query_status is empty' do 
-        task1 = Task.create!(
-          title: 'Task 1',
-          description: 'Description 1',
-          due: Time.zone.now + 5,
-          status: 'pending'
-        )
+  describe '.search_with_sort' do 
+    it 'sorted without status' do 
+      task1 = Task.create!(
+        title: 'Task 1',
+        description: 'Description 1',
+        due: Time.zone.now + 5,
+        status: 'pending'
+      )
 
-        task2 = Task.create!(
-          title: 'Task 2',
-          description: 'Description 2',
-          due: Time.zone.now + 10,
-          status: 'in_progress'
-        )
+      task2 = Task.create!(
+        title: 'Task 2',
+        description: 'Description 2',
+        due: Time.zone.now + 10,
+        status: 'in_progress'
+      )
 
-        task3 = Task.create!(
-          title: 'No title',
-          description: 'Description 3',
-          due: Time.zone.now + 15,
-          status: 'completed'
-        )
+      task3 = Task.create!(
+        title: 'No title',
+        description: 'Description 3',
+        due: Time.zone.now + 15,
+        status: 'completed'
+      )
 
-        result = Task.search_with_sort('Task', '', 'created_at', 'desc')
+      result = Task.search_with_sort('Task', '', 'created_at', 'desc')
 
-        expect(result).to eq([task2, task1])
-      end
+      expect(result).to eq([task2, task1])
+    end
 
-      it 'when query_status is not empty' do 
-        task1 = Task.create!(
-          title: 'Task 1',
-          description: 'Description 1',
-          due: Time.zone.now + 5,
-          status: 'pending'
-        )
+    it 'sorted with status specified' do 
+      task1 = Task.create!(
+        title: 'Task 1',
+        description: 'Description 1',
+        due: Time.zone.now + 5,
+        status: 'pending'
+      )
 
-        task2 = Task.create!(
-          title: 'Task 2',
-          description: 'Description 2',
-          due: Time.zone.now + 10,
-          status: 'in_progress'
-        )
+      task2 = Task.create!(
+        title: 'Task 2',
+        description: 'Description 2',
+        due: Time.zone.now + 10,
+        status: 'in_progress'
+      )
 
-        task3 = Task.create!(
-          title: 'No title',
-          description: 'Description 3',
-          due: Time.zone.now + 15,
-          status: 'completed'
-        )
+      task3 = Task.create!(
+        title: 'No title',
+        description: 'Description 3',
+        due: Time.zone.now + 15,
+        status: 'completed'
+      )
 
-        result = Task.search_with_sort('Task', 'pending', 'created_at', 'desc')
+      result = Task.search_with_sort('Task', 'pending', 'created_at', 'desc')
 
-        expect(result).to eq([task1])
-      end
+      expect(result).to eq([task1])
+    end
 
-      it 'sort by created_at in desc order without title and status specified' do 
-        task1 = Task.create!(
-          title: 'Task 1',
-          description: 'Description 1',
-          due: Time.zone.now + 5,
-          status: 'pending'
-        )
+    it 'sorted by created_at in desc order without title and status specified' do 
+      task1 = Task.create!(
+        title: 'Task 1',
+        description: 'Description 1',
+        due: Time.zone.now + 5,
+        status: 'pending'
+      )
 
-        task2 = Task.create!(
-          title: 'Task 2',
-          description: 'Description 2',
-          due: Time.zone.now + 10,
-          status: 'in_progress'
-        )
+      task2 = Task.create!(
+        title: 'Task 2',
+        description: 'Description 2',
+        due: Time.zone.now + 10,
+        status: 'in_progress'
+      )
 
-        task3 = Task.create!(
-          title: 'No title',
-          description: 'Description 3',
-          due: Time.zone.now + 15,
-          status: 'completed'
-        )
+      task3 = Task.create!(
+        title: 'No title',
+        description: 'Description 3',
+        due: Time.zone.now + 15,
+        status: 'completed'
+      )
 
-        result = Task.search_with_sort('', '', 'created_at', 'desc')
+      result = Task.search_with_sort('', '', 'created_at', 'desc')
 
-        expect(result).to eq([task3, task2, task1])
-      end
+      expect(result).to eq([task3, task2, task1])
+    end
 
-      it 'sort by created_at in asc order without title and status specified' do 
-        task1 = Task.create!(
-          title: 'Task 1',
-          description: 'Description 1',
-          due: Time.zone.now + 5,
-          status: 'pending'
-        )
+    it 'sorted by created_at in asc order without title and status specified' do 
+      task1 = Task.create!(
+        title: 'Task 1',
+        description: 'Description 1',
+        due: Time.zone.now + 5,
+        status: 'pending'
+      )
 
-        task2 = Task.create!(
-          title: 'Task 2',
-          description: 'Description 2',
-          due: Time.zone.now + 10,
-          status: 'in_progress'
-        )
+      task2 = Task.create!(
+        title: 'Task 2',
+        description: 'Description 2',
+        due: Time.zone.now + 10,
+        status: 'in_progress'
+      )
 
-        task3 = Task.create!(
-          title: 'No title',
-          description: 'Description 3',
-          due: Time.zone.now + 15,
-          status: 'completed'
-        )
+      task3 = Task.create!(
+        title: 'No title',
+        description: 'Description 3',
+        due: Time.zone.now + 15,
+        status: 'completed'
+      )
 
-        result = Task.search_with_sort('', '', 'created_at', 'asc')
+      result = Task.search_with_sort('', '', 'created_at', 'asc')
 
-        expect(result).to eq([task1, task2, task3])
-      end
+      expect(result).to eq([task1, task2, task3])
+    end
 
-      it 'sort by due in desc order without title and status specified' do 
-        task1 = Task.create!(
-          title: 'Task 1',
-          description: 'Description 1',
-          due: Time.zone.now + 10,
-          status: 'pending'
-        )
+    it 'sorted by due in desc order without title and status specified' do 
+      task1 = Task.create!(
+        title: 'Task 1',
+        description: 'Description 1',
+        due: Time.zone.now + 10,
+        status: 'pending'
+      )
 
-        task2 = Task.create!(
-          title: 'Task 2',
-          description: 'Description 2',
-          due: Time.zone.now + 1000,
-          status: 'in_progress'
-        )
+      task2 = Task.create!(
+        title: 'Task 2',
+        description: 'Description 2',
+        due: Time.zone.now + 1000,
+        status: 'in_progress'
+      )
 
-        task3 = Task.create!(
-          title: 'No title',
-          description: 'Description 3',
-          due: Time.zone.now + 100,
-          status: 'completed'
-        )
+      task3 = Task.create!(
+        title: 'No title',
+        description: 'Description 3',
+        due: Time.zone.now + 100,
+        status: 'completed'
+      )
 
-        result = Task.search_with_sort('', '', 'due', 'desc')
+      result = Task.search_with_sort('', '', 'due', 'desc')
 
-        expect(result).to eq([task2, task3, task1])
-      end
+      expect(result).to eq([task2, task3, task1])
+    end
 
-      it 'sort by due in asc order without title and status specified' do 
-        task1 = Task.create!(
-          title: 'Task 1',
-          description: 'Description 1',
-          due: Time.zone.now + 10,
-          status: 'pending'
-        )
+    it 'sorted by due in asc order without title and status specified' do 
+      task1 = Task.create!(
+        title: 'Task 1',
+        description: 'Description 1',
+        due: Time.zone.now + 10,
+        status: 'pending'
+      )
 
-        task2 = Task.create!(
-          title: 'Task 2',
-          description: 'Description 2',
-          due: Time.zone.now + 1000,
-          status: 'in_progress'
-        )
+      task2 = Task.create!(
+        title: 'Task 2',
+        description: 'Description 2',
+        due: Time.zone.now + 1000,
+        status: 'in_progress'
+      )
 
-        task3 = Task.create!(
-          title: 'No title',
-          description: 'Description 3',
-          due: Time.zone.now + 100,
-          status: 'completed'
-        )
+      task3 = Task.create!(
+        title: 'No title',
+        description: 'Description 3',
+        due: Time.zone.now + 100,
+        status: 'completed'
+      )
 
-        result = Task.search_with_sort('', '', 'due', 'asc')
+      result = Task.search_with_sort('', '', 'due', 'asc')
 
-        expect(result).to eq([task1, task3, task2])
-      end
+      expect(result).to eq([task1, task3, task2])
     end
   end
 end
