a90d6e9667fb4564d7f35d1d870bc5737387ddd1
shihao xu <shihao.a.xu@rakuten.com>
2024-05-30 19:03:20 +0900

    Set up Bootstrap and refine index page

--- myapp/app/javascript/packs/application.js
+++ myapp/app/javascript/packs/application.js
@@ -2,6 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@popperjs/core'
+import $ from 'jquery'
+window.$ = $
+window.jQuery = $
 
 require("@rails/ujs").start()
 require("turbolinks").start()
