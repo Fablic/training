211bd3d06ca602321d2d954e0d3d686b8d9ee5b2
shihao xu <shihao.a.xu@rakuten.com>
2024-06-03 15:45:25 +0900

    Add controller for users

---
+++ myapp/app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def signup
+  end
+
+  def signin
+  end
+
+  def signout
+  end
+end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/signup'
+  get 'users/signin'
+  get 'users/signout'
   root 'tasks#index'
   resources :tasks
 end
