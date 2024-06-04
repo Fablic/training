e4610c9787096eaa010bcc2b85a1bcb32775e518
shihao xu <shihao.a.xu@rakuten.com>
2024-06-04 14:32:24 +0900

    Change users column name to fix bug

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_03_021119) do
+ActiveRecord::Schema.define(version: 2024_06_04_052229) do
 
   create_table "labels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "label_name", null: false
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2024_06_03_021119) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "username", null: false
-    t.text "password", null: false
+    t.text "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
