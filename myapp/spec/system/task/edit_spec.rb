07eac040b8788589b944fb83fb219b4e423bf9d0
fanfannb <siqi.fan@rakuten.com>
2024-06-06 17:36:39 +0900

    add spec test

--- myapp/spec/system/task/edit_spec.rb
+++ myapp/spec/system/task/edit_spec.rb
@@ -19,4 +19,37 @@ RSpec.describe 'visit /tasks/:id/edit', type: :system do
     expect(page).to have_content('Edited Task')
     expect(page).to have_content('This task has been edited.')
   end
+
+  it 'shows an error message if task creation fails due to missing title' do
+    task = Task.create(title: 'Edit Me', description: 'Edit this task.')
+    visit "/tasks/#{task.id}/edit"
+
+    fill_in 'Title', with: ''
+    fill_in 'Description', with: 'This is a test task without a title.'
+    click_on 'Update Task'
+
+    expect(page).to have_text("タイトルを入力してください。")
+  end
+
+  it 'shows an error message if the task title is too long' do
+    task = Task.create(title: 'Edit Me', description: 'Edit this task.')
+    visit "/tasks/#{task.id}/edit"
+
+    fill_in 'Title', with: 'a' * 51
+    fill_in 'Description', with: 'A valid description.'
+    click_on 'Update Task'
+
+    expect(page).to have_text("タイトルは50文字以内で入力してください。")
+  end
+
+  it 'shows an error message if the task description is too long' do
+    task = Task.create(title: 'Edit Me', description: 'Edit this task.')
+    visit "/tasks/#{task.id}/edit"
+
+    fill_in 'Title', with: 'My Task'
+    fill_in 'Description', with: 'a' * 501
+    click_on 'Update Task'
+
+    expect(page).to have_text("説明文は500文字以内で入力してください。")
+  end
 end
