1a92f4bfadc11f3ef12e23b5e080e8e0b78661b2
Uchimaki, Shinya <shinya.uchimaki@rakuten.com>
2024-06-06 19:03:56 +0900

    Fixed .rubocop.yml

--- myapp/db/migrate/20240529071947_create_tasks.rb
+++ myapp/db/migrate/20240529071947_create_tasks.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CreateTasks < ActiveRecord::Migration[6.0] # rubocop:disable Style/Documentation
-  def change
+  def change # rubocop:disable Metrics/MethodLength
     create_table :tasks do |t|
       t.references :user, null: false, foreign_key: true
       t.string :title, null: false
