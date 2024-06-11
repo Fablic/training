1fbbeba746e73f2571cf9a71505d4f4ad927fb06
shihao xu <shihao.a.xu@rakuten.com>
2024-06-11 13:57:25 +0900

    Add length validation for password

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, length: { in: 3..20 }
-  validates :password, presence: true, confirmation: true, format: { with: /\A[^\s]*\z/ }
+  validates :password, presence: true, confirmation: true, format: { with: /\A[^\s]*\z/ }, length: { in: 5..15 }
 
   has_many :tasks, dependent: :destroy
   validates :username, format: { with: /\A[^\s]*\z/ }
