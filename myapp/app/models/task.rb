e8257245638d85fde1d195c887d91ab4ebe5a510
fanfannb <siqi.fan@rakuten.com>
2024-06-11 17:13:18 +0900

    add deadline

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -3,4 +3,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50, too_long: :title_too_long }
   validates :description, length: { maximum: 500, too_long: :desc_too_long }, allow_blank: true
+  validates :deadline, presence: true
 end
