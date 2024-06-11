ea73c8cb37393536aab737685c3afe0cee85fe66
Uchimaki, Shinya <shinya.uchimaki@rakuten.com>
2024-06-11 11:29:13 +0900

    Removed foreign key

--- myapp/db/migrate/20240531105229_create_task_label_relations.rb
+++ myapp/db/migrate/20240531105229_create_task_label_relations.rb
@@ -1,8 +1,8 @@
 class CreateTaskLabelRelations < ActiveRecord::Migration[6.0]
   def change
     create_table :task_label_relations do |t|
-      t.references :task, null: false, foreign_key: true
-      t.references :label, null: false, foreign_key: true
+      t.references :task, null: false, foreign_key: false
+      t.references :label, null: false, foreign_key: false
 
       t.timestamps
     end
