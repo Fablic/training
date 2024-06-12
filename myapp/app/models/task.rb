3286739ccd3c97708034344f14e871d35c76b61a
fanfannb <siqi.fan@rakuten.com>
2024-06-12 17:17:55 +0900

    Changed some deadline limits

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -3,15 +3,22 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50, too_long: :title_too_long }
   validates :description, length: { maximum: 500, too_long: :desc_too_long }, allow_blank: true
-  validates :deadline, presence: true
-  validate :deadline_cannot_be_in_the_past
+  validate :deadline_cannot_be_in_the_past, if: -> { deadline.present? }
+  validate :valid_date_format, if: -> { deadline.present? }
 
   private
 
   def deadline_cannot_be_in_the_past
-    if deadline.present? && deadline < Time.now
+    if deadline < Time.now
       errors.add(:deadline, :past_deadline)
     end
+  end
+
+  def valid_date_format
+    parsed_date = Date.strptime(deadline.to_s, '%Y-%m-%d')
+      unless parsed_date.year.between?(Date.today.year, Date.today.year + 10)
+        errors.add(:deadline, :out_of_range)
+      end
   end
 end
 
