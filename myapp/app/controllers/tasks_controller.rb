ca7ce8b0d10bd9e8d593e2c68bd02c4afe8e20b0
Uchimaki, Shinya <shinya.uchimaki@rakuten.com>
2024-06-12 12:02:27 +0900

    Fixed some rubycop issues.

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,20 +1,25 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController # rubocop:disable Style/Documentation
-  rescue_from StandardError, with: :render_500
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from StandardError, with: :render_internal_server_error
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: %i[show edit update destroy]
 
   def index
     @tasks = Task.all
   end
 
-  def show
-  end
-
   def new
     @task = Task.new
+  end
+
+  def show
+    ## show
+  end
+
+  def edit
+    ## edit
   end
 
   def create
@@ -27,9 +32,6 @@ class TasksController < ApplicationController # rubocop:disable Style/Documentat
       flash[:warn] = I18n.t('tasks.create_failure')
       render :new
     end
-  end
-
-  def edit
   end
 
   def update
@@ -52,6 +54,7 @@ class TasksController < ApplicationController # rubocop:disable Style/Documentat
   end
 
   private
+
   def set_task
     @task = Task.find(params[:id])
   end
@@ -60,14 +63,14 @@ class TasksController < ApplicationController # rubocop:disable Style/Documentat
     params.require(:task).permit(:title, :description, :status, :priority, :due_date)
   end
 
-  def render_404
-    render file: "#{Rails.root}/public/404.html", status: :not_found
+  def render_not_found
+    render file: Rails.root.join('public/404.html'), status: :not_found
   end
 
-  def render_500(exception)
+  def render_internal_server_error(exception)
     logger.error(exception.message)
     logger.error(exception.backtrace.join("\n"))
 
-    render file: "#{Rails.root}/public/500.html", status: :internal_server_error
+    render file: Rails.root.join('public/500.html'), status: :internal_server_error
   end
 end
