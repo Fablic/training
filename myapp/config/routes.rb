dfb346793070f850df5132813e2a3cd46128e63c
shihao xu <shihao.a.xu@rakuten.com>
2024-06-13 14:46:00 +0900

    Reorganized routing for admin pages

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -11,15 +11,16 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   # index page
   root 'tasks#index'
-  # admin page
-  get 'admin', to: 'users#admin'
-  # admin create user
-  post 'admin', to: 'users#create_user'
-  # admin user tasks
-  get 'admin/:id/tasks', to: 'users#user_tasks', as: :user_tasks
-  # admin new user
-  get 'admin/new', to: 'users#new_user'
-  # use users #destroy only
-  resources :users, only: [:destroy]
+  scope '/admin' do 
+    # admin page
+    get '/', to: 'users#admin', as: :admin
+    # admin create user
+    post '/', to: 'users#create_user'
+    # admin user tasks
+    get ':id/tasks', to: 'users#user_tasks', as: :user_tasks
+    # admin new user
+    get 'new', to: 'users#new_user', as: :new_user
+    resources :users, only: [:edit, :update, :destroy]
+  end
   resources :tasks
 end
