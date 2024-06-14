f888b418d47a78dbd412dd1c749f11d6db2de941
fanfannb <siqi.fan@rakuten.com>
2024-06-14 11:24:35 +0900

    add status

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_240_607_064_500) do
-  create_table 'tasks', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.string 'title', limit: 50, null: false
-    t.string 'description', limit: 500
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.datetime 'deadline'
+ActiveRecord::Schema.define(version: 2024_06_13_063617) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title", limit: 50, null: false
+    t.string "description", limit: 500
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deadline"
+    t.integer "status", default: 0, null: false
   end
+
 end
