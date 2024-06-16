9631f0412205691c2cf3c7dc23d9e380a5fcadef
Uchimaki, Shinya <shinya.uchimaki@rakuten.com>
2024-06-17 08:54:24 +0900

    Fixed the handling of flash

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -23,7 +23,7 @@ class TasksController < ApplicationController # rubocop:disable Style/Documentat
       flash[:info] = I18n.t('tasks.create_success')
       redirect_to @task
     else
-      flash[:warn] = I18n.t('tasks.create_failure')
+      flash.now[:warn] = I18n.t('tasks.create_failure')
       render :new
     end
   end
@@ -37,7 +37,7 @@ class TasksController < ApplicationController # rubocop:disable Style/Documentat
       flash[:info] = I18n.t('tasks.update_success')
       redirect_to @task
     else
-      flash[:warn] = I18n.t('tasks.update_failure')
+      flash.now[:warn] = I18n.t('tasks.update_failure')
       render :edit
     end
   end
