c20d77f3bcfac601ada51bd47c1658a2de31aa29
shihao xu <shihao.a.xu@rakuten.com>
2024-06-17 19:05:35 +0900

    Fix bug for deleting task

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -9,7 +9,7 @@ class Task < ApplicationRecord
   validate :due_cannot_be_earlier_than_now, if: :due_changed?
 
   belongs_to :user
-  has_many :tasks_labels
+  has_many :tasks_labels, dependent: :destroy
   has_many :labels, through: :tasks_labels
 
   def self.search_with_sort(query_title, query_status, sort_by, order)
