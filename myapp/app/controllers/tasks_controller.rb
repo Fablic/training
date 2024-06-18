5a67808b3fc80a4eaa7544e47c1e7b5a5eb63793
Uchimaki, Shinya <shinya.uchimaki@rakuten.com>
2024-06-19 08:55:39 +0900

    Removed features of STEP12

--- myapp/app/controllers/tasks_controller.rb
+++ myapp/app/controllers/tasks_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController # rubocop:disable Style/Documentation
-  #　Added `rescue_from` here for the assignment, but it is planned to be removed
-  rescue_from StandardError, with: :render_internal_server_error
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-
   before_action :set_task, only: %i[show edit update destroy]
 
   def index
@@ -61,16 +57,5 @@ class TasksController < ApplicationController # rubocop:disable Style/Documentat
 
   def task_params
     params.require(:task).permit(:title, :description, :status, :priority, :due_date)
-  end
-
-  def render_not_found
-    render file: Rails.root.join('public/404.html'), status: :not_found
-  end
-
-  def render_internal_server_error(exception)
-    logger.error(exception.message)
-    logger.error(exception.backtrace.join("\n"))
-
-    render file: Rails.root.join('public/500.html'), status: :internal_server_error
   end
 end
