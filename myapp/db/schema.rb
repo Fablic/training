97574902836de2b6a72d9baabd08eee4506ebdce
fanfannb <siqi.fan@rakuten.com>
2024-06-19 16:15:05 +0900

    create users

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -12,13 +12,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_240_613_063_617) do
+ActiveRecord::Schema.define(version: 20_240_617_094_636) do
   create_table 'tasks', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
     t.string 'title', limit: 50, null: false
     t.string 'description', limit: 500
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.bigint 'user_id'
     t.datetime 'deadline'
     t.integer 'status', default: 0, null: false
+    t.index ['user_id'], name: 'index_tasks_on_user_id'
   end
+
+  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.string 'password_digest'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  add_foreign_key 'tasks', 'users'
 end
