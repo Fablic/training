66ebf6cdec8d2cd8451f2a47b9392fb782563dc8
Uchimaki, Shinya <shinya.uchimaki@rakuten.com>
2024-06-21 17:32:20 +0900

    Added models test cases

--- myapp/spec/models/task_spec.rb
+++ myapp/spec/models/task_spec.rb
@@ -52,4 +52,38 @@ RSpec.describe Task, type: :model do
     end
 
   end
+
+  describe 'Search' do
+    before do
+      @task1 = Task.create!(title: 'test1', description: 'desc1', due_date: '2024-01-01', updated_at: '2024-03-01', status: :open)
+      @task2 = Task.create!(title: 'test2', description: 'desc2', due_date: '2024-03-01', updated_at: '2024-02-01', status: :in_progress)
+      @task3 = Task.create!(title: 'test3', description: 'desc3', due_date: '2024-02-01', updated_at: '2024-01-01', status: :in_progress)
+      @task4 = Task.create!(title: 'test11', description: 'desc3', due_date: '2024-02-01', updated_at: '2024-01-01',status: :in_progress)
+    end
+
+    it 'search title, result is not empty' do
+      task_list = Task.search('test1', nil)
+      ## order by created_at desc
+      expect(task_list.count).to eq 2
+      expect(task_list[0].title).to include('test1')
+      expect(task_list[1].title).to include('test1')
+    end
+
+    it 'search title, result is empty' do
+      task_list = Task.search('test100', nil)
+      expect(task_list.count).to eq 0
+    end
+
+    it 'search status, result is not empty' do
+      task_list = Task.search(nil, :open)
+      ## order by created_at desc
+      expect(task_list.count).to eq 1
+      expect(task_list[0]).to have_attributes(status: 'open')
+    end
+
+    it 'search status, result is empty' do
+      task_list = Task.search(nil, :closed)
+      expect(task_list.count).to eq 0
+    end
+  end
 end
