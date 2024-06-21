967f171352008385964382d18cc9e933ae65ccdf
fanfannb <siqi.fan@rakuten.com>
2024-06-21 15:42:51 +0900

    add validation

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
-require 'bcrypt'
-
 class User < ApplicationRecord
+  require 'bcrypt'
   attr_accessor :password
 
   has_many :tasks
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: true, if: :password_required?
+
   before_save :encrypt_password
+  before_save { email.downcase! }
 
   def encrypt_password
     return unless password.present?
