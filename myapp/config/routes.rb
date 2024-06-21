840afc53b102712907030b45fa7d9ab0d2dd8506
fanfannb <siqi.fan@rakuten.com>
2024-06-21 14:33:12 +0900

    add Logout function

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: :login
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy', as: :logout
+  get 'logout', to: 'sessions#destroy', as: :logout
   root 'tasks#index'
   resources :tasks
 end
