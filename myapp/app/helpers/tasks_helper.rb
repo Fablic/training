15b3d4adf8961569ebed8e7747ef977934a41b0c
Uchimaki, Shinya <shinya.uchimaki@rakuten.com>
2024-06-25 12:03:31 +0900

    Fixed some erbs

--- myapp/app/helpers/tasks_helper.rb
+++ myapp/app/helpers/tasks_helper.rb
@@ -4,11 +4,14 @@ module TasksHelper # rubocop:disable Style/Documentation
   def sortable(column, title = nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-    link_to title, { sort: column, direction: direction, title: params[:title], status: params[:status] }
+    arrow = column == sort_column ? (sort_direction == 'asc' ? '↑' : '↓') : ''
+    link_to "#{title} #{arrow}".html_safe, { sort: column, direction: direction, title: params[:title], status: params[:status] }
   end
 
+  private
+
   def sort_column
-    Task.column_names.include?(params[:sort]) ? params[:sort] : 'title'
+    Task.column_names.include?(params[:sort]) ? params[:sort] : 'unknown'
   end
 
   def sort_direction
