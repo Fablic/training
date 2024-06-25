e433843923a3dbea16ebf5c342e1168cefa06c8d
fanfannb <siqi.fan@rakuten.com>
2024-06-25 16:04:31 +0900

    When delete a user, delete the tasks that the user is carrying.

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   require 'bcrypt'
   attr_accessor :password, :password_confirmation
 
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
--- myapp/spec/models/user_spec.rb
+++ myapp/spec/models/user_spec.rb
@@ -3,5 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'deleting a user' do
+    it 'deletes all associated tasks' do
+      user = User.create!(name: 'test', email: 'user@example.com', password: 'password')
+      user.tasks.create!(title: 'Task 1')
+      user.tasks.create!(title: 'Task 2')
+
+      expect { user.destroy }.to change { Task.count }.by(-2)
+    end
+  end
 end
