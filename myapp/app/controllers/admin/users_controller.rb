668748cdb61f3a1e436a01a70b71c41831df8d33
fanfannb <siqi.fan@rakuten.com>
2024-06-25 15:48:01 +0900

    admin can edit user

--- myapp/app/controllers/admin/users_controller.rb
+++ myapp/app/controllers/admin/users_controller.rb
@@ -14,6 +14,10 @@ module Admin
       @user = User.find(params[:id])
     end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+
     def create
       @user = User.new(user_params)
       if @user.save
