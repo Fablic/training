38f41d0759154385f8b5ef435ee36b896f778da0
fanfannb <siqi.fan@rakuten.com>
2024-06-26 14:11:48 +0900

    change model

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -2,6 +2,8 @@
 
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :task_labels, dependent: :destroy
+  has_many :labels, through: :task_labels
   validates :title, presence: true, length: { maximum: 50, too_long: :title_too_long }
   validates :description, length: { maximum: 500, too_long: :desc_too_long }, allow_blank: true
   validate :deadline_cannot_be_in_the_past, if: -> { deadline.present? }
