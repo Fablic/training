2d178efc816a1395830c2261133bc5e99ec76491
fanfannb <siqi.fan@rakuten.com>
2024-06-26 14:24:42 +0900

    add spec

---
+++ myapp/spec/models/task_label_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TaskLabel, type: :model do
+  it "belongs to a task" do
+    association = described_class.reflect_on_association(:task)
+    expect(association.macro).to eq :belongs_to
+  end
+
+  it "belongs to a label" do
+    association = described_class.reflect_on_association(:label)
+    expect(association.macro).to eq :belongs_to
+  end
+end
