9a93dda3afe07d13423642fe6097578891d0a949
fanfannb <siqi.fan@rakuten.com>
2024-06-26 17:44:58 +0900

    rubocop

--- myapp/spec/models/label_spec.rb
+++ myapp/spec/models/label_spec.rb
@@ -3,25 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Label, type: :model do
-  it "is valid with a name" do
-    label = Label.new(name: "Urgent")
+  it 'is valid with a name' do
+    label = Label.new(name: 'Urgent')
     expect(label).to be_valid
   end
 
-  it "is invalid without a name" do
+  it 'is invalid without a name' do
     label = Label.new(name: nil)
     label.valid?
-    expect(label.errors[:name]).to include("を入力してください")
+    expect(label.errors[:name]).to include('を入力してください')
   end
 
-  it "is invalid with a duplicate name" do
-    Label.create(name: "Urgent")
-    label = Label.new(name: "Urgent")
+  it 'is invalid with a duplicate name' do
+    Label.create(name: 'Urgent')
+    label = Label.new(name: 'Urgent')
     label.valid?
-    expect(label.errors[:name]).to include("はすでに存在します")
+    expect(label.errors[:name]).to include('はすでに存在します')
   end
 
-  it "has many tasks through task_labels" do
+  it 'has many tasks through task_labels' do
     association = described_class.reflect_on_association(:tasks)
     expect(association.macro).to eq :has_many
     expect(association.options[:through]).to eq :task_labels
--- myapp/spec/models/task_label_spec.rb
+++ myapp/spec/models/task_label_spec.rb
@@ -3,12 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe TaskLabel, type: :model do
-  it "belongs to a task" do
+  it 'belongs to a task' do
     association = described_class.reflect_on_association(:task)
     expect(association.macro).to eq :belongs_to
   end
 
-  it "belongs to a label" do
+  it 'belongs to a label' do
     association = described_class.reflect_on_association(:label)
     expect(association.macro).to eq :belongs_to
   end
