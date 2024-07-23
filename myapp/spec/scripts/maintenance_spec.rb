b321342a5c4978c946cb94d3a429b5544a0d41fc
Uchimaki, Shinya <shinya.uchimaki@rakuten.com>
2024-07-23 16:35:32 +0900

    Added spec

---
+++ myapp/spec/scripts/maintenance_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Maintenance', type: :system do
+
+  it 'Enable maintenance' do
+    output = `rails runner scripts/enable_maintenance.rb`
+    expect(output).to include('Maintenance mode enabled')
+
+    visit '/'
+    expect(page.status_code).to eq(503)
+
+  end
+
+  it 'Disable maintenance' do
+    output = `rails runner scripts/disable_maintenance.rb`
+    expect(output).to include('Maintenance mode is not enabled')
+
+    visit '/'
+    expect(page.status_code).to eq(200)
+
+  end
+
+  after do
+    # 後処理: メンテナンスモードファイルを削除
+    FileUtils.rm_f(Rails.root.join('tmp', 'maintenance.txt'))
+  end
+end
