2206aec8a0216eb5164e53a154bafb8e57ea9a4b
Sunao Uehara <uehara.sunao@gmail.com>
2024-09-10 15:49:15 +0900

    step 16, 17, implement strong password rule

--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -2,6 +2,15 @@ class User < ApplicationRecord
   has_many :tasks
 
   has_secure_password
+  # 8~20, lowercase, uppercase, digit and special chars must be used at least once
+  PASSWORD_FORMAT = /\A(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?]).{8,20}\z/
 
   validates :name, presence: true, length: { minimum: 5, maximum: 20 }, uniqueness: { case_sensitive: false }
+  validates :password,
+    format: {
+      with: PASSWORD_FORMAT,
+      message: :invalid_password,
+      min: 8,
+      max: 20
+  }
 end
--- myapp/spec/models/user_spec.rb
+++ myapp/spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validation test' do
+  describe 'validation' do
     context 'name column' do
       it 'success' do
         t = User.new(
@@ -58,14 +58,57 @@ RSpec.describe User, type: :model do
         expect(t.errors[:password]).to include('を入力してください')
       end
 
+      it 'failed when it is too short' do
+        t = User.new(
+          name: 'JohnDoe',
+          password: 'Abc123!'
+        )
+        t.valid?
+        expect(t.errors[:password]).to include('は半角8~20文字英大文字・小文字・数字、シンボルをそれぞれ１文字以上含めてください')
+      end
+
       it 'failed when it is too long' do
         t = User.new(
           name: 'JohnDoe',
-          password: SecureRandom.alphanumeric(73),
+          password: 'Abcdefg1234567!?%tltr'
         )
         t.valid?
-        expect(t.errors[:password]).to include('は72文字以内で入力してください')
+        expect(t.errors[:password]).to include('は半角8~20文字英大文字・小文字・数字、シンボルをそれぞれ１文字以上含めてください')
       end
+
+      it 'failed when not containing lowercase' do
+        t = User.new(
+          name: 'JohnDoe',
+          password: 'DUMMYPASSWORD123!?'
+        )
+        t.valid?
+        expect(t.errors[:password]).to include('は半角8~20文字英大文字・小文字・数字、シンボルをそれぞれ１文字以上含めてください')
+      end
+      it 'failed when not containing uppercase' do
+        t = User.new(
+          name: 'JohnDoe',
+          password: 'dummypassword123!?'
+        )
+        t.valid?
+        expect(t.errors[:password]).to include('は半角8~20文字英大文字・小文字・数字、シンボルをそれぞれ１文字以上含めてください')
+      end
+      it 'failed when not containing digit' do
+        t = User.new(
+          name: 'JohnDoe',
+          password: 'dummyPassword!?'
+        )
+        t.valid?
+        expect(t.errors[:password]).to include('は半角8~20文字英大文字・小文字・数字、シンボルをそれぞれ１文字以上含めてください')
+      end
+      it 'failed when not containing symbol' do
+        t = User.new(
+          name: 'JohnDoe',
+          password: 'dummyPassword123'
+        )
+        t.valid?
+        expect(t.errors[:password]).to include('は半角8~20文字英大文字・小文字・数字、シンボルをそれぞれ１文字以上含めてください')
+      end
+
     end
   end
 end
