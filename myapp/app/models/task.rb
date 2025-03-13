5858377a9aad5a3fb5d0626011477cc177a2e94c
PangMangkaja <nipun_mangkaja@fablic.co.jp>
2025-03-13 16:09:39 +0900

    remove space

--- myapp/app/models/task.rb
+++ myapp/app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    enum priority: { low: 0, medium: 1, high: 2 }  
+    enum priority: { low: 0, medium: 1, high: 2 }
     enum status:   { "to do": 0, "in progress": 1, "done": 2 }
 end
