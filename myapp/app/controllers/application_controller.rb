04dbd83f9666596ce848c4003dc759b0f26f1efd
PangMangkaja <nipun_mangkaja@fablic.co.jp>
2025-03-18 15:57:00 +0900

    remove space

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-    before_action :set_locale
+  before_action :set_locale
   
-    private
+  private
   
-    def set_locale
-      locale = params[:locale] || session[:locale] || I18n.default_locale
-      I18n.locale = locale
-      session[:locale] = locale
-    end
-  
-    def default_url_options
-      { locale: I18n.locale }
-    end
+  def set_locale
+    locale = params[:locale] || session[:locale] || I18n.default_locale
+    I18n.locale = locale
+    session[:locale] = locale
   end
+  
+  def default_url_options
+    { locale: I18n.locale }
+  end
+end
   
