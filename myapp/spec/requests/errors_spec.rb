b185e9803c43ccce7f6139ed9c9d84db0e588196
PangMangkaja <nipun_mangkaja@fablic.co.jp>
2025-03-18 16:16:38 +0900

    modify comment

--- myapp/spec/requests/errors_spec.rb
+++ myapp/spec/requests/errors_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Errors", type: :request do
-  # Test valid error codes that match the whitelist in your ErrorsController.
   %w[400 404 406 422 500].each do |code|
     describe "GET /#{code}" do
       it "renders the error page for #{code}" do
         get "/#{code}"
         expect(response).to have_http_status(code.to_i)
         
-        # Optionally, check for specific text in the rendered error page.
         case code.to_i
         when 400
           expect(response.body).to include("Bad Request")
@@ -25,7 +23,6 @@ RSpec.describe "Errors", type: :request do
     end
   end
 
-  # Test for an invalid code; should default to 500:
   describe "GET /test (invalid code)" do
     it "returns 404" do
       get "/test"
